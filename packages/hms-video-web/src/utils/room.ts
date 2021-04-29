50982e96787b361aee3c29ac3094f1efd978d8f8
Praveen <apnerve@gmail.com>
2021-04-29 23:14:03 +0530

    chore: Clean up package.json and reduce package size a lot

--- packages/hms-video-web/src/utils/room.ts
+++ packages/hms-video-web/src/utils/room.ts
@@ -1,8 +1,5 @@
-import jwt from 'jsonwebtoken';
-
 function getRoomId(token: string) {
-  const decoded = jwt.decode(token, { json: true });
-  return decoded!.room_id;
+  return JSON.parse(atob(token.split('.')[1])).room_id;
 }
 
 export { getRoomId };
