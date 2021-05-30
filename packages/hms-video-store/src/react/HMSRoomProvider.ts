8fd0ad8736f431f3bf58b3e098873f644b427518
Tushar <tushar@100ms.live>
2021-05-30 12:02:40 +0530

    refactor: put react wrapper in single file

--- packages/hms-video-store/src/react/HMSRoomProvider.ts
+++ packages/hms-video-store/src/react/HMSRoomProvider.ts
@@ -1,14 +1,25 @@
 import React, { createContext, useContext } from 'react';
-import { HMSReactiveStore, IHMSActions } from '../core';
-import { HMSContextProviderProps, makeHMSStoreHook } from './storeHook';
+import { HMSReactiveStore, HMSStore, IHMSActions, IHMSStore } from '../core';
 import { IHMSStoreReadOnly } from '../core/IHMSStore';
+import { EqualityChecker, StateSelector } from 'zustand';
+import shallow from 'zustand/shallow';
+import { HMSLogger } from '../common/ui-logger';
 
-const HMSContext = createContext<HMSContextProviderProps | null>(null);
+interface HMSContextProviderProps {
+  actions: IHMSActions; // for actions which may also mutate store
+  store: IHMSStoreReadOnly; // readonly store, don't mutate this
+}
 
 export interface HMSRoomProviderProps {
   actions?: IHMSActions;
   store?: IHMSStoreReadOnly;
 }
+
+/**
+ * only one context is being created currently. This would need to be changed if multiple
+ * rooms have to be supported, where every room will have its own context, provider, store and actions.
+ */
+const HMSContext = createContext<HMSContextProviderProps | null>(null);
 
 let providerProps: HMSContextProviderProps;
 export const HMSRoomProvider: React.FC<HMSRoomProviderProps> = ({ children, ...props }) => {
@@ -33,9 +44,26 @@ export const HMSRoomProvider: React.FC<HMSRoomProviderProps> = ({ children, ...p
 /*
 UseHMSStore is a read only hook which can be passed a selector to read data.
 The hook can only be used in a component if HMSRoomProvider is present in its ancestors.
-One HMSRoomProvider will need to be created per room in the UI.
  */
-export const useHMSStore = makeHMSStoreHook(HMSContext);
+export const useHMSStore = () => {
+  const useHMSStore = <StateSlice>(
+    selector: StateSelector<HMSStore, StateSlice>,
+    equalityFn: EqualityChecker<StateSlice> = shallow,
+  ) => {
+    if (!selector) {
+      HMSLogger.w(
+        'fetching full store without passing any selector may have a heavy performance impact on your website.',
+      );
+    }
+    const HMSContextConsumer = useContext(HMSContext);
+    if (!HMSContextConsumer) {
+      throw new Error('HMSContext state variables are not set');
+    }
+    const useStore = HMSContextConsumer.store as IHMSStore;
+    return useStore(selector, equalityFn);
+  };
+  return useHMSStore;
+};
 
 /*
 UseHMSActions is a write ony hook which can be used to dispatch actions.
