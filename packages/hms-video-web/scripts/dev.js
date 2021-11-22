907481abca791ef5011cc17b5c29d37029b10c88
raviteja83 <ravi.theja@100ms.live>
2021-11-22 11:01:53 +0530

    build: use esbuild instead of tsdx

---
+++ packages/hms-video-web/scripts/dev.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+const esbuild = require('esbuild');
+const pkg = require('../package.json');
+
+async function main() {
+  esbuild.build({
+    entryPoints: ['./src/index.ts'],
+    outdir: 'dist',
+    minify: false,
+    bundle: true,
+    format: 'esm',
+    target: 'es6',
+    tsconfig: './tsconfig.build.json',
+    external: Object.keys(pkg.dependencies),
+    sourcemap: true,
+    incremental: true,
+    treeShaking: true,
+    watch: {
+      onRebuild(error) {
+        if (error) {
+          console.log(`× ${pkg.name}: An error in prevented the rebuild.`);
+          return;
+        }
+        console.log(`✔ ${pkg.name}: Rebuilt.`);
+      },
+    },
+  });
+}
+
+main();
