6502cbd71a9146c7071b31edd5eece6b182f015c
raviteja83 <ravi.theja@100ms.live>
2021-11-22 07:59:24 +0530

    ci: add test to workflow

--- packages/hms-video-web/jest.config.js
+++ packages/hms-video-web/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   transform: {
-    '.(ts|tsx)$': './node_modules/ts-jest/dist/index.js',
-    '.(js|jsx)$': './node_modules/babel-jest/build/index.js',
+    '.(ts|tsx)$': '../../node_modules/ts-jest/dist/index.js',
+    '.(js|jsx)$': '../../node_modules/babel-jest/build/index.js',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}'],
