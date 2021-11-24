58addfe9d45126da1d27db0a225e14018f51f19b
raviteja83 <ravi.theja@100ms.live>
2021-11-24 19:39:02 +0530

    ci: update labeller script

--- .github/labeller.js
+++ .github/labeller.js
@@ -6,10 +6,10 @@ module.exports = async ({ github, context }) => {
     issue: { number: issue },
   } = context;
 
-  const currentLabels = await github.rest.issues.listLabelsOnIssue({
+  const { data } = await github.rest.issues.listLabelsOnIssue({
     owner,
     repo,
     issue_number: issue,
   });
-  console.log({ currentLabels, CHANGES });
+  console.log({ data, CHANGES });
 };
