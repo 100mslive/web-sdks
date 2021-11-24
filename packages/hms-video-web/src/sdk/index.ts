fa21590725cccbb58119cb05982192e6a380d6b6
raviteja83 <ravi.theja@100ms.live>
2021-11-24 10:44:25 +0530

    revert: test changes

--- packages/hms-video-web/src/sdk/index.ts
+++ packages/hms-video-web/src/sdk/index.ts
@@ -193,9 +193,6 @@ export class HMSSdk implements HMSInterface {
       }
 
       this.transportState = state;
-      console.log('test', this.transportState);
-      console.log('test1', this.transportState);
-      console.log('test2', this.transportState);
     },
   };
 
