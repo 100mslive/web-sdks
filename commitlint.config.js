48b9f3c56ea8e77376d21acdedd6482bbd08ed3e
Tushar <tushar@100ms.live>
2021-11-26 23:06:02 +0530

    lint: no lint errors apart from complexity

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,11 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-min-length': [2, 'always', 5],
+    'type-enum': [
+      2,
+      'always',
+      ['build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'lint'],
+    ],
+  },
+};
