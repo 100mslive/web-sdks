31cfcc5d9edbf46b0d41d62af712d156b15289d8
Ravi theja <raviteja83@users.noreply.github.com>
2021-12-02 08:34:31 +0530

    feat: move cypress tests to root, add wf (#54)

--- cypress/integration/preview.spec.ts
+++ cypress/integration/preview.spec.ts
@@ -1,6 +1,11 @@
-import { HMSReactiveStore, selectIsInPreview, selectLocalVideoTrackID, selectRoomState } from '../../src';
-import { HMSSDKActions } from '../../src/core/hmsSDKStore/HMSSDKActions';
-import { IHMSStoreReadOnly } from '../../src/core/IHMSStore';
+import {
+  HMSReactiveStore,
+  selectIsInPreview,
+  selectLocalVideoTrackID,
+  selectRoomState,
+} from '../../packages/hms-video-store/src';
+import { HMSSDKActions } from '../../packages/hms-video-store/src/core/hmsSDKStore/HMSSDKActions';
+import { IHMSStoreReadOnly } from '../../packages/hms-video-store/src/core/IHMSStore';
 
 let HMSStore;
 let actions: HMSSDKActions;
@@ -28,7 +33,7 @@ describe('preview api', () => {
       .preview({ userName: 'test', authToken: '', initEndpoint })
       .then(() => {})
       .catch(error => {
-        expect(error.message).to.equal('Token is not in proper JWT format');
+        expect(error.message).to.include('Token is not in proper JWT format');
       });
   });
 
