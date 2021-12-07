32f9322ceb503e8efcdef0236254ef0630a71f32
Ravi theja <raviteja83@users.noreply.github.com>
2021-12-07 19:08:53 +0530

    fix: cypress tests to run wrt local peer (#64)

--- cypress/global.d.ts
+++ cypress/global.d.ts
@@ -1,11 +1,16 @@
 /// <reference types="cypress" />
 
-declare namespace Cypress {
-  interface Chainable<> {
-    /**
-     * Get token required for preview/join
-     * @param title
-     */
-    getToken(): Chainable<string>;
+import type { HMSLocalPeer } from '../packages/hms-video-store/src/core/hmsSDKStore/sdkTypes';
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      /**
+       * Get token required for preview/join
+       * @param title
+       */
+      getToken(): Chainable<string>;
+      localTracksAdded(localPeer: HMSLocalPeer): Chainable<void>;
+    }
   }
 }
