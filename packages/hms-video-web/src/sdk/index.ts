b957edde01c19f6a8a671ec06287686f4e86a508
Ashutosh Vishnoi <ashutoshvishnoi75@gmail.com>
2021-12-09 11:28:24 +0530

    refactor: update record/rtmp status from notifications

--- packages/hms-video-web/src/sdk/index.ts
+++ packages/hms-video-web/src/sdk/index.ts
@@ -580,15 +580,6 @@ export class HMSSdk implements HMSInterface {
       );
     }
     await this.transport?.startRTMPOrRecording(params);
-    // emit this notification to update current peer recording status
-    const rtmpStart = { method: HMSNotificationMethod.RTMP_START, params: {} };
-    const recordingStart = { method: HMSNotificationMethod.RECORDING_START, params: { type: 'Browser' } };
-    if (params.rtmpURLs?.length) {
-      this.notificationManager.handleNotification(rtmpStart);
-    }
-    if (params.record) {
-      this.notificationManager.handleNotification(recordingStart);
-    }
   }
 
   async stopRTMPAndRecording() {
@@ -599,17 +590,6 @@ export class HMSSdk implements HMSInterface {
       );
     }
     await this.transport?.stopRTMPOrRecording();
-    // emit this notification to update current peer recording status
-    const { recording, rtmp } = this.store.getRoom();
-    if (recording?.browser.running) {
-      this.notificationManager.handleNotification({
-        method: HMSNotificationMethod.RECORDING_STOP,
-        params: { type: 'Browser' },
-      });
-    }
-    if (rtmp?.running) {
-      this.notificationManager.handleNotification({ method: HMSNotificationMethod.RTMP_STOP, params: {} });
-    }
   }
 
   async changeName(name: string) {
