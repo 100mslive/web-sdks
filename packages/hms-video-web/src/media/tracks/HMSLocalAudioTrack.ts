eccf96658c7129f0ef5e739aa4386fd7ab2a83fd
raviteja83 <ravi.theja@100ms.live>
2021-12-10 10:17:24 +0530

    fix: update check for audio track to update device

--- packages/hms-video-web/src/media/tracks/HMSLocalAudioTrack.ts
+++ packages/hms-video-web/src/media/tracks/HMSLocalAudioTrack.ts
@@ -44,7 +44,7 @@ export class HMSLocalAudioTrack extends HMSAudioTrack {
     this.settings = settings;
     // Replace the 'default' deviceId with the actual deviceId
     // This is to maintain consistency with selected devices as in some cases there will be no 'default' device
-    if (settings.deviceId === 'default' && track.enabled) {
+    if (settings.deviceId === 'default' && !isEmptyTrack(track)) {
       this.settings = this.buildNewSettings({ deviceId: track.getSettings().deviceId });
     }
     this.pluginsManager = new HMSAudioPluginsManager(this);
