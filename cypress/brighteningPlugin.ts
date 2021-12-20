97dca8c06cca80319bd780f3f7f18976aa3d8550
Vishal dhull <47358116+vishaldhull09@users.noreply.github.com>
2021-12-20 13:13:00 +0530

    feat: multiple video plugins support (#53)
    * feat: added support for mutiple video plugin processing
    
    * feat: added support for mutiple video plugin processing
    
    * build: update versions
    
    * feat: added support for mutiple video plugin processing
    
    * feat: multiple video plugins support
    
    * feat: multiple video plugins support
    
    * build: update versions
    
    * refactor: minor refactoring video plugin code and added tests for multiple video plugin
    
    * refactor: minor refactoring video plugin code and added tests for multiple video plugin
    
    * build: update versions
    
    * refactor: minor refactoring video plugin code and added tests for multiple video plugin
    
    * refactor: removed extra code in video plugin test
    
    * refactor: added yarn.lock
    
    * test: add tests for video plugins
    
    * test: add tests for video plugins
    
    * build: update versions
    
    * test: fix failing video plugin tests
    
    * refactor: refactoring
    
    Co-authored-by: apple <apple@apples-MacBook-Pro.local>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: raviteja83 <ravi.theja@100ms.live>

---
+++ cypress/brighteningPlugin.ts
@@ -0,0 +1,48 @@
+import { HMSVideoPluginType } from '@100mslive/hms-video';
+export class brighteningPlugin {
+  private brightnessMul: number;
+  private name;
+
+  constructor(brightnessMul = 1, name = 'brightening-plugin') {
+    this.brightnessMul = brightnessMul;
+    this.name = name;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  isSupported() {
+    return true;
+  }
+
+  async init() {}
+
+  getPluginType() {
+    return HMSVideoPluginType.TRANSFORM;
+  }
+
+  stop() {}
+
+  /**
+   * @param input {HTMLCanvasElement}
+   * @param output {HTMLCanvasElement}
+   */
+  processVideoFrame(input, output) {
+    const width = input.width;
+    const height = input.height;
+    output.width = width;
+    output.height = height;
+    const inputCtx = input.getContext('2d');
+    const outputCtx = output.getContext('2d');
+    const imgData = inputCtx.getImageData(0, 0, width, height);
+    const pixels = imgData.data;
+
+    for (let i = 0; i < pixels.length; i += 4) {
+      pixels[i] = pixels[i] * this.brightnessMul;
+      pixels[i + 1] = pixels[i + 1] * this.brightnessMul;
+      pixels[i + 2] = pixels[i + 2] * this.brightnessMul;
+    }
+    outputCtx.putImageData(imgData, 0, 0);
+  }
+}
