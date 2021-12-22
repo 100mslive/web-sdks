711bc10453ccea054cac4d797ec34e146d106998
eswarclynn <esuclintblazer@gmail.com>
2021-12-22 07:50:31 +0530

    feat: webrtc stats selectors as single object

--- packages/hms-video-store/src/core/webrtc-internals/index.ts
+++ packages/hms-video-store/src/core/webrtc-internals/index.ts
@@ -1,2 +1,2 @@
 export { HMSWebrtcInternals } from './HMSWebrtcInternals';
-export * as selectHMSInternals from './selectors';
+export * as selectHMSStats from './selectors';
--- packages/hms-video-store/src/test/unit/webrtc-internals.test.ts
+++ packages/hms-video-store/src/test/unit/webrtc-internals.test.ts
@@ -8,7 +8,7 @@ import {
   remoteAudioTrackStats,
   remoteVideoTrackStats,
 } from '../fakeStore';
-import { HMSInternalsStore, selectHMSInternals } from '../../core';
+import { HMSInternalsStore, selectHMSStats } from '../../core';
 
 let fakeStore: HMSInternalsStore;
 
@@ -19,77 +19,75 @@ beforeEach(() => {
 
 describe('local peer stats selectors', () => {
   test('selectPacketsLost', () => {
-    expect(selectHMSInternals.packetsLost(fakeStore)).toBe(packetsLost);
+    expect(selectHMSStats.packetsLost(fakeStore)).toBe(packetsLost);
   });
 
   test('selectJitter', () => {
-    expect(selectHMSInternals.jitter(fakeStore)).toBe(jitter);
+    expect(selectHMSStats.jitter(fakeStore)).toBe(jitter);
   });
 
   test('selectLocalPeerStats', () => {
-    expect(selectHMSInternals.localPeerStats(fakeStore)).toBe(localPeerStats);
+    expect(selectHMSStats.localPeerStats(fakeStore)).toBe(localPeerStats);
   });
 
   test('selectPublishBitrate', () => {
-    expect(selectHMSInternals.publishBitrate(fakeStore)).toBe(localPeerStats.publish?.bitrate);
+    expect(selectHMSStats.publishBitrate(fakeStore)).toBe(localPeerStats.publish?.bitrate);
   });
 
   test('selectSubscribeBitrate', () => {
-    expect(selectHMSInternals.subscribeBitrate(fakeStore)).toBe(localPeerStats.subscribe?.bitrate);
+    expect(selectHMSStats.subscribeBitrate(fakeStore)).toBe(localPeerStats.subscribe?.bitrate);
   });
 
   test('selectAvailablePublishBitrate', () => {
-    expect(selectHMSInternals.availablePublishBitrate(fakeStore)).toBe(
-      localPeerStats.publish?.availableOutgoingBitrate,
-    );
+    expect(selectHMSStats.availablePublishBitrate(fakeStore)).toBe(localPeerStats.publish?.availableOutgoingBitrate);
   });
 
   test('selectAvailableSubscribeBitrate', () => {
-    expect(selectHMSInternals.availableSubscribeBitrate(fakeStore)).toBe(
+    expect(selectHMSStats.availableSubscribeBitrate(fakeStore)).toBe(
       localPeerStats.subscribe?.availableIncomingBitrate,
     );
   });
 
   test('selectTotalBytesSent', () => {
-    expect(selectHMSInternals.totalBytesSent(fakeStore)).toBe(localPeerStats.publish?.bytesSent);
+    expect(selectHMSStats.totalBytesSent(fakeStore)).toBe(localPeerStats.publish?.bytesSent);
   });
 
   test('selectTotalBytesReceived', () => {
-    expect(selectHMSInternals.totalBytesReceived(fakeStore)).toBe(localPeerStats.subscribe?.bytesReceived);
+    expect(selectHMSStats.totalBytesReceived(fakeStore)).toBe(localPeerStats.subscribe?.bytesReceived);
   });
 });
 
 describe('local tracks stats selectors', () => {
   test('selectLocalAudioTrackStats', () => {
-    expect(selectHMSInternals.localAudioTrackStats(fakeStore)).toBe(localAudioTrackStats);
+    expect(selectHMSStats.localAudioTrackStats(fakeStore)).toBe(localAudioTrackStats);
   });
 
   test('selectLocalVideoTrackStats', () => {
-    expect(selectHMSInternals.localVideoTrackStats(fakeStore)).toBe(localVideoTrackStats);
+    expect(selectHMSStats.localVideoTrackStats(fakeStore)).toBe(localVideoTrackStats);
   });
 
   test('selectLocalAudioTrackBitrate', () => {
-    expect(selectHMSInternals.localAudioTrackBitrate(fakeStore)).toBe(localAudioTrackStats.bitrate);
+    expect(selectHMSStats.localAudioTrackBitrate(fakeStore)).toBe(localAudioTrackStats.bitrate);
   });
 
   test('selectLocalVideoTrackBitrate', () => {
-    expect(selectHMSInternals.localVideoTrackBitrate(fakeStore)).toBe(localVideoTrackStats.bitrate);
+    expect(selectHMSStats.localVideoTrackBitrate(fakeStore)).toBe(localVideoTrackStats.bitrate);
   });
 
   test('selectLocalAudioTrackBytesSent', () => {
-    expect(selectHMSInternals.localAudioTrackBytesSent(fakeStore)).toBe(localAudioTrackStats.bytesSent);
+    expect(selectHMSStats.localAudioTrackBytesSent(fakeStore)).toBe(localAudioTrackStats.bytesSent);
   });
 
   test('selectLocalVideoTrackBytesSent', () => {
-    expect(selectHMSInternals.localVideoTrackBytesSent(fakeStore)).toBe(localVideoTrackStats.bytesSent);
+    expect(selectHMSStats.localVideoTrackBytesSent(fakeStore)).toBe(localVideoTrackStats.bytesSent);
   });
 
   test('selectLocalVideoTrackBytesSent', () => {
-    expect(selectHMSInternals.localVideoTrackFramerate(fakeStore)).toBe(localVideoTrackStats.framesPerSecond);
+    expect(selectHMSStats.localVideoTrackFramerate(fakeStore)).toBe(localVideoTrackStats.framesPerSecond);
   });
 
   test('selectLocalVideoTrackBytesSent', () => {
-    expect(selectHMSInternals.localVideoTrackQualityLimitationReason(fakeStore)).toBe(
+    expect(selectHMSStats.localVideoTrackQualityLimitationReason(fakeStore)).toBe(
       localVideoTrackStats.qualityLimitationReason,
     );
   });
@@ -97,34 +95,34 @@ describe('local tracks stats selectors', () => {
 
 describe('track stats by ID', () => {
   test('selectTrackStatsByID', () => {
-    expect(selectHMSInternals.trackStatsByID('103')(fakeStore)).toBe(remoteVideoTrackStats);
-    expect(selectHMSInternals.trackStatsByID('104')(fakeStore)).toBe(remoteAudioTrackStats);
+    expect(selectHMSStats.trackStatsByID('103')(fakeStore)).toBe(remoteVideoTrackStats);
+    expect(selectHMSStats.trackStatsByID('104')(fakeStore)).toBe(remoteAudioTrackStats);
   });
 
   test('selectBitrateByTrackID', () => {
-    expect(selectHMSInternals.bitrateByTrackID('103')(fakeStore)).toBe(remoteVideoTrackStats.bitrate);
-    expect(selectHMSInternals.bitrateByTrackID('104')(fakeStore)).toBe(remoteAudioTrackStats.bitrate);
+    expect(selectHMSStats.bitrateByTrackID('103')(fakeStore)).toBe(remoteVideoTrackStats.bitrate);
+    expect(selectHMSStats.bitrateByTrackID('104')(fakeStore)).toBe(remoteAudioTrackStats.bitrate);
   });
 
   test('selectBytesReceivedByTrackID', () => {
-    expect(selectHMSInternals.bytesReceivedByTrackID('103')(fakeStore)).toBe(remoteVideoTrackStats.bytesReceived);
+    expect(selectHMSStats.bytesReceivedByTrackID('103')(fakeStore)).toBe(remoteVideoTrackStats.bytesReceived);
 
-    expect(selectHMSInternals.bytesReceivedByTrackID('104')(fakeStore)).toBe(remoteAudioTrackStats.bytesReceived);
+    expect(selectHMSStats.bytesReceivedByTrackID('104')(fakeStore)).toBe(remoteAudioTrackStats.bytesReceived);
   });
 
   test('selectJitterByTrackID', () => {
-    expect(selectHMSInternals.jitterByTrackID('103')(fakeStore)).toBe(remoteVideoTrackStats.jitter);
+    expect(selectHMSStats.jitterByTrackID('103')(fakeStore)).toBe(remoteVideoTrackStats.jitter);
 
-    expect(selectHMSInternals.jitterByTrackID('104')(fakeStore)).toBe(remoteAudioTrackStats.jitter);
+    expect(selectHMSStats.jitterByTrackID('104')(fakeStore)).toBe(remoteAudioTrackStats.jitter);
   });
 
   test('selectPacketsLostByTrackID', () => {
-    expect(selectHMSInternals.packetsLostByTrackID('103')(fakeStore)).toBe(remoteVideoTrackStats.packetsLost);
+    expect(selectHMSStats.packetsLostByTrackID('103')(fakeStore)).toBe(remoteVideoTrackStats.packetsLost);
 
-    expect(selectHMSInternals.packetsLostByTrackID('104')(fakeStore)).toBe(remoteAudioTrackStats.packetsLost);
+    expect(selectHMSStats.packetsLostByTrackID('104')(fakeStore)).toBe(remoteAudioTrackStats.packetsLost);
   });
 
   test('selectFramerateByTrackID', () => {
-    expect(selectHMSInternals.framerateByTrackID('103')(fakeStore)).toBe(remoteVideoTrackStats.framesPerSecond);
+    expect(selectHMSStats.framerateByTrackID('103')(fakeStore)).toBe(remoteVideoTrackStats.framesPerSecond);
   });
 });
