9aefc6948d4162d939c1186ef9763a677699f530
eswarclynn <esuclintblazer@gmail.com>
2021-12-23 16:46:58 +0530

    revert: remove appName from storeName

--- packages/hms-video-store/src/common/storeName.ts
+++ packages/hms-video-store/src/common/storeName.ts
@@ -1,11 +1,5 @@
 import { isBrowser } from '@100mslive/hms-video';
 
 export const storeNameWithTabTitle = (storeName: string) => {
-  let appName: string;
-  if (window.location.host.includes('localhost')) {
-    appName = 'localhost';
-  } else {
-    appName = window.location.host.split('.')[0];
-  }
-  return isBrowser ? `${storeName} ${document.title}(${appName})` : storeName;
+  return isBrowser ? `${storeName} ${document.title}` : storeName;
 };
