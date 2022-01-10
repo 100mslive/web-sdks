ba6b71be6335c78845204a2639272694cfbaa436
Ravi theja <raviteja83@users.noreply.github.com>
2022-01-10 11:33:21 +0530

    ci: fix push to webapp, versioning
    * ci: use fetch-depth 0 for push to webapp
    
    * ci: use main branch as ref
    
    * ci: fix update versions script
    
    * ci: fix versioning script, remove logs
    
    * ci: add more logging for versioning
    
    * ci: fix versioning script
    
    * ci: add code comments

--- scripts/setversions.js
+++ scripts/setversions.js
@@ -2,13 +2,20 @@ const { dependencyMapping } = require('./constants');
 const path = require('path');
 
 module.exports = ({ core }) => {
-  console.log(JSON.stringify(process.env));
-  console.log(core);
-  const versions = Object.keys(dependencyMapping).reduce((pkgVersions, pkgName) => {
+  const { versions } = process.env;
+  const currentVersions = Object.keys(dependencyMapping).reduce((pkgVersions, pkgName) => {
     const location = path.resolve(`packages/${pkgName}`);
     const version = require(`${location}/package.json`).version;
     pkgVersions[pkgName] = version;
     return pkgVersions;
   }, {});
-  core.exportVariable('versions', JSON.stringify(versions));
+
+  // Verions is not present when the script is run for main branch
+  if (!versions) {
+    core.exportVariable('versions', JSON.stringify(currentVersions));
+    return;
+  }
+  core.exportVariable('currentVersions', JSON.stringify(currentVersions));
+  // versions from main will also have to be exported from here
+  core.exportVariable('mainVersions', versions);
 };
