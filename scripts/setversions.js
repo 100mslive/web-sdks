5a6d73e4bd40773f77794df9a82ad5f2f3291256
raviteja83 <ravi.theja@100ms.live>
2022-01-10 11:21:59 +0530

    ci: fix versioning script

--- scripts/setversions.js
+++ scripts/setversions.js
@@ -1,13 +1,18 @@
 const { dependencyMapping } = require('./constants');
 const path = require('path');
 
-module.exports = ({ core, name }) => {
-  console.log({ name });
-  const versions = Object.keys(dependencyMapping).reduce((pkgVersions, pkgName) => {
+module.exports = ({ core }) => {
+  const { versions } = process.env;
+  const currentVersions = Object.keys(dependencyMapping).reduce((pkgVersions, pkgName) => {
     const location = path.resolve(`packages/${pkgName}`);
     const version = require(`${location}/package.json`).version;
     pkgVersions[pkgName] = version;
     return pkgVersions;
   }, {});
-  core.exportVariable(name, JSON.stringify(versions));
+  if (!versions) {
+    core.exportVariable('versions', JSON.stringify(currentVersions));
+    return;
+  }
+  core.exportVariable('currentVersions', JSON.stringify(currentVersions));
+  core.exportVariable('mainVersions', versions);
 };
