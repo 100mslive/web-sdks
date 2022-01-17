88b2b2887fa7f8d9d30a45ef8902783d54828353
raviteja83 <ravi.theja@100ms.live>
2022-01-17 14:24:31 +0530

    fix: chat when screenshared

--- apps/100ms-web/src/views/components/gridView.jsx
+++ apps/100ms-web/src/views/components/gridView.jsx
@@ -65,7 +65,7 @@ export const GridCenterView = ({
         <Box
           className={`${getBlurClass(isParticipantListOpen, totalPeers)}`}
           css={{
-            height: "50%",
+            height: "45%",
             position: "absolute",
             zIndex: 40,
             right: "$2",
