745d9188d38de3dba1e26c872463b50f462a52f3
eswarclynn <esuclintblazer@gmail.com>
2022-01-17 15:33:58 +0530

    feat: add InfoIcon, PencilDrawIcon

---
+++ packages/react-icons/src/PencilDrawIcon.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+function SvgPencilDrawIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg width="24px" height="24px" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M23.435 3.25c-.808 0-1.583.321-2.155.892l-9.068 9.069-.002.002-.002.002-1.477 1.477a.93.93 0 00-.252.42l-1.3 4.271a.938.938 0 001.17 1.17l4.272-1.3a.933.933 0 00.42-.253l1.478-1.478 9.07-9.07a3.047 3.047 0 00-2.154-5.202zM12.21 15.864l.663-.663 1.657 1.658-.663.663-2.383.725.726-2.383zm1.989-1.988l1.657 1.657 8.407-8.407a1.172 1.172 0 10-1.657-1.658L14.2 13.876zm-7.636 6.249a3.047 3.047 0 100 6.094h14.765a.938.938 0 000-1.875H6.563a1.172 1.172 0 010-2.344h.7a.937.937 0 100-1.875h-.7z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
+
+export default SvgPencilDrawIcon;
