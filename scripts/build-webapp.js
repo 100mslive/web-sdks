6ee258527d94f055dd6a7dc70f8bcfd1430f30e7
Ravi theja <raviteja83@users.noreply.github.com>
2022-01-24 18:32:31 +0530

    feat: import custom app (#145)

---
+++ scripts/build-webapp.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+const esbuild = require('esbuild');
+const { gzip } = require('zlib');
+
+async function main() {
+  if (fs.existsSync('./dist')) {
+    fs.rmSync('./dist', { recursive: true }, e => {
+      if (e) {
+        throw e;
+      }
+    });
+  }
+  const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+  const source = './src/App.js';
+  const external = Object.keys(pkg.dependencies || {});
+  const loader = { '.js': 'jsx', '.svg': 'file' };
+  try {
+    esbuild.buildSync({
+      entryPoints: [source],
+      outfile: 'dist/index.cjs.js',
+      minify: false,
+      bundle: true,
+      format: 'cjs',
+      target: 'es6',
+      external,
+      metafile: true,
+      loader,
+    });
+
+    const esmResult = esbuild.build({
+      entryPoints: [source],
+      outfile: 'dist/index.js',
+      minify: true,
+      bundle: true,
+      format: 'esm',
+      target: 'es6',
+      external,
+      metafile: true,
+      loader,
+    });
+
+    let esmSize = 0;
+    Object.values(esmResult.metafile?.outputs || {}).forEach(output => {
+      esmSize += output.bytes;
+    });
+
+    fs.readFile('./dist/index.js', (_err, data) => {
+      gzip(data, (_err, result) => {
+        console.log(
+          `✔ ${pkg.name}: Built pkg. ${(esmSize / 1000).toFixed(2)}kb (${(result.length / 1000).toFixed(
+            2,
+          )}kb minified)`,
+        );
+      });
+    });
+  } catch (e) {
+    console.log(`× ${pkg.name}: Build failed due to an error.`);
+    console.log(e);
+  }
+}
+
+main();
