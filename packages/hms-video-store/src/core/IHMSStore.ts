0b04f69c78d2f3de77362e1fc36684a018ab04a3
eswarclynn <esuclintblazer@gmail.com>
2022-01-25 21:19:32 +0530

    refactor: use interfaces instead of types

--- packages/hms-video-store/src/core/IHMSStore.ts
+++ packages/hms-video-store/src/core/IHMSStore.ts
@@ -42,27 +42,13 @@ export interface GetState<T extends State> {
   <StateSlice>(selector?: StateSelector<T, StateSlice>): StateSlice;
 }
 
-/**
- * HMS store can be used to:
- * - Get a part of the current store or state(getState)
- * - Subscribe to different parts of the store using selectors and execute a callback when the value changes.
- *
- * Both getState and subscribe use selectors to query a part the store.
- *
- *
- * Selectors are functions with HMSStore as an argument and returns a part of the store.
- *
- * **StoreSelector** is a type alias for this type of function.
- *
- * @category Core
- */
 export type IStoreReadOnly<T extends State> = Omit<IStore<T>, 'setState' | 'namedSetState' | 'destroy'>;
 
 /**
  * HMS Reactive store can be used to subscribe to different parts of the store using selectors
  * and get a callback when the value changes.
  */
-export type IHMSStore = IStore<HMSStore>;
+export interface IHMSStore extends IStore<HMSStore> {}
 
 /**
  * HMS store can be used to:
@@ -78,7 +64,7 @@ export type IHMSStore = IStore<HMSStore>;
  *
  * @category Core
  */
-export type IHMSStoreReadOnly = IStoreReadOnly<HMSStore>;
+export interface IHMSStoreReadOnly extends IStoreReadOnly<HMSStore> {}
 
-export type IHMSStatsStore = IStore<HMSStatsStore>;
-export type IHMSStatsStoreReadOnly = IStoreReadOnly<HMSStatsStore>;
+export interface IHMSStatsStore extends IStore<HMSStatsStore> {}
+export interface IHMSStatsStoreReadOnly extends IStoreReadOnly<HMSStatsStore> {}
