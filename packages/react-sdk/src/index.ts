670550535d919e73c424d29e579d7c8b3e4e4a1b
Tushar <tushar@100ms.live>
2022-02-01 09:54:03 +0530

    feat(react-sdk): export usescreenshare

--- packages/react-sdk/src/index.ts
+++ packages/react-sdk/src/index.ts
@@ -10,6 +10,7 @@ export { usePreview } from './hooks/usePreview';
 export { useAVToggle } from './hooks/useAVToggle';
 export { useDevices } from './hooks/useDevices';
 export { useVideo } from './hooks/useVideo';
+export { useScreenShare } from './hooks/useScreenShare';
 export { useAudio } from './hooks/useAudio';
 export { useVideoList } from './hooks/useVideoList';
 export { useAudioLevelStyles } from './hooks/useAudioLevelStyles';
@@ -22,6 +23,7 @@ export type { hooksErrHandler } from './hooks/types';
 export type { usePreviewInput, usePreviewResult } from './hooks/usePreview';
 export type { useAVToggleResult } from './hooks/useAVToggle';
 export type { useDevicesResult } from './hooks/useDevices';
+export type { useScreenShareResult } from './hooks/useScreenShare';
 
 // helpers
 export { throwErrorHandler } from './utils/commons';
