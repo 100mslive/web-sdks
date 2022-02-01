daa4969b868d361209953f661e8f1631df761516
Eswar Prasad Clinton. A <64120992+eswarclynn@users.noreply.github.com>
2022-02-01 15:21:29 +0530

    feat: add remote stats, PL rate [HMS-5552] (#168)
    * feat: add remote stats, PL rate
    
    * refactor: remove passing prevStats
    
    * feat: add remote stats to UI
    
    * refactor(react-ui): refactor stats component
    
    Co-authored-by: Tushar <tushar@100ms.live>

--- packages/react-ui/src/Stats/Stats.tsx
+++ packages/react-ui/src/Stats/Stats.tsx
@@ -8,34 +8,10 @@ export interface VideoTileStatsProps {
   audioTrackID?: HMSTrackID;
 }
 
-const RawStatsRow = ({ label = '', value = '', show = true }) => {
-  return (
-    <>
-      {show ? (
-        <Stats.Row>
-          <Stats.Label>{label}</Stats.Label>
-          {value === '' ? <Stats.Value /> : <Stats.Value>{value}</Stats.Value>}
-        </Stats.Row>
-      ) : null}
-    </>
-  );
-};
-
-// memoize so only the rows which change rerender
-const StatsRow = React.memo(RawStatsRow);
-
-const TrackPacketsLostRow = ({ stats, label }: { stats?: HMSTrackStats; label: string }) => {
-  const packetsLostRate = (stats?.packetsLostRate ? stats.packetsLostRate.toFixed(2) : 0) + '/s';
-
-  return (
-    <StatsRow
-      show={isNotNullishAndNot0(stats?.packetsLost)}
-      label={label}
-      value={`${stats?.packetsLost}(${packetsLostRate})`}
-    />
-  );
-};
-
+/**
+ * This component can be used to overlay webrtc stats over the Video Tile. For the local tracks it also includes
+ * remote inbound stats as sent by the SFU in receiver report.
+ */
 export function VideoTileStats({ videoTrackID, audioTrackID }: VideoTileStatsProps) {
   const audioTrackStats = useHMSStatsStore(selectHMSStats.trackStatsByID(audioTrackID));
   const videoTrackStats = useHMSStatsStore(selectHMSStats.trackStatsByID(videoTrackID));
@@ -77,25 +53,69 @@ export function VideoTileStats({ videoTrackID, audioTrackID }: VideoTileStatsPro
             value={formatBytes(audioTrackStats?.bitrate, 'b/s')}
           />
 
-          <TrackPacketsLostRow label="Packet Loss(V)" stats={videoTrackStats} />
-          <TrackPacketsLostRow label="Packet Loss(A)" stats={audioTrackStats} />
-
-          <StatsRow
-            show={isNotNullish(videoTrackStats?.jitter)}
-            label="Jitter(V)"
-            value={videoTrackStats?.jitter?.toString()}
-          />
-
-          <StatsRow
-            show={isNotNullish(audioTrackStats?.jitter)}
-            label="Jitter(A)"
-            value={audioTrackStats?.jitter?.toString()}
-          />
+          <PacketLostAndJitter audioTrackStats={audioTrackStats} videoTrackStats={videoTrackStats} />
         </tbody>
       </table>
     </Stats.Root>
   );
 }
+
+const PacketLostAndJitter = ({
+  audioTrackStats,
+  videoTrackStats,
+}: {
+  audioTrackStats?: HMSTrackStats;
+  videoTrackStats?: HMSTrackStats;
+}) => {
+  // for local peer, we'll use the remote inbound stats to get packet loss and jitter, to know whether the track is
+  // local we check if the stats type has outbound in it as it's being published from local. Both audio and video
+  // tracks are checked in case the user has permission to publish only one of them.
+  const isLocalPeer = audioTrackStats?.type.includes('outbound') || videoTrackStats?.type.includes('outbound');
+  const audioStats = isLocalPeer ? audioTrackStats?.remote : audioTrackStats;
+  const videoStats = isLocalPeer ? videoTrackStats?.remote : videoTrackStats;
+  return (
+    <>
+      <TrackPacketsLostRow label="Packet Loss(V)" stats={videoStats} />
+      <TrackPacketsLostRow label="Packet Loss(A)" stats={audioStats} />
+      <StatsRow show={isNotNullish(videoStats?.jitter)} label="Jitter(V)" value={videoStats?.jitter?.toFixed(4)} />
+      <StatsRow show={isNotNullish(audioStats?.jitter)} label="Jitter(A)" value={audioStats?.jitter?.toFixed(4)} />
+    </>
+  );
+};
+
+const TrackPacketsLostRow = ({
+  stats,
+  label,
+}: {
+  stats?: Pick<HMSTrackStats, 'packetsLost' | 'packetsLostRate'>;
+  label: string;
+}) => {
+  const packetsLostRate = (stats?.packetsLostRate ? stats.packetsLostRate.toFixed(2) : 0) + '/s';
+
+  return (
+    <StatsRow
+      show={isNotNullishAndNot0(stats?.packetsLost)}
+      label={label}
+      value={`${stats?.packetsLost}(${packetsLostRate})`}
+    />
+  );
+};
+
+const RawStatsRow = ({ label = '', value = '', show = true }) => {
+  return (
+    <>
+      {show ? (
+        <Stats.Row>
+          <Stats.Label>{label}</Stats.Label>
+          {value === '' ? <Stats.Value /> : <Stats.Value>{value}</Stats.Value>}
+        </Stats.Row>
+      ) : null}
+    </>
+  );
+};
+
+// memoize so only the rows which change rerender
+const StatsRow = React.memo(RawStatsRow);
 
 export function isNotNullishAndNot0(value: number | undefined | null) {
   return isNotNullish(value) && value !== 0;
