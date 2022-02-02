c69f1bba6b3c8fbaed042487fa5dd4edfbc79519
Tushar <tushar@100ms.live>
2022-02-02 09:08:30 +0530

    docs(react-sdk): add ideas for next set of hooks

--- packages/react-sdk/src/hooks/useVideoList.ts
+++ packages/react-sdk/src/hooks/useVideoList.ts
@@ -12,6 +12,10 @@ import { useResizeDetector } from 'react-resize-detector';
 
 export interface useVideoListInput {
   /**
+   * peers is the list of all peers you need to render.
+   */
+  peers: HMSPeer[];
+  /**
    * Max tiles in a  page. Overrides maxRowCount and maxColCount
    */
   maxTileCount?: number;
@@ -24,24 +28,38 @@ export interface useVideoListInput {
    */
   maxColCount?: number;
   /**
-   * Given a screensharing peer the function should return true if their screenshare should be
+   * Given a screensharing peer the function should return true if their screenShare should be
    * included for the video tile, and false otherwise.
    * This can be useful if there are multiple screenshares in the room where you may want to show one in the
-   * center view and others in video list along side other tiles.
-   * e.g. showScreenFn
+   * center view and others in video list along side other tiles. No screenShare is included by default.
+   * e.g. includeScreenShare = (peer) => return peer.id !== mainScreenSharingPeer.id
    */
-  includeScreenShare?: (peer: HMSPeer) => boolean;
-  peers: HMSPeer[];
+  includeScreenShareForPeer?: (peer: HMSPeer) => boolean;
+  /**
+   *
+   */
   overflow?: 'scroll-x' | 'scroll-y' | 'hidden';
   /**
-   * Aspect ratio of VideoTiles
+   * Aspect ratio of VideoTiles, ideally this should be the same as the aspect ratio selected for
+   * capture in the dashboard template.
    */
   aspectRatio?: { width: number; height: number };
   /**
    * By default this will be true. Only publishing(audio/video/screen) peers in the passed in peer list
-   * will be filtered by default. If you wish to show all peers, pass false for this.
+   * will be filtered. If you wish to show all peers, pass false for this.
    */
   filterNonPublishingPeers?: boolean;
+}
+
+export interface useVideoResult {
+  /**
+   * This returns a list of all pages with every page containing the list of all tiles on it.
+   */
+  pagesWithTiles: (TrackWithPeer & {
+    width: number;
+    height: number;
+  })[][];
+  ref: React.MutableRefObject<any>;
 }
 
 const DEFAULTS = {
@@ -51,29 +69,29 @@ const DEFAULTS = {
   },
 };
 
+/**
+ * This hook can be used to build a paginated gallery view of video tiles. You can give the hook
+ * a list of all the peers which need to be shown and it tells you how to structure the UI by giving
+ * a list of pages with every page having a 2D matrix of video tiles, tiles showing per row.
+ * Please check the documentation of input and output types for more details.
+ */
 export const useVideoList = ({
+  peers,
   maxTileCount,
   maxColCount,
   maxRowCount,
-  includeScreenShare = () => false,
-  peers,
+  includeScreenShareForPeer = () => false,
   overflow = 'scroll-x',
   aspectRatio = DEFAULTS.aspectRatio,
   filterNonPublishingPeers = true,
-}: useVideoListInput): {
-  pagesWithTiles: (TrackWithPeer & {
-    width: number;
-    height: number;
-  })[][];
-  ref: React.MutableRefObject<any>;
-} => {
+}: useVideoListInput): useVideoResult => {
   const { width = 0, height = 0, ref } = useResizeDetector();
   const store = useHMSVanillaStore();
   const tracksMap: Record<HMSTrackID, HMSTrack> = store.getState(selectTracksMap);
   const tracksWithPeer: TrackWithPeer[] = getVideoTracksFromPeers(
     peers,
     tracksMap,
-    includeScreenShare,
+    includeScreenShareForPeer,
     filterNonPublishingPeers,
   );
   const finalAspectRatio = useMemo(() => {
