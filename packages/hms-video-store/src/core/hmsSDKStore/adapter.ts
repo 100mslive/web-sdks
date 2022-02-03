72ecf2aedee66840cfe0b1a49767e943e2ca4dfc
eswarclynn <esuclintblazer@gmail.com>
2022-02-03 20:50:41 +0530

    feat(sdk): store error from biz

--- packages/hms-video-store/src/core/hmsSDKStore/adapter.ts
+++ packages/hms-video-store/src/core/hmsSDKStore/adapter.ts
@@ -239,11 +239,36 @@ export class SDKToHMS {
         browser: {
           running: !!recording?.browser?.running,
           startedAt: recording?.browser?.startedAt,
+          error: recording?.browser?.error && {
+            code: recording?.browser?.error?.code,
+            message: recording?.browser?.error?.message,
+          },
         },
-        server: { running: !!recording?.server?.running, startedAt: recording?.server?.startedAt },
+        server: {
+          running: !!recording?.server?.running,
+          startedAt: recording?.server?.startedAt,
+          error: recording?.server?.error && {
+            code: recording?.server?.error?.code,
+            message: recording?.server.error?.message,
+          },
+        },
       },
-      rtmp: { running: !!rtmp?.running, startedAt: rtmp?.startedAt },
-      hls: { variants: hls?.variants?.map(variant => variant) || [], running: !!hls?.running },
+      rtmp: {
+        running: !!rtmp?.running,
+        startedAt: rtmp?.startedAt,
+        error: rtmp?.error && {
+          code: rtmp.error.code,
+          message: rtmp.error.message,
+        },
+      },
+      hls: {
+        variants: hls?.variants?.map(variant => variant) || [],
+        running: !!hls?.running,
+        error: hls?.error && {
+          code: hls.error.code,
+          message: hls.error.message,
+        },
+      },
     };
   }
 }
