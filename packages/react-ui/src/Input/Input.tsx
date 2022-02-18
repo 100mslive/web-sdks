d5fed3a4f13e7aa273fd7401346486046796a4b7
Tushar <tushar@100ms.live>
2022-02-18 23:29:34 +0530

    refactor(app): simplify preview input css

--- packages/react-ui/src/Input/Input.tsx
+++ packages/react-ui/src/Input/Input.tsx
@@ -1,30 +1,22 @@
 import { styled } from '../Theme';
 
 export const Input = styled('input', {
-  height: '30px',
   backgroundColor: '$grayDark',
   borderRadius: '8px',
   outline: 'none',
   border: 'none',
-  padding: '5px 10px',
+  padding: '0.5rem 0.75rem',
   color: '$textPrimary',
   fontSize: '$md',
   '&:focus': {
     boxShadow: '0 0 0 3px $colors$brandDefault',
   },
-  marginBottom: '20px',
   variants: {
     error: {
       true: {
         '&:focus': {
           boxShadow: '0 0 0 3px $colors$error',
         },
-      },
-    },
-    variant: {
-      compact: {
-        width: '240px',
-        height: '36px',
       },
     },
   },
