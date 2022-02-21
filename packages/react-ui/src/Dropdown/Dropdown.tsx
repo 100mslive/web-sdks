60f1fbb4f40d99634edf6cc3507d3ab0e2ad4f4f
raviteja83 <ravi.theja@100ms.live>
2022-02-21 12:06:54 +0530

    refactor(react-ui): dropdown component

--- packages/react-ui/src/Dropdown/Dropdown.tsx
+++ packages/react-ui/src/Dropdown/Dropdown.tsx
@@ -1,9 +1,11 @@
+import React from 'react';
+import type * as Stitches from '@stitches/react';
 import { Root, Trigger, Content, Item, Separator, Label, Group } from '@radix-ui/react-dropdown-menu';
 import { styled } from '../Theme';
 
-export const Dropdown = styled(Root, {});
+const DropdownRoot = styled(Root, {});
 
-export const DropdownTrigger = styled(Trigger, {
+const DropdownTrigger = styled(Trigger, {
   padding: '$2 $4',
   cursor: 'pointer',
   '&[data-state="open"]': {
@@ -12,7 +14,7 @@ export const DropdownTrigger = styled(Trigger, {
   },
 });
 
-export const DropdownItem = styled(Item, {
+const DropdownItem = styled(Item, {
   h: '$14',
   w: '100%',
   color: '$textPrimary',
@@ -22,13 +24,13 @@ export const DropdownItem = styled(Item, {
   alignItems: 'center',
 });
 
-export const DropdownItemSeparator = styled(Separator, {
+const DropdownItemSeparator = styled(Separator, {
   h: 1,
   backgroundColor: '$grayDefault',
   m: '$4 $8',
 });
 
-export const DropdownContent = styled(Content, {
+const DropdownContent = styled(Content, {
   w: '$56',
   maxHeight: '$56',
   r: '$2',
@@ -37,15 +39,29 @@ export const DropdownContent = styled(Content, {
   overflowY: 'auto',
 });
 
-export const DropdownLabel = styled(Label, {
+const DropdownLabel = styled(Label, {
   display: 'flex',
   alignItems: 'center',
 });
-export const DropdownGroup = styled(Group, {});
+
+const DropdownGroup = styled(Group, {});
+
+type DropdownProps = Stitches.VariantProps<typeof DropdownRoot>;
+
+export const Dropdown: React.FC<DropdownProps> & {
+  Trigger: typeof Trigger;
+  Item: typeof DropdownItem;
+  Content: typeof DropdownContent;
+  ItemSeparator: typeof DropdownItemSeparator;
+  Label: typeof DropdownLabel;
+  Group: typeof DropdownGroup;
+} = props => <DropdownRoot {...props} />;
+
+Dropdown.Trigger = DropdownTrigger;
+Dropdown.Content = DropdownContent;
+Dropdown.Item = DropdownItem;
+Dropdown.Label = DropdownLabel;
+Dropdown.Group = DropdownGroup;
+Dropdown.ItemSeparator = DropdownItemSeparator;
 
 Dropdown.displayName = 'Dropdown';
-DropdownItem.displayName = 'DropdownItem';
-DropdownItemSeparator.displayName = 'DropdownItemSeparator';
-DropdownContent.displayName = 'DropdownContent';
-DropdownLabel.displayName = 'DropdownLabel';
-DropdownGroup.displayName = 'DropdownGroup';
