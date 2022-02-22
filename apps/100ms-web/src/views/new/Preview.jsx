d8448f6fd5f4e225349452007db39ac642c347e6
Deepankar Bhade <deep14bhade@gmail.com>
2022-02-22 17:56:00 +0530

    fix: aspect ratio in preview tile for mobile

--- apps/100ms-web/src/views/new/Preview.jsx
+++ apps/100ms-web/src/views/new/Preview.jsx
@@ -122,6 +122,10 @@ const PreviewTile = ({ name }) => {
         aspectRatio: width / height,
         width: "unset",
         height: "min(360px, 60vh)",
+        "@sm": {
+          height: "unset",
+          width: "min(360px, 90%)",
+        },
       }}
       ref={borderAudioRef}
     >
