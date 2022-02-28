b4e74a8046f46f3111733d198148e4c6addf1a67
raviteja83 <ravi.theja@100ms.live>
2022-02-28 17:26:19 +0530

    test: remove invalid test from vb

--- packages/hms-virtual-background/test/importing.test.ts
+++
@@ -1,7 +0,0 @@
-import { blurBackground } from '../src';
-
-describe('importing', () => {
-  it('works', async () => {
-    await expect(blurBackground({background:"blur", localStream: true})).toBeTruthy()
-  });
-});
