15e11bd4fdac328d066dc028dbd9973fcecb2ec5
raviteja83 <ravi.theja@100ms.live>
2022-03-02 16:55:20 +0530

    build: fix unused code for webapp build

--- scripts/build-webapp.js
+++ scripts/build-webapp.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const esbuild = require('esbuild');
-const { gzip } = require('zlib');
 const PostCssPlugin = require('esbuild-plugin-postcss2');
 const autoprefixer = require('autoprefixer');
 const tailwindcss = require('tailwindcss');
@@ -40,7 +39,7 @@ async function main() {
       define,
     });
 
-    const esmResult = esbuild.build({
+    esbuild.build({
       entryPoints: [source],
       outfile: 'dist/index.js',
       assetNames: '[name]',
@@ -53,21 +52,6 @@ async function main() {
       loader,
       plugins,
       define,
-    });
-
-    let esmSize = 0;
-    Object.values(esmResult.metafile?.outputs || {}).forEach(output => {
-      esmSize += output.bytes;
-    });
-
-    fs.readFile('./dist/index.js', (_err, data) => {
-      gzip(data, (_err, result) => {
-        console.log(
-          `✔ ${pkg.name}: Built pkg. ${(esmSize / 1000).toFixed(2)}kb (${(result.length / 1000).toFixed(
-            2,
-          )}kb minified)`,
-        );
-      });
     });
   } catch (e) {
     console.log(`× ${pkg.name}: Build failed due to an error.`);
