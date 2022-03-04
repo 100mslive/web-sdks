6d646afdee28b5f895b2cb4e28a1757996f1402a
Ashish1793 <ashish@100ms.live>
2022-03-04 12:24:28 +0530

    fix(sdk): minor bug

--- packages/hms-video-web/src/media/tracks/HMSLocalAudioTrack.ts
+++ packages/hms-video-web/src/media/tracks/HMSLocalAudioTrack.ts
@@ -72,7 +72,6 @@ export class HMSLocalAudioTrack extends HMSAudioTrack {
     try {
       await this.pluginsManager.reprocessPlugins();
     } catch (e) {
-      console.log('error in reprocess plugin', e.message);
       this.eventBus.audioPluginFailed.publish(e as HMSException);
     }
   }
