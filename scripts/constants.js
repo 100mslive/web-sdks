70f98fdf281a3bc86cfee0cc814678dbf1d1ecf2
eswarclynn <esuclintblazer@gmail.com>
2022-03-06 19:14:46 +0530

    fix(solid-sdk): tsconfig and build deps

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,6 +1,6 @@
 const dependencyMapping = {
-  'hms-video-web': ['hms-video-web', 'hms-video-store', 'react-sdk', 'react-ui'],
-  'hms-video-store': ['hms-video-store', 'react-sdk', 'react-ui'],
+  'hms-video-web': ['hms-video-web', 'hms-video-store', 'react-sdk', 'react-ui', 'solid-sdk'],
+  'hms-video-store': ['hms-video-store', 'react-sdk', 'react-ui', 'solid-sdk'],
   'react-sdk': ['react-sdk', 'react-ui'],
   'react-icons': ['react-icons', 'react-ui'],
   'react-ui': ['react-ui'],
