040e725f0d40ae6e900cf4e405598e3838b9d90c
eswarclynn <esuclintblazer@gmail.com>
2022-03-06 19:14:00 +0530

    feat(solid-sdk): port react-intersection-observer to solid

---
+++ packages/solid-sdk/src/ports/solid-intersection-observer.ts
@@ -0,0 +1,106 @@
+import { observe } from 'react-intersection-observer';
+import type { InViewHookResponse, IntersectionOptions } from 'react-intersection-observer';
+import { createEffect, createSignal } from 'solid-js';
+
+type State = {
+  inView: boolean;
+  entry?: IntersectionObserverEntry;
+};
+
+/**
+ * React Hooks make it easy to monitor the `inView` state of your components. Call
+ * the `useInView` hook with the (optional) [options](#options) you need. It will
+ * return an array containing a `ref`, the `inView` status and the current
+ * [`entry`](https://developer.mozilla.org/en-US/docs/Web/API/IntersectionObserverEntry).
+ * Assign the `ref` to the DOM element you want to monitor, and the hook will
+ * report the status.
+ *
+ * @example
+ * ```jsx
+ * import React from 'react';
+ * import { useInView } from 'react-intersection-observer';
+ *
+ * const Component = () => {
+ *   const { ref, inView, entry } = useInView({
+ *       threshold: 0,
+ *   });
+ *
+ *   return (
+ *     <div ref={ref}>
+ *       <h2>{`Header inside viewport ${inView}.`}</h2>
+ *     </div>
+ *   );
+ * };
+ * ```
+ */
+export function useInView({
+  threshold,
+  delay,
+  trackVisibility,
+  rootMargin,
+  root,
+  triggerOnce,
+  skip,
+  initialInView,
+  fallbackInView,
+}: IntersectionOptions = {}): InViewHookResponse {
+  let unobserve: (() => void) | undefined;
+  const [state, setState] = createSignal<State>({
+    inView: !!initialInView,
+  });
+  const setRef = (node: Element) => {
+    if (unobserve !== undefined) {
+      unobserve();
+      unobserve = undefined;
+    }
+
+    // Skip creating the observer
+    if (skip) {
+      return;
+    }
+
+    if (node) {
+      unobserve = observe(
+        node,
+        (inView, entry) => {
+          setState({ inView, entry });
+
+          if (entry.isIntersecting && triggerOnce && unobserve) {
+            // If it should only trigger once, unobserve the element after it's inView
+            unobserve();
+            unobserve = undefined;
+          }
+        },
+        {
+          root,
+          rootMargin,
+          threshold,
+          // @ts-ignore
+          trackVisibility,
+          // @ts-ignore
+          delay,
+        },
+        fallbackInView,
+      );
+    }
+  };
+
+  createEffect(() => {
+    if (!unobserve && state().entry && !triggerOnce && !skip) {
+      // If we don't have a ref, then reset the state (unless the hook is set to only `triggerOnce` or `skip`)
+      // This ensures we correctly reflect the current state - If you aren't observing anything, then nothing is inView
+      setState({
+        inView: !!initialInView,
+      });
+    }
+  });
+
+  const result = [setRef, state().inView, state().entry] as InViewHookResponse;
+
+  // Support object destructuring, by adding the specific values.
+  result.ref = result[0];
+  result.inView = result[1];
+  result.entry = result[2];
+
+  return result;
+}
