9671a70f3d38feccc16b517637f6dd28980f4560
eswarclynn <esuclintblazer@gmail.com>
2022-03-06 19:20:26 +0530

    feat(solid-web): init webapp

---
+++ apps/100ms-web-solid/src/App.jsx
@@ -0,0 +1,18 @@
+import styles from './App.module.css';
+
+function App() {
+  return (
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <p>
+          Edit <code>src/App.jsx</code> and save to reload.
+        </p>
+        <a class={styles.link} href="https://github.com/solidjs/solid" target="_blank" rel="noopener noreferrer">
+          Learn Solid
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
