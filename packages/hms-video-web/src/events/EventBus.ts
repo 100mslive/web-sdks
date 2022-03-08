4b0c6f7a4164af6adbd98b3e4abffdb53c892e53
Ravi theja <raviteja83@users.noreply.github.com>
2022-03-08 14:47:13 +0530

    fix(sdk): use instance of analyticseventservice instead of singleton
    https://github.com/100mslive/web-sdks/pull/294

--- packages/hms-video-web/src/events/EventBus.ts
+++ packages/hms-video-web/src/events/EventBus.ts
@@ -5,6 +5,7 @@ import { HMSInternalEvent } from './HMSInternalEvent';
 import { HMSLocalAudioTrack, HMSRemoteVideoTrack } from '../media/tracks';
 import { HMSWebrtcStats } from '../rtc-stats';
 import { ITrackAudioLevelUpdate } from '../utils/track-audio-level-monitor';
+import AnalyticsEvent from '../analytics/AnalyticsEvent';
 
 export class EventBus {
   private eventEmitter: EventEmitter = new EventEmitter();
@@ -32,4 +33,6 @@ export class EventBus {
     HMSEvents.LOCAL_AUDIO_SILENCE,
     this.eventEmitter,
   );
+
+  readonly analytics = new HMSInternalEvent<AnalyticsEvent>(HMSEvents.ANALYTICS, this.eventEmitter);
 }
