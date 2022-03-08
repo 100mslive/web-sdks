5eaa5fade11f05e821a18065a0c933367dbc002f
iamajunravi <ajun@100ms.live>
2022-03-08 17:54:32 +0530

    fix(folder path changes): plugin folder path is changed

--- apps/100ms-web/src/components/Footer.jsx
+++ apps/100ms-web/src/components/Footer.jsx
@@ -28,11 +28,10 @@ import { ScreenShareHintModal } from "./ScreenshareHintModal";
 import { NoiseSuppression } from "../plugins/NoiseSuppression";
 import { ToggleWhiteboard } from "../plugins/whiteboard";
 import { VirtualBackground } from "../plugins/VirtualBackground";
-import { TranscriptionButton } from "../plugins/transcription";
 import { useMyMetadata } from "./hooks/useMetadata";
 import { FeatureFlags } from "../services/FeatureFlags";
 import { isScreenshareSupported } from "../common/utils";
-import { TranscriptionButton } from "../../../plugins/transcription";
+import { TranscriptionButton } from "../plugins/transcription";
 
 const ScreenshareAudio = () => {
   const {
