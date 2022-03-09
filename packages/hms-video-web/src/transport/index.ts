2ff9be795f6a83510880866582b6ccc6665ecb6d
eswarclynn <esuclintblazer@gmail.com>
2022-03-09 14:50:04 +0530

    fix(sdk): dont send conn failed on retries

--- packages/hms-video-web/src/transport/index.ts
+++ packages/hms-video-web/src/transport/index.ts
@@ -696,7 +696,7 @@ export default class HMSTransport implements ITransport {
       this.analyticsEventsService.addTransport(this.analyticsSignalTransport);
       this.analyticsEventsService.flush();
     } catch (error) {
-      if (error instanceof HMSException) {
+      if (error instanceof HMSException && this.state !== TransportState.Reconnecting) {
         this.eventBus.analytics.publish(AnalyticsEventFactory.connect(error, connectRequestedAt, new Date(), endpoint));
       }
       HMSLogger.d(TAG, '❌ internal connect: failed', error);
