acd10e6014c453b7b05bba055c0789022882683f
Ram <raghuram@100ms.live>
2022-03-17 12:16:41 +0530

    feat(Webapp): move audio only settings from more settings to UI settings

--- apps/100ms-web/src/components/MoreSettings/UISettings.jsx
+++ apps/100ms-web/src/components/MoreSettings/UISettings.jsx
@@ -119,7 +119,7 @@ export const UISettings = ({ open, onOpenChange }) => {
             css={{ margin: "0 0" }}
           />
           <DialogCheckbox
-            title="Audio only mode"
+            title="Audio Only Mode"
             id="audioOnlyMode"
             value={isAudioOnly}
             onChange={value => {
