0dff0126214a6080491b6ba3a31517777e95bbb7
raviteja83 <ravi.theja@100ms.live>
2022-04-07 14:23:43 +0530

    fix(custom-app): invite modal with more roles

--- apps/100ms-custom-app/src/components/InviteLinksModal.jsx
+++ apps/100ms-custom-app/src/components/InviteLinksModal.jsx
@@ -7,7 +7,7 @@ const InviteLinksModal = ({ onClose, roomLinks }) => {
   return (
     <Dialog.Root defaultOpen onOpenChange={value => !value && onClose()}>
       <DialogContent title="Role Urls">
-        <Box css={{ mt: '$8' }}>
+        <Box css={{ mt: '$8', maxHeight: '60vh', overflowY: 'auto', mr: '-$10', pr: '$10' }}>
           {roomLinks &&
             Object.keys(roomLinks).map(role => {
               const roomRole = roomLinks[role];
@@ -15,12 +15,22 @@ const InviteLinksModal = ({ onClose, roomLinks }) => {
                 let roleUrl = `https://${window.location.hostname}/preview/${roomRole.identifier}`;
                 return (
                   <Fragment key={role}>
-                    <Flex align="center" css={{ my: '$8' }}>
+                    <Flex
+                      align="center"
+                      css={{ my: '$8', '@sm': { flexDirection: 'column', alignItems: 'flex-start' } }}
+                    >
                       <Text css={{ width: '$36', flexShrink: 0 }}>{role}</Text>
                       <Flex
                         justify="between"
                         align="center"
-                        css={{ border: '1px solid $grayDefault', r: '$1', p: '$4', flex: '1 1 0', minWidth: 0 }}
+                        css={{
+                          border: '1px solid $grayDefault',
+                          r: '$1',
+                          p: '$4',
+                          flex: '1 1 0',
+                          minWidth: 0,
+                          '@sm': { width: '100%', mt: '$4' },
+                        }}
                       >
                         <Text css={{ ...textEllipsis('90%'), flex: '1 1 0' }}>{roleUrl}</Text>
                         <IconButton
