9493e4e06f8f4553ad0428bbe40e1cbf4c299b31
raviteja83 <ravi.theja@100ms.live>
2022-04-12 17:05:49 +0530

    fix(app): maintain aspect ratio in mobile

--- apps/100ms-web/src/components/Preview.jsx
+++ apps/100ms-web/src/components/Preview.jsx
@@ -139,7 +139,11 @@ const PreviewTile = ({ name }) => {
         aspectRatio: width / height,
         width: "unset",
         height: "min(360px, 60vh)",
-        "@sm": { maxWidth: "90%" },
+        "@sm": {
+          height: "unset",
+          width: "min(360px, 90%)",
+          maxWidth: "90%",
+        },
       }}
       ref={borderAudioRef}
     >
