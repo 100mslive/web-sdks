879335ec512dddb630fea1d2c3acc496c0030a18
raviteja83 <ravi.theja@100ms.live>
2022-05-04 17:07:36 +0530

    docs(sdk): add code comments

--- packages/hms-video-web/src/notification-manager/managers/PeerManager.ts
+++ packages/hms-video-web/src/notification-manager/managers/PeerManager.ts
@@ -52,6 +52,10 @@ export class PeerManager {
     const hmsPeers: HMSRemotePeer[] = [];
     const newPeers = new Set(peers.map(peer => peer.peer_id));
     this.store.getRemotePeers().forEach(({ peerId, fromRoomState }) => {
+      /**
+       * Remove only if the peer join happened from preview roomstate update. This will prevent the peer joined
+       * from peer-join event post join from being removed from the store.
+       */
       if (!newPeers.has(peerId) && fromRoomState) {
         this.store.removePeer(peerId);
       }
