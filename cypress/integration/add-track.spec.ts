e36fcb35e4e5216a4ce7e57dcdbbdb99ccfb320c
raviteja83 <ravi.theja@100ms.live>
2022-05-08 17:06:05 +0530

    test: add a test for add/remove track

---
+++ cypress/integration/add-track.spec.ts
@@ -0,0 +1,56 @@
+import { HMSReactiveStore, selectLocalPeer } from '../../packages/hms-video-store/src';
+import { HMSSDKActions } from '../../packages/hms-video-store/src/core/hmsSDKStore/HMSSDKActions';
+import { IHMSStoreReadOnly } from '../../packages/hms-video-store/src/core/IHMSStore';
+
+let HMSStore;
+let actions: HMSSDKActions;
+let store: IHMSStoreReadOnly;
+let initEndpoint;
+
+let token;
+
+describe('add/remove track api', () => {
+  before(() => {
+    cy.getToken().then(authToken => {
+      token = authToken;
+    });
+  });
+  beforeEach(() => {
+    HMSStore = new HMSReactiveStore();
+    actions = HMSStore.getHMSActions();
+    store = HMSStore.getStore();
+    initEndpoint = Cypress.env('CYPRESS_INIT_ENDPOINT');
+    //@ts-ignore
+    cy.spy(actions, 'onJoin').as('onJoin');
+  });
+
+  describe('Add/Remove Track', () => {
+    afterEach(() => {
+      if (actions) {
+        return actions.leave();
+      }
+    });
+
+    it('should add/remove a track to aux track on addTrack', () => {
+      actions.join({ userName: 'test', authToken: token, initEndpoint });
+      //@ts-ignore
+      cy.localTracksAdded(actions.sdk.getLocalPeer())
+        .then(() => {
+          return navigator.mediaDevices.getUserMedia({ video: true, audio: false });
+        })
+        .then(stream => {
+          const videoTrack = stream.getVideoTracks()[0];
+          actions.addTrack(videoTrack).then(() => {
+            const localPeer = store.getState(selectLocalPeer);
+            expect(localPeer.auxiliaryTracks[0]).to.equal(videoTrack.id);
+            expect(localPeer.videoTrack).to.not.equal(videoTrack.id);
+            actions.removeTrack(videoTrack.id).then(() => {
+              const localPeer = store.getState(selectLocalPeer);
+              expect(localPeer.auxiliaryTracks.length).to.equal(0);
+              expect(localPeer.videoTrack).to.not.equal(undefined);
+            });
+          });
+        });
+    });
+  });
+});
