66ce2f78caeae59b2625a33b83ad12411a694118
Ram <raghuram@100ms.live>
2022-05-10 10:45:00 +0530

    fix(app): let local peer toggle video on audiOnlyMode

--- apps/100ms-web/src/components/AudioVideoToggle.jsx
+++ apps/100ms-web/src/components/AudioVideoToggle.jsx
@@ -7,15 +7,12 @@ import {
 } from "@100mslive/react-icons";
 import { Tooltip, IconButton } from "@100mslive/react-ui";
 import { useAVToggle, parsedUserAgent } from "@100mslive/react-sdk";
-import { UI_SETTINGS } from "../common/constants";
-import { useUISettings } from "./AppData/useUISettings";
 
 const isMacOS = parsedUserAgent.getOS().name.toLowerCase() === "mac os";
 
 export const AudioVideoToggle = ({ compact = false }) => {
   const { isLocalVideoEnabled, isLocalAudioEnabled, toggleAudio, toggleVideo } =
     useAVToggle();
-  const isAudioOnly = useUISettings(UI_SETTINGS.isAudioOnly);
   return (
     <Fragment>
       {toggleAudio ? (
