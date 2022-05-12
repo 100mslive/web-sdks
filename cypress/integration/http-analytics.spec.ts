eddc881febdea662da5baaa8bf411fafe99b0bb3
raviteja83 <ravi.theja@100ms.live>
2022-05-12 15:08:00 +0530

    test: add case for sending events when join called

--- cypress/integration/http-analytics.spec.ts
+++ cypress/integration/http-analytics.spec.ts
@@ -2,12 +2,16 @@ import { HMSSdk } from '../../packages/hms-video-web/src/sdk';
 import { HTTPAnalyticsTransport } from '../../packages/hms-video-web/src/analytics/HTTPAnalyticsTransport';
 import { HMSPreviewListener } from '../../packages/hms-video-web/src/interfaces/preview-listener';
 import { CLIENT_ANAYLTICS_QA_ENDPOINT } from '../../packages/hms-video-web/src/utils/constants';
+import AnalyticsEventFactory from '../../packages/hms-video-web/src/analytics/AnalyticsEventFactory';
+import { ErrorFactory, HMSAction } from '../../packages/hms-video-web/src/error/ErrorFactory';
+import { HMSUpdateListener } from '../../packages/hms-video-web/src';
 
 let sdk: HMSSdk;
 let token: string;
 let invalidRoleToken: string;
 let initEndpoint: string;
 let previewListener: HMSPreviewListener;
+let joinListener: HMSUpdateListener;
 
 describe('Http Analytics tests', () => {
   before(() => {
@@ -29,6 +33,22 @@ describe('Http Analytics tests', () => {
       onPeerUpdate: cy.stub().as('onPeerUpdate'),
       onReconnecting: cy.stub().as('onReconnecting'),
       onReconnected: cy.stub().as('onReconnected'),
+    };
+    joinListener = {
+      onJoin: cy.stub().as('onJoin'),
+      onRoomUpdate: cy.stub().as('onJoinRoomUpdate'),
+      onPeerUpdate: cy.stub().as('onJoinPeerUpdate'),
+      onError: cy.stub().as('onJoinError'),
+      onDeviceChange: cy.stub().as('onJoinDeviceChange'),
+      onReconnecting: cy.stub().as('onJoinReconnecting'),
+      onReconnected: cy.stub().as('onJoinReconnected'),
+      onTrackUpdate: cy.stub().as('onTrackUpdate'),
+      onMessageReceived: cy.stub().as('onMessageReceived'),
+      onRoleChangeRequest: cy.stub().as('onRoleChangeRequest'),
+      onRoleUpdate: cy.stub().as('onRoleUpdate'),
+      onChangeTrackStateRequest: cy.stub().as('onChangeTrackStateRequest'),
+      onChangeMultiTrackStateRequest: cy.stub().as('onChangeMultiTrackStateRequest'),
+      onRemovedFromRoom: cy.stub().as('onRemovedFromRoom'),
     };
     cy.spy(HTTPAnalyticsTransport, 'sendEvent').as('sendEvent');
     cy.spy(HTTPAnalyticsTransport, 'setEnv').as('setEnv');
@@ -77,5 +97,17 @@ describe('Http Analytics tests', () => {
       newSdk.preview({ userName: 'test', authToken: token, initEndpoint }, previewListener).catch(console.error);
       cy.get('@sendEvent').should('be.calledTwice');
     });
+  });
+
+  it('should send previous local storage events on join', () => {
+    cy.intercept(
+      { url: CLIENT_ANAYLTICS_QA_ENDPOINT, method: 'POST' },
+      { statusCode: 200, statusText: 'Event recorded' },
+    );
+    const event = AnalyticsEventFactory.connect(ErrorFactory.InitAPIErrors.InitConfigNotAvailable(HMSAction.INIT));
+    event.properties.token = token;
+    localStorage.setItem('client-events', JSON.stringify([event]));
+    sdk.join({ userName: 'test', authToken: token, initEndpoint }, joinListener);
+    cy.get('@sendEvent').should('be.calledOnce');
   });
 });
