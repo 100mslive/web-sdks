a3955adf5e6f268b810f0f21b6fa096689338e68
Ram <raghuram@100ms.live>
2022-05-31 18:03:55 +0530

    feat(app): integrate video.js for hls playback

--- apps/100ms-web/src/layouts/HLSView.jsx
+++ apps/100ms-web/src/layouts/HLSView.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useRef, Fragment } from "react";
 import { useHMSStore, selectHLSState } from "@100mslive/react-sdk";
 import { Box, Flex, styled, Text } from "@100mslive/react-ui";
-import Hls from "hls.js";
+import videojs from "video.js";
 import { ChatView } from "../components/chatView";
-import { FeatureFlags } from "../services/FeatureFlags";
+// import { FeatureFlags } from "../services/FeatureFlags";
 import { useIsChatOpen } from "../components/AppData/useChatState";
 
 const HLSVideo = styled("video", {
@@ -13,22 +13,66 @@ const HLSVideo = styled("video", {
 
 const HLSView = () => {
   const videoRef = useRef(null);
+  const playerRef = useRef(null);
   const hlsState = useHMSStore(selectHLSState);
   const isChatOpen = useIsChatOpen();
   const hlsUrl = hlsState.variants[0]?.url;
+  // useEffect(() => {
+  //   if (videoRef.current && hlsUrl) {
+  //     if (Hls.isSupported()) {
+  //       let hls = new Hls(getHLSConfig());
+  //       hls.loadSource(hlsUrl);
+  //       hls.attachMedia(videoRef.current);
+  //     } else if (
+  //       videoRef.current.canPlayType("application/vnd.apple.mpegurl")
+  //     ) {
+  //       videoRef.current.src = hlsUrl;
+  //     }
+  //   }
+  // }, [hlsUrl]);
+
+  const videoJsOptions = {
+    autoplay: true,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [
+      {
+        src: hlsUrl,
+        type: "application/x-mpegURL",
+      },
+    ],
+  };
+
   useEffect(() => {
-    if (videoRef.current && hlsUrl) {
-      if (Hls.isSupported()) {
-        let hls = new Hls(getHLSConfig());
-        hls.loadSource(hlsUrl);
-        hls.attachMedia(videoRef.current);
-      } else if (
-        videoRef.current.canPlayType("application/vnd.apple.mpegurl")
-      ) {
-        videoRef.current.src = hlsUrl;
-      }
+    // Make sure Video.js player is only initialized once
+    if (!playerRef.current) {
+      const videoElement = videoRef.current;
+
+      if (!videoElement) return;
+
+      playerRef.current = videojs(videoElement, videoJsOptions, () => {
+        // onReady && onReady(player);
+        console.log(playerRef.current, "is READY");
+      });
+
+      // You can update player in the `else` block here, for example:
+    } else {
+      //   player.autoplay(options.autoplay);
+      //   player.src(options.sources);
     }
-  }, [hlsUrl]);
+  }, [hlsUrl, videoRef]);
+
+  // Dispose the Video.js player when the functional component unmounts
+  useEffect(() => {
+    const player = playerRef.current;
+    return () => {
+      if (player) {
+        player.dispose();
+        playerRef.current = null;
+      }
+    };
+  }, [playerRef]);
 
   return (
     <Fragment>
@@ -62,18 +106,18 @@ const HLSView = () => {
   );
 };
 
-function getHLSConfig() {
-  if (FeatureFlags.optimiseHLSLatency()) {
-    // should reduce the latency by around 2-3 more seconds. Won't work well without good internet.
-    return {
-      enableWorker: true,
-      liveSyncDuration: 1,
-      liveMaxLatencyDuration: 5,
-      liveDurationInfinity: true,
-      highBufferWatchdogPeriod: 1,
-    };
-  }
-  return {};
-}
+// function getHLSConfig() {
+//   if (FeatureFlags.optimiseHLSLatency()) {
+//     // should reduce the latency by around 2-3 more seconds. Won't work well without good internet.
+//     return {
+//       enableWorker: true,
+//       liveSyncDuration: 1,
+//       liveMaxLatencyDuration: 5,
+//       liveDurationInfinity: true,
+//       highBufferWatchdogPeriod: 1,
+//     };
+//   }
+//   return {};
+// }
 
 export default HLSView;
