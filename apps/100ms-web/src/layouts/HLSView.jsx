add684b1afaa67bb044f43df052054563562afa8
Ram <raghuram@100ms.live>
2022-06-02 15:40:38 +0530

    feat(app): enable users to select different HLS quality levels

--- apps/100ms-web/src/layouts/HLSView.jsx
+++ apps/100ms-web/src/layouts/HLSView.jsx
@@ -1,39 +1,191 @@
-import React, { useEffect, useRef, Fragment } from "react";
+import React, {
+  useEffect,
+  useRef,
+  Fragment,
+  useState,
+  useCallback,
+} from "react";
 import { useHMSStore, selectHLSState } from "@100mslive/react-sdk";
-import { Box, Flex, styled, Text } from "@100mslive/react-ui";
+import {
+  Box,
+  Dropdown,
+  Flex,
+  styled,
+  Text,
+  Tooltip,
+} from "@100mslive/react-ui";
 import Hls from "hls.js";
 import { ChatView } from "../components/chatView";
 import { FeatureFlags } from "../services/FeatureFlags";
 import { useIsChatOpen } from "../components/AppData/useChatState";
+import {
+  ChevronDownIcon,
+  ChevronUpIcon,
+  SettingIcon,
+} from "@100mslive/react-icons";
 
 const HLSVideo = styled("video", {
   h: "100%",
   margin: "0 auto",
 });
 
+let hls;
 const HLSView = () => {
   const videoRef = useRef(null);
   const hlsState = useHMSStore(selectHLSState);
   const isChatOpen = useIsChatOpen();
   const hlsUrl = hlsState.variants[0]?.url;
+  const [isManifestLoaded, setIsManifestLoaded] = useState(false);
+  const [availableLevels, setAvailableLevels] = useState([]);
+  const [isHLSInitialized, setIsHLSInitialized] = useState(false);
+  const [currentSelectedQualityText, setCurrentSelectedQualityText] =
+    useState("");
+  const [qualityDropDownOpen, setQualityDropDownOpen] = useState(false);
+
   useEffect(() => {
     if (videoRef.current && hlsUrl) {
-      if (Hls.isSupported()) {
-        let hls = new Hls(getHLSConfig());
-        hls.loadSource(hlsUrl);
-        hls.attachMedia(videoRef.current);
-      } else if (
-        videoRef.current.canPlayType("application/vnd.apple.mpegurl")
-      ) {
-        videoRef.current.src = hlsUrl;
+      hls = new Hls(getHLSConfig());
+      setIsHLSInitialized(true);
+      hls.loadSource(hlsUrl);
+      hls.attachMedia(videoRef.current);
+
+      if (!isManifestLoaded) {
+        hls.on(Hls.Events.MANIFEST_LOADED, (event, data) => {
+          setIsManifestLoaded(true);
+          setAvailableLevels(data.levels);
+          setCurrentSelectedQualityText("Auto");
+        });
       }
+    } else if (
+      videoRef?.current?.canPlayType("application/vnd.apple.mpegurl")
+    ) {
+      videoRef.current.src = hlsUrl;
     }
   }, [hlsUrl]);
+
+  const qualitySelectorHandler = useCallback(
+    (event, qualityLevel) => {
+      if (isHLSInitialized) {
+        hls.currentLevel = getCurrentLevel(qualityLevel);
+        const levelText =
+          qualityLevel.height === "auto" ? "Auto" : `${qualityLevel.height}p`;
+        setCurrentSelectedQualityText(levelText);
+      }
+    },
+    [isHLSInitialized, availableLevels]
+  );
+
+  /**
+   *
+   * @param {the current quality level clicked by the user. It is the level object } qualityLevel
+   * @returns an integer ranging from 0 to (availableLevels.length - 1).
+   * (e.g) if 4 levels are available, 0 is the lowest quality and 3 is the highest.
+   *
+   * This function is used rather than just using availableLevels.findIndex(quality) because, HLS gives the
+   * levels in reverse.
+   * (e.g) if available levels in the m3u8 are 360p,480p,720p,1080p,
+   *
+   * hls.levels gives us an array of level objects in the order [1080p,720p,480p,360p];
+   *
+   * so setting hls.currentLevel = availableLevels.getIndexOf(1080p) will set the stream to 360p instead of 1080p
+   * because availableLevels.getIndexOf(1080p) will give 0 but level 0 is 360p.
+   */
+  const getCurrentLevel = qualityLevel => {
+    if (qualityLevel.height === "auto") {
+      return -1;
+    }
+    const urls = availableLevels.map(level => level.url);
+    const index = urls.findIndex(url => {
+      return url === qualityLevel.url;
+    });
+
+    return availableLevels.length - 1 - index;
+  };
 
   return (
     <Fragment>
       {hlsUrl ? (
-        <HLSVideo ref={videoRef} autoPlay controls />
+        <>
+          <Flex align="center" css={{ position: "absolute", right: "$4" }}>
+            <Dropdown.Root
+              open={qualityDropDownOpen}
+              onOpenChange={value => setQualityDropDownOpen(value)}
+            >
+              <Dropdown.Trigger asChild data-testid="participant_list">
+                <Flex
+                  css={{
+                    color: "$textPrimary",
+                    borderRadius: "$1",
+                    cursor: "pointer",
+                    border: "1px solid $textDisabled",
+                    padding: "$2 $4",
+                  }}
+                >
+                  <Tooltip title="Select Quality">
+                    <Flex>
+                      <SettingIcon />
+                      <Text variant="md">{currentSelectedQualityText}</Text>
+                    </Flex>
+                  </Tooltip>
+
+                  <Box
+                    css={{ "@lg": { display: "none" }, color: "$textDisabled" }}
+                  >
+                    {qualityDropDownOpen ? (
+                      <ChevronUpIcon />
+                    ) : (
+                      <ChevronDownIcon />
+                    )}
+                  </Box>
+                </Flex>
+              </Dropdown.Trigger>
+              {availableLevels.length > 0 && (
+                <Dropdown.Content
+                  sideOffset={5}
+                  align="end"
+                  css={{ height: "auto", maxHeight: "$96" }}
+                >
+                  <Dropdown.Item
+                    onClick={event =>
+                      qualitySelectorHandler(event, { height: "auto" })
+                    }
+                    css={{
+                      h: "auto",
+                      flexDirection: "column",
+                      flexWrap: "wrap",
+                      cursor: "pointer",
+                      alignItems: "flex-start",
+                    }}
+                    key="auto"
+                  >
+                    <Text>Automatic</Text>
+                  </Dropdown.Item>
+                  {availableLevels.map(level => {
+                    return (
+                      <Dropdown.Item
+                        onClick={event => qualitySelectorHandler(event, level)}
+                        css={{
+                          h: "auto",
+                          flexDirection: "column",
+                          flexWrap: "wrap",
+                          cursor: "pointer",
+                          alignItems: "flex-start",
+                        }}
+                        key={level.url}
+                      >
+                        <Text>{`${level.height}p (${(
+                          Number(level.bitrate / 1024) / 1024
+                        ).toFixed(2)} Mbps)`}</Text>
+                      </Dropdown.Item>
+                    );
+                  })}
+                </Dropdown.Content>
+              )}
+            </Dropdown.Root>
+          </Flex>
+
+          <HLSVideo ref={videoRef} autoPlay controls />
+        </>
       ) : (
         <Flex align="center" justify="center" css={{ size: "100%" }}>
           <Text variant="md" css={{ textAlign: "center" }}>
