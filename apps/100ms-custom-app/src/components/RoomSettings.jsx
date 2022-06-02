f59da8eaf90b80f1f71eabc884ebe83d8f8e901e
Deepankar Bhade <deep14bhade@gmail.com>
2022-06-02 22:39:16 +0530

    feat: meta settings audio level,mute, name on tile

--- apps/100ms-custom-app/src/components/RoomSettings.jsx
+++ apps/100ms-custom-app/src/components/RoomSettings.jsx
@@ -1,6 +1,17 @@
-import React, { Fragment } from 'react';
-import { UploadIcon } from '@100mslive/react-icons';
-import { Box, Button, Dialog, Flex, HorizontalDivider, Select, styled, Text } from '@100mslive/react-ui';
+import React, { Fragment, useState } from 'react';
+import { UploadIcon, CheckIcon } from '@100mslive/react-icons';
+import {
+  Box,
+  Button,
+  Dialog,
+  Flex,
+  HorizontalDivider,
+  Select,
+  styled,
+  Text,
+  Checkbox,
+  Label,
+} from '@100mslive/react-ui';
 import { DialogContent } from './DialogContent';
 
 const ItemRoot = React.memo(({ title, children }) => (
@@ -65,109 +76,207 @@ const ThemeType = ({ title, active, onClick }) => {
   );
 };
 
+const TabButton = styled('button', {
+  p: '$8',
+  bg: 'transparent',
+  r: '$1',
+  my: '$2',
+  fontWeight: '$medium',
+  textAlign: 'left',
+  variants: {
+    active: {
+      true: {
+        bg: '$bgSecondary',
+      },
+    },
+  },
+});
+
+const HeadlessItem = React.memo(({ title, children }) => (
+  <Fragment>
+    <Flex direction="column" css={{ p: '$8 $4' }}>
+      <Text variant="h6" css={{ mb: '$4' }}>
+        {title}
+      </Text>
+      {children}
+    </Flex>
+    <HorizontalDivider />
+  </Fragment>
+));
+
+const CheckboxItem = ({ onClick, type, label, checked }) => {
+  return (
+    <Flex align="center" key={type} css={{ my: '$2' }}>
+      <Checkbox.Root id={label} checked={checked} onCheckedChange={onClick}>
+        <Checkbox.Indicator>
+          <CheckIcon width={16} height={16} />
+        </Checkbox.Indicator>
+      </Checkbox.Root>
+      <Label htmlFor={label} css={{ ml: '$4', fontSize: '$sm', cursor: 'pointer' }}>
+        {label}
+      </Label>
+    </Flex>
+  );
+};
+
 const Fonts = ['Inter', 'Roboto', 'Lato', 'Montserrat', 'Open Sans', 'IBM Plex Sans'];
 
 export default function RoomSettings({ onClose, settings, change, handleLogoChange, onSave, onCancel }) {
+  const [tab, setTab] = useState(1);
+  const metaData = JSON.parse(settings.metadataFields.metadata);
+  const { headlessConfig } = metaData;
+  const handleSettingsMetaData = (key, value) => {
+    return {
+      ...settings.metadataFields,
+      metadata: JSON.stringify({
+        headlessConfig: {
+          ...metaData.headlessConfig,
+          [key]: value,
+        },
+      }),
+    };
+  };
   return (
     <Dialog.Root defaultOpen onOpenChange={value => !value && onClose()}>
       <DialogContent title="Customise your app" css={{ width: 'min(700px, 100%)' }}>
         <Flex css={{ size: '100%', overflow: 'hidden' }}>
           <Box css={{ flex: '1 1 0', pt: '$6' }}>
-            <Text css={{ p: '$8', bg: '$bgSecondary', r: '$1' }}>Theme</Text>
+            <Flex direction="column">
+              <TabButton active={tab === 0} onClick={() => setTab(0)}>
+                Theme
+              </TabButton>
+              <TabButton active={tab === 1} onClick={() => setTab(1)}>
+                Headless
+              </TabButton>
+            </Flex>
           </Box>
-          <Box css={{ flex: '3 1 0', ml: '$8' }}>
-            <ItemRoot title="Logo">
-              <Button
-                as="label"
-                htmlFor="logoInput"
-                variant="standard"
-                css={{ bg: '$bgSecondary', cursor: 'pointer', color: '$textPrimary', display: 'flex' }}
-              >
-                <UploadIcon />
-                &nbsp; Upload
-              </Button>
-              <input
-                onChange={handleLogoChange}
-                type="file"
-                accept="image/*"
-                name="logo"
-                id="logoInput"
-                style={{ display: 'none' }}
-              />
-            </ItemRoot>
-            <ItemRoot title="Appearance">
-              <Flex align="center" css={{ bg: '$bgSecondary', r: '$1' }}>
-                <ThemeType
-                  title="Dark"
-                  active={settings.theme === 'dark'}
-                  onClick={() => {
-                    change('theme', 'dark');
-                  }}
+
+          {tab === 0 && (
+            <Box css={{ flex: '3 1 0', ml: '$8' }}>
+              <ItemRoot title="Logo">
+                <Button
+                  as="label"
+                  htmlFor="logoInput"
+                  variant="standard"
+                  css={{ bg: '$bgSecondary', cursor: 'pointer', color: '$textPrimary', display: 'flex' }}
+                >
+                  <UploadIcon />
+                  &nbsp; Upload
+                </Button>
+                <input
+                  onChange={handleLogoChange}
+                  type="file"
+                  accept="image/*"
+                  name="logo"
+                  id="logoInput"
+                  style={{ display: 'none' }}
                 />
-                <ThemeType
-                  title="Light"
-                  active={settings.theme === 'light'}
-                  onClick={() => {
-                    change('theme', 'light');
-                  }}
-                />
-              </Flex>
-            </ItemRoot>
-            <ItemRoot title="Brand Color">
-              <Flex
-                as="label"
-                htmlFor="brandColorPicker"
-                align="center"
-                css={{ bg: '$bgSecondary', p: '$4', r: '$1', cursor: 'pointer', overflow: 'hidden' }}
-              >
-                <ColorPicker
-                  type="color"
-                  id="brandColorPicker"
-                  name="brandColorPicker"
-                  css={{ bg: settings.brand_color }}
-                  onChange={e => {
-                    change('brand_color', e.target.value);
-                  }}
-                  value={settings.brand_color}
-                />
-                <Text>{settings.brand_color}</Text>
-              </Flex>
-            </ItemRoot>
-            <ItemRoot title="Tile Shape">
-              <Flex>
-                <TileType type="Square" value="1-1" active={settings.tile_shape === '1-1'} change={change} />
-                <TileType type="Landscape" value="4-3" active={settings.tile_shape === '4-3'} change={change} />
-                <TileType type="Wide" value="16-9" active={settings.tile_shape === '16-9'} change={change} />
-              </Flex>
-            </ItemRoot>
-            <ItemRoot title="Font Family">
-              <Select.Root>
-                <Select.DefaultDownIcon />
-                <Select.Select onChange={e => change('font', e.target.value)} value={settings.font}>
-                  {Fonts.map(font => {
-                    return (
-                      <option value={font} key={font}>
-                        {font}
-                      </option>
-                    );
-                  })}
-                </Select.Select>
-              </Select.Root>
-            </ItemRoot>
-            {settings.metadataFields.clicks > 4 && (
-              <ItemRoot title="Metadata">
-                <TextArea
-                  onChange={e => {
-                    change('metadataFields', {
-                      ...settings.metadataFields,
-                      metadata: e.target.value,
-                    });
-                  }}
-                  value={settings.metadataFields.metadata}
-                ></TextArea>
               </ItemRoot>
-            )}
-          </Box>
+              <ItemRoot title="Appearance">
+                <Flex align="center" css={{ bg: '$bgSecondary', r: '$1' }}>
+                  <ThemeType
+                    title="Dark"
+                    active={settings.theme === 'dark'}
+                    onClick={() => {
+                      change('theme', 'dark');
+                    }}
+                  />
+                  <ThemeType
+                    title="Light"
+                    active={settings.theme === 'light'}
+                    onClick={() => {
+                      change('theme', 'light');
+                    }}
+                  />
+                </Flex>
+              </ItemRoot>
+              <ItemRoot title="Brand Color">
+                <Flex
+                  as="label"
+                  htmlFor="brandColorPicker"
+                  align="center"
+                  css={{ bg: '$bgSecondary', p: '$4', r: '$1', cursor: 'pointer', overflow: 'hidden' }}
+                >
+                  <ColorPicker
+                    type="color"
+                    id="brandColorPicker"
+                    name="brandColorPicker"
+                    css={{ bg: settings.brand_color }}
+                    onChange={e => {
+                      change('brand_color', e.target.value);
+                    }}
+                    value={settings.brand_color}
+                  />
+                  <Text>{settings.brand_color}</Text>
+                </Flex>
+              </ItemRoot>
+              <ItemRoot title="Tile Shape">
+                <Flex>
+                  <TileType type="Square" value="1-1" active={settings.tile_shape === '1-1'} change={change} />
+                  <TileType type="Landscape" value="4-3" active={settings.tile_shape === '4-3'} change={change} />
+                  <TileType type="Wide" value="16-9" active={settings.tile_shape === '16-9'} change={change} />
+                </Flex>
+              </ItemRoot>
+              <ItemRoot title="Font Family">
+                <Select.Root>
+                  <Select.DefaultDownIcon />
+                  <Select.Select onChange={e => change('font', e.target.value)} value={settings.font}>
+                    {Fonts.map(font => {
+                      return (
+                        <option value={font} key={font}>
+                          {font}
+                        </option>
+                      );
+                    })}
+                  </Select.Select>
+                </Select.Root>
+              </ItemRoot>
+              {settings.metadataFields.clicks > 4 && (
+                <ItemRoot title="Metadata">
+                  <TextArea
+                    onChange={e => {
+                      change('metadataFields', {
+                        ...settings.metadataFields,
+                        metadata: e.target.value,
+                      });
+                    }}
+                    value={settings.metadataFields.metadata}
+                  ></TextArea>
+                </ItemRoot>
+              )}
+            </Box>
+          )}
+
+          {tab === 1 && (
+            <Box css={{ flex: '3 1 0', ml: '$8' }}>
+              <HeadlessItem title="Visible UI Elements">
+                <CheckboxItem
+                  checked={headlessConfig.hideAudioLevel}
+                  label="Audio Level Border"
+                  onClick={() => {
+                    change('metadataFields', handleSettingsMetaData('hideAudioLevel', !headlessConfig.hideAudioLevel));
+                  }}
+                />
+                <CheckboxItem
+                  checked={headlessConfig.hideTileAudioMute}
+                  label="Audio Mute on Tile"
+                  onClick={() => {
+                    change(
+                      'metadataFields',
+                      handleSettingsMetaData('hideTileAudioMute', !headlessConfig.hideTileAudioMute),
+                    );
+                  }}
+                />
+                <CheckboxItem
+                  checked={headlessConfig.hideTileName}
+                  label="Name on Tile"
+                  onClick={() => {
+                    change('metadataFields', handleSettingsMetaData('hideTileName', !headlessConfig.hideTileName));
+                  }}
+                />
+              </HeadlessItem>
+            </Box>
+          )}
         </Flex>
         <Flex justify="end" align="center" css={{ mt: '$8' }}>
           <Button variant="standard" css={{ mr: '$8' }} onClick={onCancel}>
