ba207864acd681285361dcdb0ca9789f188b1333
Eswar Prasad Clinton. A <64120992+eswarclynn@users.noreply.github.com>
2022-06-06 10:05:32 +0530

    fix(sdk): system denied error for cam, mic and different error codes for GUM fail [WEB-915, WEB-112]
    * fix(sdk): system denied error for cam, mic
    
    * fix(sdk): remove screen from unknown get media action

--- packages/hms-video-web/src/error/utils.ts
+++ packages/hms-video-web/src/error/utils.ts
@@ -3,6 +3,7 @@ import { ErrorFactory, HMSAction } from './ErrorFactory';
 import { HMSException } from './HMSException';
 
 export enum HMSGetMediaActions {
+  UNKNOWN = 'unknown(video or audio)',
   AUDIO = 'audio',
   VIDEO = 'video',
   AV = 'audio, video',
@@ -29,18 +30,23 @@ function getDefaultError(error: string, deviceInfo: string) {
  * System blocked - NotAllowedError - Permission denied by system
  */
 // eslint-disable-next-line complexity
-function convertMediaErrorToHMSException(err: Error, deviceInfo: string): HMSException {
+function convertMediaErrorToHMSException(err: Error, deviceInfo = ''): HMSException {
   /**
    * Note: Adapter detects all chromium browsers as 'chrome'
    */
-  const deniedBySystem =
-    deviceInfo === 'screen' &&
-    ((adapter.browserDetails.browser === 'chrome' &&
-      err.name === 'NotAllowedError' &&
-      err.message.includes('denied by system')) ||
-      err.name === 'NotFoundError');
-  if (deniedBySystem) {
+  const chromeSystemDenied =
+    adapter.browserDetails.browser === 'chrome' &&
+    err.name === 'NotAllowedError' &&
+    err.message.includes('denied by system');
+
+  if (chromeSystemDenied) {
     return ErrorFactory.TracksErrors.SystemDeniedPermission(HMSAction.TRACK, deviceInfo, err.message);
+  }
+
+  if (adapter.browserDetails.browser === 'firefox' && err.name === 'NotFoundError') {
+    const hmsError = ErrorFactory.TracksErrors.SystemDeniedPermission(HMSAction.TRACK, deviceInfo, err.message);
+    hmsError.description = `Capture device is either blocked at Operating System level or not available - ${deviceInfo}`;
+    return hmsError;
   }
 
   switch (err.name) {
