ff75c2c35466ed2a9d67b5d0c5d0d226111e550a
eswarclynn <esuclintblazer@gmail.com>
2022-06-21 12:57:59 +0530

    fix(sdk): make peer list update empty arrays instead of undefined

--- packages/hms-video-web/src/interfaces/peer-list-update.ts
+++ packages/hms-video-web/src/interfaces/peer-list-update.ts
@@ -2,9 +2,9 @@ import { HMSPeer } from '../sdk/models/peer';
 
 export interface HMSPeerListUpdate {
   peers: HMSPeer[];
-  peersAdded?: HMSPeer[];
-  peersRemoved?: HMSPeer[];
-  peersWithNameChanged?: HMSPeer[];
-  peersWithMetadataChanged?: HMSPeer[];
-  peersWithRoleChanged?: HMSPeer[];
+  peersAdded: HMSPeer[];
+  peersRemoved: HMSPeer[];
+  peersWithNameChanged: HMSPeer[];
+  peersWithMetadataChanged: HMSPeer[];
+  peersWithRoleChanged: HMSPeer[];
 }
