44fae04385a894c766cf662658f4db150df80118
raviteja83 <ravi.theja@100ms.live>
2022-06-22 18:46:43 +0530

    fix: simplify krisp plugin

--- apps/100ms-web/src/plugins/krisp/KrispPlugin.jsx
+++ apps/100ms-web/src/plugins/krisp/KrispPlugin.jsx
@@ -5,15 +5,15 @@ import {
   useHMSStore,
 } from "@100mslive/react-sdk";
 import { IconButton, Tooltip } from "@100mslive/react-ui";
-import { useCallback, useRef } from "react";
+import { useCallback } from "react";
 import FilterFactory from "./krispsdk.mjs";
 
 const ncOptions = {
   modelOption: FilterFactory.ModelOptions.model_nc_auto,
   processor: "./krisp.processor.js",
-  model_8: "weights/model_8.kw", // Narrow band model path
-  model_16: "weights/model_16.kw", // Wide band model path
-  model_32: "weights/model_32.kw",
+  model_8: "./weights/model_8.kw", // Narrow band model path
+  model_16: "./weights/model_16.kw", // Wide band model path
+  model_32: "./weights/model_32.kw",
 };
 
 class Plugin {
@@ -29,11 +29,17 @@ class Plugin {
     if (!source) {
       throw new Error("source is not defined");
     }
-    const ncFilter = await FilterFactory.create(ctx, ncOptions);
-    source.connect(ncFilter);
-    console.error("KrispPlugin", ctx.sampleRate, ncFilter, ctx);
-    this.ncFilter = ncFilter;
-    return ncFilter;
+    if (!this.ncFilter) {
+      this.ncFilter = await FilterFactory.create(ctx, ncOptions);
+    }
+    if (ctx.state === "suspended") {
+      console.error("audio context suspended, resuming");
+      await ctx.resume();
+    }
+    this.ncFilter.enable();
+    window.ncFilter = this.ncFilter;
+    source.connect(this.ncFilter);
+    return this.ncFilter;
   }
 
   init() {}
@@ -48,26 +54,24 @@ class Plugin {
 
   stop() {
     this.ncFilter?.disconnect();
-    this.ncFilter = undefined;
+    this.ncFilter = null;
   }
 }
+
+const plugin = new Plugin();
 
 export const KrispPlugin = () => {
   const isPluginPresent = useHMSStore(
     selectIsLocalAudioPluginPresent("KrispPlugin")
   );
   const actions = useHMSActions();
-  const pluginRef = useRef(null);
 
   const addPlugin = useCallback(() => {
-    pluginRef.current = new Plugin();
-    actions.addPluginToAudioTrack(pluginRef.current);
+    actions.addPluginToAudioTrack(plugin);
   }, [actions]);
 
   const removePlugin = useCallback(() => {
-    if (pluginRef.current) {
-      actions.removePluginFromAudioTrack(pluginRef.current);
-    }
+    actions.removePluginFromAudioTrack(plugin);
   }, [actions]);
 
   return (
