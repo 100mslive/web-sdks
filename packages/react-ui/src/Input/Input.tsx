6d2cbb6a58b466021bb7ac8966c2a7d2758b1c56
Deepankar Bhade <deep14bhade@gmail.com>
2022-06-28 13:59:51 +0530

    feat: input style changes

--- packages/react-ui/src/Input/Input.tsx
+++ packages/react-ui/src/Input/Input.tsx
@@ -1,16 +1,19 @@
 import { styled } from '../Theme';
 
 export const Input = styled('input', {
-  backgroundColor: '$inputBg',
+  backgroundColor: '$surfaceLight',
   borderRadius: '8px',
   outline: 'none',
-  border: 'none',
+  border: '1px solid $borderLight',
   padding: '0.5rem 0.75rem',
   minHeight: '30px',
   color: '$textPrimary',
   fontSize: '$md',
   '&:focus': {
-    boxShadow: '0 0 0 3px $colors$brandDefault',
+    boxShadow: '0 0 0 1px $colors$borderAccent',
+  },
+  '&::placeholder': {
+    color: '$textDisabled';
   },
   variants: {
     error: {
