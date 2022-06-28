82cc9ec5e80b86d738877b4c33171c9b3a3fc8bd
raviteja83 <ravi.theja@100ms.live>
2022-06-28 16:32:35 +0530

    feat: add new hooks for participants with filters

---
+++ packages/react-sdk/src/hooks/useParticipants.ts
@@ -0,0 +1,35 @@
+import {
+  HMSPeer,
+  HMSRoleName,
+  selectAvailableRoleNames,
+  selectIsConnectedToRoom,
+  selectPeerCount,
+  selectPeerMetadata,
+  selectPeers,
+  selectRemotePeers,
+} from '@100mslive/hms-video-store';
+import { useHMSStore, useHMSVanillaStore } from '../primitives/HmsRoomProvider';
+
+export interface useParticipantsResult {
+  participants: HMSPeer;
+  peerCount: number;
+  isConnected: boolean;
+}
+
+export type useParticipantsParams = 'isHandRaise' | { role: HMSRoleName };
+export const useParticipants = (params?: useParticipantsParams) => {
+  const isConnected = useHMSStore(selectIsConnectedToRoom);
+  const peerCount = useHMSStore(selectPeerCount);
+  const availableRoles = useHMSStore(selectAvailableRoleNames);
+  let participantList = useHMSStore(isConnected ? selectPeers : selectRemotePeers);
+  const vanillaStore = useHMSVanillaStore();
+  if (params === 'isHandRaise') {
+    participantList = participantList.filter(peer => {
+      return vanillaStore.getState(selectPeerMetadata(peer.id)).isHandRaised;
+    });
+  }
+  if (typeof params === 'object' && availableRoles.includes(params.role)) {
+    participantList = participantList.filter(peer => peer.roleName === params.role);
+  }
+  return { participants: participantList, isConnected, peerCount };
+};
