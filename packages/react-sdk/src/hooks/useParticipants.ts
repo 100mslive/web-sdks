e33ef342ccbf9441839d34b787a9310c7fd51853
raviteja83 <ravi.theja@100ms.live>
2022-06-29 12:08:05 +0530

    docs: add tsdoc for useparticipants hook

--- packages/react-sdk/src/hooks/useParticipants.ts
+++ packages/react-sdk/src/hooks/useParticipants.ts
@@ -11,13 +11,40 @@ import {
 import { useHMSStore, useHMSVanillaStore } from '../primitives/HmsRoomProvider';
 
 export interface useParticipantsResult {
-  participants: HMSPeer;
+  /**
+   * list of participants that match the given filters
+   */
+  participants: HMSPeer[];
+  /**
+   * Total number of participants in the room
+   */
   peerCount: number;
+  /**
+   * is joined in the room
+   */
   isConnected: boolean;
+  /**
+   * role names with atleast one participant present with that role
+   */
   rolesWithParticipants: HMSRoleName[];
 }
 
-export type useParticipantsParams = { role: HMSRoleName; metadata: Record<string, any> };
+export type useParticipantsParams = {
+  /** To filter by particular role */
+  role: HMSRoleName;
+  /**
+   * To filter by particular by metadata. only supports { isHandRaised: true } for now
+   * @beta
+   */
+  metadata: Record<string, any>;
+};
+
+/**
+ * This can be used to get the total count of participants in the room, participants
+ * filtered by role or metadata with isHandRaised or the entire participants if no params are passed
+ * @param {useParticipantsParams} params
+ * @returns {useParticipantsResult}
+ */
 export const useParticipants = (params?: useParticipantsParams) => {
   const isConnected = useHMSStore(selectIsConnectedToRoom);
   const peerCount = useHMSStore(selectPeerCount);
