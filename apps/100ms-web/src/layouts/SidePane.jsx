2b60bb445f77c689027be873010e5ed9fd27b85f
raviteja83 <ravi.theja@100ms.live>
2022-06-29 15:10:48 +0530

    feat: add sidepane to render multiple components

---
+++ apps/100ms-web/src/layouts/SidePane.jsx
@@ -0,0 +1,26 @@
+import { selectAppData, useHMSStore } from "@100mslive/react-sdk";
+import { Box } from "@100mslive/react-ui";
+import React from "react";
+import { APP_DATA, SIDE_PANE_OPTIONS } from "../common/constants";
+import { Chat } from "../components/Chat/Chat";
+import { ParticipantList } from "../components/Header/ParticipantList";
+
+const SidePane = () => {
+  const sidepane = useHMSStore(selectAppData(APP_DATA.sidePane));
+  const renderComponent = () => {
+    switch (sidepane) {
+      case SIDE_PANE_OPTIONS.PARTICIPANTS:
+        return <ParticipantList />;
+      case SIDE_PANE_OPTIONS.CHAT:
+        return <Chat />;
+      default:
+        return null;
+    }
+  };
+  if (!sidepane) {
+    return null;
+  }
+  return <Box css={{ flex: "0 0 33.3%" }}>{renderComponent()}</Box>;
+};
+
+export default SidePane;
