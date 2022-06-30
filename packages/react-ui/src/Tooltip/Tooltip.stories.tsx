1b38fe1b4e89c3e15fa77ae18ddce24525d99cfe
hardik1003 <hardik@100ms.live>
2022-06-30 16:14:19 +0530

    style: fix

--- packages/react-ui/src/Tooltip/Tooltip.stories.tsx
+++ packages/react-ui/src/Tooltip/Tooltip.stories.tsx
@@ -1,18 +1,37 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { ComponentMeta } from '@storybook/react';
 import { Tooltip } from './Tooltip';
+import { Text } from '../Text';
 
 export default {
   title: 'UI Components/Tooltip',
   component: Tooltip,
 } as ComponentMeta<typeof Tooltip>;
 
-const TooltipStory = () => {
+const TooltipStory = args => {
   return (
-    <Tooltip title="Tooltip Text">
+    <Tooltip {...args}>
       <span>Hover to see Tooltip</span>
     </Tooltip>
   );
 };
 
-export const Example = TooltipStory.bind({});
+export const ExampleWithString = TooltipStory.bind({});
+
+ExampleWithString.args = {
+  title: 'hi',
+};
+
+const TooltipStorywithReactNode = (args: any, title: ReactNode) => {
+  return (
+    <Tooltip title={<div dangerouslySetInnerHTML={{ _html: title }} />} {...args}>
+      <span>Hover to see Tooltip</span>
+    </Tooltip>
+  );
+};
+
+export const ExampleWithReactNode = TooltipStorywithReactNode.bind({});
+
+ExampleWithReactNode.args = {
+  title: 'Hello',
+};
