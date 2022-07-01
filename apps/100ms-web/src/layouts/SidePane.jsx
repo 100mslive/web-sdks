7b26c345a813c4e41ba2a68fc0383d9d43b40564
raviteja83 <ravi.theja@100ms.live>
2022-07-01 17:22:16 +0530

    fix: import of streaming page

--- apps/100ms-web/src/layouts/SidePane.jsx
+++ apps/100ms-web/src/layouts/SidePane.jsx
@@ -3,7 +3,7 @@ import { selectAppData, useHMSStore } from "@100mslive/react-sdk";
 import { Box } from "@100mslive/react-ui";
 import { Chat } from "../components/Chat/Chat";
 import { ParticipantList } from "../components/Header/ParticipantList";
-import StreamingLanding from "../components/StreamingLanding";
+import { StreamingLanding } from "../components/StreamingLanding";
 import { APP_DATA, SIDE_PANE_OPTIONS } from "../common/constants";
 
 const SidePane = ({ css = {} }) => {
