5773f273bbb144effafa6b7dc11c2e165d06a991
raviteja83 <ravi.theja@100ms.live>
2022-07-05 16:05:31 +0530

    feat: make the dropdown take the correct width

--- apps/100ms-web/src/components/DeviceSettings.jsx
+++ apps/100ms-web/src/components/DeviceSettings.jsx
@@ -115,66 +115,93 @@ const DeviceSelector = ({
   return (
     <Box css={{ mb: "$10" }}>
       <Text css={{ mb: "$4" }}>{title}</Text>
-      <Flex align="center">
-        <Dropdown.Root open={open} onOpenChange={setOpen}>
-          <Dropdown.Trigger
-            asChild
-            data-testid={`${title}_selector`}
-            css={{
-              border: "1px solid $borderLight",
-              bg: "$surfaceLight",
-              r: "$1",
-              p: "$6 $9",
-              ...(children
-                ? {
-                    flex: "1 1 0",
-                    minWidth: 0,
-                  }
-                : {}),
-            }}
-          >
-            <Flex
+      <Flex
+        align="center"
+        css={{
+          w: "100%",
+        }}
+      >
+        <Box
+          css={{
+            position: "relative",
+            flex: "1 1 0",
+            minWidth: 0,
+            "[data-radix-popper-content-wrapper]": {
+              width: "100%",
+              transform: "translateY(60px) !important",
+              zIndex: 11,
+            },
+          }}
+        >
+          <Dropdown.Root open={open} onOpenChange={setOpen}>
+            <Dropdown.Trigger
+              asChild
+              data-testid={`${title}_selector`}
               css={{
-                display: "flex",
-                justifyContent: "space-between",
-                color: "$textHighEmp",
-                w: "100%",
+                border: "1px solid $borderLight",
+                bg: "$surfaceLight",
+                r: "$1",
+                p: "$6 $9",
+                zIndex: 10,
+                ...(children
+                  ? {
+                      flex: "1 1 0",
+                      minWidth: 0,
+                    }
+                  : {}),
               }}
             >
-              {icon}
-              <Text
+              <Flex
                 css={{
-                  color: "inherit",
-                  ...textEllipsis("90%"),
-                  flex: "1 1 0",
-                  mx: "$6",
+                  display: "flex",
+                  justifyContent: "space-between",
+                  color: "$textHighEmp",
+                  w: "100%",
                 }}
               >
-                {devices.find(({ deviceId }) => deviceId === selection)?.label}
-              </Text>
-              {open ? <ChevronUpIcon /> : <ChevronDownIcon />}
-            </Flex>
-          </Dropdown.Trigger>
-          <Dropdown.Content align="start" sideOffset={8} css={{ w: "unset" }}>
-            {devices.map(device => {
-              return (
-                <Dropdown.Item
-                  key={device.label}
-                  onSelect={() => onChange(device.deviceId)}
+                {icon}
+                <Text
                   css={{
-                    px: "$9",
-                    bg:
-                      device.deviceId === selection
-                        ? "$primaryDark"
-                        : undefined,
+                    color: "inherit",
+                    ...textEllipsis("90%"),
+                    flex: "1 1 0",
+                    mx: "$6",
                   }}
                 >
-                  {device.label}
-                </Dropdown.Item>
-              );
-            })}
-          </Dropdown.Content>
-        </Dropdown.Root>
+                  {
+                    devices.find(({ deviceId }) => deviceId === selection)
+                      ?.label
+                  }
+                </Text>
+                {open ? <ChevronUpIcon /> : <ChevronDownIcon />}
+              </Flex>
+            </Dropdown.Trigger>
+            <Dropdown.Content
+              align="start"
+              sideOffset={8}
+              css={{ w: "100%" }}
+              portalled={false}
+            >
+              {devices.map(device => {
+                return (
+                  <Dropdown.Item
+                    key={device.label}
+                    onSelect={() => onChange(device.deviceId)}
+                    css={{
+                      px: "$9",
+                      bg:
+                        device.deviceId === selection
+                          ? "$primaryDark"
+                          : undefined,
+                    }}
+                  >
+                    {device.label}
+                  </Dropdown.Item>
+                );
+              })}
+            </Dropdown.Content>
+          </Dropdown.Root>
+        </Box>
         {children}
       </Flex>
     </Box>
