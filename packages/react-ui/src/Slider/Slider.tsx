31911dab1ef8bd315c4da7fdb61da3d3c9163310
Kaustubh Kumar <kaustubhkumar05@gmail.com>
2022-07-06 18:09:35 +0530

    Prettier update

--- packages/react-ui/src/Slider/Slider.tsx
+++ packages/react-ui/src/Slider/Slider.tsx
@@ -1,48 +1,48 @@
-import React from "react";
-import { CSS } from "@stitches/react";
-import * as BaseSlider from "@radix-ui/react-slider";
-import { Tooltip } from "../Tooltip";
-import { styled } from "../Theme";
+import React from 'react';
+import { CSS } from '@stitches/react';
+import * as BaseSlider from '@radix-ui/react-slider';
+import { Tooltip } from '../Tooltip';
+import { styled } from '../Theme';
 
 const Root = styled(BaseSlider.Root, {
-  position: "relative",
-  display: "flex",
-  alignItems: "center",
-  userSelect: "none",
-  touchAction: "none",
-  width: "100%",
+  position: 'relative',
+  display: 'flex',
+  alignItems: 'center',
+  userSelect: 'none',
+  touchAction: 'none',
+  width: '100%',
   '&[data-orientation="horizontal"]': {
     height: 20,
   },
 });
 
 const Track = styled(BaseSlider.Track, {
-  backgroundColor: "$grayDefault",
-  position: "relative",
+  backgroundColor: '$grayDefault',
+  position: 'relative',
   flexGrow: 1,
-  borderRadius: "$round",
-  cursor: "pointer",
+  borderRadius: '$round',
+  cursor: 'pointer',
   '&[data-orientation="horizontal"]': { height: 3 },
 });
 
 const Range = styled(BaseSlider.Range, {
-  position: "absolute",
-  backgroundColor: "$brandDefault",
-  borderRadius: "$round",
-  height: "100%",
+  position: 'absolute',
+  backgroundColor: '$brandDefault',
+  borderRadius: '$round',
+  height: '100%',
 });
 
 const Thumb = styled(BaseSlider.Thumb, {
-  all: "unset",
-  display: "block",
-  width: "$8",
-  height: "$8",
-  backgroundColor: "$brandDefault",
-  cursor: "pointer",
+  all: 'unset',
+  display: 'block',
+  width: '$8',
+  height: '$8',
+  backgroundColor: '$brandDefault',
+  cursor: 'pointer',
   boxShadow: `0 2px 10px $colors$grayDefault`,
   borderRadius: 10,
-  "&:hover": { backgroundColor: "$brandDefault" },
-  "&:focus": { boxShadow: "none" },
+  '&:hover': { backgroundColor: '$brandDefault' },
+  '&:focus': { boxShadow: 'none' },
 });
 
 type SliderProps = React.ComponentProps<typeof Root> & { thumbStyles?: CSS };
@@ -62,4 +62,4 @@ export const Slider: React.FC<SliderProps> = ({
   </Root>
 );
 
-Slider.displayName = "Slider";
+Slider.displayName = 'Slider';
