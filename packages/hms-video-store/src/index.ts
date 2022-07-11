d77577642bb52d05cb079ed0abd79829845372ee
Tushar <tushar@100ms.live>
2022-07-11 09:34:16 +0530

    feat(store): mocker class

--- packages/hms-video-store/src/index.ts
+++ packages/hms-video-store/src/index.ts
@@ -1,1 +1,2 @@
 export * from './core';
+export { HMSStoreMock } from './test/HMSStoreMock';
---
+++ packages/hms-video-store/src/test/HMSStoreMock.ts
@@ -0,0 +1,69 @@
+import { isBrowser } from '@100mslive/hms-video';
+import { HMSReactiveStore } from '../core/hmsSDKStore/HMSReactiveStore';
+import { HMSPeer, HMSStore } from '../core/schema';
+import { HMSLogger } from '../common/ui-logger';
+import { NamedSetState } from '../core/hmsSDKStore/internalTypes';
+
+export interface HMSStoreMockPeerConfig {
+  roleName?: string;
+}
+
+export class HMSStoreMock {
+  private TAG = 'MockStore';
+  private readonly setState: NamedSetState<HMSStore>;
+  private peerCounter = 0;
+
+  constructor(storeSDK?: HMSReactiveStore) {
+    if (!storeSDK && isBrowser && window?.__hms) {
+      storeSDK = window.__hms;
+    }
+    // @ts-ignore
+    this.setState = storeSDK!.getActions().setState;
+  }
+
+  /**
+   * add lots of peers in one go, good for getting to a number quickly
+   */
+  addPeersBatched(count: number, config?: HMSStoreMockPeerConfig) {
+    const start = performance.now();
+    this.addPeersInternal(count, config);
+    const timeDiff = performance.now() - start;
+    HMSLogger.i(this.TAG, `Time taken in adding ${count} batched mock peers = ${timeDiff}ms`);
+  }
+
+  /**
+   * add lots of peers one at a time, simulates the real world scenario better
+   */
+  addPeersSequentially(count: number, config?: HMSStoreMockPeerConfig) {
+    // add one by one
+    const start = performance.now();
+    while (count--) {
+      this.addPeersInternal(1, config);
+    }
+    const timeDiff = performance.now() - start;
+    HMSLogger.i(
+      this.TAG,
+      `Time taken in adding ${count} sequential mock peers = ${timeDiff}ms, avg = ${timeDiff / count}ms`,
+    );
+  }
+
+  /**
+   * TODO: simulate with calling onPeerUpdate, should this be on sdk level? would simulate everything even better
+   */
+  private addPeersInternal(count: number, config?: HMSStoreMockPeerConfig) {
+    const roleName = config?.roleName || 'host';
+    const peerBlueprint: HMSPeer = { auxiliaryTracks: [], id: '', isLocal: false, name: '', roleName };
+    this.setState(store => {
+      while (count--) {
+        const peer = {
+          ...peerBlueprint,
+          id: this.peerCounter.toString(),
+          name: `peer${this.peerCounter}`,
+        };
+        this.peerCounter++;
+        store.room.peers.push(peer.id);
+        store.peers[peer.id] = peer;
+      }
+    }, `add ${count} mock peers`);
+  }
+}
