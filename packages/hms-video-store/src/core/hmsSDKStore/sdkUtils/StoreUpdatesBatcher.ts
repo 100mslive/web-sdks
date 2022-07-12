039123d8335bef29e40ad82740604a145db8c0e1
Tushar <tushar@100ms.live>
2022-07-12 15:41:40 +0530

    feat(store): dynamic timeouts in batching

--- packages/hms-video-store/src/core/hmsSDKStore/sdkUtils/StoreUpdatesBatcher.ts
+++ packages/hms-video-store/src/core/hmsSDKStore/sdkUtils/StoreUpdatesBatcher.ts
@@ -15,8 +15,11 @@ export class StoreUpdatesBatcher {
   private queuedActions: Map<string, number> = new Map();
   private queuedUpdates: PartialState<any>[] = [];
   private nextUpdatePromise?: Promise<void>;
-  private DEFAULT_INTERVAL_MS = 5;
+  private DEFAULT_INTERVAL_MS = 10;
   private store: IHMSStore;
+  private prevBatchCompletedAt = 0;
+  // to ensure we're giving other things a chance to run as well
+  private MINIMUM_GAP_BETWEEN_BATCHES_MS = 100;
 
   constructor(store: IHMSStore) {
     this.store = store;
@@ -29,12 +32,17 @@ export class StoreUpdatesBatcher {
     }
     this.queuedUpdates.push(fn);
     if (!this.nextUpdatePromise) {
+      const timeSincePrevUpdate = performance.now() - this.prevBatchCompletedAt;
+      const extraTime = Math.max(0, this.MINIMUM_GAP_BETWEEN_BATCHES_MS - timeSincePrevUpdate);
+      if (extraTime > 0) {
+        HMSLogger.d(this.TAG, `extraTime=${extraTime}ms, defaultInterval=${this.DEFAULT_INTERVAL_MS}ms`);
+      }
       // there is no schedule update, schedule an update and create a promise
       this.nextUpdatePromise = new Promise<void>(resolve => {
         setTimeout(() => {
           this.setStateBatched();
           resolve();
-        }, this.DEFAULT_INTERVAL_MS);
+        }, Math.max(this.DEFAULT_INTERVAL_MS, extraTime));
       });
     }
     return this.nextUpdatePromise;
@@ -46,13 +54,7 @@ export class StoreUpdatesBatcher {
       const start = performance.now();
       let timeDiffFnsRun = 0;
       const batchedFn = (draftStore: HMSStore) => {
-        this.queuedUpdates.forEach(fn => {
-          try {
-            fn(draftStore);
-          } catch (err) {
-            HMSLogger.w(this.TAG, 'failed to update store', err, fn.name);
-          }
-        });
+        this.applyQueuedUpdates(draftStore);
         timeDiffFnsRun = performance.now() - start;
       };
       this.store.namedSetState(batchedFn, `batched-${action}`);
@@ -65,11 +67,28 @@ export class StoreUpdatesBatcher {
           )}ms, renderTime=${(timeDiffSetState - timeDiffFnsRun).toFixed(2)}ms, actions="${action}"`,
         );
       }
+      if (timeDiffSetState > this.DEFAULT_INTERVAL_MS) {
+        // if updates are taking too much time, have a higher interval for batching
+        const deltaTimeMs = 10; // some extra time to have a margin
+        this.DEFAULT_INTERVAL_MS = Math.round(timeDiffSetState) + deltaTimeMs;
+        HMSLogger.d(this.TAG, `updated batching interval to ${this.DEFAULT_INTERVAL_MS}`);
+      }
+      this.prevBatchCompletedAt = performance.now();
     }
     // cleanup
     this.queuedUpdates = [];
     this.queuedActions.clear();
     this.nextUpdatePromise = undefined;
+  }
+
+  private applyQueuedUpdates(draftStore: HMSStore) {
+    this.queuedUpdates.forEach(fn => {
+      try {
+        fn(draftStore);
+      } catch (err) {
+        HMSLogger.w(this.TAG, 'failed to update store', err, fn.name);
+      }
+    });
   }
 
   /**
