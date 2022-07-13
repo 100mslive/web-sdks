1d20504e6a339d0df689acd6761e3ea651ea5e5d
hardik1003 <hardik@100ms.live>
2022-07-13 19:56:49 +0530

    refactor: move reconnecting notification to the HMS toast config

--- apps/100ms-web/src/components/Notifications/ReconnectNotifications.jsx
+++ apps/100ms-web/src/components/Notifications/ReconnectNotifications.jsx
@@ -1,39 +1,24 @@
 import { useEffect } from "react";
 import LogRocket from "logrocket";
+import { ToastBatcher } from "../Toast/ToastBatcher";
 import {
   HMSNotificationTypes,
   useHMSNotifications,
 } from "@100mslive/react-sdk";
-import { ConnectivityIcon, PoorConnectivityIcon } from "@100mslive/react-icons";
-import { ToastManager } from "../Toast/ToastManager";
-import { TextWithIcon } from "./TextWithIcon";
 
 const notificationTypes = [
   HMSNotificationTypes.RECONNECTED,
   HMSNotificationTypes.RECONNECTING,
 ];
-let notificationId = null;
 export const ReconnectNotifications = () => {
   const notification = useHMSNotifications(notificationTypes);
   useEffect(() => {
     if (notification?.type === HMSNotificationTypes.RECONNECTED) {
       LogRocket.track("Reconnected");
-      notificationId = ToastManager.replaceToast(notificationId, {
-        title: `You are now connected`,
-        icon: <ConnectivityIcon />,
-        variant: "success",
-        duration: 3000,
-      });
+      ToastBatcher.showToast({ notification });
     } else if (notification?.type === HMSNotificationTypes.RECONNECTING) {
       LogRocket.track("Reconnecting");
-      notificationId = ToastManager.replaceToast(notificationId, {
-        title: `You are offline for now. while we try to reconnect, please check
-        your internet connection.
-      `,
-        icon: <PoorConnectivityIcon />,
-        variant: "warning",
-        duration: 10000,
-      });
+      ToastBatcher.showToast({ notification });
     }
   }, [notification]);
   return null;
