67b52fa1bbaae9de6efdcbe14b7d276dac04cbb0
raviteja83 <ravi.theja@100ms.live>
2022-07-13 18:59:43 +0530

    fix: text with icon component

--- apps/100ms-web/src/components/Notifications/TextWithIcon.jsx
+++ apps/100ms-web/src/components/Notifications/TextWithIcon.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Box, Text } from "@100mslive/react-ui";
 
 export const TextWithIcon = ({ Icon, children }) => (
-  <Text>
+  <Text css={{ display: "flex", alignItems: "center" }}>
     <Box as="span" css={{ flexShrink: 0, mr: "$4" }}>
       <Icon />
     </Box>
