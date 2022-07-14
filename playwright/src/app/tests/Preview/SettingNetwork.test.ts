36d51ea093f0afc0285f3ea755f9d181b2bb3c76
Sahil Kashyap <sahil@100ms.live>
2022-07-14 17:53:55 +0530

    test(hls starter kit changes): updated preview page tests

--- playwright/src/app/tests/Preview/SettingNetwork.test.ts
+++ playwright/src/app/tests/Preview/SettingNetwork.test.ts
@@ -1,21 +1,16 @@
-import { PageWrapper } from "../../PageWrapper";
-import { test } from "@playwright/test";
+import { PageWrapper } from '../../PageWrapper';
+import { test } from '@playwright/test';
 
 let page: PageWrapper;
-
+const name = `${process.env.peer_name}1`;
 test.beforeEach(async ({ page: nativePage }) => {
   page = new PageWrapper(nativePage);
-  await page.preview.gotoPreviewPage();
+  await page.prepreview.gotoPreviewPage(name);
 });
 
 test.afterEach(async () => {
   await page.close();
 });
-
-// test.afterEach(async ({page}) => {
-//   await pageMethods.clickElement(page, previewPage.preview_video_btn, "preview_video_btn")
-//     await page.close()
-// });
 
 test(`Verify Network Btn`, async () => {
   await page.assertVisible(page.preview.preview_tile_network);
@@ -26,7 +21,7 @@ test(`Verify Preview Settings Btn`, async () => {
 
   for (let i = 0; i < page.preview.preview_setting_btn_list.length; i++) {
     await page.assertVisible(
-      page.preview.dialoge_select_settings.replace("?", page.preview.preview_setting_btn_list[i])
+      page.preview.dialoge_select_settings.replace('?', page.preview.preview_setting_btn_list[i]),
     );
   }
   await page.click(page.preview.dialoge_cross_icon);
