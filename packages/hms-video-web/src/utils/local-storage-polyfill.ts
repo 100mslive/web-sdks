2dd7cc73ab4143a0117812f93f77b0812e97d669
eswarclynn <esuclintblazer@gmail.com>
2022-07-14 17:06:29 +0530

    fix(sdk): simplify localstorage polyfill

--- packages/hms-video-web/src/utils/local-storage-polyfill.ts
+++ packages/hms-video-web/src/utils/local-storage-polyfill.ts
@@ -1,129 +1,41 @@
-/* eslint-disable */
-// Storage polyfill by Remy Sharp
-// https://gist.github.com/350433
-// Needed for IE7-
+const valuesMap = new Map();
 
-import HMSLogger from './logger';
+class LocalStorage {
+  getItem(key: string) {
+    const stringKey = String(key);
+    if (valuesMap.has(key)) {
+      return String(valuesMap.get(stringKey));
+    }
+    return null;
+  }
 
-// Dependencies:
-//  JSON (use json2.js if necessary)
+  setItem(key: string, val: string) {
+    valuesMap.set(String(key), String(val));
+  }
 
-// Tweaks by Joshua Bell (inexorabletash@gmail.com)
-//  * URI-encode item keys
-//  * Use String() for stringifying
-//  * added length
+  removeItem(key: string) {
+    valuesMap.delete(key);
+  }
 
-if (!window.localStorage || !window.sessionStorage)
-  (function () {
-    var Storage = function (type: string) {
-      HMSLogger.w(`Polyfillling ${type} storage using cookies`);
+  clear() {
+    valuesMap.clear();
+  }
 
-      function createCookie(name: string, value: string, days: number) {
-        var date, expires;
+  key(i: number) {
+    if (arguments.length === 0) {
+      throw new TypeError("Failed to execute 'key' on 'Storage': 1 argument required, but only 0 present."); // this is a TypeError implemented on Chrome, Firefox throws Not enough arguments to Storage.key.
+    }
+    const arr = Array.from(valuesMap.keys());
+    return arr[i];
+  }
 
-        if (days) {
-          date = new Date();
-          date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-          expires = '; expires=' + date.toUTCString();
-        } else {
-          expires = '';
-        }
-        document.cookie = name + '=' + value + expires + '; path=/';
-      }
+  get length() {
+    return valuesMap.size;
+  }
+}
 
-      function readCookie(name: string) {
-        var nameEQ = name + '=',
-          ca = document.cookie.split(';'),
-          i,
-          c;
-
-        for (i = 0; i < ca.length; i++) {
-          c = ca[i];
-          while (c.charAt(0) == ' ') {
-            c = c.substring(1, c.length);
-          }
-
-          if (c.indexOf(nameEQ) == 0) {
-            return c.substring(nameEQ.length, c.length);
-          }
-        }
-        return null;
-      }
-
-      function setData(data: any) {
-        data = JSON.stringify(data);
-        if (type == 'session') {
-          window.name = data;
-        } else {
-          createCookie('localStorage', data, 365);
-        }
-      }
-
-      function clearData() {
-        if (type == 'session') {
-          window.name = '';
-        } else {
-          createCookie('localStorage', '', 365);
-        }
-      }
-
-      function getData() {
-        var data = type == 'session' ? window.name : readCookie('localStorage');
-        return data ? JSON.parse(data) : {};
-      }
-
-      // initialise if there's already data
-      var data = getData();
-
-      function numKeys() {
-        var n = 0;
-        for (var k in data) {
-          if (data.hasOwnProperty(k)) {
-            n += 1;
-          }
-        }
-        return n;
-      }
-
-      return {
-        clear: function () {
-          data = {};
-          clearData();
-          this.length = numKeys();
-        },
-        getItem: function (key: string) {
-          key = encodeURIComponent(key);
-          return data[key] === undefined ? null : data[key];
-        },
-        key: function (i: number) {
-          // not perfect, but works
-          var ctr = 0;
-          for (var k in data) {
-            if (ctr == i) return decodeURIComponent(k);
-            else ctr++;
-          }
-          return null;
-        },
-        removeItem: function (key: string) {
-          key = encodeURIComponent(key);
-          delete data[key];
-          setData(data);
-          this.length = numKeys();
-        },
-        setItem: function (key: string, value: string) {
-          key = encodeURIComponent(key);
-          data[key] = String(value);
-          setData(data);
-          this.length = numKeys();
-        },
-        length: 0,
-      };
-    };
-
-    // @ts-ignore
-    if (!window.localStorage) window.localStorage = new Storage('local');
-    // @ts-ignore
-    if (!window.sessionStorage) window.sessionStorage = new Storage('session');
-  })();
+if (!window.localStorage) {
+  window.localStorage = new LocalStorage();
+}
 
 export {};
