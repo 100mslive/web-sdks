9bfce809130c1693f450f3f614ddf2bf6f438659
raviteja83 <ravi.theja@100ms.live>
2022-07-14 14:49:39 +0530

    feat: show popup for permission errors

---
+++ apps/100ms-web/src/components/Notifications/PermissionErrorModal.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+import {
+  HMSNotificationTypes,
+  useHMSNotifications,
+} from "@100mslive/react-sdk";
+import { Dialog, Text } from "@100mslive/react-ui";
+
+export function PermissionErrorModal() {
+  const notification = useHMSNotifications(HMSNotificationTypes.ERROR);
+  const [error, setError] = useState("");
+  useEffect(() => {
+    if (
+      !notification ||
+      notification.data?.code !== 3001 ||
+      notification.data?.message.includes("screen")
+    ) {
+      return;
+    }
+    console.debug(`[${notification.type}]`, notification);
+    const errorMessage = notification.data?.message;
+    const hasAudio = errorMessage.includes("audio");
+    const hasVideo = errorMessage.includes("video");
+    let deviceType;
+    if (hasAudio && hasVideo) {
+      deviceType = "Camera and Microphone";
+    } else if (hasAudio) {
+      deviceType = "Microphone";
+    } else if (hasVideo) {
+      deviceType = "Camera";
+    }
+    setError(deviceType);
+  }, [notification]);
+  return error ? (
+    <Dialog.Root
+      open={error}
+      onOpenChange={value => {
+        if (!value) {
+          setError("");
+        }
+      }}
+    >
+      <Dialog.Content css={{ w: "min(480px, 90%)" }}>
+        <Dialog.Title
+          css={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            borderBottom: "1px solid $borderDefault",
+            pb: "$8",
+          }}
+        >
+          <Text css={{ fontWeight: "$semiBold" }}>
+            {error} permissions are blocked
+          </Text>
+          <Dialog.DefaultClose data-testid="dialoge_cross_icon" />
+        </Dialog.Title>
+        <Text variant="md" css={{ py: "$10" }}>
+          Access to {error} is required. Please grant access to {error}
+        </Text>
+      </Dialog.Content>
+    </Dialog.Root>
+  ) : null;
+}
