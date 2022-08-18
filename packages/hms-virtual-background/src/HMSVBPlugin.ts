1ca4f47fe5685db7348369c213e70d48cf20d348
raviteja83 <ravi.theja@100ms.live>
2022-08-19 01:37:22 +0530

    refactor: remove unused code from new plugin

--- packages/hms-virtual-background/src/HMSVBPlugin.ts
+++ packages/hms-virtual-background/src/HMSVBPlugin.ts
@@ -11,12 +11,6 @@ import {
 } from '@100mslive/hms-video';
 
 const TAG = 'VBProcessor';
-// const DEFAULT_DELAY = 33;
-
-// const minVideoWidthForSharpening = 214;
-// const maxVideoWidthForSharpening = 855;
-// const minVideoHeightForSharpening = 120;
-// const maxVideoHeightForSharpening = 720;
 
 export class HMSVBPlugin implements HMSVideoPlugin {
   background: string | HTMLImageElement | HTMLVideoElement;
@@ -28,13 +22,9 @@ export class HMSVBPlugin implements HMSVideoPlugin {
   backgroundType = 'none';
   loadModelCalled: boolean;
   blurValue: any;
-  //   tfLite: any;
-  //   tfLitePromise: any;
   segmenter!: bodySegmentation.BodySegmenter;
   modelName: string;
 
-  input: HTMLCanvasElement | null;
-  output: HTMLCanvasElement | null;
   outputCtx: CanvasRenderingContext2D | null;
   timerID: number;
   imageAspectRatio: number;
@@ -65,12 +55,9 @@ export class HMSVBPlugin implements HMSVideoPlugin {
     this.isVirtualBackground = false;
     this.blurValue = '10px';
     this.loadModelCalled = false;
-    // this.tfLite = null;
     this.modelName = 'landscape-segmentation';
 
     this.outputCtx = null;
-    this.input = null;
-    this.output = null;
     this.timerID = 0;
     this.imageAspectRatio = 1;
 
@@ -186,8 +173,8 @@ export class HMSVBPlugin implements HMSVideoPlugin {
       }
     }
     if (this.outputCtx) {
-      this.outputCtx!.fillStyle = `rgb(0, 0, 0)`;
-      this.outputCtx!.fillRect(0, 0, this.output!.width, this.output!.height);
+      this.outputCtx!.fillStyle = `rgba(0, 0, 0, 0)`;
+      this.outputCtx!.fillRect(0, 0, this.outputCtx!.canvas.width, this.outputCtx!.canvas.height);
     }
 
     //gif related
@@ -201,11 +188,6 @@ export class HMSVBPlugin implements HMSVideoPlugin {
     if (!input || !output) {
       throw new Error('Plugin invalid input/output');
     }
-
-    if (typeof this.background === 'string') {
-      return;
-    }
-
     const segmentationConfig = { flipHorizontal: true };
     const people = await this.segmenter.segmentPeople(input, segmentationConfig);
     const foregroundColor = { r: 0, g: 0, b: 0, a: 0 };
@@ -214,53 +196,23 @@ export class HMSVBPlugin implements HMSVideoPlugin {
     output.width = input.width;
     output.height = input.height;
     const ctx = output.getContext('2d');
-    ctx!.imageSmoothingEnabled = true;
-    ctx!.imageSmoothingQuality = 'high';
-    ctx!.drawImage(this.background, 0, 0, input.width, input.height);
-    this.personMaskCanvas.width = mask.width;
-    this.personMaskCanvas.height = mask.height;
-    this.personMaskCtx!.putImageData(mask, 0, 0);
-    ctx!.globalCompositeOperation = 'destination-atop';
-    ctx!.drawImage(this.personMaskCanvas, 0, 0, mask.width, mask.height, 0, 0, input.width, input.height);
-    ctx!.globalCompositeOperation = 'destination-over';
-    // ctx!.filter = 'none';
-    // Draw the foreground
-    ctx!.drawImage(input, 0, 0, mask.width, mask.height, 0, 0, input.width, input.height);
-
-    // Draw background
-    // ctx!.globalCompositeOperation = 'exclusion';
-
-    /*
-    console.error('skipProcessing', skipProcessing, people[0]); */
-    /* if (ctx!.canvas.width !== input.width) {
-      ctx!.canvas.width = input.width;
+    this.outputCtx = ctx;
+    if (typeof this.background !== 'string') {
+      ctx!.imageSmoothingEnabled = true;
+      ctx!.imageSmoothingQuality = 'high';
+      ctx!.drawImage(this.background, 0, 0, input.width, input.height);
+      ctx!.filter = 'none';
+      this.personMaskCanvas.width = mask.width;
+      this.personMaskCanvas.height = mask.height;
+      this.personMaskCtx!.putImageData(mask, 0, 0);
+      ctx!.globalCompositeOperation = 'destination-atop';
+      ctx!.drawImage(this.personMaskCanvas, 0, 0, mask.width, mask.height, 0, 0, input.width, input.height);
+      ctx!.globalCompositeOperation = 'destination-over';
+      // Draw the foreground
+      ctx!.drawImage(input, 0, 0, mask.width, mask.height, 0, 0, input.width, input.height);
+    } else if (this.background === 'blur') {
+      await bodySegmentation.drawBokehEffect(output, input, people, 0.5, 15, 1, false);
     }
-    if (ctx!.canvas.height !== input.height) {
-      ctx!.canvas.height = input.height;
-    }
-
-    if (this.backgroundType === 'video') {
-      this.backgroundVideo!.width = input.width;
-      this.backgroundVideo!.height = input.height;
-    }
-
-    this.outputCtx = ctx!;
-    this.imageAspectRatio = input.width / input.height;
-    if (this.imageAspectRatio <= 0) {
-      throw new Error('Invalid input width/height');
-    } */
-
-    // const process = async () => {
-    //   await this.runSegmentation(skipProcessing);
-    // };
-
-    /* if ((this.background === 'none' && !this.isVirtualBackground) || skipProcessing) {
-      this.outputCtx!.globalCompositeOperation = 'copy';
-      this.outputCtx!.filter = 'none';
-      this.outputCtx!.drawImage(input, 0, 0, input.width, input.height);
-    } else {
-      //   process();
-    } */
   }
 
   private async setImage(image: HTMLImageElement): Promise<any> {
@@ -283,207 +235,4 @@ export class HMSVBPlugin implements HMSVideoPlugin {
   private log(tag: string, ...data: any[]) {
     console.info(tag, ...data);
   }
-
-  /* private resizeInputData() {
-    this.personMaskCtx!.drawImage(
-      this.input,
-      0,
-      0,
-      this.input!.width,
-      this.input!.height,
-      0,
-      0,
-      this.personMaskWidth,
-      this.personMaskHeight,
-    );
-
-    const imageData = this.personMaskCtx!.getImageData(0, 0, this.personMaskWidth, this.personMaskHeight);
-    //
-    const inputMemoryOffset = this.tfLite._getInputMemoryOffset() / 4;
-    for (let i = 0; i < this.personMaskPixelCount; i++) {
-      this.tfLite.HEAPF32[inputMemoryOffset + i * 3] = imageData.data[i * 4] / 255;
-      this.tfLite.HEAPF32[inputMemoryOffset + i * 3 + 1] = imageData.data[i * 4 + 1] / 255;
-      this.tfLite.HEAPF32[inputMemoryOffset + i * 3 + 2] = imageData.data[i * 4 + 2] / 255;
-    }
-  }
-  private infer(skipProcessing?: boolean) {
-    if (!skipProcessing) {
-      this.tfLite._runInference();
-    }
-    const outputMemoryOffset = this.tfLite._getOutputMemoryOffset() / 4;
-
-    for (let i = 0; i < this.personMaskPixelCount; i++) {
-      if (this.modelName === 'meet') {
-        const background = this.tfLite.HEAPF32[outputMemoryOffset + i * 2];
-        const person = this.tfLite.HEAPF32[outputMemoryOffset + i * 2 + 1];
-        const shift = Math.max(background, person);
-        const backgroundExp = Math.exp(background - shift);
-        const personExp = Math.exp(person - shift);
-        // Sets only the alpha component of each pixel.
-        this.personMask.data[i * 4 + 3] = (255 * personExp) / (backgroundExp + personExp);
-      } else if (this.modelName === 'landscape-segmentation') {
-        const person = this.tfLite.HEAPF32[outputMemoryOffset + i];
-        this.personMask.data[i * 4 + 3] = 255 * person;
-      }
-    }
-
-    this.personMaskCtx!.putImageData(this.personMask, 0, 0);
-  }
-
-  private postProcessing() {
-    this.outputCtx!.globalCompositeOperation = 'copy';
-    this.outputCtx!.filter = 'none';
-
-    if (this.isVirtualBackground) {
-      this.outputCtx!.filter = 'blur(4px)';
-    } else {
-      this.outputCtx!.filter = 'blur(8px)';
-    }
-    this.drawPersonMask();
-    this.outputCtx!.globalCompositeOperation = 'source-in';
-    this.outputCtx!.filter = 'none';
-    // //Draw the foreground
-    this.outputCtx!.drawImage(this.input!, 0, 0);
-
-    if (
-      this.enableSharpening &&
-      this.output!.width > minVideoWidthForSharpening && // minimum and maximum resolution to enable sharpening filter
-      this.output!.height > minVideoHeightForSharpening &&
-      this.output!.width < maxVideoWidthForSharpening &&
-      this.output!.height < maxVideoHeightForSharpening
-    ) {
-      this.sharpenFilter();
-    }
-
-    // //Draw the background
-    this.drawSegmentedBackground();
-  }
-
-  private sharpenFilter() {
-    // adding sharpening filter to each frame to improve edges and brightness
-    // The basic idea is that you take the weighed sum of a rectangle of pixels from the source image and use that as the output value using convolution filter
-    // It is applied intermediate output with black background and only mask data in frame
-    // Filter currently used is 3 x 3 sharpening filter with values as shown:
-    // [  0, -1,  0,
-    //   -1,  5, -1,
-    //   0, -1,  0 ]
-    const outputImageData = this.outputCtx!.getImageData(0, 0, this.output!.width, this.output!.height);
-
-    // filters you may try
-    // [-1, -1, -1, -1, 9, -1, -1, -1, -1]
-    //[0, -1, 0, -1, 5, -1, 0, -1, 0]
-    const output = this.filters.convolute(outputImageData);
-
-    this.outputCtx!.putImageData(output, 0, 0);
-  }
-
-  private drawPersonMask() {
-    this.outputCtx!.drawImage(
-      this.personMaskCanvas,
-      0,
-      0,
-      this.personMaskWidth,
-      this.personMaskHeight,
-      0,
-      0,
-      this.output!.width,
-      this.output!.height,
-    );
-  }
-
-  private drawSegmentedBackground() {
-    this.outputCtx!.globalCompositeOperation = 'destination-over';
-    this.outputCtx!.imageSmoothingEnabled = true;
-    this.outputCtx!.imageSmoothingQuality = 'high';
-    if (this.isVirtualBackground) {
-      if (this.backgroundType === 'video' && this.backgroundVideo != null && this.backgroundVideo!.readyState >= 4) {
-        this.fitVideoToBackground();
-      } else if (this.backgroundType === 'image') {
-        this.fitImageToBackground();
-      } else if (this.backgroundType === 'gif') {
-        if (this.giflocalCount > this.gifFrames[this.gifFramesIndex].delay / DEFAULT_DELAY) {
-          this.gifFramesIndex++;
-          if (this.gifFramesIndex >= this.gifFrames.length) {
-            this.gifFramesIndex = 0;
-          }
-          this.giflocalCount = 0;
-        } else {
-          this.giflocalCount++;
-        }
-        this.fitGifToBackground();
-      }
-    } else {
-      this.addBlurToBackground();
-    }
-  }
-
-  private fitVideoToBackground() {
-    this.fitData(this.backgroundVideo, this.backgroundVideo!.videoWidth, this.backgroundVideo!.videoHeight);
-  }
-
-  private fitImageToBackground() {
-    this.fitData(this.backgroundImage, this.backgroundImage!.width, this.backgroundImage!.height);
-  }
-
-  private fitGifToBackground() {
-    if (this.gifFrameImageData == null) {
-      const dims = this.gifFrames[this.gifFramesIndex].dims;
-      this.tempGifCanvas!.width = dims.width;
-      this.tempGifCanvas!.height = dims.height;
-      this.gifFrameImageData = this.tempGifContext.createImageData(dims.width, dims.height);
-    }
-    // set the patch data as an override
-    this.gifFrameImageData.data.set(this.gifFrames[this.gifFramesIndex].patch);
-    this.tempGifContext.putImageData(this.gifFrameImageData, 0, 0);
-
-    this.fitData(this.tempGifCanvas, this.gifFrameImageData!.width, this.gifFrameImageData!.height);
-  }
-
-  private fitData(data: any, dataWidth: number, dataHeight: number) {
-    let inputWidth: any, inputHeight: any, xoffset: any, yoffset: any;
-    if (dataWidth / dataHeight < this.imageAspectRatio) {
-      inputWidth = dataWidth;
-      inputHeight = dataWidth / this.imageAspectRatio;
-      xoffset = 0;
-      yoffset = (dataHeight - inputHeight) / 2;
-    } else {
-      inputHeight = dataHeight;
-      inputWidth = dataHeight * this.imageAspectRatio;
-      yoffset = 0;
-      xoffset = (dataWidth - inputWidth) / 2;
-    }
-    this.outputCtx!.drawImage(
-      data,
-      xoffset,
-      yoffset,
-      inputWidth,
-      inputHeight,
-      0,
-      0,
-      this.output!.width,
-      this.output!.height,
-    );
-  }
-
-  private async addBlurToBackground() {
-    let blurValue = '15px';
-    if (this.input!.width <= 160) {
-      blurValue = '5px';
-    } else if (this.input!.width <= 320) {
-      blurValue = '10px';
-    } else if (this.input!.width <= 640) {
-      blurValue = '15px';
-    } else if (this.input!.width <= 960) {
-      blurValue = '20px';
-    } else if (this.input!.width <= 1280) {
-      blurValue = '25px';
-    } else if (this.input!.width <= 1920) {
-      blurValue = '30px';
-    }
-
-    this.outputCtx!.filter = `blur(${blurValue})`;
-    this.outputCtx!.drawImage(this.input!, 0, 0, this.output!.width, this.output!.height);
-  }
-
-  */
 }
