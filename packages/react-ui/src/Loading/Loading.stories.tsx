c994bde17f71487dfae55cfb20cfc151915f4aed
Muhammad Zaki <muhammad.zaki93@ui.ac.id>
2022-08-18 18:54:29 +0700

    feat: added loading story

---
+++ packages/react-ui/src/Loading/Loading.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { Loading } from '.';
+
+export default {
+  title: 'UI Components/Loading',
+  component: Loading,
+} as ComponentMeta<typeof Loading>;
+
+const Template: ComponentStory<typeof Loading> = ({ size, color }) => {
+  return <Loading size={size} color={color} />;
+};
+
+export const Example = Template.bind({});
