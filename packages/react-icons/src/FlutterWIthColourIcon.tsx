46b2462e180b084af4119c41419de244ea884ca4
Ritik Agrawal <64203150+bmbshlly@users.noreply.github.com>
2022-08-25 18:08:01 +0530

    feat: update colors, add icons

---
+++ packages/react-icons/src/FlutterWIthColourIcon.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+function SvgFlutterWIthColourIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg width="24px" height="24px" viewBox="0 0 20 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path
+        d="M12.047.008L19.38 0 7.077 12.27 3.87 15.453.222 11.807 12.047.008zm-.069 10.956c.096-.125.27-.063.404-.08l6.992.002L13 17.24l-3.667-3.637 2.644-2.638z"
+        fill="#44D1FD"
+      />
+      <path d="M5.679 17.245l3.655-3.643 3.667 3.637.015.016-3.681 3.64-3.656-3.65z" fill="#1FBCFD" />
+      <path
+        d="M9.335 20.895l3.681-3.64 6.36 6.353c-2.395.005-4.79-.002-7.185.003-.136.03-.22-.094-.308-.171l-2.548-2.546z"
+        fill="#08589C"
+      />
+    </svg>
+  );
+}
+
+export default SvgFlutterWIthColourIcon;
