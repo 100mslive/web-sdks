b254f56f15585d21c39aac475633651b6a5a8d6d
Muhammad Zaki <muhammad.zaki93@ui.ac.id>
2022-08-29 15:00:09 +0700

    feat: add icon button examples on footer

--- packages/react-ui/src/Footer/Footer.stories.tsx
+++ packages/react-ui/src/Footer/Footer.stories.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
+import {
+  BrbIcon,
+  ChatIcon,
+  EndStreamIcon,
+  MicOnIcon,
+  PipIcon,
+  ShareScreenIcon,
+  VideoOnIcon,
+} from '@100mslive/react-icons';
 
 import { Footer } from '.';
+import { IconButton } from '../IconButton';
+import { Button } from '../Button';
 
 export default {
   title: 'UI Components/Footer',
@@ -15,9 +26,34 @@ export default {
 const Template: ComponentStory<typeof Footer.Root> = ({ css }) => {
   return (
     <Footer.Root css={css}>
-        <Footer.Left>This is the left side of the footer</Footer.Left>
-        <Footer.Center>This is the center of the footer</Footer.Center>
-        <Footer.Right>This is the right side of the footer</Footer.Right>
+      <Footer.Left>
+        <IconButton>
+          <MicOnIcon />
+        </IconButton>
+        <IconButton>
+          <VideoOnIcon />
+        </IconButton>
+      </Footer.Left>
+      <Footer.Center>
+        <IconButton>
+          <ShareScreenIcon />
+        </IconButton>
+        <IconButton>
+          <PipIcon />
+        </IconButton>
+        <Button>
+          <EndStreamIcon />
+          End Stream
+        </Button>
+      </Footer.Center>
+      <Footer.Right>
+        <IconButton>
+          <BrbIcon />
+        </IconButton>
+        <IconButton>
+          <ChatIcon />
+        </IconButton>
+      </Footer.Right>
     </Footer.Root>
   );
 };
