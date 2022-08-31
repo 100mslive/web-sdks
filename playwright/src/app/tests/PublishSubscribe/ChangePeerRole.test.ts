0541c35e456290b28d62b4cefea93a34fce846ce
Ronit Roy <93121334+ronit100ms@users.noreply.github.com>
2022-08-31 14:03:40 +0530

    test: removed beam tests (#885)

--- playwright/src/app/tests/PublishSubscribe/ChangePeerRole.test.ts
+++ playwright/src/app/tests/PublishSubscribe/ChangePeerRole.test.ts
@@ -1,7 +1,7 @@
 import { PageWrapper } from '../../PageWrapper';
 import { test } from '@playwright/test';
 
-test(`Change peer Role`, async ({ context }) => {
+test.skip(`Change peer Role`, async ({ context }) => {
   const pages = await PageWrapper.openPages(context, 2);
 
   for (let i = 0; i < pages[0].header.role_list.length; i++) {
