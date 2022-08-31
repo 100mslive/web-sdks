d96e595bc7c3443897d55200ed8f21154ea68ce6
raviteja83 <ravi.theja@100ms.live>
2022-08-31 16:19:56 +0530

    feat: upgrade svgr cli

--- packages/react-icons/src/BranchIcon.tsx
+++ packages/react-icons/src/BranchIcon.tsx
@@ -1,16 +1,15 @@
 import * as React from 'react';
+import { SVGProps } from 'react';
 
-function SvgBranchIcon(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M8 4.8a.8.8 0 00-1.6 0v8.902a3.2 3.2 0 101.637.01 6.4 6.4 0 015.674-5.675 3.2 3.2 0 10-.017-1.606A8 8 0 008 9.6V4.8zm-.8 10.4a1.6 1.6 0 100 3.2 1.6 1.6 0 000-3.2zm9.6-1.6a.8.8 0 01.8.8V16h1.6a.8.8 0 010 1.6h-1.6v1.6a.8.8 0 01-1.6 0v-1.6h-1.6a.8.8 0 010-1.6H16v-1.6a.8.8 0 01.8-.8z"
-        fill="currentColor"
-      />
-    </svg>
-  );
-}
+const SvgBranchIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M8 4.8a.8.8 0 0 0-1.6 0v8.902a3.2 3.2 0 1 0 1.637.01 6.4 6.4 0 0 1 5.674-5.675 3.2 3.2 0 1 0-.017-1.606A8 8 0 0 0 8 9.6V4.8Zm-.8 10.4a1.6 1.6 0 1 0 0 3.2 1.6 1.6 0 0 0 0-3.2Zm9.6-1.6a.8.8 0 0 1 .8.8V16h1.6a.8.8 0 0 1 0 1.6h-1.6v1.6a.8.8 0 0 1-1.6 0v-1.6h-1.6a.8.8 0 0 1 0-1.6H16v-1.6a.8.8 0 0 1 .8-.8Z"
+      fill="currentColor"
+    />
+  </svg>
+);
 
 export default SvgBranchIcon;
--- packages/react-icons/src/CalendarIcon.tsx
+++ packages/react-icons/src/CalendarIcon.tsx
@@ -1,16 +1,15 @@
 import * as React from 'react';
+import { SVGProps } from 'react';
 
-function SvgCalendarIcon(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M16.273 3.818a.818.818 0 10-1.637 0v.818H9.727v-.818a.818.818 0 00-1.636 0v.818H6.455A2.455 2.455 0 004 7.091V18.545A2.455 2.455 0 006.455 21h11.454a2.454 2.454 0 002.455-2.455V7.091a2.455 2.455 0 00-2.455-2.455h-1.636v-.818zm2.454 5.727V7.091a.818.818 0 00-.818-.818h-1.636v.818a.818.818 0 11-1.637 0v-.818H9.727v.818a.818.818 0 01-1.636 0v-.818H6.455a.818.818 0 00-.819.818v2.454h13.091zm-13.09 1.637h13.09v7.363a.818.818 0 01-.818.819H6.455a.818.818 0 01-.819-.819v-7.363z"
-        fill="currentColor"
-      />
-    </svg>
-  );
-}
+const SvgCalendarIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M16.273 3.818a.818.818 0 1 0-1.637 0v.818H9.727v-.818a.818.818 0 0 0-1.636 0v.818H6.455A2.455 2.455 0 0 0 4 7.091V18.545A2.455 2.455 0 0 0 6.455 21h11.454a2.454 2.454 0 0 0 2.455-2.455V7.091a2.455 2.455 0 0 0-2.455-2.455h-1.636v-.818Zm2.454 5.727V7.091a.818.818 0 0 0-.818-.818h-1.636v.818a.818.818 0 1 1-1.637 0v-.818H9.727v.818a.818.818 0 0 1-1.636 0v-.818H6.455a.818.818 0 0 0-.819.818v2.454h13.091Zm-13.09 1.637h13.09v7.363a.818.818 0 0 1-.818.819H6.455a.818.818 0 0 1-.819-.819v-7.363Z"
+      fill="currentColor"
+    />
+  </svg>
+);
 
 export default SvgCalendarIcon;
