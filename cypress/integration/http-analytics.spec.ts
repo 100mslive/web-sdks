a76835f65ead9ce27609925a437451d57280c6bb
eswarclynn <esuclintblazer@gmail.com>
2022-09-02 17:10:32 +0530

    refactor(test): http-analytics spec using peer

--- cypress/integration/http-analytics.spec.ts
+++ cypress/integration/http-analytics.spec.ts
@@ -1,17 +1,15 @@
-import { HMSSdk } from '../../packages/hms-video-web/src/sdk';
+import { CypressPeer } from '../support/peer';
 import { HTTPAnalyticsTransport } from '../../packages/hms-video-web/src/analytics/HTTPAnalyticsTransport';
-import { HMSPreviewListener } from '../../packages/hms-video-web/src/interfaces/preview-listener';
 import { CLIENT_ANAYLTICS_QA_ENDPOINT } from '../../packages/hms-video-web/src/utils/constants';
 import AnalyticsEventFactory from '../../packages/hms-video-web/src/analytics/AnalyticsEventFactory';
 import { ErrorFactory, HMSAction } from '../../packages/hms-video-web/src/error/ErrorFactory';
-import { HMSUpdateListener } from '../../packages/hms-video-web/src';
 
-let sdk: HMSSdk;
 let token: string;
 let invalidRoleToken: string;
-let initEndpoint: string;
-let previewListener: HMSPreviewListener;
-let joinListener: HMSUpdateListener;
+let localPeer: CypressPeer;
+const TAG = '[http-analytics-spec]';
+
+const logError = (...args) => console.error(TAG, ...args);
 
 describe('Http Analytics tests', () => {
   before(() => {
@@ -23,33 +21,6 @@ describe('Http Analytics tests', () => {
     });
   });
   beforeEach(() => {
-    sdk = new HMSSdk();
-    initEndpoint = Cypress.env('CYPRESS_INIT_ENDPOINT');
-    previewListener = {
-      onPreview: cy.stub().as('onPreview'),
-      onRoomUpdate: cy.stub().as('onRoomUpdate'),
-      onError: cy.stub().as('onError'),
-      onDeviceChange: cy.stub().as('onDeviceChange'),
-      onPeerUpdate: cy.stub().as('onPeerUpdate'),
-      onReconnecting: cy.stub().as('onReconnecting'),
-      onReconnected: cy.stub().as('onReconnected'),
-    };
-    joinListener = {
-      onJoin: cy.stub().as('onJoin'),
-      onRoomUpdate: cy.stub().as('onJoinRoomUpdate'),
-      onPeerUpdate: cy.stub().as('onJoinPeerUpdate'),
-      onError: cy.stub().as('onJoinError'),
-      onDeviceChange: cy.stub().as('onJoinDeviceChange'),
-      onReconnecting: cy.stub().as('onJoinReconnecting'),
-      onReconnected: cy.stub().as('onJoinReconnected'),
-      onTrackUpdate: cy.stub().as('onTrackUpdate'),
-      onMessageReceived: cy.stub().as('onMessageReceived'),
-      onRoleChangeRequest: cy.stub().as('onRoleChangeRequest'),
-      onRoleUpdate: cy.stub().as('onRoleUpdate'),
-      onChangeTrackStateRequest: cy.stub().as('onChangeTrackStateRequest'),
-      onChangeMultiTrackStateRequest: cy.stub().as('onChangeMultiTrackStateRequest'),
-      onRemovedFromRoom: cy.stub().as('onRemovedFromRoom'),
-    };
     cy.spy(HTTPAnalyticsTransport, 'sendEvent').as('sendEvent');
     cy.spy(HTTPAnalyticsTransport, 'setEnv').as('setEnv');
     //@ts-ignore
@@ -57,10 +28,11 @@ describe('Http Analytics tests', () => {
   });
 
   it('should not call http analytics with valid role token', () => {
-    //@ts-ignore
-    sdk.preview({ userName: 'test', authToken: token, initEndpoint }, previewListener);
-    cy.get('@setEnv').should('be.calledOnce');
-    cy.get('@sendEvent').should('not.be.called');
+    localPeer = new CypressPeer(token);
+    localPeer.preview().then(() => {
+      cy.get('@setEnv').should('be.calledOnce');
+      cy.get('@sendEvent').should('not.be.called');
+    });
   });
 
   it('should call http analytics with invalid role token', () => {
@@ -70,13 +42,17 @@ describe('Http Analytics tests', () => {
     );
     //@ts-ignore
     window.HMS = { CLIENT_EVENTS: true };
-    //@ts-ignore
-    sdk.preview({ userName: 'test', authToken: invalidRoleToken, initEndpoint }, previewListener).catch(console.error);
-    cy.get('@onError').should('be.calledOnce');
-    cy.get('@setEnv').should('be.calledOnce');
-    // sendEvent for connect and preview
-    cy.get('@sendEvent').should('be.calledTwice');
-    cy.get('@addEventToStorage').should('not.be.called');
+    localPeer = new CypressPeer(invalidRoleToken);
+    localPeer
+      .preview()
+      .catch(logError)
+      .then(() => {
+        cy.get('@onError').should('be.calledOnce');
+        cy.get('@setEnv').should('be.calledOnce');
+        // sendEvent for connect and preview
+        cy.get('@sendEvent').should('be.calledTwice');
+        cy.get('@addEventToStorage').should('not.be.called');
+      });
   });
 
   it('should store in storage if analytics fails and send again on preview', () => {
@@ -84,24 +60,28 @@ describe('Http Analytics tests', () => {
       { url: CLIENT_ANAYLTICS_QA_ENDPOINT, method: 'POST' },
       { statusCode: 500, statusText: 'Something went wrong' },
     );
-    //@ts-ignore
-    sdk.preview({ userName: 'test', authToken: invalidRoleToken, initEndpoint }, previewListener).catch(console.error);
-    cy.get('@onError').should('be.calledOnce');
-    cy.get('@setEnv').should('be.calledOnce');
-    cy.get('@sendEvent').should('be.calledTwice');
-    cy.get('@addEventToStorage')
-      .should('be.calledTwice')
+    localPeer = new CypressPeer(invalidRoleToken);
+    localPeer
+      .preview()
+      .catch(logError)
       .then(() => {
-        expect(localStorage.getItem('client-events')).to.not.equal(null);
-        const newSdk = new HMSSdk();
-        cy.intercept(
-          { url: CLIENT_ANAYLTICS_QA_ENDPOINT, method: 'POST' },
-          { statusCode: 200, statusText: 'Event recorded' },
-        );
-        //@ts-ignore
-        newSdk.preview({ userName: 'test', authToken: token, initEndpoint }, previewListener).catch(console.error);
-        // 2 for new sdk connect/preview success, 2 from storage for old sdk
-        cy.get('@sendEvent').should('have.callCount', 4);
+        cy.get('@onError').should('be.calledOnce');
+        cy.get('@setEnv').should('be.calledOnce');
+        cy.get('@sendEvent').should('be.calledTwice');
+        cy.get('@addEventToStorage')
+          .should('be.calledTwice')
+          .then(() => {
+            expect(localStorage.getItem('client-events')).to.not.equal(null);
+            const remotePeer = new CypressPeer(token);
+            cy.intercept(
+              { url: CLIENT_ANAYLTICS_QA_ENDPOINT, method: 'POST' },
+              { statusCode: 200, statusText: 'Event recorded' },
+            );
+            remotePeer.preview().then(() => {
+              // 2 for new sdk connect/preview success, 2 from storage for old sdk
+              cy.get('@sendEvent').should('have.callCount', 4);
+            });
+          });
       });
   });
 
@@ -113,7 +93,9 @@ describe('Http Analytics tests', () => {
     const event = AnalyticsEventFactory.connect(ErrorFactory.InitAPIErrors.InitConfigNotAvailable(HMSAction.INIT));
     event.properties.token = token;
     localStorage.setItem('client-events', JSON.stringify([event]));
-    sdk.join({ userName: 'test', authToken: token, initEndpoint }, joinListener);
-    cy.get('@sendEvent').should('be.calledOnce');
+    localPeer = new CypressPeer(token);
+    localPeer.join().then(() => {
+      cy.get('@sendEvent').should('be.calledOnce');
+    });
   });
 });
