88b83d01b4abfcc24a22063077122177b6df47ce
mackarelfish <mackarelfish93@gmail.com>
2022-09-06 12:49:46 +0700

    fix: make size as type number

--- packages/react-ui/src/Loading/Loading.stories.tsx
+++ packages/react-ui/src/Loading/Loading.stories.tsx
@@ -6,6 +6,9 @@ import { Loading } from '.';
 export default {
   title: 'UI Components/Loading',
   component: Loading,
+  argTypes: {
+    size: { control: { type: 'number' }, defaultValue: 12 },
+  }
 } as ComponentMeta<typeof Loading>;
 
 const Template: ComponentStory<typeof Loading> = ({ size, color }) => {
