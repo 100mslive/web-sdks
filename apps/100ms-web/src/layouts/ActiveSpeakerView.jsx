9b9cf627e5316c946ff76dbf6f838f47664fad20
raviteja83 <ravi.theja@100ms.live>
2022-09-07 21:07:45 +0530

    fix: center blank for beam in active speaker

--- apps/100ms-web/src/layouts/ActiveSpeakerView.jsx
+++ apps/100ms-web/src/layouts/ActiveSpeakerView.jsx
@@ -4,21 +4,26 @@ import {
   useHMSStore,
   selectDominantSpeaker,
   selectLocalPeer,
+  selectRemotePeers,
 } from "@100mslive/react-sdk";
 import { Flex } from "@100mslive/react-ui";
 import { GridCenterView, GridSidePaneView } from "../components/gridView";
+import { useIsHeadless } from "../components/AppData/useUISettings";
 
-const ActiveSpeakerView = ({ showStats }) => {
-  const peers = useHMSStore(selectPeers);
+const ActiveSpeakerView = () => {
   const localPeer = useHMSStore(selectLocalPeer);
   const dominantSpeaker = useHMSStore(selectDominantSpeaker);
   const latestDominantSpeakerRef = useRef(dominantSpeaker);
+  const isHeadless = useIsHeadless();
+  const peers = useHMSStore(isHeadless ? selectRemotePeers : selectPeers);
   // if there is no current dominant speaker latest keeps pointing to last
   if (dominantSpeaker) {
     latestDominantSpeakerRef.current = dominantSpeaker;
   }
   // show local peer if there hasn't been any dominant speaker
-  const activeSpeaker = latestDominantSpeakerRef.current || localPeer;
+  const activeSpeaker =
+    latestDominantSpeakerRef.current ||
+    (isHeadless && peers.length > 0 ? peers[0] : localPeer);
   const showSidePane = activeSpeaker && peers.length > 1;
 
   return (
@@ -27,12 +32,10 @@ const ActiveSpeakerView = ({ showStats }) => {
         peers={[activeSpeaker]}
         maxTileCount={1}
         hideSidePane={!showSidePane}
-        showStatsOnTiles={showStats}
       />
       {showSidePane && (
         <GridSidePaneView
           peers={peers.filter(peer => peer.id !== activeSpeaker.id)}
-          showStatsOnTiles={showStats}
         />
       )}
     </Flex>
