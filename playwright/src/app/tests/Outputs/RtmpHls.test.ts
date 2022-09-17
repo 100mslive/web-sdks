aa626c3c0e77545f174dbb81c9d2f484680c34c2
ronitroy <ronit@100ms.live>
2022-09-18 01:46:31 +0530

    test: hls rtmp fixes

--- playwright/src/app/tests/Outputs/RtmpHls.test.ts
+++ playwright/src/app/tests/Outputs/RtmpHls.test.ts
@@ -17,49 +17,43 @@ test.afterEach(async () => {
   await page.close();
 });
 
-test.describe('Beam tests @beam', () => {
-  test(`Start and Stop Browser Recording`, async () => {
-    await page.click(page.header.start_recording_btn, page.header.start_recording_confirm_btn);
+test(`Start and Stop Browser Recording`, async () => {
+  await page.click(page.header.start_recording_btn, page.header.start_recording_confirm_btn);
 
-    await page.clickWithTimeout(
-      beamWaitTimeout,
-      page.header.stop_recording_btn,
-      page.header.stop_recording_confirm_btn,
-    );
-  });
+  await page.clickWithTimeout(beamWaitTimeout, page.header.stop_recording_btn, page.header.stop_recording_confirm_btn);
+});
 
-  test(`Start and Stop HLS`, async () => {
-    await page.click(page.header.go_live_btn, page.header.hls_stream_btn, page.header.start_hls_btn);
-    await page.clickWithTimeout(beamWaitTimeout, page.header.end_stream_btn, page.header.stop_hls_btn);
-  });
+test(`Start and Stop HLS`, async () => {
+  await page.click(page.header.go_live_btn, page.header.hls_stream_btn, page.header.start_hls_btn);
+  await page.clickWithTimeout(beamWaitTimeout, page.header.end_stream_btn, page.header.stop_hls_btn);
+});
 
-  test(`Start and Stop HLS Recording`, async () => {
-    await page.click(
-      page.header.go_live_btn,
-      page.header.hls_stream_btn,
-      page.header.hls_recording_toggle,
-      page.header.start_hls_btn,
-    );
-    await page.clickWithTimeout(beamWaitTimeout, page.header.end_stream_btn, page.header.stop_hls_btn);
-  });
+test(`Start and Stop HLS Recording`, async () => {
+  await page.click(
+    page.header.go_live_btn,
+    page.header.hls_stream_btn,
+    page.header.hls_recording_toggle,
+    page.header.start_hls_btn,
+  );
+  await page.clickWithTimeout(beamWaitTimeout, page.header.end_stream_btn, page.header.stop_hls_btn);
+});
 
-  test(`Start and Stop Rtmp`, async () => {
-    await page.click(page.header.go_live_btn, page.header.rtmp_stream_btn);
-    await page.sendText(page.header.rtmp_url_btn, process.env.twitch_rtmp_url);
-    await page.sendText(page.header.rtmp_key_btn, process.env.twitch_rtmp_key);
-    await page.click(page.header.start_rtmp_btn);
+test(`Start and Stop Rtmp`, async () => {
+  await page.click(page.header.go_live_btn, page.header.rtmp_stream_btn);
+  await page.sendText(page.header.rtmp_url_btn, process.env.twitch_rtmp_url);
+  await page.sendText(page.header.rtmp_key_btn, process.env.twitch_rtmp_key);
+  await page.click(page.header.start_rtmp_btn);
 
-    await page.clickWithTimeout(beamWaitTimeout, page.header.end_stream_btn, page.header.stop_rtmp_btn);
-  });
+  await page.clickWithTimeout(beamWaitTimeout, page.header.end_stream_btn, page.header.stop_rtmp_btn);
+});
 
-  test(`Start and Stop Rtmp Recording`, async () => {
-    await page.click(page.header.go_live_btn, page.header.rtmp_stream_btn);
+test(`Start and Stop Rtmp Recording`, async () => {
+  await page.click(page.header.go_live_btn, page.header.rtmp_stream_btn);
 
-    await page.sendText(page.header.rtmp_url_btn, process.env.twitch_rtmp_url);
-    await page.sendText(page.header.rtmp_key_btn, process.env.twitch_rtmp_key);
+  await page.sendText(page.header.rtmp_url_btn, process.env.twitch_rtmp_url);
+  await page.sendText(page.header.rtmp_key_btn, process.env.twitch_rtmp_key);
 
-    await page.click(page.header.rtmp_recording_btn, page.header.start_rtmp_btn);
+  await page.click(page.header.rtmp_recording_btn, page.header.start_rtmp_btn);
 
-    await page.clickWithTimeout(beamWaitTimeout, page.header.end_stream_btn, page.header.stop_rtmp_btn);
-  });
+  await page.clickWithTimeout(beamWaitTimeout, page.header.end_stream_btn, page.header.stop_rtmp_btn);
 });
