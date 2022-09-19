80b3a91db998bda561c88d7c66a85291fb597b38
Ronit Roy <93121334+ronit100ms@users.noreply.github.com>
2022-09-19 15:25:44 +0530

    Sdet rec (#940)

--- playwright/src/app/tests/Outputs/RtmpHls.test.ts
+++ playwright/src/app/tests/Outputs/RtmpHls.test.ts
@@ -16,18 +16,18 @@ test.afterEach(async () => {
   await page.close();
 });
 
-test(`Start and Stop Browser Recording`, async () => {
+test(`Start and Stop Browser Recording @qaonly`, async () => {
   await page.click(page.header.start_recording_btn, page.header.start_recording_confirm_btn);
 
   await page.clickWithTimeout(beamWaitTimeout, page.header.stop_recording_btn, page.header.stop_recording_confirm_btn);
 });
 
-test(`Start and Stop HLS`, async () => {
+test(`Start and Stop HLS @qaonly`, async () => {
   await page.click(page.header.go_live_btn, page.header.hls_stream_btn, page.header.start_hls_btn);
   await page.clickWithTimeout(beamWaitTimeout, page.header.end_stream_btn, page.header.stop_hls_btn);
 });
 
-test(`Start and Stop HLS Recording`, async () => {
+test(`Start and Stop HLS Recording @qaonly`, async () => {
   await page.click(
     page.header.go_live_btn,
     page.header.hls_stream_btn,
@@ -37,7 +37,7 @@ test(`Start and Stop HLS Recording`, async () => {
   await page.clickWithTimeout(beamWaitTimeout, page.header.end_stream_btn, page.header.stop_hls_btn);
 });
 
-test(`Start and Stop Rtmp`, async () => {
+test(`Start and Stop Rtmp @qaonly`, async () => {
   await page.click(page.header.go_live_btn, page.header.rtmp_stream_btn);
   await page.sendText(page.header.rtmp_url_btn, process.env.twitch_rtmp_url);
   await page.sendText(page.header.rtmp_key_btn, process.env.twitch_rtmp_key);
@@ -46,7 +46,7 @@ test(`Start and Stop Rtmp`, async () => {
   await page.clickWithTimeout(beamWaitTimeout, page.header.end_stream_btn, page.header.stop_rtmp_btn);
 });
 
-test(`Start and Stop Rtmp Recording`, async () => {
+test(`Start and Stop Rtmp Recording @qaonly`, async () => {
   await page.click(page.header.go_live_btn, page.header.rtmp_stream_btn);
 
   await page.sendText(page.header.rtmp_url_btn, process.env.twitch_rtmp_url);
