3611ad54b4b1b090778afd371a24c6b825c75363
Muhammad Zaki <muhammad.zaki93@ui.ac.id>
2022-10-12 21:09:31 +0700

    feat: useRemoteAVToggle example

---
+++ packages/react-ui/src/Video/UseRemoteAVToggle.stories.tsx
@@ -0,0 +1,28 @@
+import { useRemoteAVToggle } from '@100mslive/react-sdk';
+import Video, { StyledVideo } from './Video';
+import React from 'react';
+import { ComponentStory } from '@storybook/react';
+
+const VideoHook: ComponentStory<typeof StyledVideo> = () => {
+  const videoTrackId = '104';
+  const { isVideoEnabled, isAudioEnabled, toggleVideo, toggleAudio } = useRemoteAVToggle('1', videoTrackId);
+
+  return (
+    <div>
+      <button onClick={() => toggleVideo && toggleVideo()}>{isVideoEnabled ? 'Disable video' : 'Enable video'}</button>
+      <button onClick={() => toggleAudio && toggleAudio()}>{isAudioEnabled ? 'Disable audio' : 'Enable audio'}</button>
+      <Video css={{ bg: '$backgroundDark' }} trackId={videoTrackId} />
+    </div>
+  );
+};
+
+const VideoStories = {
+  title: 'Hooks/useRemoteAVToggle',
+  component: VideoHook,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+};
+
+export default VideoStories;
+
+export const UseVideoHook = VideoHook.bind({});
+UseVideoHook.storyName = 'useRemoteAVToggle';
