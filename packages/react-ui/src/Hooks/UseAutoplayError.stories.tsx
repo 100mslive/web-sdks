6432ff2b399f5b8424130559c3b101d3be6400d1
amar-1995 <amar@100ms.live>
2022-10-19 02:37:53 +0530

    feat: added story for useautoplayError

--- packages/react-ui/src/Hooks/UseAutoplayError.stories.tsx
+++ packages/react-ui/src/Hooks/UseAutoplayError.stories.tsx
@@ -1,15 +1,83 @@
 import React from 'react';
+import { useAutoplayError } from '@100mslive/react-sdk';
+import { Dialog, Text, Button, Flex, HorizontalDivider, Box } from '@100mslive/react-ui';
 
-const VideoHook = () => {
-  return <></>;
+export const DialogContent = ({ title, closeable = true, children }) => {
+  return (
+    <Dialog.Portal>
+      <Dialog.Overlay />
+      <Dialog.Content css={{ width: 'min(600px, 100%)' }}>
+        <Dialog.Title>
+          <Flex justify="between">
+            <Flex align="center" css={{ mb: '$1' }}>
+              <Text variant="h6" inline>
+                {title}
+              </Text>
+            </Flex>
+            {closeable && <Dialog.DefaultClose data-testid="dialoge_cross_icon" />}
+          </Flex>
+        </Dialog.Title>
+        <HorizontalDivider css={{ mt: '0.8rem' }} />
+        <Box>{children}</Box>
+      </Dialog.Content>
+    </Dialog.Portal>
+  );
 };
 
-const VideoStories = {
+export const DialogRow = ({ justify = 'between', children }) => {
+  let finalCSS = {
+    margin: '$10 0',
+    w: '100%',
+  };
+  return (
+    <Flex align="center" justify={justify} css={finalCSS}>
+      {children}
+    </Flex>
+  );
+};
+
+const AutoplayErrorHook = () => {
+  const { error, resetError, unblockAudio } = useAutoplayError();
+  return (
+    <>
+      <Dialog.Root
+        open={!!error}
+        onOpenChange={value => {
+          if (!value) {
+            unblockAudio();
+          }
+          resetError();
+        }}
+      >
+        <DialogContent title="Autoplay Error" closeable={false}>
+          <DialogRow>
+            <Text variant="md">
+              The browser wants us to get a confirmation for playing the Audio. Please allow audio to proceed.
+            </Text>
+          </DialogRow>
+          <DialogRow justify="end">
+            <Button
+              variant="primary"
+              onClick={() => {
+                unblockAudio();
+                resetError();
+              }}
+            >
+              Allow Audio
+            </Button>
+          </DialogRow>
+        </DialogContent>
+      </Dialog.Root>
+    </>
+  );
+};
+
+const AutoplayErrorStories = {
   title: 'Hooks/useAutoplayError',
-  component: VideoHook,
+  component: AutoplayErrorHook,
 };
 
-export default VideoStories;
+export default AutoplayErrorStories;
 
-export const UseVideoHook = VideoHook.bind({});
-UseVideoHook.storyName = 'useAutoplayError';
+export const UseAutoplayErroHook = AutoplayErrorHook.bind({});
+AutoplayErrorStories.storyName = 'useAutoplayError';
