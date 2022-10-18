ec71727505a6bf4f79ae6d93aae2fe6885e09be8
Muhammad Zaki <muhammad.zaki93@ui.ac.id>
2022-10-18 23:08:22 +0700

    feat: stub foir useAutoPlayError hook

---
+++ packages/react-ui/src/Hooks/UseAutoplayError.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const VideoHook = () => {
+  return <></>;
+};
+
+const VideoStories = {
+  title: 'Hooks/useAutoplayError',
+  component: VideoHook,
+};
+
+export default VideoStories;
+
+export const UseVideoHook = VideoHook.bind({});
+UseVideoHook.storyName = 'useAutoplayError';
