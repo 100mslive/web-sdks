b3fb00e2d4d6e59b382631e5cb678f5c3dc91368
Muhammad Zaki <muhammad.zaki93@ui.ac.id>
2022-10-20 01:54:09 +0700

    feat: useAutoplayError

--- packages/react-ui/src/Hooks/UseAutoplayError.stories.tsx
+++ packages/react-ui/src/Hooks/UseAutoplayError.stories.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
-import { useAutoplayError } from '@100mslive/react-sdk';
+import { useAutoplayError, useHMSActions } from '@100mslive/react-sdk';
 import { Dialog, Text, Button, Flex, HorizontalDivider, Box } from '@100mslive/react-ui';
+import { HMSException } from '@100mslive/hms-video';
 
-const DialogContent = ({ title, closeable = true, children }) => {
+import { StoryBookSDK } from '../store/StorybookSDK';
+import mdx from './UseAutoplayError.mdx';
+
+const DialogContent = ({
+  title,
+  closeable = true,
+  children,
+}: {
+  title: string;
+  closeable: boolean;
+  children: React.ReactNode;
+}) => {
   return (
     <Dialog.Portal>
       <Dialog.Overlay />
@@ -24,7 +36,13 @@ const DialogContent = ({ title, closeable = true, children }) => {
   );
 };
 
-const DialogRow = ({ justify = 'between', children }) => {
+const DialogRow = ({
+  justify = 'between',
+  children,
+}: {
+  justify?: React.ComponentProps<typeof Flex>['justify'];
+  children: React.ReactNode;
+}) => {
   const finalCSS = {
     margin: '$10 0',
     w: '100%',
@@ -38,44 +56,63 @@ const DialogRow = ({ justify = 'between', children }) => {
 
 const UseAutoplayErrorHook = () => {
   const { error, resetError, unblockAudio } = useAutoplayError();
+
+  const actions = useHMSActions();
+  function sendAutoplayError() {
+    (actions as unknown as StoryBookSDK).sendError(
+      // @ts-ignore
+      new HMSException(3008, 'Autoplay error', 'NONE', 'Nothing has happened.', 'Nothing has happened.'),
+    );
+  }
+
   return (
-    <Dialog.Root
-      open={!!error}
-      onOpenChange={value => {
-        if (!value) {
-          unblockAudio();
-        }
-        resetError();
-      }}
-    >
-      <DialogContent title="Autoplay Error" closeable={false}>
-        <DialogRow>
-          <Text variant="md">
-            The browser wants us to get a confirmation for playing the Audio. Please allow audio to proceed.
-          </Text>
-        </DialogRow>
-        <DialogRow justify="end">
-          <Button
-            variant="primary"
-            onClick={() => {
-              unblockAudio();
-              resetError();
-            }}
-          >
-            Allow Audio
-          </Button>
-        </DialogRow>
-      </DialogContent>
-    </Dialog.Root>
+    <Box>
+      <Button variant="primary" onClick={() => sendAutoplayError()}>
+        Send Error
+      </Button>
+      <Dialog.Root
+        open={!!error}
+        onOpenChange={value => {
+          if (!value) {
+            unblockAudio();
+          }
+          resetError();
+        }}
+      >
+        <DialogContent title="Autoplay Error" closeable={false}>
+          <DialogRow>
+            <Text variant="md">
+              The browser wants us to get a confirmation for playing the Audio. Please allow audio to proceed.
+            </Text>
+          </DialogRow>
+          <DialogRow justify="end">
+            <Button
+              variant="primary"
+              onClick={() => {
+                unblockAudio();
+                resetError();
+              }}
+            >
+              Allow Audio
+            </Button>
+          </DialogRow>
+        </DialogContent>
+      </Dialog.Root>
+    </Box>
   );
 };
 
 const AutoplayErrorStories = {
   title: 'Hooks/useAutoplayError',
   component: UseAutoplayErrorHook,
+  parameters: {
+    docs: {
+      page: mdx,
+    },
+  },
 };
 
 export default AutoplayErrorStories;
 
 export const UseAutoplayErroHook = UseAutoplayErrorHook.bind({});
-AutoplayErrorStories.storyName = 'useAutoplayError';
+UseAutoplayErroHook.storyName = 'useAutoplayError';
