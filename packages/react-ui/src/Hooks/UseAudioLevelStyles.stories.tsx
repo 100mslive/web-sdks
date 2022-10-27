2fd45556a372e04fbe682a95de9e9959e653433f
Muhammad Zaki <muhammad.zaki93@ui.ac.id>
2022-10-27 16:19:55 +0700

    feat: added working example useAudioLevelStyles hook

--- packages/react-ui/src/Hooks/UseAudioLevelStyles.stories.tsx
+++ packages/react-ui/src/Hooks/UseAudioLevelStyles.stories.tsx
@@ -1,11 +1,42 @@
 import React from 'react';
-import { Box } from '../Layout';
-
+import { useBorderAudioLevel } from '../AudioLevel';
+import { Box, Flex } from '../Layout';
+import { selectTrackAudioByID, useHMSActions, useHMSStore } from '@100mslive/react-sdk';
+import { StoryBookSDK } from '../store/StorybookSDK';
+import { Slider } from '../Slider';
+import { Avatar } from '../Avatar';
+import { Text } from '../Text';
 import mdx from './UseAudioLevelStyles.mdx';
 
 const AudioLevelStyles = () => {
+  const trackId = '101';
+  const ref = useBorderAudioLevel(trackId);
+
+  // ignore: use to simulate changing speaker audio levels
+  const currVolume = useHMSStore(selectTrackAudioByID(trackId));
+  const actions = useHMSActions() as unknown as StoryBookSDK;
+
+  const username = 'John Doe';
   return (
     <Box>
+      <Flex
+        ref={ref}
+        justify="center"
+        align="center"
+        direction="column"
+        css={{ bg: '$bgSecondary', height: '300px', maxWidth: '300px', mb: '$4', r: '$2' }}
+      >
+        <Avatar name={username} css={{ width: '100px', height: '100px', mb: '$4' }} />
+        <Text>{username}</Text>
+      </Flex>
+      <Text>Set Audio Level</Text>
+      <Slider
+        defaultValue={[0]}
+        step={1}
+        value={[currVolume]}
+        onValueChange={e => actions.setAudioLevel(e[0], trackId)}
+        css={{ width: '300px' }}
+      />
     </Box>
   );
 };
