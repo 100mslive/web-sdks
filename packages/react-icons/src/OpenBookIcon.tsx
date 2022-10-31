80f7a95b02b9fe643d1418d0914127ef958a7385
Muhammad Zaki <muhammad.zaki93@ui.ac.id>
2022-10-31 16:51:38 +0700

    fix: svg fill to currentColor

--- packages/react-icons/src/OpenBookIcon.tsx
+++ packages/react-icons/src/OpenBookIcon.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { SVGProps } from 'react';
 
 const SvgOpenBookIcon = (props: SVGProps<SVGSVGElement>) => (
-  <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+  <svg width="24px" height="24px" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" {...props}>
     <path
       fillRule="evenodd"
       clipRule="evenodd"
