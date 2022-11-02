dddc67841ec03505941586fda652737f33a14f73
amar-1995 <amar@100ms.live>
2022-11-02 16:32:27 +0530

    fix: removed hms provider in main.js

--- packages/react-ui/.storybook/main.js
+++ packages/react-ui/.storybook/main.js
@@ -1,10 +1,26 @@
+
+const path = require('path');
+const webpack = require('webpack');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  core: {
+    builder: 'webpack5',
+  },
   addons: [
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        actions: false,
+        controls: false,
+        measure: false,
+        outline: false,
+        backgrounds: false
+      }
+    },
+    '@storybook/addon-controls',
     '@storybook/addon-interactions',
-    '@storybook/addon-a11y',
     'storybook-dark-mode'
   ],
   framework: '@storybook/react',
--- packages/react-ui/.storybook/preview.tsx
+++ packages/react-ui/.storybook/preview.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { HMSRoomProvider } from '@100mslive/react-sdk';
 import { useDarkMode } from 'storybook-dark-mode';
 import { themes } from '@storybook/theming';
 
-import { setUpFakeStore, storyBookSDK, storyBookStore } from '../src/store/SetupFakeStore';
-import { HMSThemeProvider, ThemeTypes } from '../src/Theme';
+import { setUpFakeStore } from '../src/store/SetupFakeStore';
 import {DecoratorFn} from '@storybook/react';
 
 export const parameters = {
@@ -30,12 +28,7 @@ export const decorators: DecoratorFn[] = [
   Story => {
     const isDark = useDarkMode();
     return (
-      //@ts-ignore
-      <HMSRoomProvider store={storyBookStore} actions={storyBookSDK}>
-        <HMSThemeProvider themeType={ isDark ? ThemeTypes.dark : ThemeTypes.light}>
-          {Story()}
-        </HMSThemeProvider>
-      </HMSRoomProvider>
+      <Story />
     );
   },
 ];
