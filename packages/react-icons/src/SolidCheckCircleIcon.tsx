dd3f72b22e69acb79d68eadd9e36d8e0864d0907
ritik <ritikagrawal0@gmail.com>
2022-11-03 20:33:35 +0530

    fix: add solidCheckIcon

---
+++ packages/react-icons/src/SolidCheckCircleIcon.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { SVGProps } from 'react';
+
+const SvgSolidCheckCircleIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M3 12a9 9 0 0 1 9-9 9 9 0 0 1 9 9 9 9 0 0 1-9 9 9 9 0 0 1-9-9Zm12.707-2.688c.39.417.39 1.092 0 1.509l-4 4.267a.956.956 0 0 1-1.414 0l-2-2.134a1.117 1.117 0 0 1 0-1.508.956.956 0 0 1 1.414 0L11 12.825l3.293-3.513a.956.956 0 0 1 1.414 0Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
+export default SvgSolidCheckCircleIcon;
