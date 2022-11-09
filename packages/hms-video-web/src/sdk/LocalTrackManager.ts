86c00850be2abfff5dcf5e0ca0a256d7ea8ec339
Tushar <tushar@100ms.live>
2022-11-09 18:18:25 +0530

    feat(sdk): ability to choose between screenshare modal tabs

--- packages/hms-video-web/src/sdk/LocalTrackManager.ts
+++ packages/hms-video-web/src/sdk/LocalTrackManager.ts
@@ -156,7 +156,7 @@ export class LocalTrackManager {
     const config = await this.getOrDefaultScreenshareConfig(partialConfig);
     const { video: videoSettings, audio: audioSettings } = this.getScreenshareSettings(config.videoOnly);
     const constraints = {
-      video: videoSettings.toConstraints(),
+      video: { ...videoSettings.toConstraints(), displaySurface: config.displaySurface },
       preferCurrentTab: config.preferCurrentTab,
       selfBrowserSurface: config.selfBrowserSurface,
       surfaceSwitching: config.surfaceSwitching,
@@ -527,6 +527,7 @@ export class LocalTrackManager {
         selfBrowserSurface: 'exclude', // don't give self tab in options
         surfaceSwitching: 'include', // give option to switch tabs while sharing
         systemAudio: 'exclude', // system audio share leads to echo in windows
+        displaySurface: 'monitor',
       },
       partialConfig || {},
     );
