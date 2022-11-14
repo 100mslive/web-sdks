3b17709c4ccb7844f68bd2bef5480f7290bcbe54
Ram <raghuram@100ms.live>
2022-11-14 11:41:03 +0530

    feat(app): make video player generic

--- apps/100ms-web/src/components/HMSVideo/PlayButton.jsx
+++ apps/100ms-web/src/components/HMSVideo/PlayButton.jsx
@@ -4,12 +4,12 @@ import { IconButton, Tooltip } from "@100mslive/react-ui";
 export const PlayButton = ({ onClick, isPaused }) => {
   return (
     <>
-      <Tooltip title={`${isPaused ? "Play" : "Pause"}`}>
+      <Tooltip title={`${isPaused ? "Pause" : "Play"}`}>
         <IconButton onClick={onClick} data-testid="play_pause_btn">
           {isPaused ? (
-            <PlayIcon width={32} height={32} />
-          ) : (
             <PauseIcon width={32} height={32} />
+          ) : (
+            <PlayIcon width={32} height={32} />
           )}
         </IconButton>
       </Tooltip>
