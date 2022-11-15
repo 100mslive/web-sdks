4695de30fa9ac16e2008b0752898ca1bce6d86e1
raviteja83 <ravi.theja@100ms.live>
2022-11-15 17:40:01 +0530

    fix: pass playlist track through loopback

--- packages/hms-video-web/src/playlist-manager/PlaylistAudioManager.ts
+++ packages/hms-video-web/src/playlist-manager/PlaylistAudioManager.ts
@@ -1,5 +1,6 @@
 import { AudioContextManager } from './AudioContextManager';
 import HMSLogger from '../utils/logger';
+import { RTCLoopback } from '../utils/rtcloopback';
 import { TypedEventEmitter } from '../utils/typed-event-emitter';
 
 /**
@@ -45,7 +46,9 @@ export class PlaylistAudioManager extends TypedEventEmitter<{ ended: null; progr
           // Create audio track only once and reuse, it will be updated with current content
           if (!this.track) {
             await this.audioElement.play();
-            const audioTrack = this.audioContextManager.getAudioTrack();
+            const audioTrack = await RTCLoopback.getInstance().processAudioFromTrack(
+              this.audioContextManager.getAudioTrack(),
+            );
             this.track = audioTrack;
             resolve([audioTrack]);
           } else {
