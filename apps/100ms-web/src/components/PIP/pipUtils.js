f29f43fca2f691e39d159d6f4beb246dc09b4a04
Ram <raghuram@100ms.live>
2022-11-20 21:06:34 +0530

    fix(app): fix aspect ratio of PIP when there are one and two tiles

--- apps/100ms-web/src/components/PIP/pipUtils.js
+++ apps/100ms-web/src/components/PIP/pipUtils.js
@@ -12,21 +12,61 @@ export function drawVideoElementsOnCanvas(videoElements, canvas) {
 
   const ctx = canvas.getContext("2d");
   ctx.fillStyle = "#000000";
-  const canvasWidth = canvas.width;
-  const canvasHeight = canvas.height;
+  let canvasWidth = canvas.width;
+  let canvasHeight = canvas.height;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   if (numberOfTiles === 0) {
     // no tile to render, render black image
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
     return;
-  }
-  // TODO: Remove case of 2 tiles after aspect-ratio issue is resolved.
-  else if (numberOfTiles === 1 || numberOfTiles === 2) {
+  } else if (numberOfTiles === 1) {
     // draw the video element on full canvas
-    ctx.drawImage(videoElements[0], 0, 0, canvasWidth, canvasHeight);
+    const video = videoElements[0];
+    const videoWidth = video.videoWidth;
+    const videoHeight = video.videoHeight;
+    /**
+     * crop the video to canvas by finding the
+     * difference between the video size and the
+     * canvas size and then crop the video by half
+     * that difference on each side.
+     */
+    const differenceInWidth = videoWidth - canvasWidth;
+    const differenceInHeight = videoHeight - canvasHeight;
+    const sX = differenceInWidth / 2;
+    const sY = differenceInHeight / 2;
+    ctx.drawImage(
+      video,
+      sX,
+      sY,
+      canvasWidth,
+      canvasHeight,
+      0,
+      0,
+      canvasWidth,
+      canvasHeight
+    );
+    return;
+  } else if (numberOfTiles === 2) {
+    // for two tiles, draw them as one over other to preserve aspect ratio.
+    const videoOne = videoElements[0];
+    const videoTwo = videoElements[1];
+    /**
+     * the canvas are of the same width as the videos and twice their height
+     * because we are stacking them one over the other.
+     */
+    canvas.width = videoOne.videoWidth;
+    canvas.height = videoOne.videoHeight + videoTwo.videoHeight;
+    ctx.drawImage(videoOne, 0, 0, videoOne.videoWidth, videoOne.videoHeight);
+    ctx.drawImage(
+      videoTwo,
+      0,
+      videoOne.videoHeight,
+      videoTwo.videoWidth,
+      videoTwo.videoHeight
+    );
     return;
   }
-
   // if there are more than 2 videos to show, split into two rows
   const numRows = numberOfTiles <= 2 ? 1 : 2;
   const numCols = Number(Math.ceil(numberOfTiles / numRows));
@@ -48,7 +88,7 @@ export function dummyChangeInCanvas(canvas) {
 
 /**
  * Imagine the canvas as a grid with passed in number of rows and columns. Go
- * over the the tiles in the grid in order while drawing the videoElements upon them.
+ * over the tiles in the grid in order while drawing the videoElements upon them.
  */
 function fillGridTiles(
   numRows,
