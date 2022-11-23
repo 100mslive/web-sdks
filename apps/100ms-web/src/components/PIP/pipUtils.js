576c32729a06632b232de3f69d1899d06e659e65
Ram <raghuram@100ms.live>
2022-11-23 10:43:52 +0530

    fix(app): fix aspect ratio issues in PIP

--- apps/100ms-web/src/components/PIP/pipUtils.js
+++ apps/100ms-web/src/components/PIP/pipUtils.js
@@ -1,7 +1,7 @@
 /**
  * no tile - blank canvas, black image
  * 1 tile - takes full space on canvas
- * 2 tile - vertically equally split
+ * 2 tile - stack one video on top of the other
  * 3 tile - two rows two columns - bottom right column is blank
  * 4 tiles - two rows two columns - all equal size
  */
@@ -25,55 +25,53 @@ export function drawVideoElementsOnCanvas(videoElements, canvas) {
     const video = videoElements[0];
     const videoWidth = video.videoWidth;
     const videoHeight = video.videoHeight;
-    /**
-     * crop the video to canvas by finding the
-     * difference between the video size and the
-     * canvas size and then crop the video by half
-     * that difference on each side.
-     */
-    const differenceInWidth = videoWidth - canvasWidth;
-    const differenceInHeight = videoHeight - canvasHeight;
-    const sX = differenceInWidth / 2;
-    const sY = differenceInHeight / 2;
-    ctx.drawImage(
-      video,
-      sX,
-      sY,
-      canvasWidth,
-      canvasHeight,
-      0,
-      0,
-      canvasWidth,
-      canvasHeight
-    );
+    canvas.width = videoWidth;
+    canvas.height = videoHeight;
+
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
     return;
   } else if (numberOfTiles === 2) {
     // for two tiles, draw them as one over other to preserve aspect ratio.
     const videoOne = videoElements[0];
     const videoTwo = videoElements[1];
     /**
-     * the canvas are of the same width as the videos and twice their height
-     * because we are stacking them one over the other.
+     * WebRTC Ramps up the video resolution than giving direct resolution
+     * immediately, so it takes a while for videos to reach full resolution,
+     * thus causing skewed aspect ratios/stretched videos. To circumvent that,
+     * we try to resize the canvas' dimensions by taking the biggest video
+     * of both and use that as a dimension for the canvas.
      */
-    canvas.width = videoOne.videoWidth;
-    canvas.height = videoOne.videoHeight + videoTwo.videoHeight;
+    canvas.width = Math.max(videoOne.videoWidth, videoTwo.videoWidth);
+    canvas.height = Math.max(videoOne.videoHeight, videoTwo.videoHeight) * 2;
+
     ctx.drawImage(videoOne, 0, 0, videoOne.videoWidth, videoOne.videoHeight);
     ctx.drawImage(
       videoTwo,
       0,
       videoOne.videoHeight,
-      videoTwo.videoWidth,
-      videoTwo.videoHeight
+      videoOne.videoWidth,
+      videoOne.videoHeight
     );
     return;
   }
   // if there are more than 2 videos to show, split into two rows
   const numRows = numberOfTiles <= 2 ? 1 : 2;
   const numCols = Number(Math.ceil(numberOfTiles / numRows));
-  const tileHeight = canvasHeight / numRows;
-  const tileWidth = canvasWidth / numCols;
-
-  fillGridTiles(numRows, numCols, tileHeight, tileWidth, videoElements, ctx);
+  const tileHeight = Math.max(
+    ...videoElements.map(videoEl => videoEl.videoHeight)
+  );
+  const tileWidth = Math.max(
+    ...videoElements.map(videoEl => videoEl.videoWidth)
+  );
+  fillGridTiles(
+    numRows,
+    numCols,
+    tileHeight,
+    tileWidth,
+    videoElements,
+    ctx,
+    canvas
+  );
 }
 
 // this is to send some data for stream and resolve video element's play for a
@@ -96,9 +94,12 @@ function fillGridTiles(
   tileHeight,
   tileWidth,
   videoElements,
-  ctx
+  ctx,
+  canvas
 ) {
   let videoElementPos = 0;
+  canvas.width = tileWidth * numCols;
+  canvas.height = tileHeight * numRows;
   for (let row = 0; row < numRows; row++) {
     const startY = row * tileHeight;
     for (let col = 0; col < numCols; col++) {
