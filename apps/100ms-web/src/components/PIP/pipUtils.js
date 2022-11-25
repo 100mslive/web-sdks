e49c2d0dcd4787f920679c11c336e6483e5cad53
Ram <raghuram@100ms.live>
2022-11-25 15:52:56 +0530

    fix(app): fix aspect ratio issues in PIP

--- apps/100ms-web/src/components/PIP/pipUtils.js
+++ apps/100ms-web/src/components/PIP/pipUtils.js
@@ -13,13 +13,11 @@ export function drawVideoElementsOnCanvas(videoElements, canvas) {
 
   const ctx = canvas.getContext("2d");
   ctx.fillStyle = "#000000";
-  let canvasWidth = canvas.width;
-  let canvasHeight = canvas.height;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   if (numberOfTiles === 0) {
     // no tile to render, render black image
-    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
     return;
   }
 
@@ -33,9 +31,7 @@ export function drawVideoElementsOnCanvas(videoElements, canvas) {
 export function dummyChangeInCanvas(canvas) {
   const ctx = canvas.getContext("2d");
   ctx.fillStyle = "#000000";
-  const canvasWidth = canvas.width;
-  const canvasHeight = canvas.height;
-  ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 /**
@@ -60,13 +56,48 @@ function fillGridTiles(
   for (let row = 0; row < numRows; row++) {
     for (let col = 0; col < numCols; col++) {
       const video = videoElements[videoElementPos];
+      /**
+       * there are two dimensions here. One is the
+       * dimension of the actual video and the other is
+       * the dimension of the tile where we render the video.
+       * Since we don't have control over what camera the user
+       * will use, it's impossible to render it in the tiles
+       * while still maintaining the aspect ratio.
+       *
+       * To circumvent that, we try to shrink either the width or height
+       * of the render tile based on the video's aspect ratio.
+       *
+       */
+      //the original aspect ratio of the video
       const originalAspectRatio = video.videoWidth / video.videoHeight;
+      // the aspect ratio of the tile we are going to render the video
       const renderTileAspectRatio = renderTileHeight / renderTileWidth;
       let renderVideoWidth = renderTileWidth;
       let renderVideoHeight = renderTileHeight;
+      /**
+       * Note: AspectRatio = width/height
+       * therefore,
+       * Width = aspectRatio * height;
+       * height = width / aspectRatio.
+       */
       if (originalAspectRatio > renderTileAspectRatio) {
+        /**
+         * if original aspect ratio is greater than the tile's aspect ratio,
+         * that means, we have to either shrink the height of the render
+         * tile or increase the width of the render tile to maintain the aspect ratio.
+         * Since we can't increase the tile size without affecting the
+         * size of the canvas itself, we are choosing to shrink
+         * the height.
+         */
         renderVideoHeight = renderTileWidth / originalAspectRatio;
       } else {
+        /**
+         * if the aspect ratio of original video is less than or equal
+         * to the tile's aspect ratio, then to maintain aspect ratio, we have to
+         * either shrink the width or increase the height. Since we
+         * can't increase the tile height without affecting the canvas height,
+         * we shrink the width.
+         */
         renderVideoWidth = renderTileHeight * originalAspectRatio;
       }
 
