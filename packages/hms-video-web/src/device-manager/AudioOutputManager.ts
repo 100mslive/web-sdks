33e5844315fa4611fd6ff13e09b8edb4a46ff1d7
eswarclynn <esuclintblazer@gmail.com>
2022-12-11 15:06:41 +0530

    fix: comment about resume context in unblock autoplay

--- packages/hms-video-web/src/device-manager/AudioOutputManager.ts
+++ packages/hms-video-web/src/device-manager/AudioOutputManager.ts
@@ -33,6 +33,11 @@ export class AudioOutputManager implements IAudioOutputManager {
 
   async unblockAutoplay() {
     await this.audioSinkManager.unblockAutoplay();
+    /**
+     * similar to autoplay error whent there's no user interaction,
+     * audio context is paused due to which empty audio tracks do not send any data and therefore it doesn't reach SFu.
+     * resume audio context on user interaction to enable empty audio tracks to send data and be forwarded to remote peers
+     */
     await HMSAudioContextHandler.resumeContext();
   }
 }
