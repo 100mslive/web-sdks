4f5b21d96c36f2237dceb41f76dea0604b242b50
amar-1995 <amar@100ms.live>
2022-12-12 19:44:24 +0530

    feat: added multi select dropdown ui

--- apps/100ms-web/src/components/MoreSettings/BulkRoleChangeModal.jsx
+++ apps/100ms-web/src/components/MoreSettings/BulkRoleChangeModal.jsx
@@ -1,47 +1,111 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import { useHMSActions } from "@100mslive/react-sdk";
-import { ChangeRoleIcon } from "@100mslive/react-icons";
-import { Button, Dialog } from "@100mslive/react-ui";
-import {
-  DialogContent,
-  DialogRow,
-  DialogSelect,
-} from "../../primitives/DialogContent";
+import { ChangeRoleIcon, CheckIcon } from "@100mslive/react-icons";
+import { Button, Dialog, Dropdown, Text } from "@100mslive/react-ui";
+import { DialogContent, DialogRow } from "../../primitives/DialogContent";
+import { DialogDropdownTrigger } from "../../primitives/DropdownTrigger";
 import { useFilteredRoles } from "../../common/hooks";
 
 export const BulkRoleChangeModal = ({ onOpenChange }) => {
   const roles = useFilteredRoles();
   const hmsActions = useHMSActions();
-  const [selectBulkRole, setBulkRole] = useState([]);
-  const [selectedRole, setRole] = useState();
+  const ref = useRef(null);
+  const roleRef = useRef(null);
+  const [selectedBulkRole, setBulkRole] = useState([]);
+  const [selectedRole, setRole] = useState("");
+  const [bulkRoleDialog, setBulkRoleDialog] = useState(false);
+  const [roleDialog, setRoleDialog] = useState(false);
 
   const changeBulkRole = useCallback(async () => {
-    await hmsActions.changeRoleOfPeersWithRoles({
-      roles: selectBulkRole,
-      toRole: selectedRole,
-    });
+    if (selectedBulkRole.length > 0 && selectedRole) {
+      await hmsActions.changeRoleOfPeersWithRoles(
+        selectedBulkRole,
+        selectedRole
+      );
+    }
     onOpenChange(false);
-  }, [selectedRole, selectBulkRole, hmsActions]);
+  }, [selectedBulkRole, selectedRole, onOpenChange, hmsActions]);
 
   return (
     <Dialog.Root defaultOpen onOpenChange={onOpenChange}>
       <DialogContent title="Bulk Role Change" Icon={ChangeRoleIcon}>
-        <DialogSelect
-          title="From Roles"
-          options={[...roles.map(role => ({ label: role, value: role }))]}
-          selected={setBulkRole}
-          keyField="value"
-          labelField="label"
-          onChange={selectBulkRole}
-        />
-        <DialogSelect
-          title="To Role"
-          options={[...roles.map(role => ({ label: role, value: role }))]}
-          selected={selectedRole}
-          keyField="value"
-          labelField="label"
-          onChange={setRole}
-        />
+        <DialogRow>
+          <Text>For Roles: </Text>
+          <Dropdown.Root
+            open={bulkRoleDialog}
+            onOpenChange={value => setBulkRoleDialog(value)}
+          >
+            <DialogDropdownTrigger
+              ref={ref}
+              title={
+                selectedBulkRole.length === 0
+                  ? "Select Multiple Roles"
+                  : selectedBulkRole.toString()
+              }
+              css={{
+                w: "70%",
+                p: "$8",
+              }}
+              open={bulkRoleDialog}
+            />
+            <Dropdown.Content
+              css={{ w: ref.current?.clientWidth, zIndex: 1000 }}
+            >
+              {roles &&
+                roles.map(role => {
+                  return (
+                    <Dropdown.CheckboxItem
+                      key={role}
+                      checked={selectedBulkRole.includes(role)}
+                      onCheckedChange={value =>
+                        setBulkRole(selection => {
+                          return value
+                            ? [...selection, role]
+                            : selection.filter(
+                                selectedRole => selectedRole !== role
+                              );
+                        })
+                      }
+                    >
+                      {role}
+                      <Dropdown.ItemIndicator>
+                        <CheckIcon />
+                      </Dropdown.ItemIndicator>
+                    </Dropdown.CheckboxItem>
+                  );
+                })}
+            </Dropdown.Content>
+          </Dropdown.Root>
+        </DialogRow>
+        <DialogRow>
+          <Text>To Role: </Text>
+          <Dropdown.Root
+            open={roleDialog}
+            onOpenChange={value => setRoleDialog(value)}
+          >
+            <DialogDropdownTrigger
+              ref={roleRef}
+              title={selectedRole || "Select Role"}
+              css={{
+                w: "70%",
+                p: "$8",
+              }}
+              open={roleDialog}
+            />
+            <Dropdown.Content
+              css={{ w: roleRef.current?.clientWidth, zIndex: 1000 }}
+            >
+              {roles &&
+                roles.map(role => {
+                  return (
+                    <Dropdown.Item key={role} onSelect={() => setRole(role)}>
+                      {role}
+                    </Dropdown.Item>
+                  );
+                })}
+            </Dropdown.Content>
+          </Dropdown.Root>
+        </DialogRow>
         <DialogRow justify="end">
           <Button variant="primary" onClick={changeBulkRole}>
             Apply
