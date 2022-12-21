5230f86438fcb70cd649eace2d2c59c717c9b643
raviteja83 <ravi.theja@100ms.live>
2022-12-21 12:18:47 +0530

    fix: move try catch to handle entire section

--- packages/hms-video-web/src/connection/HMSConnection.ts
+++ packages/hms-video-web/src/connection/HMSConnection.ts
@@ -111,11 +111,11 @@ export default abstract class HMSConnection {
      * For all peers joining after this peer, we have published and subscribed at the time of join itself
      * so we're able to log both publish and subscribe ice candidates.
      */
-    const transmitters = this.role === HMSConnectionRole.Publish ? this.getSenders() : this.getReceivers();
+    try {
+      const transmitters = this.role === HMSConnectionRole.Publish ? this.getSenders() : this.getReceivers();
 
-    transmitters.forEach(transmitter => {
-      const kindOfTrack = transmitter.track?.kind;
-      try {
+      transmitters.forEach(transmitter => {
+        const kindOfTrack = transmitter.track?.kind;
         if (transmitter.transport) {
           const iceTransport = transmitter.transport.iceTransport;
 
@@ -140,14 +140,14 @@ export default abstract class HMSConnection {
           }
           logSelectedCandidate();
         }
-      } catch (error) {
-        HMSLogger.w(
-          TAG,
-          `Error in logging selected ice candidate pair for ${HMSConnectionRole[this.role]} connection`,
-          error,
-        );
-      }
-    });
+      });
+    } catch (error) {
+      HMSLogger.w(
+        TAG,
+        `Error in logging selected ice candidate pair for ${HMSConnectionRole[this.role]} connection`,
+        error,
+      );
+    }
   }
 
   removeTrack(sender: RTCRtpSender) {
