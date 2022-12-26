89b1662982b277efdf8a66bbb3af5e609c878eb2
Ravi theja <raviteja83@users.noreply.github.com>
2022-12-26 14:39:53 +0530

    feat: add staging app check

--- apps/100ms-custom-app/src/App.js
+++ apps/100ms-custom-app/src/App.js
@@ -29,6 +29,10 @@ if (!hostname.endsWith('app.100ms.live')) {
   // route dev-app appropriately to qa or prod
   const envSuffix = process.env.REACT_APP_ENV === 'prod' ? 'app.100ms.live' : 'qa-app.100ms.live';
   hostname = hostname.replace('dev-app.100ms.live', envSuffix);
+} else if (hostname.endsWith('staging-app.100ms.live')) {
+  // route staging-app appropriately to qa or prod
+  const envSuffix = process.env.REACT_APP_ENV === 'prod' ? 'app.100ms.live' : 'qa-app.100ms.live';
+  hostname = hostname.replace('staging-app.100ms.live', envSuffix);
 } else if (hostname.endsWith('qa-app.100ms.live') && process.env.REACT_APP_ENV === 'prod') {
   hostname = hostname.replace('qa-app.100ms.live', 'app.100ms.live');
 }
