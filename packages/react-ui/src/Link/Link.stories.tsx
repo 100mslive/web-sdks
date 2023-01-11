66fb8aa1616366736557c2e00276fb543a8b1716
mackarelfish <mackarelfish93@gmail.com>
2023-01-11 20:09:26 +0700

    fix: storybook props

--- packages/react-ui/src/Link/Link.stories.tsx
+++ packages/react-ui/src/Link/Link.stories.tsx
@@ -5,12 +5,11 @@ import { Link } from '.';
 export default {
   title: 'UI Components/Link',
   component: Link,
-  parameters: {},
 } as ComponentMeta<typeof Link>;
 
 const Template: ComponentStory<typeof Link> = props => {
   return (
-    <Link icon="ExternalLinkIcon" iconSide="right" color="highEmp" href="#" target="_blank">
+    <Link icon={props.icon} iconSide={props.iconSide} color={props.color} href="#">
       Link Text
     </Link>
   );
