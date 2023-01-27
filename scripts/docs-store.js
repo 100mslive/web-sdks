fa3b81fe03fb0133f2b2ddb3ca746a09d5595e90
raviteja83 <ravi.theja@100ms.live>
2023-01-27 15:31:24 +0530

    fix: use typedoc.json for docs

--- scripts/docs-store.js
+++ scripts/docs-store.js
@@ -34,6 +34,8 @@ const replaceTitleWithMetadata = content => {
   let title = firstLineParts.at(2) || firstLineParts.at(1);
   if (title === '@100mslive/hms-video-store') {
     title = 'Web SDK API Reference'; // for the root content.mdx
+  } else if (title === '@100mslive/react-sdk') {
+    title = 'React Hooks API Reference'; // for the root content.
   }
   const meta = `---\ntitle: ${title}\n---`;
   lines.splice(0, 1, meta);
