c41f785ad4eb46d965ef8d00a8339d47767197ca
Hardik Maheshwari <93115614+hdz-666@users.noreply.github.com>
2023-02-01 14:48:58 +0530

    Update apps/100ms-web/src/layouts/mainGridView.jsx

--- apps/100ms-web/src/layouts/mainGridView.jsx
+++ apps/100ms-web/src/layouts/mainGridView.jsx
@@ -20,7 +20,6 @@ export const MainGridView = () => {
   const sidebarPeers = peers.filter(peer =>
     sidepaneRoles.includes(peer.roleName)
   );
-  console.log(sidebarPeers, centerPeers);
   /**
    * If there are peers from many publishing roles, then it's possible to divide
    * them into two parts, those who show in center and those who show in sidepane.
