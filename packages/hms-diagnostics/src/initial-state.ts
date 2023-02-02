918322ebc959a7c622a88f1e7ccbf4bf25345c0e
eswarclynn <esuclintblazer@gmail.com>
2023-02-02 18:15:13 +0530

    fix: id in initial state

--- packages/hms-diagnostics/src/initial-state.ts
+++ packages/hms-diagnostics/src/initial-state.ts
@@ -2,18 +2,18 @@ import { HMSDiagnosticsOutput } from './interfaces';
 
 export const initialState: HMSDiagnosticsOutput = {
   connectivity: {
-    stunUDP: { success: null, errorMessage: '' },
-    stunTCP: { success: null, errorMessage: '' },
-    turnUDP: { success: null, errorMessage: '' },
-    turnTCP: { success: null, errorMessage: '' },
-    init: { success: null, errorMessage: '' },
-    websocket: { success: null, errorMessage: '' },
+    stunUDP: { id: 'stunUDP', success: null, errorMessage: '' },
+    stunTCP: { id: 'stunTCP', success: null, errorMessage: '' },
+    turnUDP: { id: 'turnUDP', success: null, errorMessage: '' },
+    turnTCP: { id: 'turnTCP', success: null, errorMessage: '' },
+    init: { id: 'init', success: null, errorMessage: '' },
+    websocket: { id: 'websocket', success: null, errorMessage: '' },
   },
   devices: {
     success: null,
     errorMessage: '',
-    camera: { success: null, errorMessage: '' },
-    microphone: { success: null, errorMessage: '' },
+    camera: { id: 'camera', success: null, errorMessage: '' },
+    microphone: { id: 'microphone', success: null, errorMessage: '' },
   },
-  webRTC: { success: null, errorMessage: '' },
+  webRTC: { id: 'webRTC', success: null, errorMessage: '' },
 };
