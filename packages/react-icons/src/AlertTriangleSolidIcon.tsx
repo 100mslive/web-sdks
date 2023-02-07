cf4eee17b26fc3585c154101bf87698b937d5e4a
Ritik Agrawal <ritikagrawal0@gmail.com>
2023-02-07 10:43:35 +0530

    feat(ui): add collapsible component, extra icons

---
+++ packages/react-icons/src/AlertTriangleSolidIcon.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { SVGProps } from 'react';
+const SvgAlertTriangleSolidIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path
+      d="M5.1 20.1h13.79c1.363 0 2.242-1.01 2.242-2.25 0-.369-.097-.747-.29-1.098l-6.908-12.34A2.192 2.192 0 0 0 12 3.261c-.756 0-1.52.386-1.942 1.151l-6.9 12.34a2.32 2.32 0 0 0-.299 1.099c0 1.239.88 2.25 2.242 2.25Zm6.909-6.046c-.519 0-.818-.29-.835-.809l-.123-4.394c-.018-.545.369-.923.949-.923.563 0 .976.386.958.931l-.14 4.377c-.018.537-.308.818-.81.818Zm0 3.04c-.598 0-1.09-.43-1.09-1.01 0-.58.483-1.01 1.09-1.01.597 0 1.08.43 1.08 1.01 0 .589-.491 1.01-1.08 1.01Z"
+      fill="currentColor"
+    />
+  </svg>
+);
+export default SvgAlertTriangleSolidIcon;
