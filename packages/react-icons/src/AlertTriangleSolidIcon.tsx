e8efa0dcf72a288750a4ed2c9fca0942aa74081f
ritik <ritikagrawal0@gmail.com>
2023-02-07 19:30:20 +0530

    fix: add icons - money, card

--- packages/react-icons/src/AlertTriangleSolidIcon.tsx
+++ packages/react-icons/src/AlertTriangleSolidIcon.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { SVGProps } from 'react';
+
 const SvgAlertTriangleSolidIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
     <path
@@ -8,4 +9,5 @@ const SvgAlertTriangleSolidIcon = (props: SVGProps<SVGSVGElement>) => (
     />
   </svg>
 );
+
 export default SvgAlertTriangleSolidIcon;
