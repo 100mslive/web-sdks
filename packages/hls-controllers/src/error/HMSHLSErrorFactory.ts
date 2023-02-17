814f51456e3616f981f9de14aff6c53da55e8af3
amar-1995 <amar@100ms.live>
2023-02-17 06:35:18 +0530

    feat: added error handling

---
+++ packages/hls-controllers/src/error/HMSHLSErrorFactory.ts
@@ -0,0 +1,77 @@
+import { HMSHLSException } from './HMSHLSException';
+import { HMSHLSExceptionEvents } from '../utilies/constants';
+
+export type HLSErrorDetails = {
+  details: string;
+  fatal?: boolean;
+};
+export const HMSHLSErrorFactory = {
+  HLSNetworkError: {
+    manifestLoadError(data: HLSErrorDetails): HMSHLSException {
+      return new HMSHLSException(
+        HMSHLSExceptionEvents.MANIFEST_LOAD_ERROR,
+        data.details,
+        'Unable to load manifest file',
+        data.fatal,
+      );
+    },
+    nanifestParsingError(data: HLSErrorDetails): HMSHLSException {
+      return new HMSHLSException(
+        HMSHLSExceptionEvents.MANIFEST_PARSING_ERROR,
+        data.details,
+        'Unable to parse manifest file',
+        data.fatal,
+      );
+    },
+    levelLoadError(data: HLSErrorDetails): HMSHLSException {
+      return new HMSHLSException(
+        HMSHLSExceptionEvents.LEVEL_LOAD_ERROR,
+        data.details,
+        'Unable to load levels',
+        data.fatal,
+      );
+    },
+  },
+  HLSMediaError: {
+    manifestIncompatibleCodecsError(data: HLSErrorDetails): HMSHLSException {
+      return new HMSHLSException(
+        HMSHLSExceptionEvents.MANIFEST_INCOMPATIBLE_CODECS_ERROR,
+        data.details,
+        'Incompatible manifest codecs',
+        data.fatal,
+      );
+    },
+    fragDecryptError(data: HLSErrorDetails): HMSHLSException {
+      return new HMSHLSException(
+        HMSHLSExceptionEvents.FRAG_DECRYPT_ERROR,
+        data.details,
+        'Unable to decrypt fragment',
+        data.fatal,
+      );
+    },
+    bufferIncompatibleCodecsError(data: HLSErrorDetails): HMSHLSException {
+      return new HMSHLSException(
+        HMSHLSExceptionEvents.BUFFER_INCOMPATIBLE_CODECS_ERROR,
+        data.details,
+        'Incompatible buffer codecs',
+        data.fatal,
+      );
+    },
+    videoElementNotFound(): HMSHLSException {
+      return new HMSHLSException(
+        HMSHLSExceptionEvents.VIDEO_ELEMENT_NOT_FOUND,
+        'Video element not found',
+        'Video element not found',
+        false,
+      );
+    },
+  },
+  UnknownError: (data: HLSErrorDetails): HMSHLSException => {
+    return new HMSHLSException(
+      HMSHLSExceptionEvents.VIDEO_ELEMENT_NOT_FOUND,
+      data.details,
+      'Unknown error',
+      data.fatal,
+    );
+  },
+};
---
+++ packages/hls-controllers/src/utilies/constants.ts
@@ -0,0 +1,28 @@
+export const HLS_DEFAULT_ALLOWED_MAX_LATENCY_DELAY = 3;
+export const IS_OPTIMIZED = false;
+
+export enum HMSHLSControllerEvents {
+  HLS_TIMED_METADATA_LOADED = 'hls-timed-metadata',
+  HLS_STREAM_NO_LONGER_LIVE = 'hls-stream-no-longer-live',
+
+  HLS_PLAY = 'hls-play',
+  HLS_PAUSE = 'hls-pause',
+  HLS_CURRENT_TIME = 'hls-current-time',
+  HLS_AUTOPLAY_BLOCKED = 'hls-autoplay-blocked',
+
+  HLS_MANIFEST_LOADED = 'hls-manifest-loaded',
+  HLS_LEVEL_UPDATED = 'hls-level-updated',
+}
+
+export enum HMSHLSExceptionEvents {
+  MANIFEST_LOAD_ERROR = 'manifest-load-error',
+  MANIFEST_PARSING_ERROR = 'manifest-parsing-error',
+  LEVEL_LOAD_ERROR = 'level-load-error',
+
+  MANIFEST_INCOMPATIBLE_CODECS_ERROR = 'manifest-incompatible-codecs-error',
+  FRAG_DECRYPT_ERROR = 'frag-decrypt-error',
+  BUFFER_INCOMPATIBLE_CODECS_ERROR = 'buffer-incompatible-codecs-error',
+
+  VIDEO_ELEMENT_NOT_FOUND = 'video-element-not-found',
+  UNKNOWN_ERROR = 'unknown-error',
+}
