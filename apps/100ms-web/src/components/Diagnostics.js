2af54b856042dacb5bef740fd5fed66e3037b7c0
eswarclynn <esuclintblazer@gmail.com>
2023-03-01 16:10:26 +0530

    feat: display descriptions

--- apps/100ms-web/src/components/Diagnostics.js
+++ apps/100ms-web/src/components/Diagnostics.js
@@ -4,7 +4,8 @@ import { useSearchParam } from "react-use";
 import { HMSDiagnostics } from "@100mslive/hms-diagnostics";
 import { v4 } from "uuid";
 import { CheckIcon, CrossIcon } from "@100mslive/react-icons";
-import { Accordion, Button, Flex, Text } from "@100mslive/react-ui";
+import { Accordion, Flex, Text, VerticalDivider } from "@100mslive/react-ui";
+import { Logo } from "./Header/HeaderComponents";
 import { ErrorDialog } from "../primitives/DialogContent";
 import FullPageProgress from "./FullPageProgress";
 import { useTokenEndpoint } from "./AppData/useUISettings";
@@ -22,42 +23,74 @@ const DiagnosticsItem = ({ title, properties } = {}) => {
     return null;
   }
   return (
-    <Accordion.Item value={properties.id} css={{ p: "$4 $8" }}>
+    <Accordion.Item
+      value={properties.id}
+      css={{ p: "$4 $8", borderBottom: "1px solid $backgroundDefault" }}
+    >
       <Accordion.Header>
-        <Flex align="center">
-          <Text variant="body" css={{ mr: "$2" }}>
+        <Flex
+          align="center"
+          css={{ width: "100%", justifyContent: "space-between" }}
+        >
+          <Text variant="lg" css={{ mr: "$2" }}>
             {title}
           </Text>
           {properties.success === null ? null : properties.success ? (
-            <Text vairant="body2" css={{ color: "$green" }}>
+            <Flex css={{ color: "$success", alignItems: "center", mx: "$10" }}>
+              <Text variant="body2" css={{ color: "$success" }}>
+                Passed
+              </Text>
               <CheckIcon />
-            </Text>
+            </Flex>
           ) : (
-            <Text variant="body2" css={{ color: "$error" }}>
+            <Flex css={{ color: "$error", alignItems: "center", mx: "$10" }}>
+              <Text variant="body2" css={{ color: "$error" }}>
+                Failed
+              </Text>
               <CrossIcon />
-            </Text>
+            </Flex>
           )}
         </Flex>
       </Accordion.Header>
       <Accordion.Content>
-        {properties.errorMessage && (
-          <Text variant="body">Error: {properties.errorMessage}</Text>
-        )}
-        {properties.info && (
-          <Text variant="body" css={{ my: "$4" }}>
-            Info:
-            <pre>{JSON.stringify(properties.info, null, "\t")}</pre>
-          </Text>
-        )}
+        <Flex direction="column" css={{ overflowX: "auto" }}>
+          {properties.errorMessage && (
+            <Text variant="body" css={{ my: "$4" }}>
+              Error: {properties.errorMessage}
+            </Text>
+          )}
+          {properties.description && (
+            <Text variant="body" css={{ my: "$4" }}>
+              {properties.description}
+            </Text>
+          )}
+          {properties.info && (
+            <Text variant="body" css={{ my: "$4" }}>
+              Info:
+              <pre>{JSON.stringify(properties.info, null, "\t")}</pre>
+            </Text>
+          )}
+        </Flex>
       </Accordion.Content>
     </Accordion.Item>
+  );
+};
+
+const Header = () => {
+  return (
+    <Flex css={{ p: "$8" }}>
+      <Flex align="center">
+        <Logo />
+        <VerticalDivider css={{ mx: "$4" }} />
+        <Text variant="h4">Diagnostics</Text>
+      </Flex>
+    </Flex>
   );
 };
 
 const env = process.env.REACT_APP_ENV;
 const Diagnostics = () => {
   const [result, setResult] = useState([]);
-  const [inProgress, setInProgress] = useState(false);
   const tokenEndpoint = useTokenEndpoint();
   const { roomId: urlRoomId, role: userRole } = useParams(); // from the url
   const [token, setToken] = useState(null);
@@ -85,6 +118,26 @@ const Diagnostics = () => {
       });
   }, [tokenEndpoint, urlRoomId, userRole, authToken]);
 
+  useEffect(() => {
+    if (token) {
+      diagnostics.start(
+        {
+          authToken: token,
+          initEndpoint: `https://${env}-init.100ms.live/`,
+        },
+        {
+          onUpdate: (result, path) => {
+            setResult(res =>
+              res.concat({ ...result, title: path.split(".").at(-1) })
+            );
+          },
+        }
+      );
+    }
+  }, [token]);
+
+  useEffect(() => console.log(result), [result]);
+
   if (error.title) {
     return <ErrorDialog title={error.title}>{error.body}</ErrorDialog>;
   }
@@ -95,46 +148,32 @@ const Diagnostics = () => {
 
   return (
     <Flex direction="column" css={{ size: "100%", overflowY: "auto" }}>
-      <Button
-        disabled={inProgress}
-        css={{ width: 200, m: "$8" }}
-        onClick={async () => {
-          setResult([]);
-          setInProgress(true);
-          await diagnostics.start(
-            {
-              authToken: token,
-              initEndpoint: `https://${env}-init.100ms.live/`,
-            },
-            {
-              onUpdate: (result, path) => {
-                setResult(res =>
-                  res.concat({ ...result, title: path.split(".").at(-1) })
-                );
-              },
-            }
-          );
-        }}
-      >
-        Start Diagnostics
-      </Button>
+      <Header />
       {result && (
-        <Accordion.Root
-          type="single"
-          defaultValue="WebRTC"
-          collapsible
-          css={{ w: "50%", r: "$1", m: "$8" }}
+        <Flex
+          direction={{ "@md": "column" }}
+          css={{
+            w: "50%",
+            m: "$8",
+          }}
         >
-          {result.map(item => {
-            return (
-              <DiagnosticsItem
-                key={item.id}
-                title={item.title}
-                properties={item}
-              />
-            );
-          })}
-        </Accordion.Root>
+          <Accordion.Root
+            type="single"
+            defaultValue="WebRTC"
+            collapsible
+            css={{ w: "100%" }}
+          >
+            {result.map(item => {
+              return (
+                <DiagnosticsItem
+                  key={item.id}
+                  title={item.title}
+                  properties={item}
+                />
+              );
+            })}
+          </Accordion.Root>
+        </Flex>
       )}
     </Flex>
   );
