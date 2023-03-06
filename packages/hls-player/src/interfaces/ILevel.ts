e3a8ffa1482f10657939c669d5defeb1db312b06
amar-1995 <amar@100ms.live>
2023-03-07 01:00:35 +0530

    fix: removed unwanted code

--- packages/hls-player/src/interfaces/ILevel.ts
+++ packages/hls-player/src/interfaces/ILevel.ts
@@ -1,4 +1,4 @@
-import { AttrList, LevelParsed } from 'hls.js';
+import { LevelParsed } from 'hls.js';
 
 export declare interface ILevel extends Partial<LevelParsed> {
   readonly bitrate: number;
@@ -7,30 +7,4 @@ export declare interface ILevel extends Partial<LevelParsed> {
   readonly width?: number;
   url: string;
   resolution?: string;
-}
-
-export declare interface LevelAttributes extends AttrList {
-  'ALLOWED-CPC'?: string;
-  AUDIO?: string;
-  AUTOSELECT?: string;
-  'AVERAGE-BANDWIDTH'?: string;
-  BANDWIDTH?: string;
-  BYTERANGE?: string;
-  'CLOSED-CAPTIONS'?: string;
-  CHARACTERISTICS?: string;
-  CODECS?: string;
-  DEFAULT?: string;
-  FORCED?: string;
-  'FRAME-RATE'?: string;
-  'HDCP-LEVEL'?: string;
-  LANGUAGE?: string;
-  NAME?: string;
-  'PATHWAY-ID'?: string;
-  'PROGRAM-ID'?: string;
-  RESOLUTION?: string;
-  SCORE?: string;
-  SUBTITLES?: string;
-  TYPE?: string;
-  URI?: string;
-  'VIDEO-RANGE'?: string;
 }
