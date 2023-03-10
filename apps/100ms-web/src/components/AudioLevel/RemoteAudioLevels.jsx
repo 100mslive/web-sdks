76f203fcd1a71e8cd530c474a462ef6158315813
Tushar <tushar@100ms.live>
2023-03-10 19:56:59 +0530

    feat: move remote speakers logging to store

--- apps/100ms-web/src/components/AudioLevel/RemoteAudioLevels.jsx
+++ apps/100ms-web/src/components/AudioLevel/RemoteAudioLevels.jsx
@@ -1,19 +1,11 @@
 import { useEffect } from "react";
-import { useHMSActions, useHMSVanillaStore } from "@100mslive/react-sdk";
-import { remoteAudioLevels } from "./audioLevel";
+import { useHMSActions } from "@100mslive/react-sdk";
 
 export function RemoteAudioLevels() {
   const hmsActions = useHMSActions();
-  const vanillaStore = useHMSVanillaStore();
 
   useEffect(() => {
-    console.log("setting store actions");
-    remoteAudioLevels.setStoreActions(vanillaStore, hmsActions);
-    remoteAudioLevels.start();
-    return () => {
-      remoteAudioLevels.stop();
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    hmsActions.enableBeamSpeakerLabelsLogging();
+  }, [hmsActions]);
   return null;
 }
