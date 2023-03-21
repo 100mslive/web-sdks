e8d4665cfdbcaa84a92260a88196cef5878f4f8c
raviteja83 <ravi.theja@100ms.live>
2023-03-21 16:00:42 +0530

    feat: show background and image together

--- packages/hms-virtual-background/src/canvasUtils.ts
+++ packages/hms-virtual-background/src/canvasUtils.ts
@@ -41,7 +41,10 @@ export class CanvasHandler {
               vec4 background = texture2D(u_texture, v_texCoord);
               vec4 segmentation = texture2D(u_segmentation, v_texCoord);
               vec4 source = texture2D(u_input, v_texCoord);
-              gl_FragColor = segmentation;
+              if (segmentation.r == 1.0) 
+                  gl_FragColor = source;
+               else 
+                  gl_FragColor = background;
           }
           `;
 
