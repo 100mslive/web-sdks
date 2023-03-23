751f27c9b5c322186468f8296c4b8b1a80429a15
amar-1995 <amar@100ms.live>
2023-03-23 08:41:59 +0530

    fix: updated interface

--- packages/hls-player/src/interfaces/IHMSHLSPlayer.ts
+++ packages/hls-player/src/interfaces/IHMSHLSPlayer.ts
@@ -1,6 +1,16 @@
 import { ILevel } from './ILevel';
 interface IHMSHLSPlayer {
   /**
+   * @returns get html video element
+   */
+  getVideoElement(): HTMLVideoElement;
+
+  /**
+   * set video volumne
+   * @param { volume } - define volume in range [1,100]
+   */
+  setVolume(volume: number): void;
+  /**
    *
    * @returns returns a ILevel which represents current
    * quality level. -1 if currentlevel is set to "Auto"
@@ -16,6 +26,24 @@ interface IHMSHLSPlayer {
    * move the video to Live
    */
   seekToLivePosition(): void;
+  /**
+   * play stream
+   */
+  play(): void;
+  /**
+   * pause stream
+   */
+  pause(): void;
+
+  /**
+   * It will update the video element current time
+   * @param seekValue Pass currentTime in second
+   */
+  seekTo(seekValue: number): void;
+  /**
+   * unblock autoplay
+   */
+  unblockAutoPlay(): void;
 }
 
 export default IHMSHLSPlayer;
