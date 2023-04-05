62590afec1749e9cdf353a7e1b7c4178c3af8146
Eswar Prasad Clinton. A <64120992+eswarclynn@users.noreply.github.com>
2023-04-05 13:37:41 +0530

    fix: deprecate legacy session metadata (#1319)
    * fix: deprecate legacy session metadata
    
    * fix: optional value in set
    
    * docs: spelling in ts comments

--- packages/hms-video-store/src/core/schema/session-store.ts
+++ packages/hms-video-store/src/core/schema/session-store.ts
@@ -1,11 +1,11 @@
 export interface IHMSSessionStoreActions<T> {
-  set<K extends keyof T>(key: K, value: T[K]): Promise<void>;
+  set<K extends keyof T>(key: K, value?: T[K]): Promise<void>;
   /**
-   * observe a particular key or set of keys to receive udpates of its latest value when its changed
+   * observe a particular key or set of keys to receive updates of its latest value when its changed
    */
   observe(keys: keyof T | Array<keyof T>): Promise<void>;
   /**
-   * unobserve a particular key or set of keys to stop receiving udpates of its latest value
+   * unobserve a particular key or set of keys to stop receiving updates of its latest value
    */
   unobserve(keys: keyof T | Array<keyof T>): Promise<void>;
 }
