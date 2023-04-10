0602f29e377eeaede6c90654c6e29bb9d950091d
harshil-100ms <harshil@100ms.live>
2023-04-10 19:26:32 +0530

    fix: remove unused import

--- apps/100ms-web/src/components/MoreSettings/MoreSettings.jsx
+++ apps/100ms-web/src/components/MoreSettings/MoreSettings.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { Fragment, useState } from "react";
 import { useMedia } from "react-use";
 import Hls from "hls.js";
 import {
