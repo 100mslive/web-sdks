ed0e71ff7bb325d8e907cc74715939368a85f870
raviteja83 <ravi.theja@100ms.live>
2023-04-14 16:39:02 +0530

    fix: try calculating width on resize

--- apps/100ms-web/src/components/init/Init.jsx
+++ apps/100ms-web/src/components/init/Init.jsx
@@ -25,6 +25,7 @@ const Init = () => {
     function resetHeight() {
       // reset the body height to that of the inner browser
       document.body.style.height = `${window.innerHeight}px`;
+      document.body.style.width = `${window.innerWidth}px`;
     }
     // reset the height whenever the window's resized
     window.addEventListener("resize", resetHeight);
