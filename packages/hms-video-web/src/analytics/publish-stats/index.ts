1a00ef71b9928b20be452dd4fd9e3202ad964e97
eswarclynn <esuclintblazer@gmail.com>
2023-04-27 19:54:06 +0530

    feat: publish stats analytics

---
+++ packages/hms-video-web/src/analytics/publish-stats/index.ts
@@ -0,0 +1,140 @@
+import { BaseSample, TrackAnalytics, VideoSample } from './interfaces';
+import { EventBus } from '../../events/EventBus';
+import { HMSLocalTrackStats, HMSTrackStats, RTCRemoteInboundRtpStreamStats } from '../../interfaces';
+import { HMSWebrtcStats } from '../../rtc-stats';
+import { IStore } from '../../sdk/store';
+
+const SAMPLE_WINDOW = 30;
+
+export class PublishStatsAnalytics {
+  private trackAnalytics: Map<string, RunningTrackAnalytics> = new Map();
+
+  constructor(private store: IStore, private eventBus: EventBus) {
+    this.eventBus.statsUpdate.subscribe(hmsStats => this.handleStatsUpdate(hmsStats));
+  }
+
+  private handleStatsUpdate(hmsStats: HMSWebrtcStats) {
+    const localTracksStats = hmsStats.getLocalTrackStats();
+    Object.keys(localTracksStats).forEach(trackId => {
+      const trackStats = localTracksStats[trackId];
+      Object.keys(trackStats).forEach(statId => {
+        const layerStats = trackStats[statId];
+        const identifier = this.getTrackIdentifier(trackId, layerStats);
+        if (this.trackAnalytics.has(identifier)) {
+          this.trackAnalytics.get(identifier)?.push(layerStats);
+        } else {
+          const source = this.store.getTrackById(trackId)?.source!;
+          const trackAnalytics = new RunningTrackAnalytics({
+            trackId,
+            rid: layerStats.rid,
+            ssrc: layerStats.ssrc.toString(),
+            kind: layerStats.kind,
+            source,
+          });
+          this.trackAnalytics.set(identifier, trackAnalytics);
+        }
+      });
+    });
+  }
+
+  private getTrackIdentifier(trackId: string, stats: HMSTrackStats) {
+    return stats.rid ? `${trackId}:${stats.rid}` : trackId;
+  }
+}
+
+class RunningTrackAnalytics implements TrackAnalytics {
+  track_id: string;
+  ssrc: string;
+  source: string;
+  kind: string;
+  rid?: string;
+  samples: BaseSample[] = [];
+
+  private tempStats: HMSLocalTrackStats[] = [];
+
+  constructor({
+    trackId,
+    ssrc,
+    source,
+    rid,
+    kind,
+  }: {
+    trackId: string;
+    ssrc: string;
+    source: string;
+    kind: string;
+    rid?: string;
+  }) {
+    this.track_id = trackId;
+    this.ssrc = ssrc;
+    this.source = source;
+    this.rid = rid;
+    this.kind = kind;
+  }
+
+  push(stat: HMSLocalTrackStats) {
+    this.tempStats.push(stat);
+
+    if (this.shouldCreateSample()) {
+      this.samples.push(this.createSample());
+      this.tempStats.length = 0;
+    }
+  }
+
+  private createSample(): BaseSample | VideoSample {
+    return {
+      timestamp: Date.now(),
+      avg_bitrate: this.calculateAverage('bitrate'),
+      avg_fps: this.calculateAverage('framesPerSecond'),
+      avg_jitter: this.calculateAverage('jitter', true),
+      avg_round_trip_time: this.calculateAverage('roundTripTime', true),
+      total_packets_lost: this.calculateSum('packetsLost', true),
+      // @ts-expect-error
+      total_quality_limitation: this.getLatestStat().qualityLimitationDurations,
+      resolution:
+        this.kind === 'video'
+          ? {
+              height: this.getLatestStat().frameHeight,
+              width: this.getLatestStat().frameWidth,
+            }
+          : undefined,
+    };
+  }
+
+  private getLatestStat() {
+    return this.tempStats[this.tempStats.length - 1];
+  }
+
+  private shouldCreateSample() {
+    const length = this.tempStats.length;
+    const newStat = this.tempStats[length - 1];
+    const prevStat = this.tempStats[length - 2];
+
+    const hasResolutionChanged = (newStat: HMSLocalTrackStats, prevStat: HMSLocalTrackStats) =>
+      newStat &&
+      prevStat &&
+      (newStat.frameWidth !== prevStat.frameWidth || newStat.frameHeight !== prevStat.frameHeight);
+
+    return length === SAMPLE_WINDOW || (newStat.kind === 'video' && hasResolutionChanged(newStat, prevStat));
+  }
+
+  private calculateSum(key: keyof RTCRemoteInboundRtpStreamStats | keyof HMSLocalTrackStats, remote = false) {
+    const checkStat = remote
+      ? this.tempStats[0].remote?.[key as keyof RTCRemoteInboundRtpStreamStats]
+      : this.tempStats[0][key as keyof HMSLocalTrackStats];
+    if (typeof checkStat !== 'number') {
+      return;
+    }
+    return this.tempStats.reduce((partialSum, stat) => {
+      const value = ((remote
+        ? stat.remote?.[key as keyof RTCRemoteInboundRtpStreamStats]
+        : stat[key as keyof HMSLocalTrackStats]) || 0) as number;
+      return partialSum + value;
+    }, 0);
+  }
+
+  private calculateAverage(key: keyof RTCRemoteInboundRtpStreamStats | keyof HMSLocalTrackStats, remote = false) {
+    const sum = this.calculateSum(key, remote);
+    return sum ? sum / this.tempStats.length : undefined;
+  }
+}
--- packages/hms-video-web/src/analytics/publish-stats/interfaces.ts
+++ packages/hms-video-web/src/analytics/publish-stats/interfaces.ts
@@ -2,48 +2,42 @@ export interface PublishAnalyticPayload {
   sequence_num: number; // An increasing seq number starting from 1
   max_window_sec: number; // Take this value from the INIT response
   joined_at: number; // Local peer's joinedAt time in milliseconds
-  video: Array<VideoAnalytics>; // Each element represents a video track that the peer is uploading
-  audio: Array<AudioAnalytics>; // Each element represents an audio track that the peer is uploading
+  video: Array<TrackAnalytics>; // Each element represents a video track that the peer is uploading
+  audio: Array<TrackAnalytics>; // Each element represents an audio track that the peer is uploading
 }
 
 export interface TrackAnalytics {
   track_id: string; // Track id of the track
+  rid?: string; // rid of the track if simulcast is present, else null
   ssrc: string;
   source: string; // Source of the track - [regular, screen, playlist]
-}
-
-export interface AudioAnalytics extends TrackAnalytics {
-  audio_samples: Array<BaseSample>;
-}
-
-export interface VideoAnalytics extends TrackAnalytics {
-  rid?: string; // rid of the track if simulcast is present, else null
-  video_samples: Array<VideoSample>;
+  samples: Array<BaseSample | VideoSample>;
 }
 
 // One sample would contain the data of the last 30 seconds window
 export interface BaseSample {
   timestamp: number; // the ts at the end of the 30s window
-  avg_round_trip_time: number;
-  avg_jitter: number;
-  total_packets_lost: number;
-  avg_bitrate: number;
-  avg_available_outgoing_bitrate: number;
+  avg_round_trip_time?: number;
+  avg_jitter?: number;
+  total_packets_lost?: number;
+  avg_bitrate?: number;
+  avg_available_outgoing_bitrate?: number;
 }
 
 export interface VideoSample extends BaseSample {
-  total_quality_limitation: QualityLimitation;
-  avg_fps: number;
-  resolution: Resolution;
+  total_quality_limitation?: QualityLimitation;
+  avg_fps?: number;
+  resolution?: Resolution;
 }
 
 export interface QualityLimitation {
-  bandwith: number; // Total time in millis in the last 30s where this video track was limited by bandwidth
-  cpu: number; // Total time in millis in the last 30s where this video track was limited by CPU
-  none: number; // Total time in millis in the last 30s where this video track was not limited
+  bandwith?: number; // Total time in millis in the last 30s where this video track was limited by bandwidth
+  cpu?: number; // Total time in millis in the last 30s where this video track was limited by CPU
+  none?: number; // Total time in millis in the last 30s where this video track was not limited
+  other?: number;
 }
 
 export interface Resolution {
-  width: number; // the frame width in the last
-  height: number;
+  width?: number; // the frame width in the last
+  height?: number;
 }
