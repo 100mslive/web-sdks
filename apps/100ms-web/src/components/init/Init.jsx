d2bde8804b48e5c0b0e233e78906f5128b6b46f0
harshil-100ms <harshil@100ms.live>
2023-05-01 15:09:34 +0530

    fix: body height

--- apps/100ms-web/src/components/init/Init.jsx
+++ apps/100ms-web/src/components/init/Init.jsx
@@ -24,7 +24,9 @@ const Init = () => {
   useEffect(() => {
     function resetHeight() {
       // reset the body height to that of the inner browser
-      document.body.style.height = `${window.innerHeight}px`;
+      document.body.style.height = `${
+        window.visualViewport?.height || window.innerHeight
+      }px`;
     }
     // reset the height whenever the window's resized
     window.addEventListener("resize", resetHeight);
