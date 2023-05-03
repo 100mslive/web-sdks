a141904a1f3811b39d67ee910ae3f41f97afa8a7
harshil-100ms <harshil@100ms.live>
2023-05-03 14:51:20 +0530

    fix: test setting volume  through gain node

--- packages/hms-video-web/src/media/tracks/HMSAudioTrack.ts
+++ packages/hms-video-web/src/media/tracks/HMSAudioTrack.ts
@@ -8,17 +8,21 @@ export class HMSAudioTrack extends HMSTrack {
   readonly type: HMSTrackType = HMSTrackType.AUDIO;
   private audioElement: HTMLAudioElement | null = null;
   private outputDevice?: MediaDeviceInfo;
-  private volume: number | null = null;
+  private volume = 1;
+  private audioCtx: AudioContext | null = null;
+  private audioSource: MediaElementAudioSourceNode | null = null;
+  private gainNode: GainNode | null = null;
 
   constructor(stream: HMSMediaStream, track: MediaStreamTrack, source?: string) {
     super(stream, track, source as HMSTrackSource);
+    this.audioCtx = new AudioContext();
     if (track.kind !== 'audio') {
       throw new Error("Expected 'track' kind = 'audio'");
     }
   }
 
   getVolume() {
-    return this.audioElement ? this.audioElement.volume * 100 : null;
+    return this.gainNode ? this.gainNode.gain.value * 100 : null;
   }
 
   async setVolume(value: number) {
@@ -26,19 +30,31 @@ export class HMSAudioTrack extends HMSTrack {
       throw Error('Please pass a valid number between 0-100');
     }
     // Don't subscribe to audio when volume is 0
-    await this.subscribeToAudio(value === 0 ? false : this.enabled);
-    if (this.audioElement) {
-      this.volume = value / 100;
+    // await this.subscribeToAudio(value === 0 ? false : this.enabled);
+    if (this.audioElement && this.gainNode) {
       this.audioElement.volume = value / 100;
+      this.gainNode.gain.value = value / 100;
+      this.volume = value / 100;
     }
   }
   setAudioElement(element: HTMLAudioElement | null) {
     HMSLogger.d('[HMSAudioTrack]', this.logIdentifier, 'adding audio element', `${this}`, element);
     this.audioElement = element;
-    console.log('setAudioElement::volume::', this.audioElement?.volume, this.volume);
-    if (this.audioElement !== null && this.volume !== null) {
-      this.audioElement.volume = this.volume;
+    if (!this.audioElement || !this.audioCtx) {
+      return;
     }
+
+    // Create a MediaElementAudioSourceNode
+    // Feed the HTMLMediaElement into it
+    this.audioSource = this.audioCtx.createMediaElementSource(this.audioElement);
+    // Create a gain node
+    this.gainNode = this.audioCtx.createGain();
+
+    this.audioSource.connect(this.gainNode);
+    this.gainNode.connect(this.audioCtx.destination);
+    // this.audioElement.volume = this.volume;
+    // this.gainNode.gain.value = this.volume;
+    console.log('setAudioElement::volume::', this.audioElement?.volume, this.gainNode.gain.value, this.volume);
   }
   /**
    * @internal
