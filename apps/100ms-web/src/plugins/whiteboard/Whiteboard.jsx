1b965efa24a29f8b83d1d879ca56062ccfc2a246
amar-1995 <amar@100ms.live>
2023-05-19 14:25:30 +0530

    fix: move css to css file

--- apps/100ms-web/src/plugins/whiteboard/Whiteboard.jsx
+++ apps/100ms-web/src/plugins/whiteboard/Whiteboard.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Tldraw } from "@tldraw/tldraw";
-import { Box } from "@100mslive/react-ui";
 import { useMultiplayerState } from "./useMultiplayerState";
 import "./Whiteboard.css";
 
@@ -8,21 +7,13 @@ export const Whiteboard = React.memo(({ roomId }) => {
   const events = useMultiplayerState(roomId);
 
   return (
-    <Box
-      css={{
-        "#TD-PrimaryTools-Image": {
-          display: "none",
-        },
-      }}
-    >
-      <Tldraw
-        autofocus
-        disableAssets={true}
-        showSponsorLink={false}
-        showPages={false}
-        showMenu={false}
-        {...events}
-      />
-    </Box>
+    <Tldraw
+      autofocus
+      disableAssets={true}
+      showSponsorLink={false}
+      showPages={false}
+      showMenu={false}
+      {...events}
+    />
   );
 });
