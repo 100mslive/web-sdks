be46e0692e059a8d7ca34156c396b4ee91649da3
raviteja83 <ravi.theja@100ms.live>
2023-05-26 16:14:34 +0530

    refactor: convert few more files to ts

--- apps/100ms-web/src/components/AppData/useAppLayout.ts
+++ apps/100ms-web/src/components/AppData/useAppLayout.ts
@@ -1,6 +1,6 @@
 import { selectAppDataByPath, useHMSStore } from "@100mslive/react-sdk";
 import { APP_DATA } from "../../common/constants";
 
-export const useAppLayout = path => {
+export const useAppLayout = (path: string) => {
   return useHMSStore(selectAppDataByPath(APP_DATA.appLayout, path));
 };
