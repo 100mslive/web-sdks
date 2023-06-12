ebc2841b67837083715ef53c1431f24bab9ccb56
raviteja83 <ravi.theja@100ms.live>
2023-06-12 17:03:59 +0530

    fix: volume floating point

--- packages/hms-video-web/src/playlist-manager/PlaylistManager.ts
+++ packages/hms-video-web/src/playlist-manager/PlaylistManager.ts
@@ -132,7 +132,7 @@ export class PlaylistManager
   getVolume(type: HMSPlaylistType = HMSPlaylistType.audio): number {
     const element = this.getElement(type);
     if (element) {
-      return element.volume * 100;
+      return Math.round(element.volume * 100);
     }
     return 0;
   }
