a2200a35189fdccf7fb4485ef5d21a8d439b1157
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-06-15 10:30:12 +0530

    fix: radio options

--- apps/100ms-web/src/components/Polls/SingleChoiceOptions.jsx
+++ apps/100ms-web/src/components/Polls/SingleChoiceOptions.jsx
@@ -1,37 +1,45 @@
 import { Box, Flex, RadioGroup, Text } from "@100mslive/react-ui";
 import { ProgressBar } from "./OptionComponents/ProgressBar";
-import { VoteCount } from "./OptionComponents/VoteCount";
+import { Votes } from "./OptionComponents/Votes";
 
 export const SingleChoiceOptions = ({
   options = [
     { text: "A", voters: ["Alex Kar", "San France", "Rachel"] },
-    { text: "B", voters: [] },
+    { text: "B", voters: ["Boris Johnson", "James Franco"] },
     { text: "C", voters: [] },
   ],
   voted,
 }) => {
-  const totalVotes = 3;
-  return options.map((option, index) => (
-    <Flex
-      key={`${option.text}-${index}`}
-      css={{ w: "100%", gap: "$9", mb: "$md" }}
-    >
-      <RadioGroup.Root>
-        <RadioGroup.Item value="">
-          <RadioGroup.Indicator />
-        </RadioGroup.Item>
-      </RadioGroup.Root>
+  const totalVotes = 5;
+  return (
+    <RadioGroup.Root>
+      <Flex direction="column" css={{ gap: "$md", w: "100%", mb: "$md" }}>
+        {options.map((option, index) => (
+          <Flex
+            align="center"
+            key={`${option.text}-${index}`}
+            css={{ w: "100%", gap: "$9" }}
+          >
+            <RadioGroup.Item disabled={voted} value={option.text}>
+              <RadioGroup.Indicator />
+            </RadioGroup.Item>
 
-      <Box css={{ w: "100%" }}>
-        <Flex css={{ w: "100%" }}>
-          <Text css={{ display: "flex", flexGrow: "1" }}>{option.text}</Text>
-          {voted ? <VoteCount count={option.voters.length} /> : ""}
-          {/* AvatarList */}
-        </Flex>
-        {voted ? (
-          <ProgressBar w={`${(100 * option.voters.length) / totalVotes}%`} />
-        ) : null}
-      </Box>
-    </Flex>
-  ));
+            <Flex direction="column" css={{ flexGrow: "1" }}>
+              <Flex css={{ w: "100%", mb: voted ? "$4" : "0" }}>
+                <Text css={{ display: "flex", flexGrow: "1" }}>
+                  {option.text}
+                </Text>
+                {voted ? <Votes voters={option.voters || []} /> : ""}
+              </Flex>
+              {voted ? (
+                <ProgressBar
+                  w={`${(100 * option.voters.length) / totalVotes}%`}
+                />
+              ) : null}
+            </Flex>
+          </Flex>
+        ))}
+      </Flex>
+    </RadioGroup.Root>
+  );
 };
