837df6b3b72d57d4c6ddcc1501655c61c60a1a7f
hardik1003 <hardik@100ms.live>
2023-06-18 11:45:57 +0530

    fix: left sidepane

--- apps/100ms-web/src/layouts/SidePane.jsx
+++ apps/100ms-web/src/layouts/SidePane.jsx
@@ -9,6 +9,49 @@ import { APP_DATA, SIDE_PANE_OPTIONS } from "../common/constants";
 
 const SidePane = ({ css = {} }) => {
   const sidepane = useHMSStore(selectAppData(APP_DATA.sidePane));
+  const orientSidepaneToLeft = {
+    position: "absolute",
+    w: "$100",
+    h: "100%",
+    p: "$10",
+    bg: "$surfaceDefault",
+    r: "$1",
+    top: 0,
+    left: "$10",
+    zIndex: 10,
+    boxShadow: "$md",
+    ...css,
+    "@lg": {
+      w: "100%",
+      left: 0,
+      position: "fixed",
+      bottom: 0,
+      height: "unset",
+      ...(css["@lg"] || {}),
+    },
+  };
+
+  const orientSidepaneToRight = {
+    position: "absolute",
+    w: "$100",
+    h: "100%",
+    p: "$10",
+    bg: "$surfaceDefault",
+    r: "$1",
+    top: 0,
+    right: "$10",
+    zIndex: 10,
+    boxShadow: "$md",
+    ...css,
+    "@lg": {
+      w: "100%",
+      right: 0,
+      position: "fixed",
+      bottom: 0,
+      height: "unset",
+      ...(css["@lg"] || {}),
+    },
+  };
   let ViewComponent;
   if (sidepane === SIDE_PANE_OPTIONS.PARTICIPANTS) {
     ViewComponent = ParticipantList;
@@ -25,27 +68,11 @@ const SidePane = ({ css = {} }) => {
   }
   return (
     <Box
-      css={{
-        position: "absolute",
-        w: "$100",
-        h: "100%",
-        p: "$10",
-        bg: "$surfaceDefault",
-        r: "$1",
-        top: 0,
-        right: "$10",
-        zIndex: 10,
-        boxShadow: "$md",
-        ...css,
-        "@lg": {
-          w: "100%",
-          right: 0,
-          position: "fixed",
-          bottom: 0,
-          height: "unset",
-          ...(css["@lg"] || {}),
-        },
-      }}
+      css={
+        sidepane === SIDE_PANE_OPTIONS.WIDGET
+          ? orientSidepaneToLeft
+          : orientSidepaneToRight
+      }
     >
       <ViewComponent />
     </Box>
