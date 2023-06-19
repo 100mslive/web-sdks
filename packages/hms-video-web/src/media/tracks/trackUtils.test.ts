74c6a2bdfe425150b91f3c8a8ed9cc4870a3c4d3
raviteja83 <ravi.theja@100ms.live>
2023-06-19 17:45:08 +0530

    fix: simplify layer logic

--- packages/hms-video-web/src/media/tracks/trackUtils.test.ts
+++ packages/hms-video-web/src/media/tracks/trackUtils.test.ts
@@ -1,7 +1,7 @@
 import { getClosestLayer } from './trackUtils';
 import { HMSSimulcastLayer, HMSSimulcastLayerDefinition } from '../../interfaces';
 
-describe('test closest layer', () => {
+describe('test closest layer with 720p as high', () => {
   const layerDefinitions: HMSSimulcastLayerDefinition[] = [
     {
       layer: HMSSimulcastLayer.HIGH,
@@ -35,12 +35,74 @@ describe('test closest layer', () => {
   test('closest layer should be medium', () => {
     const width = 520;
     const height = 390;
-    expect(getClosestLayer(layerDefinitions, { width, height })).toBe(HMSSimulcastLayer.MEDIUM);
+    expect(getClosestLayer(layerDefinitions, { width, height })).toBe(HMSSimulcastLayer.HIGH);
   });
 
   test('closest layer should be low', () => {
     const width = 284;
     const height = 207;
-    expect(getClosestLayer(layerDefinitions, { width, height })).toBe(HMSSimulcastLayer.LOW);
+    expect(getClosestLayer(layerDefinitions, { width, height })).toBe(HMSSimulcastLayer.MEDIUM);
+  });
+
+  test('closest layer should be medium', () => {
+    const width = 360;
+    const height = 360;
+    expect(getClosestLayer(layerDefinitions, { width, height })).toBe(HMSSimulcastLayer.MEDIUM);
+  });
+
+  test('closest layer should be high', () => {
+    const width = 720;
+    const height = 1280;
+    expect(getClosestLayer(layerDefinitions, { width, height })).toBe(HMSSimulcastLayer.HIGH);
+  });
+});
+
+describe('test closest layer with 540p as high', () => {
+  const layerDefinitions: HMSSimulcastLayerDefinition[] = [
+    {
+      layer: HMSSimulcastLayer.HIGH,
+      resolution: {
+        width: 960,
+        height: 540,
+      },
+    },
+    {
+      layer: HMSSimulcastLayer.MEDIUM,
+      resolution: {
+        width: 426,
+        height: 240,
+      },
+    },
+    {
+      layer: HMSSimulcastLayer.LOW,
+      resolution: {
+        width: 160,
+        height: 90,
+      },
+    },
+  ];
+
+  test('closest layer should be high', () => {
+    const width = 730;
+    const height = 540;
+    expect(getClosestLayer(layerDefinitions, { width, height })).toBe(HMSSimulcastLayer.HIGH);
+  });
+
+  test('closest layer should be medium', () => {
+    const width = 520;
+    const height = 390;
+    expect(getClosestLayer(layerDefinitions, { width, height })).toBe(HMSSimulcastLayer.HIGH);
+  });
+
+  test('closest layer should be low', () => {
+    const width = 284;
+    const height = 207;
+    expect(getClosestLayer(layerDefinitions, { width, height })).toBe(HMSSimulcastLayer.MEDIUM);
+  });
+
+  test('closest layer should be high', () => {
+    const width = 360;
+    const height = 360;
+    expect(getClosestLayer(layerDefinitions, { width, height })).toBe(HMSSimulcastLayer.HIGH);
   });
 });
