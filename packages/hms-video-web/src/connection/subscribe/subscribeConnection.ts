0f71a57ddf6c7037c315353c310e1f4750c15899
raviteja83 <ravi.theja@100ms.live>
2023-06-20 13:51:10 +0530

    refactor: rename var

--- packages/hms-video-web/src/connection/subscribe/subscribeConnection.ts
+++ packages/hms-video-web/src/connection/subscribe/subscribeConnection.ts
@@ -135,8 +135,8 @@ export default class HMSSubscribeConnection extends HMSConnection {
   ): Promise<PreferLayerResponse> {
     const id = uuid();
     if (message.method === 'prefer-video-track-state') {
-      const isUnsubscribeFlagEnabled = this.isFlagEnabled(InitFlags.FLAG_VIDEO_TRACK_UNSUBSCRIBE);
-      if (!isUnsubscribeFlagEnabled && message.params.max_spatial_layer === HMSSimulcastLayer.NONE) {
+      const autoUnsubscribe = this.isFlagEnabled(InitFlags.FLAG_VIDEO_TRACK_AUTO_UNSUBSCRIBE);
+      if (!autoUnsubscribe && message.params.max_spatial_layer === HMSSimulcastLayer.NONE) {
         return { id } as PreferLayerResponse;
       }
     }
