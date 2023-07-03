51470d9a2e337edc952637b1452bcc1d0a3c94a5
raviteja83 <ravi.theja@100ms.live>
2023-07-03 12:53:11 +0530

    fix: duplicate entries

--- packages/hms-video-web/src/playlist-manager/PlaylistManager.ts
+++ packages/hms-video-web/src/playlist-manager/PlaylistManager.ts
@@ -65,7 +65,7 @@ export class PlaylistManager
       return;
     }
     list.forEach((item: HMSPlaylistItem<T>) => {
-      if (!this.state[item.type].list.includes(item)) {
+      if (!this.state[item.type].list.find(_item => _item.id === item.id)) {
         this.state[item.type].list.push(item);
       }
     });
