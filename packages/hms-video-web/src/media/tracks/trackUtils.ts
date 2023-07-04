607660d13982bfa312c0069713e406e7d63110c8
raviteja83 <ravi.theja@100ms.live>
2023-07-04 17:56:55 +0530

    revert: use previous logic for closest layer

--- packages/hms-video-web/src/media/tracks/trackUtils.ts
+++ packages/hms-video-web/src/media/tracks/trackUtils.ts
@@ -6,6 +6,7 @@ export const layerToIntMapping = {
   [HMSSimulcastLayer.MEDIUM]: 1,
   [HMSSimulcastLayer.HIGH]: 2,
 };
+const DELTA_THRESHOLD = 0.5;
 
 /**
  * Given the simulcast layers and the current video element dimensions, this function finds the
@@ -16,21 +17,30 @@ export const getClosestLayer = (
   simulcastLayers: HMSSimulcastLayerDefinition[],
   videoElementDimensions: { width: number; height: number },
 ): HMSPreferredSimulcastLayer => {
+  let closestLayer: HMSPreferredSimulcastLayer = HMSSimulcastLayer.HIGH;
+  // when both width and height are equal pick height to select a better quality
+  const maxDimension = videoElementDimensions.width > videoElementDimensions.height ? 'width' : 'height';
   const layers = [...simulcastLayers].sort((a, b) => layerToIntMapping[a.layer] - layerToIntMapping[b.layer]);
-  const v = Math.max(videoElementDimensions.height, videoElementDimensions.width);
-  let closestLayer = layers[0].layer;
-  for (let i = 1; i < layers.length; i++) {
-    const a = Math.max(layers[i - 1].resolution.width, layers[i - 1].resolution.height);
-    const b = Math.max(layers[i].resolution.width, layers[i].resolution.height);
-    const mid = Math.ceil(a + Math.abs(b - a) * 0.15);
-
-    if (v > mid) {
-      closestLayer = layers[i].layer;
-    } else {
+  const videoDimesion = videoElementDimensions[maxDimension] * (window?.devicePixelRatio || 1);
+  for (let i = 0; i < layers.length; i++) {
+    const { resolution, layer } = layers[i];
+    const layerDimension = resolution[maxDimension];
+    // we break here because the layers are already sorted, the next would always be greater if the below condition satisifes
+    if (videoDimesion <= layerDimension) {
+      closestLayer = layer;
       break;
+    } else {
+      const nextLayer = layers[i + 1];
+      const nextLayerDimension = nextLayer ? nextLayer.resolution[maxDimension] : Number.POSITIVE_INFINITY;
+      // calculating which layer this dimension is closer to
+      const proximityPercent = (videoDimesion - layerDimension) / (nextLayerDimension - layerDimension);
+      console.log({ proximityPercent, videoDimesion, layerDimension });
+      if (proximityPercent < DELTA_THRESHOLD) {
+        // the element's dimension is closer to the current layer
+        closestLayer = layer;
+        break;
+      }
     }
   }
-
-  console.log(`chose:${closestLayer} for ${videoElementDimensions.width}x${videoElementDimensions.height}`, layers);
   return closestLayer;
 };
