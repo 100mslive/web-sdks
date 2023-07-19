162e1b31cf5879d71b5db6f684e289d4ecacec87
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-07-19 10:07:31 +0530

    feat: bottom action sheet

--- packages/roomkit-react/src/Prebuilt/common/utils.js
+++ packages/roomkit-react/src/Prebuilt/common/utils.js
@@ -81,3 +81,11 @@ export const metadataPayloadParser = payload => {
     return { payload };
   }
 };
+
+// For bottom action sheet, returns updated height based on drag
+export const getUpdatedHeight = e => {
+  const heightToPercentage = 100 - ((e?.touches?.[0] || e).pageY / window.innerHeight) * 100;
+  // Snap to top if height > 80%, should be at least 40vh
+  const sheetHeightInVH = Math.max(40, heightToPercentage > 80 ? 100 : heightToPercentage);
+  return `${sheetHeightInVH}vh`;
+};
---
+++ packages/roomkit-react/src/Prebuilt/components/BottomActionSheet.jsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { CrossIcon } from '@100mslive/react-icons';
+import { Box, Flex, Popover, Text } from '../../';
+import { getUpdatedHeight } from '../common/utils';
+
+export const BottomSheet = ({
+  title = '',
+  children = <></>,
+  triggerContent,
+  containerCSS = {},
+  // By default the component starts just above the trigger.
+  // A negative offset allows it to start from the bottom of the screen.
+  sideOffset = -50,
+}) => {
+  const [sheetOpen, setSheetOpen] = useState(false);
+  const [sheetHeight, setSheetHeight] = useState('50vh');
+  const closeRef = useRef < HTMLDivElement > null;
+
+  // Close the sheet if height goes under 40vh
+  useEffect(() => {
+    if (closeRef?.current && parseFloat(sheetHeight.slice(0, -2)) <= 40) {
+      setSheetOpen(false);
+      // Delay for showing the opacity animation, can be removed if not needed
+      setTimeout(() => closeRef.current?.click(), 200);
+    }
+  }, [sheetHeight]);
+
+  return (
+    <>
+      <Popover.Root
+        onOpenChange={open => {
+          if (!open) {
+            setSheetHeight('0');
+          }
+          setSheetOpen(open);
+        }}
+      >
+        <Popover.Trigger asChild>{triggerContent}</Popover.Trigger>
+        <Popover.Portal>
+          <Popover.Content sideOffset={sideOffset} style={{ zIndex: '2' }}>
+            <Box
+              css={{
+                w: '100vw',
+                py: '$8',
+                opacity: sheetOpen ? '1' : '0.5',
+                h: sheetHeight,
+                minHeight: '50vh',
+                overflowY: 'auto',
+                backgroundColor: '$surfaceDefault',
+                transition: 'all 0.2s linear',
+                ...containerCSS,
+              }}
+            >
+              <Flex
+                justify="between"
+                onTouchMove={e => {
+                  const updatedSheetHeight = getUpdatedHeight(e);
+                  setSheetHeight(updatedSheetHeight);
+                }}
+                css={{
+                  borderBottom: '1px solid $borderLight',
+                  px: '$8',
+                  pb: '$4',
+                  mb: '$4',
+                  w: '100%',
+                }}
+              >
+                <Text variant="h6" css={{ color: '$textHighEmp' }}>
+                  {title}
+                </Text>
+                <Popover.Close aria-label="Close">
+                  <Box
+                    ref={closeRef}
+                    css={{
+                      color: '$textHighEmp',
+                      bg: '$surfaceLight',
+                      p: '$2',
+                      borderRadius: '$round',
+                    }}
+                  >
+                    <CrossIcon />
+                  </Box>
+                </Popover.Close>
+              </Flex>
+              <Box css={{ px: '$8' }}>{children}</Box>
+            </Box>
+          </Popover.Content>
+        </Popover.Portal>
+      </Popover.Root>
+    </>
+  );
+};
