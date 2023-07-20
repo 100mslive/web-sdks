2946fe5c0e45163c362110c286a78a779861ab2d
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-07-20 17:56:02 +0530

    fix: flip camera in mweb preview

--- packages/roomkit-react/src/Prebuilt/components/AudioVideoToggle.jsx
+++ packages/roomkit-react/src/Prebuilt/components/AudioVideoToggle.jsx
@@ -1,6 +1,5 @@
 import React, { Fragment } from 'react';
 import {
-  selectIsConnectedToRoom,
   selectLocalVideoTrackID,
   selectVideoTrackByID,
   useAVToggle,
@@ -18,7 +17,6 @@ export const AudioVideoToggle = () => {
   const actions = useHMSActions();
   const videoTracKId = useHMSStore(selectLocalVideoTrackID);
   const localVideoTrack = useHMSStore(selectVideoTrackByID(videoTracKId));
-  const isConnectedToRoom = useHMSStore(selectIsConnectedToRoom);
 
   return (
     <Fragment>
@@ -44,7 +42,7 @@ export const AudioVideoToggle = () => {
           </IconButton>
         </Tooltip>
       ) : null}
-      {localVideoTrack?.facingMode && isConnectedToRoom ? (
+      {localVideoTrack?.facingMode ? (
         <Tooltip title="Switch Camera" key="switchCamera">
           <IconButton
             onClick={async () => {
