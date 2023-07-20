395bc08bd6c1953b87a32eadf887247b8cb3d21c
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-07-20 10:38:41 +0530

    fix: utils func

--- packages/roomkit-react/src/Prebuilt/common/utils.js
+++ packages/roomkit-react/src/Prebuilt/common/utils.js
@@ -83,9 +83,9 @@ export const metadataPayloadParser = payload => {
 };
 
 // For bottom action sheet, returns updated height based on drag
-export const getUpdatedHeight = e => {
+export const getUpdatedHeight = (e, MINIMUM_HEIGHT) => {
   const heightToPercentage = 100 - ((e?.touches?.[0] || e).pageY / window.innerHeight) * 100;
   // Snap to top if height > 80%, should be at least 40vh
-  const sheetHeightInVH = Math.max(40, heightToPercentage > 80 ? 100 : heightToPercentage);
+  const sheetHeightInVH = Math.max(MINIMUM_HEIGHT, heightToPercentage > 80 ? 100 : heightToPercentage);
   return `${sheetHeightInVH}vh`;
 };
