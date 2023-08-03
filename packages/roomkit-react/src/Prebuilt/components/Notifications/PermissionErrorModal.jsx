bdf0880fa702a4db5ca5c1ebe7721ab0f2c5d151
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-08-03 16:18:03 +0530

    fix: permission popups for mweb

--- packages/roomkit-react/src/Prebuilt/components/Notifications/PermissionErrorModal.jsx
+++ packages/roomkit-react/src/Prebuilt/components/Notifications/PermissionErrorModal.jsx
@@ -1,11 +1,24 @@
 import React, { useEffect, useState } from 'react';
+import { useMedia } from 'react-use';
 import { HMSNotificationTypes, useHMSNotifications } from '@100mslive/react-sdk';
-import { Button, Dialog, Flex, Text } from '../../../';
+import { Button, config as cssConfig, Dialog, Flex, Text } from '../../../';
+import androidPermissions from '../../../assets/android-permission-0.png';
+import androidPermissionAlert from '../../../assets/android-permission-alert.png';
+import iosPermissions from '../../../assets/ios-permission-0.png';
+import { isAndroid, isIOS } from '../../common/constants';
 
 export function PermissionErrorModal() {
   const notification = useHMSNotifications(HMSNotificationTypes.ERROR);
   const [deviceType, setDeviceType] = useState('');
   const [isSystemError, setIsSystemError] = useState(false);
+  const [showAndroidPrompt, setShowAndroidPrompt] = useState(true);
+  const isMobile = useMedia(cssConfig.media.md);
+
+  useEffect(() => {
+    if (showAndroidPrompt && isAndroid && isMobile) {
+      setDeviceType('camera and microphone');
+    }
+  }, []);
 
   useEffect(() => {
     if (
@@ -21,13 +34,13 @@ export function PermissionErrorModal() {
     const hasVideo = errorMessage.includes('video');
     const hasScreen = errorMessage.includes('screen');
     if (hasAudio && hasVideo) {
-      setDeviceType('Camera and microphone');
+      setDeviceType('camera and microphone');
     } else if (hasAudio) {
-      setDeviceType('Microphone');
+      setDeviceType('microphone');
     } else if (hasVideo) {
-      setDeviceType('Camera');
+      setDeviceType('camera');
     } else if (hasScreen) {
-      setDeviceType('Screenshare');
+      setDeviceType('screen');
     }
     setIsSystemError(notification.data.code === 3011);
   }, [notification]);
@@ -39,25 +52,81 @@ export function PermissionErrorModal() {
         <Dialog.Content css={{ w: 'min(380px, 90%)', p: '$8' }}>
           <Dialog.Title
             css={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'space-between',
               borderBottom: '1px solid $border_default',
             }}
           >
-            <Text variant="h6">{deviceType} permissions are blocked</Text>
+            {isMobile && isIOS ? <img style={{ maxWidth: '100%', maxHeight: '100%' }} src={iosPermissions} /> : null}
+            {isMobile && isAndroid ? (
+              showAndroidPrompt ? (
+                <img src={androidPermissions} style={{ maxWidth: '100%', maxHeight: '100%' }} />
+              ) : (
+                <img src={androidPermissionAlert} style={{ maxWidth: '100%', maxHeight: '100%' }} />
+              )
+            ) : null}
+
+            <Text variant="h6">
+              {showAndroidPrompt ? `Allow access to your ${deviceType}` : `We can't access your ${deviceType}`}
+            </Text>
           </Dialog.Title>
           <Text variant="sm" css={{ pt: '$4', pb: '$10', color: '$on_surface_medium' }}>
-            Access to {deviceType} is required.&nbsp;
-            {isSystemError
-              ? `Enable permissions for ${deviceType} from sytem settings`
-              : `Enable permissions for ${deviceType} from address bar or browser settings.`}
+            {isMobile && isIOS
+              ? 'Enable permissions by reloading this page and clicking “Allow” on the pop-up, or change settings from the address bar.'
+              : null}
+            {isMobile && isAndroid
+              ? showAndroidPrompt
+                ? 'In order for others to see and hear you, your browser will request camera and microphone access.'
+                : 'To allow other users to see and hear you, click the blocked camera icon in your browser’s address bar.'
+              : null}
+
+            {!isMobile
+              ? `Access to ${deviceType} is required. ${
+                  isSystemError
+                    ? `Enable permissions for ${deviceType} from sytem settings`
+                    : `Enable permissions for ${deviceType} from address bar or browser settings.`
+                }`
+              : null}
           </Text>
-          <Flex justify="end" css={{ w: '100%' }}>
-            <Button outlined variant="standard" onClick={() => setDeviceType('')}>
-              Dismiss
-            </Button>
-          </Flex>
+          {isMobile && isIOS ? (
+            <>
+              <Button onClick={() => window.location.reload()} css={{ w: '100%', mb: '$6' }}>
+                Reload
+              </Button>
+              <Button outlined variant="standard" onClick={() => setDeviceType('')} css={{ w: '100%' }}>
+                Continue anyway
+              </Button>
+            </>
+          ) : null}
+
+          {isMobile && isAndroid ? (
+            showAndroidPrompt ? (
+              <Button
+                css={{ w: '100%' }}
+                onClick={() => {
+                  setDeviceType('');
+                  setShowAndroidPrompt(false);
+                }}
+              >
+                Continue
+              </Button>
+            ) : (
+              <>
+                <Button onClick={() => setDeviceType('')} css={{ w: '100%', mb: '$6' }}>
+                  I've allowed access
+                </Button>
+                <Button outlined variant="standard" onClick={() => setDeviceType('')} css={{ w: '100%' }}>
+                  Continue anyway
+                </Button>
+              </>
+            )
+          ) : null}
+
+          {!isMobile ? (
+            <Flex justify="end" css={{ w: '100%' }}>
+              <Button outlined variant="standard" onClick={() => setDeviceType('')}>
+                Dismiss
+              </Button>
+            </Flex>
+          ) : null}
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
