1c8505e1255cf208d2b1f771e397a5f043c60b6d
raviteja83 <ravi.theja@100ms.live>
2023-08-07 19:03:15 +0530

    feat: add existing pagination

--- packages/roomkit-react/src/Prebuilt/components/EqualProminence.jsx
+++ packages/roomkit-react/src/Prebuilt/components/EqualProminence.jsx
@@ -1,5 +1,8 @@
+import { useEffect, useState } from 'react';
 import { useMeasure } from 'react-use';
-import { getPeersWithTiles, selectPeers, selectTracksMap, useHMSStore } from '@100mslive/react-sdk';
+import { getPeersWithTiles, selectPeers, selectTracksMap, useHMSStore, useHMSVanillaStore } from '@100mslive/react-sdk';
+import { Flex } from '../../Layout';
+import { Pagination } from './Pagination';
 import VideoTile from './VideoTile';
 import { useUISettings } from './AppData/useUISettings';
 import { UI_SETTINGS } from '../common/constants';
@@ -8,24 +11,32 @@ const aspectRatios = [1 / 1, 4 / 3, 16 / 9];
 
 export function EqualProminence() {
   const peers = useHMSStore(selectPeers);
-  const tracksMap = useHMSStore(selectTracksMap);
-  const peersWithTiles = getPeersWithTiles(peers, tracksMap, () => false);
+  const vanillaStore = useHMSVanillaStore();
   const maxTileCount = useUISettings(UI_SETTINGS.maxTileCount);
-
+  const [pagesWithTiles, setPagesWithTiles] = useState([]);
+  const [page, setPage] = useState(0);
   const [ref, { width, height }] = useMeasure();
 
-  if (width > 0 && height > 0) {
+  useEffect(() => {
+    if (width === 0 || height === 0) {
+      return;
+    }
+    const tracksMap = vanillaStore.getState(selectTracksMap);
+    const peersWithTiles = getPeersWithTiles(peers, tracksMap, () => false);
     const maxPerPage = Math.min(peersWithTiles.length, maxTileCount);
     const pages = Math.ceil(peersWithTiles.length / maxPerPage);
+    let pagesList = [];
+    let index = 0;
     for (let i = 0; i < pages; i++) {
       let maxCols = Math.ceil(Math.sqrt(maxPerPage));
       let maxRows = Math.ceil(maxPerPage / maxCols);
+      // eslint-disable-next-line no-loop-func
       const matrix = new Array(maxRows).fill(null).map((_, i) => {
         const numCols = Math.min(maxCols, maxPerPage - i * maxCols);
         let rowElements = [];
 
         for (let j = 0; j < numCols; j++) {
-          rowElements.push(peersWithTiles[i * maxCols + j]);
+          rowElements.push(peersWithTiles[index++]);
         }
 
         return rowElements;
@@ -33,6 +44,7 @@ export function EqualProminence() {
 
       const maxHeight = height - (maxRows - 1) * 8;
       const maxRowHeight = maxHeight / matrix.length;
+      const pageList = [];
       for (const row of matrix) {
         let tileWidth = (width - (row.length - 1) * 8) / row.length;
         let tileHeight = 0;
@@ -59,39 +71,43 @@ export function EqualProminence() {
         for (let i = 0; i < row.length; i++) {
           row[i].width = tileWidth;
           row[i].height = tileHeight;
+          pageList.push(row[i]);
         }
       }
+      pagesList.push(pageList);
     }
-  }
-
-  console.log(peersWithTiles);
+    setPagesWithTiles(pagesList);
+  }, [width, height, maxTileCount, vanillaStore, peers, page]);
 
   return (
-    <div
-      ref={ref}
-      style={{
-        width: '100%',
-        height: '100%',
-        padding: 8,
-        gap: 8,
-        display: 'flex',
-        placeContent: 'center',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexFlow: 'row wrap',
-      }}
-    >
-      {peersWithTiles.map(tile => {
-        return (
-          <VideoTile
-            width={tile.width}
-            height={tile.height}
-            peerId={tile.peer?.id}
-            trackId={tile.track?.id}
-            rootCSS={{ padding: 0 }}
-          />
-        );
-      })}
-    </div>
+    <Flex direction="column" css={{ size: '100%' }}>
+      <div
+        ref={ref}
+        style={{
+          flex: '1 1 0',
+          padding: 8,
+          gap: 8,
+          display: 'flex',
+          placeContent: 'center',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexFlow: 'row wrap',
+        }}
+      >
+        {pagesWithTiles[page]?.map(tile => {
+          return (
+            <VideoTile
+              key={tile.track?.id || tile.peer?.id}
+              width={tile.width}
+              height={tile.height}
+              peerId={tile.peer?.id}
+              trackId={tile.track?.id}
+              rootCSS={{ padding: 0 }}
+            />
+          );
+        })}
+      </div>
+      {pagesWithTiles.length > 1 && <Pagination page={page} setPage={setPage} numPages={pagesWithTiles.length} />}
+    </Flex>
   );
 }
