6b7c399ddbdbdc7391e49cc46c681882862b0554
raviteja83 <ravi.theja@100ms.live>
2023-08-10 18:22:22 +0530

    fix: uncomment hls

--- packages/roomkit-react/src/Prebuilt/layouts/mainView.jsx
+++ packages/roomkit-react/src/Prebuilt/layouts/mainView.jsx
@@ -92,11 +92,11 @@ export const ConferenceMainView = () => {
       !isHLSRunning &&
       joinForm.join_btn_type === JoinForm_JoinBtnType.JOIN_BTN_TYPE_JOIN_AND_GO_LIVE
     ) {
-      // startHLS();
+      startHLS();
     }
 
     hmsActions.sessionStore.observe([SESSION_STORE_KEY.PINNED_MESSAGE, SESSION_STORE_KEY.SPOTLIGHT]);
-  }, [isConnected, hmsActions, permissions, joinForm, isHLSRunning]);
+  }, [isConnected, hmsActions, permissions, joinForm, isHLSRunning, startHLS]);
 
   if (!localPeerRole) {
     // we don't know the role yet to decide how to render UI
