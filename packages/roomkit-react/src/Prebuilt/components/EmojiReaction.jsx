2fd427d8e68c7d53a24c53c275956b12a718e945
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-08-14 13:10:59 +0530

    fix: emoji reaction populating chat

--- packages/roomkit-react/src/Prebuilt/components/EmojiReaction.jsx
+++ packages/roomkit-react/src/Prebuilt/components/EmojiReaction.jsx
@@ -1,20 +1,71 @@
-import React, { Fragment, useState } from 'react';
-import { selectIsConnectedToRoom, useHMSStore } from '@100mslive/react-sdk';
+import React, { Fragment, useCallback, useMemo, useState } from 'react';
+import data from '@emoji-mart/data/sets/14/apple.json';
+import { init } from 'emoji-mart';
+import {
+  selectAvailableRoleNames,
+  selectIsConnectedToRoom,
+  selectLocalPeerID,
+  useCustomEvent,
+  useHMSActions,
+  useHMSStore,
+  useRecordingStreaming,
+} from '@100mslive/react-sdk';
 import { EmojiIcon } from '@100mslive/react-icons';
 import { EmojiCard } from './Footer/EmojiCard';
+import { ToastManager } from './Toast/ToastManager';
 import { Dropdown } from '../../Dropdown';
 import { Box } from '../../Layout';
 import { Tooltip } from '../../Tooltip';
 import IconButton from '../IconButton';
+import { useHLSViewerRole } from './AppData/useUISettings';
 import { useDropdownList } from './hooks/useDropdownList';
 import { useIsFeatureEnabled } from './hooks/useFeatures';
-import { FEATURE_LIST } from '../common/constants';
+import { EMOJI_REACTION_TYPE, FEATURE_LIST } from '../common/constants';
+
+init({ data });
 
 export const EmojiReaction = () => {
   const [open, setOpen] = useState(false);
   const isConnected = useHMSStore(selectIsConnectedToRoom);
   const isFeatureEnabled = useIsFeatureEnabled(FEATURE_LIST.EMOJI_REACTION);
   useDropdownList({ open: open, name: 'EmojiReaction' });
+  const hmsActions = useHMSActions();
+  const roles = useHMSStore(selectAvailableRoleNames);
+  const localPeerId = useHMSStore(selectLocalPeerID);
+  const hlsViewerRole = useHLSViewerRole();
+  const { isStreamingOn } = useRecordingStreaming();
+  const filteredRoles = useMemo(() => roles.filter(role => role !== hlsViewerRole), [roles, hlsViewerRole]);
+
+  const onEmojiEvent = useCallback(data => {
+    window.showFlyingEmoji(data?.emojiId, data?.senderId);
+  }, []);
+
+  const { sendEvent } = useCustomEvent({
+    type: EMOJI_REACTION_TYPE,
+    onEvent: onEmojiEvent,
+  });
+
+  const sendReaction = async emojiId => {
+    const data = {
+      type: EMOJI_REACTION_TYPE,
+      emojiId: emojiId,
+      senderId: localPeerId,
+    };
+    sendEvent(data, { roleNames: filteredRoles });
+    if (isStreamingOn) {
+      try {
+        await hmsActions.sendHLSTimedMetadata([
+          {
+            payload: JSON.stringify(data),
+            duration: 2,
+          },
+        ]);
+      } catch (error) {
+        console.log(error);
+        ToastManager.addToast({ title: error.message });
+      }
+    }
+  };
 
   if (!isConnected || !isFeatureEnabled) {
     return null;
@@ -32,7 +83,7 @@ export const EmojiReaction = () => {
           </IconButton>
         </Dropdown.Trigger>
         <Dropdown.Content sideOffset={5} align="center" css={{ p: '$8', bg: '$surface_default' }}>
-          <EmojiCard />
+          <EmojiCard sendReaction={sendReaction} />
         </Dropdown.Content>
       </Dropdown.Root>
     </Fragment>
--- packages/roomkit-react/src/Prebuilt/components/Footer/EmojiCard.jsx
+++ packages/roomkit-react/src/Prebuilt/components/Footer/EmojiCard.jsx
@@ -1,21 +1,6 @@
-import React, { useCallback, useMemo } from 'react';
-import data from '@emoji-mart/data/sets/14/apple.json';
-import { init } from 'emoji-mart';
-import {
-  selectAvailableRoleNames,
-  selectLocalPeerID,
-  useCustomEvent,
-  useHMSActions,
-  useHMSStore,
-  useRecordingStreaming,
-} from '@100mslive/react-sdk';
+import React from 'react';
 import { Flex } from '../../../Layout';
 import { styled } from '../../../Theme';
-import { ToastManager } from '../Toast/ToastManager';
-import { useHLSViewerRole } from '../AppData/useUISettings';
-import { EMOJI_REACTION_TYPE } from '../../common/constants';
-
-init({ data });
 
 // When changing emojis in the grid, keep in mind that the payload used in sendHLSTimedMetadata has a limit of 100 characters. Using bigger emoji Ids can cause the limit to be exceeded.
 const emojiReactionList = [
@@ -23,44 +8,7 @@ const emojiReactionList = [
   [{ emojiId: 'tada' }, { emojiId: 'heart_eyes' }, { emojiId: 'joy' }, { emojiId: 'open_mouth' }, { emojiId: 'sob' }],
 ];
 
-export const EmojiCard = () => {
-  const hmsActions = useHMSActions();
-  const roles = useHMSStore(selectAvailableRoleNames);
-  const localPeerId = useHMSStore(selectLocalPeerID);
-  const hlsViewerRole = useHLSViewerRole();
-  const { isStreamingOn } = useRecordingStreaming();
-  const filteredRoles = useMemo(() => roles.filter(role => role !== hlsViewerRole), [roles, hlsViewerRole]);
-
-  const onEmojiEvent = useCallback(data => {
-    window.showFlyingEmoji(data?.emojiId, data?.senderId);
-  }, []);
-
-  const { sendEvent } = useCustomEvent({
-    type: EMOJI_REACTION_TYPE,
-    onEvent: onEmojiEvent,
-  });
-
-  const sendReaction = async emojiId => {
-    const data = {
-      type: EMOJI_REACTION_TYPE,
-      emojiId: emojiId,
-      senderId: localPeerId,
-    };
-    sendEvent(data, { roleNames: filteredRoles });
-    if (isStreamingOn) {
-      try {
-        await hmsActions.sendHLSTimedMetadata([
-          {
-            payload: JSON.stringify(data),
-            duration: 2,
-          },
-        ]);
-      } catch (error) {
-        console.log(error);
-        ToastManager.addToast({ title: error.message });
-      }
-    }
-  };
+export const EmojiCard = ({ sendReaction }) => {
   return emojiReactionList.map((emojiLine, index) => (
     <Flex key={index} justify="between" css={{ mb: '$8' }}>
       {emojiLine.map(emoji => (
