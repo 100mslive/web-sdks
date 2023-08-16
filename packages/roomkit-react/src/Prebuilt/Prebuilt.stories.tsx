294a29bc979484677053ef8fcf1bb68001bbf294
Saikat Mitra <saikatmitra91@gmail.com>
2023-08-16 21:16:18 +0530

    fix: move qa endpoints to environment variabled

--- packages/roomkit-react/src/Prebuilt/Prebuilt.stories.tsx
+++ packages/roomkit-react/src/Prebuilt/Prebuilt.stories.tsx
@@ -1,30 +1,42 @@
 import React from 'react';
 import { Meta, StoryFn } from '@storybook/react';
-import { HMSPrebuilt } from '.';
+import { HMSPrebuilt, HMSPrebuiltOptions, HMSPrebuiltProps } from '.';
 
 export default {
   title: 'UI Components/Prebuilt',
   component: HMSPrebuilt,
   argTypes: {
-    roomCode: { control: { type: 'text' }, defaultValue: 'wxx-rjvk-sgb' },
-    logo: { control: { type: 'object' }, defaultValue: undefined },
+    roomCode: { control: { type: 'text' }, defaultValue: '' },
+    logo: { control: { type: 'object' }, defaultValue: null },
     typography: { control: { type: 'object' }, defaultValue: 'Roboto' },
   },
 } as Meta<typeof HMSPrebuilt>;
 
-const PrebuiltRoomCodeStory: StoryFn<typeof HMSPrebuilt> = ({ roomCode = '', logo, themes, typography, options }) => {
+const PrebuiltRoomCodeStory: StoryFn<typeof HMSPrebuilt> = ({
+  roomCode = '',
+  logo,
+  themes,
+  typography,
+  options,
+}: HMSPrebuiltProps) => {
   return <HMSPrebuilt roomCode={roomCode} logo={logo} options={options} themes={themes} typography={typography} />;
 };
 
 export const Example = PrebuiltRoomCodeStory.bind({});
+const endpoints: HMSPrebuiltOptions['endpoints'] = {
+  roomLayout: process.env.ROOM_LAYOUT_ENDPOINT,
+  tokenByRoomCode: process.env.TOKEN_BY_ROOM_CODE_ENDPOINT,
+  init: process.env.INIT_API_ENDPOINT,
+};
+
+const hasEndpoints = Object.values(endpoints).some(val => !!val);
+
 Example.args = {
-  roomCode: process.env.SAMPLE_ROOM_CODE || '',
+  roomCode: process.env.SAMPLE_ROOM_CODE,
   options: {
-    endpoints: {
-      roomLayout: process.env.ROOM_LAYOUT_ENDPOINT || '',
-      tokenByRoomCode: process.env.TOKEN_BY_ROOM_CODE_ENDPOINT || '',
-      init: process.env.INIT_API_ENDPOINT || '',
-    },
+    userName: '',
+    userId: '',
+    endpoints: hasEndpoints ? endpoints : undefined,
   },
   typography: {
     font_family: 'Roboto',
