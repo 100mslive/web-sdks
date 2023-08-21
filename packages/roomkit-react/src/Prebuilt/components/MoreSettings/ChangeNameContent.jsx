22b0db186676882249e56a1c99283752e6f03701
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-08-21 18:25:26 +0530

    fix: enter to submit

--- packages/roomkit-react/src/Prebuilt/components/MoreSettings/ChangeNameContent.jsx
+++ packages/roomkit-react/src/Prebuilt/components/MoreSettings/ChangeNameContent.jsx
@@ -49,6 +49,12 @@ export const ChangeNameContent = ({
           autoComplete="name"
           required
           data-testid="change_name_field"
+          onKeyDown={async e => {
+            if (e.key === 'Enter') {
+              e.preventDefault();
+              changeName();
+            }
+          }}
         />
       </Flex>
 
