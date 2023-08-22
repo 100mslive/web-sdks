fda6c17b55a83e52cc9c2536bc0bc4feda89490d
amar-1995 <110378139+amar-1995@users.noreply.github.com>
2023-08-22 18:42:27 +0530

    feat: update toast content, actions for notifications

--- packages/roomkit-react/src/Prebuilt/components/Toast/ToastConfig.jsx
+++ packages/roomkit-react/src/Prebuilt/components/Toast/ToastConfig.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
-import { ChatIcon, ConnectivityIcon, HandIcon, PersonIcon, PoorConnectivityIcon } from '@100mslive/react-icons';
+import React, { useCallback } from 'react';
+import { selectPeerByID, useHMSActions, useHMSStore } from '@100mslive/react-sdk';
+import {
+  ChatUnreadIcon,
+  ConnectivityIcon,
+  HandIcon,
+  PeopleAddIcon,
+  PeopleRemoveIcon,
+  PoorConnectivityIcon,
+} from '@100mslive/react-icons';
 import { Button } from '../../../Button';
+import { useRoomLayout } from '../../provider/roomLayoutProvider';
 import { useIsSidepaneTypeOpen, useSidepaneToggle } from '../AppData/useSidepane';
 import { SIDE_PANE_OPTIONS } from '../../common/constants';
 
@@ -18,26 +27,56 @@ const ChatAction = React.forwardRef((_, ref) => {
     </Button>
   );
 });
+
+const HandRaiseAction = React.forwardRef(({ id = '', isSingleHandRaise = true }, ref) => {
+  const hmsActions = useHMSActions();
+  const toggleSidepane = useSidepaneToggle(SIDE_PANE_OPTIONS.PARTICIPANTS);
+  const isParticipantsOpen = useIsSidepaneTypeOpen(SIDE_PANE_OPTIONS.PARTICIPANTS);
+  const peer = useHMSStore(selectPeerByID(id));
+  const roomLayout = useRoomLayout();
+  const onStageRole =
+    roomLayout.screens?.conferencing?.default?.elements?.participant_list?.viewer_on_stage?.on_stage_role;
+  const onViewerRole =
+    roomLayout.screens?.conferencing?.default?.elements?.participant_list?.viewer_on_stage?.off_stage_role;
+
+  const onClickHandler = useCallback(() => {
+    if (isSingleHandRaise) {
+      hmsActions.changeRoleOfPeer(id, onStageRole, true);
+    } else {
+      !isParticipantsOpen && toggleSidepane();
+    }
+  }, [hmsActions, id, isParticipantsOpen, isSingleHandRaise, onStageRole, toggleSidepane]);
+
+  // show nothing if handRaise is single and peer role is not hls
+  if (isSingleHandRaise && (!peer || peer.roleName !== onViewerRole)) {
+    return null;
+  }
+  return (
+    <Button outlined as="div" variant="standard" css={{ w: 'max-content' }} onClick={onClickHandler} ref={ref}>
+      {isSingleHandRaise ? 'Bring on stage' : 'View'}
+    </Button>
+  );
+});
 export const ToastConfig = {
   PEER_LIST: {
     single: function (notification) {
       if (notification.data.length === 1) {
         return {
           title: `${notification.data[0]?.name} joined`,
-          icon: <PersonIcon />,
+          icon: <PeopleAddIcon />,
         };
       }
       return {
         title: `${notification.data[notification.data.length - 1]?.name} and ${
           notification.data.length - 1
         } others joined`,
-        icon: <PersonIcon />,
+        icon: <PeopleAddIcon />,
       };
     },
     multiple: notifications => {
       return {
         title: `${notifications[0].data.name} and ${notifications.length - 1} others joined`,
-        icon: <PersonIcon />,
+        icon: <PeopleAddIcon />,
       };
     },
   },
@@ -45,13 +84,13 @@ export const ToastConfig = {
     single: function (notification) {
       return {
         title: `${notification.data?.name} joined`,
-        icon: <PersonIcon />,
+        icon: <PeopleAddIcon />,
       };
     },
     multiple: function (notifications) {
       return {
         title: `${notifications[notifications.length - 1].data.name} and ${notifications.length - 1} others joined`,
-        icon: <PersonIcon />,
+        icon: <PeopleAddIcon />,
       };
     },
   },
@@ -59,13 +98,13 @@ export const ToastConfig = {
     single: function (notification) {
       return {
         title: `${notification.data?.name} left`,
-        icon: <PersonIcon />,
+        icon: <PeopleRemoveIcon />,
       };
     },
     multiple: function (notifications) {
       return {
         title: `${notifications[notifications.length - 1].data.name} and ${notifications.length - 1} others left`,
-        icon: <PersonIcon />,
+        icon: <PeopleRemoveIcon />,
       };
     },
   },
@@ -74,6 +113,7 @@ export const ToastConfig = {
       return {
         title: `${notification.data?.name} raised hand`,
         icon: <HandIcon />,
+        action: <HandRaiseAction id={notification.data?.id} />,
       };
     },
     multiple: notifications => {
@@ -82,6 +122,7 @@ export const ToastConfig = {
           notifications.length - 1
         } others raised hand`,
         icon: <HandIcon />,
+        action: <HandRaiseAction isSingleHandRaise={false} />,
       };
     },
   },
@@ -89,14 +130,14 @@ export const ToastConfig = {
     single: notification => {
       return {
         title: `New message from ${notification.data?.senderName}`,
-        icon: <ChatIcon />,
+        icon: <ChatUnreadIcon />,
         action: <ChatAction />,
       };
     },
     multiple: notifications => {
       return {
         title: `${notifications.length} new messages`,
-        icon: <ChatIcon />,
+        icon: <ChatUnreadIcon />,
         action: <ChatAction />,
       };
     },
