12c7dd68f6fdb02ac75bae6717b6c58f289d4db1
Kaustubh Kumar <kaustubhkumar05@gmail.com>
2023-08-22 11:48:39 +0530

    fix: remove options from settings

--- packages/roomkit-react/src/Prebuilt/components/VideoList.jsx
+++ packages/roomkit-react/src/Prebuilt/components/VideoList.jsx
@@ -1,27 +1,20 @@
 import React, { Fragment, useEffect, useState } from 'react';
-import { selectLocalPeerID, useHMSStore, useVideoList } from '@100mslive/react-sdk';
+import { useVideoList } from '@100mslive/react-sdk';
 import { useTheme } from '../../Theme';
 import { StyledVideoList } from '../../VideoList';
 import { Pagination } from './Pagination';
 import ScreenshareTile from './ScreenshareTile';
 import VideoTile from './VideoTile';
-import useSortedPeers from '../common/useSortedPeers';
 import { useAppConfig } from './AppData/useAppConfig';
-import { useIsHeadless, useUISettings } from './AppData/useUISettings';
-import { UI_SETTINGS } from '../common/constants';
+import { useIsHeadless } from './AppData/useUISettings';
 
 const List = ({ maxTileCount, peers, maxColCount, maxRowCount, includeScreenShareForPeer }) => {
   const { aspectRatio } = useTheme();
   const tileOffset = useAppConfig('headlessConfig', 'tileOffset');
   const isHeadless = useIsHeadless();
-  const hideLocalVideo = useUISettings(UI_SETTINGS.hideLocalVideo);
-  const localPeerId = useHMSStore(selectLocalPeerID);
-  let sortedPeers = useSortedPeers({ peers, maxTileCount });
-  if (hideLocalVideo && sortedPeers.length > 1) {
-    sortedPeers = filterPeerId(sortedPeers, localPeerId);
-  }
+
   const { ref, pagesWithTiles } = useVideoList({
-    peers: sortedPeers,
+    peers,
     maxTileCount,
     maxColCount,
     maxRowCount,
@@ -69,27 +62,6 @@ const List = ({ maxTileCount, peers, maxColCount, maxRowCount, includeScreenShar
 };
 
 const VideoList = React.memo(List);
-
-/**
- * returns a new array of peers with the peer with peerId removed,
- * keeps the reference same if peer is not found
- */
-function filterPeerId(peers, peerId) {
-  const oldPeers = peers; // to keep the reference same if peer is not found
-  let foundPeerToFilterOut = false;
-  peers = [];
-  for (let i = 0; i < oldPeers.length; i++) {
-    if (oldPeers[i].id === peerId) {
-      foundPeerToFilterOut = true;
-    } else {
-      peers.push(oldPeers[i]);
-    }
-  }
-  if (!foundPeerToFilterOut) {
-    peers = oldPeers;
-  }
-  return peers;
-}
 
 const getOffset = ({ tileOffset, isHeadless }) => {
   if (!isHeadless || isNaN(Number(tileOffset))) {
