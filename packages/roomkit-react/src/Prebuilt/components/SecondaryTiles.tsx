bbc761dc93d60a5ba6cc604a5d384ab959ade2cb
raviteja83 <ravi.theja@100ms.live>
2023-08-24 11:34:04 +0530

    refactor: use secondary section

--- packages/roomkit-react/src/Prebuilt/components/SecondaryTiles.tsx
+++ packages/roomkit-react/src/Prebuilt/components/SecondaryTiles.tsx
@@ -1,12 +1,10 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import { useMedia } from 'react-use';
 import { HMSPeer, useHMSVanillaStore } from '@100mslive/react-sdk';
-import { Flex } from '../../Layout';
+import { RoleProminenceLayout } from './VideoLayouts/RoleProminenceLayout';
 import { config as cssConfig } from '../../Theme';
 // @ts-ignore: No implicit Any
 import { Pagination } from './Pagination';
-// @ts-ignore: No implicit Any
-import VideoTile from './VideoTile';
 import { usePagesWithTiles } from './hooks/useTileLayout';
 // @ts-ignore: No implicit Any
 import PeersSorter from '../common/PeersSorter';
@@ -32,23 +30,8 @@ export const SecondaryTiles = ({ peers }: { peers: HMSPeer[] }) => {
   }, [page, peers, peersSorter, maxTileCount]);
 
   return (
-    <Flex direction="column" css={{ flexShrink: 0 }}>
-      <Flex justify="center" align="center" css={{ gap: '$4' }}>
-        {pagesWithTiles[page]?.map(tile => {
-          return (
-            <VideoTile
-              key={tile.track?.id || tile.peer?.id}
-              width={tile.width}
-              height="100%"
-              peerId={tile.peer?.id}
-              trackId={tile.track?.id}
-              rootCSS={{ padding: 0, aspectRatio: isMobile ? 1 : 16 / 9 }}
-              objectFit="contain"
-            />
-          );
-        })}
-      </Flex>
+    <RoleProminenceLayout.SecondarySection tiles={pagesWithTiles[page]}>
       <Pagination page={page} onPageChange={setPage} numPages={pagesWithTiles.length} />
-    </Flex>
+    </RoleProminenceLayout.SecondarySection>
   );
 };
