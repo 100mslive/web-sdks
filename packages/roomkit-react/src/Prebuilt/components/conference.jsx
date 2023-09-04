bccced811be5dcf8dcec58d38d07b0d1fba11507
raviteja83 <ravi.theja@100ms.live>
2023-09-04 18:14:47 +0530

    fix: remove test logs

--- packages/roomkit-react/src/Prebuilt/components/conference.jsx
+++ packages/roomkit-react/src/Prebuilt/components/conference.jsx
@@ -124,8 +124,6 @@ const Conference = () => {
     return <FullPageProgress loadingText="Starting live stream..." />;
   }
 
-  console.log('conference rendering');
-
   return (
     <Flex css={{ size: '100%', overflow: 'hidden' }} direction="column">
       {!screenProps.hideSections.includes('header') && (
