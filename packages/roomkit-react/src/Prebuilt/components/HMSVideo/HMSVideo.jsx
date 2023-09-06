c14c217a2c01a8fcf39663a5716782984282b515
amar-1995 <amar@100ms.live>
2023-09-06 13:44:04 +0530

    fix: removed extra css

--- packages/roomkit-react/src/Prebuilt/components/HMSVideo/HMSVideo.jsx
+++ packages/roomkit-react/src/Prebuilt/components/HMSVideo/HMSVideo.jsx
@@ -3,7 +3,7 @@ import { Flex } from '../../../';
 
 export const HMSVideo = forwardRef(({ children }, videoRef) => {
   return (
-    <Flex data-testid="hms-video" css={{ size: '100%', position: 'relative', width: 'inherit' }} direction="column">
+    <Flex data-testid="hms-video" css={{ size: '100%', position: 'relative' }} direction="column">
       <video style={{ flex: '1 1 0', margin: '0 auto', minHeight: '0' }} ref={videoRef} playsInline />
       {children}
     </Flex>
