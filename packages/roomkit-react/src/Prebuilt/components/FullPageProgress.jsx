d6d82bd637f1872223bd59574d75bf2c72648661
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-09-07 17:00:20 +0530

    fix: going live overlay

--- packages/roomkit-react/src/Prebuilt/components/FullPageProgress.jsx
+++ packages/roomkit-react/src/Prebuilt/components/FullPageProgress.jsx
@@ -3,8 +3,8 @@ import { Flex } from '../../Layout';
 import { Loading } from '../../Loading';
 import { Text } from '../../Text';
 
-const FullPageProgress = ({ loaderColor = '$primary_default', loadingText = '' }) => (
-  <Flex direction="column" justify="center" align="center" css={{ size: '100%', color: loaderColor }}>
+const FullPageProgress = ({ loaderColor = '$primary_default', loadingText = '', loadingCSS={} }) => (
+  <Flex direction="column" justify="center" align="center" css={{ size: '100%', color: loaderColor, ...loadingCSS }}>
     <Loading color="currentColor" size={100} />
     {loadingText ? <Text css={{ mt: '$10', color: '$on_surface_high' }}>{loadingText}</Text> : null}
   </Flex>
