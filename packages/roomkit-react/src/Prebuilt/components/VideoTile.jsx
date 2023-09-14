f71b35ff728fc4fe490a4b14c2f068a13f8fd71c
amar-1995 <110378139+amar-1995@users.noreply.github.com>
2023-09-14 13:32:43 +0530

    fix: show option icon on hover for inset tile on desktop

--- packages/roomkit-react/src/Prebuilt/components/VideoTile.jsx
+++ packages/roomkit-react/src/Prebuilt/components/VideoTile.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment, useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { useMedia } from 'react-use';
 import {
   selectAudioTrackByPeerID,
   selectHasPeerHandRaised,
@@ -19,7 +20,7 @@ import TileMenu, { isSameTile } from './TileMenu/TileMenu';
 import { Avatar } from '../../Avatar';
 import { Box, Flex } from '../../Layout';
 import { VideoTileStats } from '../../Stats';
-import { keyframes } from '../../Theme';
+import { config as cssConfig, keyframes } from '../../Theme';
 import { Video } from '../../Video';
 import { StyledVideoTile } from '../../VideoTile';
 import { getVideoTileLabel } from './peerTileUtils';
@@ -44,6 +45,7 @@ const Tile = ({
 }) => {
   const trackSelector = trackId ? selectVideoTrackByID(trackId) : selectVideoTrackByPeerID(peerId);
   const track = useHMSStore(trackSelector);
+  const isMobile = useMedia(cssConfig.media.md);
   const peerName = useHMSStore(selectPeerNameByID(peerId));
   const audioTrack = useHMSStore(selectAudioTrackByPeerID(peerId));
   const localPeerID = useHMSStore(selectLocalPeerID);
@@ -139,7 +141,7 @@ const Tile = ({
               <AudioLevel trackId={audioTrack?.id} />
             )
           ) : null}
-          {isMouseHovered || isDragabble ? (
+          {isMouseHovered || (isDragabble && isMobile) ? (
             <TileMenu
               peerID={peerId}
               audioTrackID={audioTrack?.id}
