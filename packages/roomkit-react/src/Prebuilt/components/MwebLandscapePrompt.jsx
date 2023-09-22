011a830f57b2c724878f179447add6d4a52823c6
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-09-22 13:36:34 +0530

    fix: portal wrapper with prebuilt-container as container

--- packages/roomkit-react/src/Prebuilt/components/MwebLandscapePrompt.jsx
+++ packages/roomkit-react/src/Prebuilt/components/MwebLandscapePrompt.jsx
@@ -4,6 +4,7 @@ import { Button } from '../../Button';
 import { Box, Flex } from '../../Layout';
 import { Dialog } from '../../Modal';
 import { Text } from '../../Text';
+import { PrebuiltDialogPortal } from './PrebuiltDialogPortal';
 import { isAndroid, isIOS } from '../common/constants';
 
 export const MwebLandscapePrompt = () => {
@@ -24,7 +25,7 @@ export const MwebLandscapePrompt = () => {
 
   return (
     <Dialog.Root open={showMwebLandscapePrompt} onOpenChange={setShowMwebLandscapePrompt}>
-      <Dialog.Portal>
+      <PrebuiltDialogPortal>
         <Dialog.Overlay />
         <Dialog.Content css={{ w: 'min(420px, 90%)', p: '$8', bg: '$surface_dim' }}>
           <Box>
@@ -50,7 +51,7 @@ export const MwebLandscapePrompt = () => {
             </Flex>
           </Box>
         </Dialog.Content>
-      </Dialog.Portal>
+      </PrebuiltDialogPortal>
     </Dialog.Root>
   );
 };
