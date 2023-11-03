5e5a99dc27770e5821b26747e7d106d90b046dc2
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-11-03 15:08:26 +0530

    fix: vb media path

--- packages/roomkit-react/src/Prebuilt/components/VirtualBackground/constants.ts
+++ packages/roomkit-react/src/Prebuilt/components/VirtualBackground/constants.ts
@@ -2,16 +2,16 @@
 export const VB_EFFECT = { BLUR: 'blur', BEAUTIFY: 'BEAUTIFY', NONE: 'none', MEDIA: 'image' };
 
 export const defaultMedia = [
-  'https://assets.100ms.live/webapp/vb/vb-1.jpg',
-  'https://assets.100ms.live/webapp/vb/vb-2.jpg',
-  'https://assets.100ms.live/webapp/vb/vb-3.png',
-  'https://assets.100ms.live/webapp/vb/vb-4.jpg',
-  'https://assets.100ms.live/webapp/vb/vb-5.jpg',
-  'https://assets.100ms.live/webapp/vb/vb-6.jpg',
-  'https://assets.100ms.live/webapp/vb/vb-7.jpg',
-  'https://assets.100ms.live/webapp/vb/vb-8.jpg',
-  'https://assets.100ms.live/webapp/vb/vb-9.jpg',
-  'https://assets.100ms.live/webapp/vb/vb-10.jpg',
-  'https://assets.100ms.live/webapp/vb/vb-11.jpg',
-  'https://assets.100ms.live/webapp/vb/vb-12.jpg',
+  'https://assets.100ms.live/webapp/vb-mini/vb-1.jpg',
+  'https://assets.100ms.live/webapp/vb-mini/vb-2.jpg',
+  'https://assets.100ms.live/webapp/vb-mini/vb-3.png',
+  'https://assets.100ms.live/webapp/vb-mini/vb-4.jpg',
+  'https://assets.100ms.live/webapp/vb-mini/vb-5.jpg',
+  'https://assets.100ms.live/webapp/vb-mini/vb-6.jpg',
+  'https://assets.100ms.live/webapp/vb-mini/vb-7.jpg',
+  'https://assets.100ms.live/webapp/vb-mini/vb-8.jpg',
+  'https://assets.100ms.live/webapp/vb-mini/vb-9.jpg',
+  'https://assets.100ms.live/webapp/vb-mini/vb-10.jpg',
+  'https://assets.100ms.live/webapp/vb-mini/vb-11.jpg',
+  'https://assets.100ms.live/webapp/vb-mini/vb-12.jpg',
 ];
