688d2ea06044dd929f66e76531f6564f56383895
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-11-10 18:00:38 +0530

    fix: convert to ts

--- packages/roomkit-react/src/Prebuilt/components/hooks/useSetPinnedMessages.ts
+++ packages/roomkit-react/src/Prebuilt/components/hooks/useSetPinnedMessages.ts
@@ -1,13 +1,15 @@
-// @ts-check
 import { useCallback } from 'react';
 import {
+  HMSMessage,
   selectPeerNameByID,
   selectSessionStore,
   useHMSActions,
   useHMSStore,
   useHMSVanillaStore,
 } from '@100mslive/react-sdk';
+// @ts-ignore
 import { ToastManager } from '../Toast/ToastManager';
+// @ts-ignore
 import { SESSION_STORE_KEY } from '../../common/constants';
 
 /**
@@ -18,10 +20,7 @@ export const useSetPinnedMessages = () => {
   const vanillaStore = useHMSVanillaStore();
   const pinnedMessages = useHMSStore(selectSessionStore(SESSION_STORE_KEY.PINNED_MESSAGES)) || [];
   const setPinnedMessages = useCallback(
-    /**
-     * @param {import("@100mslive/react-sdk").HMSMessage | undefined} message
-     */
-    async message => {
+    async (message: HMSMessage) => {
       const peerName = vanillaStore.getState(selectPeerNameByID(message?.sender)) || message?.senderName;
       const newPinnedMessage = message ? (peerName ? `${peerName}: ${message.message}` : message.message) : null;
       if (newPinnedMessage && pinnedMessages.indexOf(newPinnedMessage) === -1) {
@@ -34,7 +33,7 @@ export const useSetPinnedMessages = () => {
   );
 
   const removePinnedMessage = useCallback(
-    async indexToRemove => {
+    async (indexToRemove: number) => {
       if (pinnedMessages[indexToRemove]) {
         await hmsActions.sessionStore
           .set(
