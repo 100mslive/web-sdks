33b78b0c66c540d23145c5d13b109d1c6c591159
raviteja83 <ravi.theja@100ms.live>
2023-11-14 16:43:57 +0530

    refactor: remove unused code from custom app

--- apps/100ms-custom-app/src/utils/utils.js
+++ apps/100ms-custom-app/src/utils/utils.js
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import cookies from 'js-cookies';
 
 function isRoomCode(str) {
@@ -40,121 +39,17 @@ export const getAuthInfo = () => {
   return info;
 };
 
-const tileShapeMapping = {
-  '1-1': 'SQUARE',
-  '4-3': 'LANDSCAPE',
-  '16-9': 'WIDE',
-  '3-4': '3-4',
-  '9-16': '9-16',
-};
-
 const env = process.env.REACT_APP_ENV || 'prod';
 export const apiBasePath =
   process.env.REACT_APP_DASHBOARD_BASE_ENDPOINT ||
   `https://${env}-in2.100ms.live/hmsapi/`;
-
-export const storeRoomSettings = async ({ hostname, settings, appInfo }) => {
-  const jwt = getAuthInfo().token;
-  const formData = new FormData();
-  const logoFile = settings.logo_obj;
-  if (logoFile) {
-    formData.append('logo', logoFile);
-  }
-
-  formData.append('color', settings.brand_color);
-  formData.append('font', settings.font.toUpperCase());
-  formData.append('tile_shape', tileShapeMapping[settings.tile_shape]);
-  formData.append('theme', settings.theme.toUpperCase());
-  formData.append('app_type', appInfo.app_type);
-  formData.append('app_name', appInfo.app_name);
-  formData.append('subdomain', hostname);
-  formData.append('metadata', settings.metadataFields.metadata);
-
-  const url = `${apiBasePath}apps/details`;
-
-  const headers = {
-    Authorization: `Bearer ${jwt}`,
-    'Content-Type': 'multipart/form-data',
-  };
-
-  try {
-    const res = await axios.post(url, formData, { headers: headers });
-    if (res.data.success) {
-      console.log('Details saved successfully!');
-    } else {
-      console.error(`Failed to update settings: ${res.statusText}`);
-    }
-  } catch (err) {
-    console.error(`Failed to update settings: ${err.message}`);
-  }
-};
-
-export const getRandomColor = () => {
-  const h = Math.floor(Math.random() * 360),
-    s = `${Math.floor(Math.random() * 100)}%`,
-    l = `${Math.floor(Math.random() * 60)}%`;
-  return `hsl(${h},${s},${l})`;
-};
-
-export const getInitialsFromEmail = () => {
-  let initials = '';
-  const email = getAuthInfo().userEmail;
-  if (!email) {
-    return initials;
-  }
-  const userEmail = email.toLowerCase();
-  for (let i = 0; i < userEmail.length; i++) {
-    if (userEmail[i] === '@') {
-      break;
-    }
-    if (userEmail[i] >= 'a' && userEmail[i] <= 'z') {
-      initials += userEmail[i];
-    }
-    if (initials.length === 2) {
-      break;
-    }
-  }
-  return initials;
-};
-
-export const mapTileShape = shape => {
-  if (shape === 'SQUARE') {
-    return '1-1';
-  } else if (shape === 'WIDE') {
-    return '16-9';
-  } else if (shape === 'LANDSCAPE') {
-    return '4-3';
-  }
-  return shape;
-};
-
-export const mapFromBackend = data => {
-  const avatars = {
-      PEBBLE: 'pebble',
-      INITIALS: 'initial',
-    },
-    fonts = {
-      LATO: 'Lato',
-      ROBOTO: 'Roboto',
-      MONTSERRAT: 'Montserrat',
-      INTER: 'Inter',
-      'OPEN SANS': 'Open Sans',
-      'IBM PLEX SANS': 'IBM Plex Sans',
-    };
-
-  return {
-    ...data,
-    video_off_avatars: avatars[data.video_off_avatars],
-    font: fonts[data.font],
-  };
-};
 
 export const getWithRetry = async (url, headers) => {
   const MAX_RETRIES = 4;
   let error = Error('something went wrong');
   for (let i = 0; i < MAX_RETRIES; i++) {
     try {
-      return await axios.get(url, { headers: headers });
+      return await fetch(url, { method: 'GET', headers: headers });
     } catch (err) {
       error = err;
     }
@@ -188,7 +83,7 @@ export const getAuthTokenUsingRoomIdRole = async function ({
   }
 };
 
-export const fetchData = async (
+export const fetchData = (
   subdomain,
   roomCode,
   setOnlyEmail,
@@ -198,13 +93,13 @@ export const fetchData = async (
   const jwt = getAuthInfo().token;
 
   const url = `${apiBasePath}apps/get-details?domain=${subdomain}&room_id=${roomCode}`;
-  const headers = {};
+  const headers = new Headers();
   if (jwt) {
-    headers['Authorization'] = `Bearer ${jwt}`;
+    headers.set('Authorization', `Bearer ${jwt}`);
   }
-  headers['Content-Type'] = 'application/json';
+  headers.set('Content-Type', 'application/json');
 
-  getWithRetry(url, headers)
+  return getWithRetry(url, headers)
     .then(res => {
       if (res.data.success) {
         setOnlyEmail(res.data.same_user);
