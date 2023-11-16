dff6c8b2f71a79d781b1ef205117fce0bdebb076
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-11-16 21:40:05 +0530

    fix: clean up

--- packages/roomkit-react/src/Prebuilt/common/constants.js
+++ packages/roomkit-react/src/Prebuilt/common/constants.js
@@ -32,7 +32,6 @@ export const APP_DATA = {
   waitingViewerRole: 'waitingViewerRole',
   subscribedNotifications: 'subscribedNotifications',
   logo: 'logo',
-  tokenEndpoint: 'tokenEndpoint',
   appLayout: 'appLayout',
   hlsStarted: 'hlsStarted',
   rtmpStarted: 'rtmpStarted',
