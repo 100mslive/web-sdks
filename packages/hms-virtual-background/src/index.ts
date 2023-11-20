a8d7bdc3a9435af18bd68a994391cee7f4a1f4f6
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-11-20 14:35:51 +0530

    refactor: remove redundant VB code

--- packages/hms-virtual-background/src/index.ts
+++ packages/hms-virtual-background/src/index.ts
@@ -1,4 +1,3 @@
-export * from './HMSVirtualBackgroundPlugin';
 export * from './HMSVBPlugin';
 export { HMSVirtualBackgroundTypes } from './interfaces';
 export type { HMSVirtualBackground } from './interfaces';
