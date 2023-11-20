d857e81008aa78c7ba32fef5ace9298ef8a679f1
raviteja83 <ravi.theja@100ms.live>
2023-11-20 18:02:10 +0530

    fix: build scripts

--- scripts/update-versions.sh
+++ scripts/update-versions.sh
@@ -1,5 +1,5 @@
 lerna version $1 --no-git-tag-version --exact --yes --no-private
-lerna add @100mslive/hms-video --peer --scope=@100mslive/hms-virtual-background --exact
+lerna add @100mslive/hms-video-store --peer --scope=@100mslive/hms-virtual-background --exact
 lerna add @100mslive/react-sdk --scope=@100mslive/roomkit-react --exact
 lerna add @100mslive/react-icons --scope=@100mslive/roomkit-react --exact
 lerna add @100mslive/hms-virtual-background --scope=@100mslive/roomkit-react --exact
