bf36555e49bcc10a98952694bb22ff8ae331da1a
raviteja83 <ravi.theja@100ms.live>
2023-11-20 18:42:16 +0530

    fix: storybook errors

--- packages/hms-video-store/src/index.ts
+++ packages/hms-video-store/src/index.ts
@@ -1,4 +1,4 @@
 export * from './core';
 export { HMSReactiveStore } from './core/hmsSDKStore/HMSReactiveStore';
 export { HMSPluginUnsupportedTypes, HMSRecordingState } from './coreSDK';
-export type { HMSVideoPlugin, HMSPluginSupportResult } from './coreSDK';
+export type { HMSVideoPlugin, HMSPluginSupportResult, HMSFrameworkInfo } from './coreSDK';
