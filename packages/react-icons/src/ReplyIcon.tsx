77718c7abc260e8dc88a7786ec52620bac70c7c7
amar-1995 <amar@100ms.live>
2023-11-27 13:33:19 +0530

    fix: changed svg file

--- packages/react-icons/src/ReplyIcon.tsx
+++ packages/react-icons/src/ReplyIcon.tsx
@@ -1,11 +1,21 @@
 import * as React from 'react';
 import { SVGProps } from 'react';
 const SvgReplyIcon = (props: SVGProps<SVGSVGElement>) => (
-  <svg width="24px" height="24px" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <g stroke="#EFF0FA" strokeWidth={1.5} strokeLinecap="round" strokeLinejoin="round">
-      <path d="M4.385 7.77 1 4.386 4.385 1" />
-      <path d="M1 4.385h7.11a3.724 3.724 0 1 1 0 7.448H5.74" />
-    </g>
+  <svg width="24px" height="24px" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path
+      d="M8.385 11.77 5 8.386 8.385 5"
+      stroke="#EFF0FA"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M5 8.385h7.11a3.724 3.724 0 0 1 0 7.448H9.74"
+      stroke="#EFF0FA"
+      strokeWidth={1.5}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
   </svg>
 );
 export default SvgReplyIcon;
