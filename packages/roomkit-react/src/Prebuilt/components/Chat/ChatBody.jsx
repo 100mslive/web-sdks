e2c29d3250ba085b4466ea32c821294a7ce5d39f
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-11-29 17:13:35 +0530

    fix: clean up

--- packages/roomkit-react/src/Prebuilt/components/Chat/ChatBody.jsx
+++ packages/roomkit-react/src/Prebuilt/components/Chat/ChatBody.jsx
@@ -119,7 +119,7 @@ const ChatActions = ({
   const { elements } = useRoomLayoutConferencingScreen();
   const { can_hide_message, can_block_user } = elements?.chat?.real_time_controls || {
     can_hide_message: false,
-    can_block_user: true,
+    can_block_user: false,
   };
   const [open, setOpen] = useState(false);
   const blacklistedPeerIDs = useHMSStore(selectSessionStore(SESSION_STORE_KEY.CHAT_PEER_BLACKLIST)) || [];
