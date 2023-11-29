7d9116421970cb0ac796a93ddaf3a671f29f2074
hardik1003 <hardik@100ms.live>
2023-11-29 13:11:01 +0530

    fix: add current color

--- packages/react-icons/src/TwitterXIcon.tsx
+++ packages/react-icons/src/TwitterXIcon.tsx
@@ -6,8 +6,7 @@ const SvgTwitterXIcon = (props: SVGProps<SVGSVGElement>) => (
       fillRule="evenodd"
       clipRule="evenodd"
       d="M18 32.273c7.456 0 13.5-6.045 13.5-13.5 0-7.456-6.044-13.5-13.5-13.5s-13.5 6.044-13.5 13.5c0 7.455 6.044 13.5 13.5 13.5Zm7.936-21h-2.76l-4.548 4.793-3.933-4.793H9l6.806 8.202-6.45 6.797h2.761l4.979-5.244 4.35 5.245H27l-7.094-8.646 6.03-6.354Zm-2.2 13.477h-1.529l-9.984-12.034h1.641l9.873 12.034Z"
-      fill="#E0ECFF"
-      fillOpacity={0.8}
+      fill="currentColor"
     />
   </svg>
 );
