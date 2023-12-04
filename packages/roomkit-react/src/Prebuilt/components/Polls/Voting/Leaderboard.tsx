abdea8c6af2b9b86d9d289897f64cbe88a1ff78e
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-12-04 21:53:31 +0530

    fix: poll results view

--- packages/roomkit-react/src/Prebuilt/components/Polls/Voting/Leaderboard.tsx
+++ packages/roomkit-react/src/Prebuilt/components/Polls/Voting/Leaderboard.tsx
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from 'react';
 // import { HMSLeaderboardEntry, HMSPollLeaderboardResponse } from '@100mslive/hms-video';
 import { selectPollByID, useHMSActions, useHMSStore } from '@100mslive/react-sdk';
+import { ChevronLeftIcon, CrossIcon } from '@100mslive/react-icons';
 // import { CheckCircleIcon, TrophyFilledIcon } from '@100mslive/react-icons';
-// import { Box, Flex } from '../../../../Layout';
-import { Box } from '../../../../Layout';
+import { Box, Flex } from '../../../../Layout';
 import { Loading } from '../../../../Loading';
 import { Text } from '../../../../Text';
+// @ts-ignore
+import { useSidepaneToggle } from '../../AppData/useSidepane';
+// @ts-ignore
+import { usePollViewState } from '../../AppData/useUISettings';
+// @ts-ignore
+import { StatusIndicator } from '../common/StatusIndicator';
+import { POLL_VIEWS } from '../../../common/constants';
 
 // const LeaderboardEntry = ({
 //   position,
@@ -54,6 +61,8 @@ export const Leaderboard = ({ pollID }: { pollID: string }) => {
   const hmsActions = useHMSActions();
   const poll = useHMSStore(selectPollByID(pollID));
   const [pollLeaderboard, setPollLeaderboard] = useState<any>();
+  const { setPollView } = usePollViewState();
+  const toggleSidepane = useSidepaneToggle();
 
   useEffect(() => {
     const fetchLeaderboardData = async () => {
@@ -69,6 +78,26 @@ export const Leaderboard = ({ pollID }: { pollID: string }) => {
 
   return (
     <Box>
+      <Flex justify="between" align="center" css={{ flexGrow: 1, py: '$8', borderBottom: '1px solid $border_bright' }}>
+        <Flex align="center" css={{ gap: '$4' }}>
+          <Flex
+            css={{ color: '$on_surface_medium', '&:hover': { color: '$on_surface_high', cursor: 'pointer' } }}
+            onClick={() => setPollView(POLL_VIEWS.VOTE)}
+          >
+            <ChevronLeftIcon />
+          </Flex>
+          <Text variant="lg" css={{ fontWeight: '$semiBold' }}>
+            {poll.title}
+          </Text>
+          <StatusIndicator isLive={false} />
+        </Flex>
+        <Flex
+          css={{ color: '$on_surface_medium', '&:hover': { color: '$on_surface_high', cursor: 'pointer' } }}
+          onClick={toggleSidepane}
+        >
+          <CrossIcon />
+        </Flex>
+      </Flex>
       <Text variant="sm" css={{ fontWeight: '$semiBold' }}>
         Leaderboard
       </Text>
