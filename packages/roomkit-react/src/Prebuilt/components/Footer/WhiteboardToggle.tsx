22708f2dfa51a82f3a3592f59fffa78d6b4e70b4
eswarclynn <esuclintblazer@gmail.com>
2023-12-05 14:26:36 +0530

    fix: whiteboard toggle error toast

--- packages/roomkit-react/src/Prebuilt/components/Footer/WhiteboardToggle.tsx
+++ packages/roomkit-react/src/Prebuilt/components/Footer/WhiteboardToggle.tsx
@@ -4,16 +4,22 @@ import { PencilDrawIcon } from '@100mslive/react-icons';
 import { Tooltip } from '../../..';
 // @ts-ignore: No implicit Any
 import IconButton from '../../IconButton';
+// @ts-ignore: No implicit Any
+import { ToastManager } from '../Toast/ToastManager';
 
 export const WhiteboardToggle = () => {
   const open = useHMSStore(selectWhiteboard)?.open;
   const actions = useHMSActions().interactivityCenter.whiteboard;
 
-  const toggle = () => {
-    if (open) {
-      actions.close();
-    } else {
-      actions.open();
+  const toggle = async () => {
+    try {
+      if (open) {
+        await actions.close();
+      } else {
+        await actions.open();
+      }
+    } catch (error) {
+      ToastManager.addToast({ title: (error as Error).message, variant: 'error' });
     }
   };
 
