c6a549ccbb6c959eac34eb44bedd34d16bed76a5
Kaustubh Kumar <kaustubhkumar05@gmail.com>
2023-12-08 15:21:38 +0530

    fix: alignment (#2249)

--- packages/roomkit-react/src/Prebuilt/components/HMSVideo/HLSCaptionSelector.tsx
+++ packages/roomkit-react/src/Prebuilt/components/HMSVideo/HLSCaptionSelector.tsx
@@ -4,7 +4,7 @@ import { IconButton, Tooltip } from '../../../';
 
 export function HLSCaptionSelector({ isEnabled, onClick }: { isEnabled: boolean; onClick: () => void }) {
   return (
-    <Tooltip title="Subtitles/closed captions">
+    <Tooltip title="Subtitles/closed captions" side="top">
       <IconButton css={{ p: '$2' }} onClick={() => onClick()}>
         {isEnabled ? <ClosedCaptionIcon width="20" height="20px" /> : <OpenCaptionIcon width="20" height="20px" />}
       </IconButton>
