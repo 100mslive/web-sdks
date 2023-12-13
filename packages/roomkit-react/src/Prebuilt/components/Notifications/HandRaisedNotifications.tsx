267e9209c47e66c0674371560d193bceb3cf160b
raviteja83 <ravi.theja@100ms.live>
2023-12-13 13:03:55 +0530

    feat: show handraise to on stage exp roles

---
+++ packages/roomkit-react/src/Prebuilt/components/Notifications/HandRaisedNotifications.tsx
@@ -0,0 +1,37 @@
+import { useEffect } from 'react';
+import {
+  HMSNotificationTypes,
+  HMSRoomState,
+  selectHasPeerHandRaised,
+  selectRoomState,
+  useHMSNotifications,
+  useHMSStore,
+  useHMSVanillaStore,
+} from '@100mslive/react-sdk';
+// @ts-ignore: No implicit Any
+import { ToastBatcher } from '../Toast/ToastBatcher';
+import { useRoomLayoutConferencingScreen } from '../../provider/roomLayoutProvider/hooks/useRoomLayoutScreen';
+
+export const HandRaisedNotifications = () => {
+  const notification = useHMSNotifications(HMSNotificationTypes.HAND_RAISE_CHANGED);
+  const roomState = useHMSStore(selectRoomState);
+  const vanillaStore = useHMSVanillaStore();
+  const { on_stage_exp } = useRoomLayoutConferencingScreen().elements || {};
+
+  useEffect(() => {
+    if (!notification?.data) {
+      return;
+    }
+    if (roomState !== HMSRoomState.Connected || notification.data.isLocal || !on_stage_exp) {
+      return;
+    }
+    const hasPeerHandRaised = vanillaStore.getState(selectHasPeerHandRaised(notification.data.id));
+    if (hasPeerHandRaised) {
+      ToastBatcher.showToast({ notification, type: 'RAISE_HAND' });
+    }
+
+    ToastBatcher.showToast({ notification });
+  }, [notification, on_stage_exp, roomState, vanillaStore]);
+
+  return null;
+};
