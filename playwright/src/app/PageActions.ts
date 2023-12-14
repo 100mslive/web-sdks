13c17593b14e54eecc176c7be2d9cb93407678c3
Sahil Kashyap <sahil@100ms.live>
2023-12-14 20:05:29 +0400

    test: added polls & quizzes p0 tests

--- playwright/src/app/PageActions.ts
+++ playwright/src/app/PageActions.ts
@@ -5,7 +5,7 @@ export class PageActions {
 
     async endSession(page: any) {
         await page.getByTestId(selectors.DropDown).click();
-        await page.getByText(selectors.EndSessionOption).click();
+        await page.getByTestId(selectors.EndSessionOption).click();
         await page.getByTestId(selectors.EndSession).click();
     }
 
@@ -19,5 +19,45 @@ export class PageActions {
         await page.getByPlaceholder('Enter name').fill(name);
         await page.getByText(selectors.JoinBtn).click();
         await page.waitForTimeout(2000);
+    }
+
+    async goLive(page: any, name: string) {
+        await page.getByPlaceholder('Enter name').fill(name);
+        await page.getByText(selectors.GoLive).click();
+        await page.waitForTimeout(2000);
+    }
+
+    async createNewPollorQuiz(isPoll: boolean, name: string, page:any) {
+        await page.getByTestId(selectors.PollsMenuBtn).click();
+        if(!isPoll) await page.getByText('Quiz').nth(0).click();
+        await page.locator(selectors.PollName).fill(name);
+        await page.getByText(selectors.CreatePollBtn).click();
+        await page.getByPlaceholder(selectors.QuestionName).fill('question no 1');
+        await page.getByPlaceholder('Option 1').fill('Option1');
+        await page.getByPlaceholder('Option 2').fill('Option2');
+        await page.getByText('Save').click();
+        await page.getByText(selectors.LaunchPollBtn).click();
+    }
+
+    async createNewQuiz(name: string, page:any) {
+        await page.getByTestId(selectors.PollsMenuBtn).click();
+        await page.getByText('Quiz').nth(1).click();
+        await page.locator(selectors.QuizName).fill(name);
+        await page.getByText(selectors.CreateQuizBtn).click();
+        await page.getByPlaceholder(selectors.QuestionName).fill('question no 1');
+        await page.getByPlaceholder('Option 1').fill('Option1');
+        await page.getByPlaceholder('Option 2').fill('Option2');
+        await page.locator('button[role=radio]').nth(1).click();
+        await page.getByText('Save').click();
+        await page.getByText(selectors.LaunchQuizBtn).click();
+    }
+    async vote(page: any, isQuiz: boolean) {
+        await page.getByTestId(selectors.PollsMenuBtn).click();
+        await page.getByText('View').click();
+        await page.locator('button[role=radio]').nth(1).click();
+        if(isQuiz) 
+           await page.getByText(selectors.AnswerBtn).click();
+        else
+        await page.getByText(selectors.VoteBtn).click();
     }
 }
