ad9e9266684acba906a50f605bfe9d8a1f6930e2
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-12-18 11:43:36 +0530

    Revert "fix: sort active speaker only on first page"
    This reverts commit 096d50e1ffbeb9f2ec895b630952d2ad9694dfee.

--- packages/roomkit-react/src/Prebuilt/components/VideoLayouts/GridLayout.tsx
+++ packages/roomkit-react/src/Prebuilt/components/VideoLayouts/GridLayout.tsx
@@ -80,7 +80,6 @@ export const GridLayout = ({
 
   useEffect(() => {
     if (mainPage !== 0) {
-      peersSorter.stop();
       return;
     }
     peersSorter.setPeersAndTilesPerPage({
@@ -88,9 +87,6 @@ export const GridLayout = ({
       tilesPerPage: pageSize,
     });
     peersSorter.onUpdate(setSortedPeers);
-    return () => {
-      peersSorter.stop();
-    };
   }, [mainPage, peersSorter, updatedPeers, pageSize]);
 
   if (peerSharing) {
