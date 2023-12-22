fb8837ad31f9d5dc70675eaa4d909bb916a6f456
sahil-100ms <105665560+sahil-100ms@users.noreply.github.com>
2023-12-22 12:51:17 +0400

    test: added force role change tests (#2340)

---
+++ playwright/src/app/tests/ForceRoleChange.test.ts
@@ -0,0 +1,111 @@
+import { test } from '@playwright/test';
+import { Selectors } from '../selectors/Selectors';
+import { expect } from '@playwright/test';
+
+const selectors = new Selectors();
+
+test.beforeEach(async () => {});
+
+test.afterEach(async ({ context }) => {
+    await context.close();
+});
+
+test('Broadcaster is able to invite VNRT on stage with one click/force.', async({context}) =>  {
+    const broadcasterUrl = 'https://sahil-livestream-1207.app.100ms.live/streaming/meeting/sim-sbtw-svm';
+    const hlsviewerUrl = 'https://sahil-livestream-1207.app.100ms.live/streaming/meeting/cfn-mafp-ewd';
+
+    const page_host = await context.newPage();
+
+    //Join as broadcaster
+    await page_host.goto(broadcasterUrl);
+    await page_host.getByPlaceholder('Enter name').fill('broadcaster');
+    await page_host.getByText(selectors.GoLive).click();
+    await page_host.waitForTimeout(5000);
+
+    //Join as vnrt
+    const page = await context.newPage();
+    await page.goto(hlsviewerUrl);
+    await page.getByPlaceholder('Enter name').fill('vnrt');
+    await page.getByText(selectors.JoinBtn).click();
+    await page.waitForTimeout(5000);
+
+    await page.getByTestId('hms-video').isVisible();
+
+     //Hand raise for vnrt
+     await page.getByTestId('hand_raise_btn').click();
+
+     //Go to broadcaster and click brint to stage notification
+     await page_host.bringToFront();
+     await page_host.getByText('Bring to stage').click();
+
+   
+   // viewer on stage assertions for broadcaster
+    await page_host.bringToFront();
+
+    await page_host.getByTestId('participant_avatar_icon').isVisible();
+    await page_host.getByTestId('participant_audio_mute_icon').isVisible();
+    await expect(page_host.locator('data-testid=participant_video_tile')).toHaveCount(2);
+
+    // viewer on stage assertions for vnrt
+    await page.bringToFront();
+
+    await page_host.getByTestId('participant_avatar_icon').isVisible();
+    await page_host.getByTestId('participant_audio_mute_icon').isVisible();
+    await expect(page.locator('data-testid=participant_video_tile')).toHaveCount(2);
+    await expect(page.locator('text=Live')).toHaveCount(2);
+
+    await page_host.bringToFront();
+    await page_host.getByTestId('leave_end_dropdown_trigger').click();
+    await page_host.getByTestId('end_room_btn').click();
+    await page_host.getByTestId('stop_stream_btn').click();
+})
+
+test('Broadcaster is able to invite VRT on stage with one click/force.', async({context}) =>  {
+    const broadcasterUrl = 'https://sahil-livestream-1207.app.100ms.live/streaming/meeting/sim-sbtw-svm';
+    const hlsviewerUrl = 'https://sahil-livestream-1207.app.100ms.live/streaming/meeting/cbg-ojjo-usk';
+
+    const page_host = await context.newPage();
+
+    //Join as broadcaster
+    await page_host.goto(broadcasterUrl);
+    await page_host.getByPlaceholder('Enter name').fill('broadcaster');
+    await page_host.getByText(selectors.GoLive).click();
+    await page_host.waitForTimeout(5000);
+
+    //Join as vnrt
+    const page = await context.newPage();
+    await page.goto(hlsviewerUrl);
+    await page.getByPlaceholder('Enter name').fill('vrt');
+    await page.getByText(selectors.JoinBtn).click();
+    await page.waitForTimeout(5000);
+
+    await page.getByTestId('hms-video').isVisible();
+
+     //Hand raise for vnrt
+     await page.getByTestId('hand_raise_btn').click();
+
+     //Go to broadcaster and click brint to stage notification
+     await page_host.bringToFront();
+     await page_host.getByText('Bring to stage').click();
+
+   
+   // viewer on stage assertions for broadcaster
+    await page_host.bringToFront();
+
+    await page_host.getByTestId('participant_avatar_icon').isVisible();
+    await page_host.getByTestId('participant_audio_mute_icon').isVisible();
+    await expect(page_host.locator('data-testid=participant_video_tile')).toHaveCount(2);
+
+    // viewer on stage assertions for vnrt
+    await page.bringToFront();
+
+    await page_host.getByTestId('participant_avatar_icon').isVisible();
+    await page_host.getByTestId('participant_audio_mute_icon').isVisible();
+    await expect(page.locator('data-testid=participant_video_tile')).toHaveCount(2);
+    
+
+    await page_host.bringToFront();
+    await page_host.getByTestId('leave_end_dropdown_trigger').click();
+    await page_host.getByTestId('end_room_btn').click();
+    await page_host.getByTestId('stop_stream_btn').click();
+})
