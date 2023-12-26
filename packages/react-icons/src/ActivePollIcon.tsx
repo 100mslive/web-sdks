28e4c4614262ab76007a8be5c0b2d12d766f43f6
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2023-12-26 12:17:39 +0530

    feat: icon for unread polls quizzes

--- packages/react-icons/src/ActivePollIcon.tsx
+++ packages/react-icons/src/ActivePollIcon.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { SVGProps } from 'react';
-const SvgQuizActiveIcon = (props: SVGProps<SVGSVGElement>) => (
+const SvgActivePollIcon = (props: SVGProps<SVGSVGElement>) => (
   <svg width="24px" height="24px" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
     <path
       fillRule="evenodd"
@@ -18,4 +18,4 @@ const SvgQuizActiveIcon = (props: SVGProps<SVGSVGElement>) => (
     <path d="M24 13.334a5.333 5.333 0 1 0 0-10.667 5.333 5.333 0 0 0 0 10.667Z" fill="#2672ED" />
   </svg>
 );
-export default SvgQuizActiveIcon;
+export default SvgActivePollIcon;
