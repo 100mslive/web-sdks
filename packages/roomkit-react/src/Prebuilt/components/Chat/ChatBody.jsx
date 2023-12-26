beb64562a19ce9f6fae64d7980e2170d3d8ef8bf
raviteja83 <ravi.theja@100ms.live>
2023-12-26 16:03:45 +0530

    refactor: chat

--- packages/roomkit-react/src/Prebuilt/components/Chat/ChatBody.jsx
+++ packages/roomkit-react/src/Prebuilt/components/Chat/ChatBody.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useCallback, useEffect, useLayoutEffect, useRef, useState } from 'react';
+import React, { Fragment, useCallback, useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
 import { useMedia } from 'react-use';
 import AutoSizer from 'react-virtualized-auto-sizer';
@@ -6,50 +6,23 @@ import { VariableSizeList } from 'react-window';
 import {
   selectHMSMessages,
   selectLocalPeerID,
-  selectLocalPeerName,
   selectLocalPeerRoleName,
   selectPeerNameByID,
-  selectPermissions,
   selectSessionStore,
   selectUnreadHMSMessagesCount,
   useHMSActions,
   useHMSStore,
   useHMSVanillaStore,
 } from '@100mslive/react-sdk';
-import {
-  CopyIcon,
-  CrossCircleIcon,
-  CrossIcon,
-  EyeCloseIcon,
-  PeopleRemoveIcon,
-  PinIcon,
-  ReplyIcon,
-  VerticalMenuIcon,
-} from '@100mslive/react-icons';
-import { Dropdown } from '../../../Dropdown';
-import { IconButton } from '../../../IconButton';
 import { Box, Flex } from '../../../Layout';
-import { Sheet } from '../../../Sheet';
 import { Text } from '../../../Text';
 import { config as cssConfig, styled } from '../../../Theme';
 import { Tooltip } from '../../../Tooltip';
 import emptyChat from '../../images/empty-chat.svg';
-import { ToastManager } from '../Toast/ToastManager';
-import { MwebChatOption } from './MwebChatOption';
+import { ChatActions } from './ChatActions';
 import { useRoomLayoutConferencingScreen } from '../../provider/roomLayoutProvider/hooks/useRoomLayoutScreen';
 import { useSetSubscribedChatSelector } from '../AppData/useUISettings';
-import { useChatBlacklist } from '../hooks/useChatBlacklist';
-import { useSetPinnedMessages } from '../hooks/useSetPinnedMessages';
 import { CHAT_SELECTOR, SESSION_STORE_KEY } from '../../common/constants';
-
-const iconStyle = { height: '1.125rem', width: '1.125rem' };
-const tooltipBoxCSS = {
-  fontSize: '$xs',
-  backgroundColor: '$surface_default',
-  p: '$1 $5',
-  fontWeight: '$regular',
-  borderRadius: '$3',
-};
 
 const formatTime = date => {
   if (!(date instanceof Date)) {
@@ -157,242 +130,6 @@ const getMessageType = ({ roles, receiver }) => {
   }
   return receiver ? 'private' : '';
 };
-const ChatActions = ({
-  onPin,
-  showPinAction,
-  onReply,
-  showReply,
-  message,
-  sentByLocalPeer,
-  isMobile,
-  openSheet,
-  setOpenSheet,
-}) => {
-  const { elements } = useRoomLayoutConferencingScreen();
-  const { can_hide_message, can_block_user } = elements?.chat?.real_time_controls || {
-    can_hide_message: false,
-    can_block_user: false,
-  };
-  const [open, setOpen] = useState(false);
-  const actions = useHMSActions();
-  const canRemoveOthers = useHMSStore(selectPermissions)?.removeOthers;
-  const { blacklistItem: blacklistPeer } = useChatBlacklist(SESSION_STORE_KEY.CHAT_PEER_BLACKLIST);
-
-  const { blacklistItem: blacklistMessage, blacklistedIDs: blacklistedMessageIDs = [] } = useChatBlacklist(
-    SESSION_STORE_KEY.CHAT_MESSAGE_BLACKLIST,
-  );
-  const { unpinBlacklistedMessages } = useSetPinnedMessages();
-
-  const pinnedMessages = useHMSStore(selectSessionStore(SESSION_STORE_KEY.PINNED_MESSAGES));
-  const updatePinnedMessages = useCallback(
-    (messageID = '') => {
-      const blacklistedMessageIDSet = new Set([...blacklistedMessageIDs, messageID]);
-      unpinBlacklistedMessages(pinnedMessages, blacklistedMessageIDSet);
-    },
-    [blacklistedMessageIDs, unpinBlacklistedMessages, pinnedMessages],
-  );
-
-  const copyMessageContent = useCallback(() => {
-    try {
-      navigator?.clipboard.writeText(message.message);
-      ToastManager.addToast({
-        title: 'Message copied successfully',
-      });
-    } catch (e) {
-      console.log(e);
-      ToastManager.addToast({
-        title: 'Could not copy message',
-      });
-    }
-  }, [message]);
-
-  const options = {
-    reply: {
-      text: message.recipientRoles?.length ? 'Reply to Group' : 'Reply Privately',
-      tooltipText: message.recipientRoles?.length ? 'Reply to Group' : 'Reply Privately',
-      icon: <ReplyIcon style={iconStyle} />,
-      onClick: onReply,
-      show: showReply,
-    },
-    pin: {
-      text: 'Pin message',
-      tooltipText: 'Pin',
-      icon: <PinIcon style={iconStyle} />,
-      onClick: onPin,
-      show: showPinAction,
-    },
-    copy: {
-      text: 'Copy text',
-      tooltipText: 'Copy',
-      icon: <CopyIcon style={iconStyle} />,
-      onClick: copyMessageContent,
-      show: true,
-    },
-    hide: {
-      text: message.recipientPeer ? 'Hide for both' : 'Hide for everyone',
-      icon: <EyeCloseIcon style={iconStyle} />,
-      onClick: async () => {
-        blacklistMessage(message.id);
-        updatePinnedMessages(message.id);
-      },
-      show: can_hide_message,
-    },
-    block: {
-      text: 'Block from chat',
-      icon: <CrossCircleIcon style={iconStyle} />,
-      onClick: async () => blacklistPeer(message?.senderUserId),
-      color: '$alert_error_default',
-      show: can_block_user && !sentByLocalPeer,
-    },
-    remove: {
-      text: 'Remove Partipant',
-      icon: <PeopleRemoveIcon style={iconStyle} />,
-      color: '$alert_error_default',
-      show: canRemoveOthers && !sentByLocalPeer,
-      onClick: async () => {
-        try {
-          await actions.removePeer(message.sender, '');
-        } catch (error) {
-          ToastManager.addToast({ title: error.message, variant: 'error' });
-        }
-      },
-    },
-  };
-
-  if (isMobile) {
-    return (
-      <Sheet.Root open={openSheet} onOpenChange={setOpenSheet}>
-        <Sheet.Content css={{ bg: '$surface_default', pb: '$14' }} onClick={() => setOpenSheet(false)}>
-          <Sheet.Title
-            css={{
-              display: 'flex',
-              color: '$on_surface_high',
-              w: '100%',
-              justifyContent: 'space-between',
-              mt: '$8',
-              fontSize: '$md',
-              px: '$10',
-              pb: '$8',
-              borderBottom: '1px solid $border_bright',
-              alignItems: 'center',
-            }}
-          >
-            Message options
-            <Sheet.Close css={{ color: '$on_surface_high' }} onClick={() => setOpenSheet(false)}>
-              <CrossIcon />
-            </Sheet.Close>
-          </Sheet.Title>
-
-          {Object.keys(options).map(optionKey => {
-            const option = options[optionKey];
-            return option.show ? (
-              <MwebChatOption
-                key={optionKey}
-                text={option.text}
-                icon={option.icon}
-                onClick={option.onClick}
-                color={option?.color}
-              />
-            ) : null;
-          })}
-        </Sheet.Content>
-      </Sheet.Root>
-    );
-  }
-
-  return (
-    <Dropdown.Root open={open} onOpenChange={setOpen} css={{ '@md': { display: 'none' } }}>
-      <Flex
-        className="chat_actions"
-        css={{
-          background: '$surface_bright',
-          borderRadius: '$1',
-          p: '$2',
-          opacity: open ? 1 : 0,
-          position: 'absolute',
-          right: 0,
-          zIndex: 1,
-          '@md': { opacity: 1 },
-        }}
-      >
-        {options.reply.show ? (
-          <Tooltip boxCss={tooltipBoxCSS} title={options.reply.tooltipText}>
-            <IconButton data-testid="reply_message_btn" onClick={options.reply.onClick}>
-              {options.reply.icon}
-            </IconButton>
-          </Tooltip>
-        ) : null}
-        {options.pin.show ? (
-          <Tooltip boxCss={tooltipBoxCSS} title={options.pin.tooltipText}>
-            <IconButton data-testid="pin_message_btn" onClick={options.pin.onClick}>
-              {options.pin.icon}
-            </IconButton>
-          </Tooltip>
-        ) : null}
-
-        {options.copy.show ? (
-          <Tooltip boxCss={tooltipBoxCSS} title={options.copy.tooltipText}>
-            <IconButton onClick={options.copy.onClick} data-testid="copy_message_btn">
-              <CopyIcon style={iconStyle} />
-            </IconButton>
-          </Tooltip>
-        ) : null}
-
-        {options.block.show || options.hide.show || options.remove.show ? (
-          <Tooltip boxCss={tooltipBoxCSS} title="More actions">
-            <Dropdown.Trigger asChild>
-              <IconButton>
-                <VerticalMenuIcon style={iconStyle} />
-              </IconButton>
-            </Dropdown.Trigger>
-          </Tooltip>
-        ) : null}
-      </Flex>
-      <Dropdown.Portal>
-        <Dropdown.Content
-          sideOffset={5}
-          align="end"
-          css={{ width: '$48', backgroundColor: '$surface_bright', py: '$0', border: '1px solid $border_bright' }}
-        >
-          {options.hide.show ? (
-            <Dropdown.Item data-testid="hide_message_btn" onClick={options.hide.onClick}>
-              {options.hide.icon}
-              <Text variant="sm" css={{ ml: '$4', fontWeight: '$semiBold' }}>
-                {options.hide.text}
-              </Text>
-            </Dropdown.Item>
-          ) : null}
-
-          {options.block.show ? (
-            <Dropdown.Item
-              data-testid="block_peer_btn"
-              onClick={options.block.onClick}
-              css={{ color: options.block.color }}
-            >
-              {options.block.icon}
-              <Text variant="sm" css={{ ml: '$4', color: 'inherit', fontWeight: '$semiBold' }}>
-                {options.block.text}
-              </Text>
-            </Dropdown.Item>
-          ) : null}
-          {options.remove.show ? (
-            <Dropdown.Item
-              data-testid="remove_peer_btn"
-              onClick={options.remove.onClick}
-              css={{ color: options.remove.color }}
-            >
-              {options.remove.icon}
-              <Text variant="sm" css={{ ml: '$4', color: 'inherit', fontWeight: '$semiBold' }}>
-                {options.remove.text}
-              </Text>
-            </Dropdown.Item>
-          ) : null}
-        </Dropdown.Content>
-      </Dropdown.Portal>
-    </Dropdown.Root>
-  );
-};
-
 const SenderName = styled(Text, {
   overflow: 'hidden',
   textOverflow: 'ellipsis',
@@ -591,15 +328,11 @@ const ChatMessage = React.memo(
 );
 const ChatList = React.forwardRef(
   ({ width, height, setRowHeight, getRowHeight, messages, scrollToBottom }, listRef) => {
-    const { setPinnedMessages } = useSetPinnedMessages();
-    const pinnedMessages = useHMSStore(selectSessionStore(SESSION_STORE_KEY.PINNED_MESSAGES)) || [];
-    const localPeerName = useHMSStore(selectLocalPeerName);
     useLayoutEffect(() => {
       if (listRef.current && listRef.current.scrollToItem) {
         scrollToBottom(1);
       }
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [listRef]);
+    }, [listRef, scrollToBottom]);
 
     return (
       <VariableSizeList
@@ -621,7 +354,6 @@ const ChatList = React.forwardRef(
             setRowHeight={setRowHeight}
             isLast={index >= messages.length - 2}
             scrollToBottom={scrollToBottom}
-            onPin={() => setPinnedMessages(pinnedMessages, messages[index], localPeerName)}
           />
         )}
       </VariableSizeList>
@@ -676,11 +408,11 @@ const VirtualizedChatMessages = React.forwardRef(({ messages, scrollToBottom }, 
 
 export const ChatBody = React.forwardRef(({ scrollToBottom }, listRef) => {
   let messages = useHMSStore(selectHMSMessages);
-  const blacklistedMessageIDs = useHMSStore(selectSessionStore(SESSION_STORE_KEY.CHAT_MESSAGE_BLACKLIST)) || [];
-  const getFilteredMessages = () => {
-    const blacklistedMessageIDSet = new Set(blacklistedMessageIDs);
+  const blacklistedMessageIDs = useHMSStore(selectSessionStore(SESSION_STORE_KEY.CHAT_MESSAGE_BLACKLIST));
+  const filteredMessages = useMemo(() => {
+    const blacklistedMessageIDSet = new Set(blacklistedMessageIDs || []);
     return messages?.filter(message => message.type === 'chat' && !blacklistedMessageIDSet.has(message.id)) || [];
-  };
+  }, [blacklistedMessageIDs, messages]);
 
   const isMobile = useMedia(cssConfig.media.md);
   const { elements } = useRoomLayoutConferencingScreen();
@@ -715,7 +447,7 @@ export const ChatBody = React.forwardRef(({ scrollToBottom }, listRef) => {
 
   return (
     <Fragment>
-      <VirtualizedChatMessages messages={getFilteredMessages()} scrollToBottom={scrollToBottom} ref={listRef} />
+      <VirtualizedChatMessages messages={filteredMessages} scrollToBottom={scrollToBottom} ref={listRef} />
     </Fragment>
   );
 });
