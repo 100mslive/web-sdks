109966e756dc91fc12fbdd213b603c47761c6c47
raviteja83 <ravi.theja@100ms.live>
2024-01-08 12:41:15 +0530

    fix: remove references to webapp

--- scripts/update-versions.sh
+++ scripts/update-versions.sh
@@ -2,8 +2,3 @@ lerna version $1 --no-git-tag-version --exact --yes --no-private
 lerna add @100mslive/hms-video-store --peer --scope=@100mslive/hms-virtual-background --exact
 lerna add @100mslive/react-icons --scope=100ms-custom-app --exact
 lerna add @100mslive/roomkit-react --scope=100ms-custom-app --exact
-lerna add @100mslive/roomkit-react --scope=100ms_edtech_template --exact
-lerna add @100mslive/hls-player --scope=100ms_edtech_template --exact
-lerna add @100mslive/hms-virtual-background --scope=100ms_edtech_template --exact
-lerna add @100mslive/react-icons --scope=100ms_edtech_template --exact
-lerna add @100mslive/react-sdk --scope=100ms_edtech_template --exact
