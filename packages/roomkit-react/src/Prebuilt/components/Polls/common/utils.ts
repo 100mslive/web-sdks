9eb401ba4c4b083668c2662ca7949676cab49760
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2024-01-24 12:41:01 +0530

    fix: quiz duration formatting

---
+++ packages/roomkit-react/src/Prebuilt/components/Polls/common/utils.ts
@@ -0,0 +1,11 @@
+export const getFormattedTime = (milliseconds = 0) => {
+  const totalSeconds = milliseconds / 1000;
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds % 60;
+
+  const timeString = `${minutes ? `${minutes}m ` : ''}${
+    Number.isInteger(seconds) || minutes ? seconds.toFixed(0) : seconds.toFixed(1)
+  }s`;
+
+  return timeString;
+};
