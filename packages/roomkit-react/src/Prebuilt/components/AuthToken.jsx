610fa7d43ad5c88bd70c58e97321bb08d7aa8a40
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2024-01-24 17:29:17 +0530

    fix: update error copy

--- packages/roomkit-react/src/Prebuilt/components/AuthToken.jsx
+++ packages/roomkit-react/src/Prebuilt/components/AuthToken.jsx
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { useSessionStorage } from 'react-use';
 import { v4 as uuid } from 'uuid';
 import { useHMSActions } from '@100mslive/react-sdk';
-import { styled } from '../../Theme';
+import { Dialog } from '../../Modal';
+import { Text } from '../../Text';
 import { useHMSPrebuiltContext } from '../AppContext';
-import { ErrorDialog } from '../primitives/DialogContent';
+import errorImage from '../images/transaction_error.svg';
 import { useSetAppDataByKey } from './AppData/useUISettings';
 import { UserPreferencesKeys } from './hooks/useUserPreferences';
 import { APP_DATA } from '../common/constants';
@@ -57,65 +58,68 @@ const AuthToken = React.memo(({ authTokenByRoomCodeEndpoint, defaultAuthToken })
   ]);
 
   if (error.title) {
-    return <ErrorDialog title={error.title}>{error.body}</ErrorDialog>;
+    return (
+      <Dialog.Root open={true}>
+        <Dialog.Content
+          css={{
+            maxWidth: '$100',
+            boxSizing: 'border-box',
+            p: '$10 $12',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <img src={errorImage} height={80} width={80} />
+          <Text variant="h4" css={{ textAlign: 'center', mb: '$4', mt: '$10' }}>
+            {error.title}
+          </Text>
+          <Text css={{ c: '$on_surface_medium', textAlign: 'center' }}>{error.body}</Text>
+        </Dialog.Content>
+      </Dialog.Root>
+    );
   }
   return null;
 });
 
 const convertError = error => {
   console.error('[error]', { error });
+  console.warn(
+    'If you think this is a mistake on our side, please reach out to us over Discord:',
+    'https://discord.com/invite/kGdmszyzq2',
+  );
   if (error.action === 'GET_TOKEN' && error.code === 403) {
     return {
-      title: 'Room code is disabled',
-      body: ErrorWithSupportLink('Room code corresponding to this link is no more active.'),
+      title: 'Psst! This room is currently inactive.',
+      body: 'Please feel free to join another open room for more conversations. Thanks for stopping by!',
     };
   } else if (error.action === 'GET_TOKEN' && error.code === 404) {
     return {
       title: 'Room code does not exist',
-      body: ErrorWithSupportLink('We could not find a room code corresponding to this link.'),
+      body: 'We could not find a room code corresponding to this link.',
     };
   } else if (error.action === 'GET_TOKEN' && error.code === 2003) {
     return {
       title: 'Endpoint is not reachable',
-      body: ErrorWithSupportLink(`Endpoint is not reachable. ${error.description}.`),
+      body: `Endpoint is not reachable. ${error.description}.`,
     };
   } else if (error.response && error.response.status === 404) {
     return {
       title: 'Room does not exist',
-      body: ErrorWithSupportLink('We could not find a room corresponding to this link.'),
+      body: 'We could not find a room corresponding to this link.',
     };
   } else if (error.response && error.response.status === 403) {
     return {
       title: 'Accessing room using this link format is disabled',
-      body: ErrorWithSupportLink('You can re-enable this from the developer section in Dashboard.'),
+      body: 'You can re-enable this from the developer section in Dashboard.',
     };
   } else {
     console.error('Token API Error', error);
     return {
       title: 'Error fetching token',
-      body: ErrorWithSupportLink(
-        'An error occurred while fetching the app token. Please look into logs for more details.',
-      ),
+      body: 'An error occurred while fetching the app token. Please look into logs for more details.',
     };
   }
 };
-
-const Link = styled('a', {
-  color: '#2f80e1',
-});
-
-export const ErrorWithSupportLink = errorMessage => (
-  <div>
-    {errorMessage} If you think this is a mistake on our side, please create{' '}
-    <Link target="_blank" href="https://github.com/100mslive/100ms-web/issues" rel="noreferrer">
-      an issue
-    </Link>{' '}
-    or reach out over{' '}
-    <Link target="_blank" href="https://discord.com/invite/kGdmszyzq2" rel="noreferrer">
-      Discord
-    </Link>
-    .
-  </div>
-);
 
 export default AuthToken;
