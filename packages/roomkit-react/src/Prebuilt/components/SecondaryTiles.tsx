6a26521dc54a1555cf2c60ce5e91e5f07d67542e
raviteja83 <ravi.theja@100ms.live>
2024-02-04 17:55:04 +0530

    fix: sort and show pin/spotlight to first page

--- packages/roomkit-react/src/Prebuilt/components/SecondaryTiles.tsx
+++ packages/roomkit-react/src/Prebuilt/components/SecondaryTiles.tsx
@@ -1,16 +1,39 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useMedia } from 'react-use';
+import { selectAppData, selectSessionStore, selectTrackByID, useHMSStore } from '@100mslive/react-sdk';
 import { LayoutProps } from './VideoLayouts/interface';
 import { ProminenceLayout } from './VideoLayouts/ProminenceLayout';
 import { config as cssConfig } from '../../Theme';
 import { Pagination } from './Pagination';
 import { usePagesWithTiles } from './hooks/useTileLayout';
+import { APP_DATA, SESSION_STORE_KEY } from '../common/constants';
 
 export const SecondaryTiles = ({ peers, onPageChange, onPageSize, edgeToEdge, hasSidebar }: LayoutProps) => {
   const isMobile = useMedia(cssConfig.media.md);
   const maxTileCount = isMobile ? 2 : 4;
-  const pagesWithTiles = usePagesWithTiles({ peers, maxTileCount });
   const [page, setPage] = useState(0);
+  const pinnedTrackId = useHMSStore(selectAppData(APP_DATA.pinnedTrackId));
+  const spotlightPeerId = useHMSStore(selectSessionStore(SESSION_STORE_KEY.SPOTLIGHT));
+  const activeScreensharePeerId = useHMSStore(selectAppData(APP_DATA.activeScreensharePeerId));
+  const pinnedPeer = useHMSStore(selectTrackByID(pinnedTrackId))?.peerId;
+  const pageChangedAfterPinning = useRef(false);
+  const pagesWithTiles = usePagesWithTiles({
+    peers:
+      spotlightPeerId || pinnedPeer
+        ? [...peers].sort((p1, p2) => {
+            const peerIdList = [activeScreensharePeerId, pinnedPeer, spotlightPeerId];
+            // put active screenshare peer, pinned peer, spotlight peer at first
+            if (peerIdList.includes(p1)) {
+              return -1;
+            }
+            if (peerIdList.includes(p2)) {
+              return 1;
+            }
+            return 0;
+          })
+        : peers,
+    maxTileCount,
+  });
   const pageSize = pagesWithTiles[0]?.length || 0;
 
   useEffect(() => {
@@ -18,6 +41,15 @@ export const SecondaryTiles = ({ peers, onPageChange, onPageSize, edgeToEdge, ha
       onPageSize?.(pageSize);
     }
   }, [pageSize, onPageSize]);
+
+  useEffect(() => {
+    if ((pinnedPeer || spotlightPeerId) && page !== 0 && !pageChangedAfterPinning.current) {
+      setPage(0);
+      pageChangedAfterPinning.current = true;
+    } else if (!pinnedPeer && !spotlightPeerId) {
+      pageChangedAfterPinning.current = false;
+    }
+  }, [pinnedPeer, spotlightPeerId, page]);
 
   return (
     <ProminenceLayout.SecondarySection tiles={pagesWithTiles[page]} edgeToEdge={edgeToEdge} hasSidebar={hasSidebar}>
