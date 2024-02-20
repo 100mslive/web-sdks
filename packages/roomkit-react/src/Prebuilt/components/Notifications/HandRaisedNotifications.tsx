47f51e2dd9ce2433499f65b274759ecfac24522f
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2024-02-20 12:37:34 +0530

    fix: check for subscription to hand raise notification

--- packages/roomkit-react/src/Prebuilt/components/Notifications/HandRaisedNotifications.tsx
+++ packages/roomkit-react/src/Prebuilt/components/Notifications/HandRaisedNotifications.tsx
@@ -11,25 +11,34 @@ import {
 // @ts-ignore: No implicit Any
 import { ToastBatcher } from '../Toast/ToastBatcher';
 import { useRoomLayoutConferencingScreen } from '../../provider/roomLayoutProvider/hooks/useRoomLayoutScreen';
+// @ts-ignore: No implicit Any
+import { useSubscribedNotifications } from '../AppData/useUISettings';
 
 export const HandRaisedNotifications = () => {
   const notification = useHMSNotifications(HMSNotificationTypes.HAND_RAISE_CHANGED);
   const roomState = useHMSStore(selectRoomState);
   const vanillaStore = useHMSVanillaStore();
   const { on_stage_exp } = useRoomLayoutConferencingScreen().elements || {};
+  const subscribedNotifications = useSubscribedNotifications() || {};
 
   useEffect(() => {
     if (!notification?.data) {
       return;
     }
-    if (roomState !== HMSRoomState.Connected || notification.data.isLocal || !on_stage_exp) {
+
+    if (
+      roomState !== HMSRoomState.Connected ||
+      notification.data.isLocal ||
+      !on_stage_exp ||
+      !subscribedNotifications.METADATA_UPDATED
+    ) {
       return;
     }
     const hasPeerHandRaised = vanillaStore.getState(selectHasPeerHandRaised(notification.data.id));
     if (hasPeerHandRaised) {
       ToastBatcher.showToast({ notification, type: 'RAISE_HAND' });
     }
-  }, [notification, on_stage_exp, roomState, vanillaStore]);
+  }, [notification, on_stage_exp, roomState, subscribedNotifications.METADATA_UPDATED, vanillaStore]);
 
   return null;
 };
