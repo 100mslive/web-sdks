90cea79b5314c03d04300a72738d8567bd5a9a3a
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2024-02-29 17:52:27 +0530

    fix: decimal places

--- packages/roomkit-react/src/Prebuilt/components/Polls/common/utils.ts
+++ packages/roomkit-react/src/Prebuilt/components/Polls/common/utils.ts
@@ -4,7 +4,7 @@ export const getFormattedTime = (milliseconds: number | undefined, precise = tru
   const totalSeconds = milliseconds / 1000;
   const hours = Math.floor(totalSeconds / 3600);
   const minutes = Math.floor((totalSeconds % 3600) / 60);
-  const seconds = precise ? totalSeconds % 60 : Math.floor(totalSeconds % 60);
+  const seconds = precise ? (totalSeconds % 60).toFixed(3) : Math.floor(totalSeconds % 60);
 
   let formattedTime = '';
   if (hours) {
