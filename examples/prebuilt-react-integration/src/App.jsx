55a2a6e41a769f98c2533f7adc5535be2fd322ff
raviteja83 <ravi.theja@100ms.live>
2024-03-06 18:31:06 +0530

    fix: add beam configuration

--- examples/prebuilt-react-integration/src/App.jsx
+++ examples/prebuilt-react-integration/src/App.jsx
@@ -1,8 +1,41 @@
 import { HMSPrebuilt } from '@100mslive/roomkit-react';
-import { getRoomCodeFromUrl } from './utils';
+// import { getRoomCodeFromUrl } from './utils';
 
 export default function App() {
-  const roomCode = getRoomCodeFromUrl();
+  // const roomCode = getRoomCodeFromUrl();
 
-  return <HMSPrebuilt roomCode={roomCode} />;
+  return (
+    <HMSPrebuilt
+      // roomCode={roomCode}
+      authToken={""}
+      options={{
+        endpoints: {
+          tokenByRoomCode: 'https://auth-nonprod.100ms.live/v2/token',
+          roomLayout: 'https://api-nonprod.100ms.live/v2/layouts/ui',
+          init: 'https://qa-in2-ipv6.100ms.live/init',
+        },
+      }}
+      screens={{
+        preview: null,
+        conferencing: {
+          default: {
+            hideSections: ['footer', 'header'],
+            elements: {
+              video_tile_layout: {
+                grid: {
+                  enable_local_tile_inset: false,
+                  hide_participant_name_on_tile: true,
+                  rounded_video_tile: false,
+                  hide_audio_mute_on_tile: true,
+                  video_object_fit: 'contain',
+                  edge_to_edge: true,
+                  hide_metadata_on_tile: true,
+                },
+              },
+            },
+          },
+        },
+      }}
+    />
+  );
 }
