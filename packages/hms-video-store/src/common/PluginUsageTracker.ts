95e6d0ce264594a8da21945c9ddc99b833c19c49
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2024-03-12 14:19:19 +0530

    fix: refactor key interpolation

--- packages/hms-video-store/src/common/PluginUsageTracker.ts
+++ packages/hms-video-store/src/common/PluginUsageTracker.ts
@@ -5,31 +5,31 @@ class PluginUsageTracker {
   private pluginLastAddedAt: Map<string, number> = new Map<string, number>();
 
   getPluginUsage = (name: string, sessionID: string) => {
-    if (!this.pluginUsage.has(`${sessionID}-${name}`)) {
-      this.pluginUsage.set(`${sessionID}-${name}`, 0);
+    const pluginKey = `${sessionID}-${name}`;
+
+    if (!this.pluginUsage.has(pluginKey)) {
+      this.pluginUsage.set(pluginKey, 0);
     }
-    if (this.pluginLastAddedAt.has(`${sessionID}-${name}`)) {
-      const lastAddedAt = this.pluginLastAddedAt.get(`${sessionID}-${name}`) || 0;
+    if (this.pluginLastAddedAt.has(pluginKey)) {
+      const lastAddedAt = this.pluginLastAddedAt.get(pluginKey) || 0;
       const extraDuration = lastAddedAt ? Date.now() - lastAddedAt : 0;
-      this.pluginUsage.set(`${sessionID}-${name}`, (this.pluginUsage.get(`${sessionID}-${name}`) || 0) + extraDuration);
-      this.pluginLastAddedAt.delete(`${sessionID}-${name}`);
+      this.pluginUsage.set(pluginKey, (this.pluginUsage.get(pluginKey) || 0) + extraDuration);
+      this.pluginLastAddedAt.delete(pluginKey);
     }
-    return this.pluginUsage.get(`${sessionID}-${name}`);
+    return this.pluginUsage.get(pluginKey);
   };
 
   updatePluginUsage = (event: AnalyticsEvent, sessionID: string) => {
+    const name = event.properties.plugin_name;
+    const pluginKey = `${sessionID}-${name}`;
+
     if (event.name === 'mediaPlugin.added') {
-      const name = event.properties.plugin_name;
       const addedAt = event.properties.added_at;
-      this.pluginLastAddedAt.set(`${sessionID}-${name}`, addedAt);
+      this.pluginLastAddedAt.set(pluginKey, addedAt);
     } else if (event.name === 'mediaPlugin.stats') {
-      const name = event.properties.plugin_name;
       const duration = event.properties.duration;
-      this.pluginUsage.set(
-        `${sessionID}-${name}`,
-        (this.pluginUsage.get(`${sessionID}-${name}`) || 0) + duration * 1000,
-      );
-      this.pluginLastAddedAt.delete(`${sessionID}-${name}`);
+      this.pluginUsage.set(pluginKey, (this.pluginUsage.get(pluginKey) || 0) + duration * 1000);
+      this.pluginLastAddedAt.delete(pluginKey);
     }
   };
 
