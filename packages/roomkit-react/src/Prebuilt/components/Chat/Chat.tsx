a3d189283f25a09c9d50831b9ed062ced2071dc2
Kaustubh Kumar <kaustubhkumar05@gmail.com>
2024-04-05 17:01:11 +0530

    fix: add null check for outerElement of chat virtualised list

--- packages/roomkit-react/src/Prebuilt/components/Chat/Chat.tsx
+++ packages/roomkit-react/src/Prebuilt/components/Chat/Chat.tsx
@@ -130,7 +130,10 @@ const NewMessageIndicator = ({
   }
   // @ts-ignore
   const outerElement = listRef.current._outerRef;
-  if (outerElement.clientHeight + outerElement.scrollTop + outerElement.offsetTop >= outerElement.scrollHeight) {
+  if (
+    outerElement &&
+    outerElement.clientHeight + outerElement.scrollTop + outerElement.offsetTop >= outerElement.scrollHeight
+  ) {
     return null;
   }
 
