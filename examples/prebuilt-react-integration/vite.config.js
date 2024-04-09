594348c1e303793548217ee815798241673a65bc
eswarclynn <esuclintblazer@gmail.com>
2024-04-09 11:34:14 +0530

    fix: inject css library

--- examples/prebuilt-react-integration/vite.config.js
+++ examples/prebuilt-react-integration/vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [cssInjectedByJsPlugin(), react()],
   define: {
     'process.env': {},
   },
