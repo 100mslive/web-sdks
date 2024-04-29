04f9d064e5c93eced282872b68e28f709cd747d7
amar-1995 <amar@100ms.live>
2024-04-29 10:06:28 +0530

    fix: added wait for 55 secs

--- packages/hms-video-store/src/sdk/index.ts
+++ packages/hms-video-store/src/sdk/index.ts
@@ -515,7 +515,7 @@ export class HMSSdk implements HMSInterface {
   // eslint-disable-next-line complexity
   async join(config: HMSConfig, listener: HMSUpdateListener) {
     console.log('join called before delay');
-    await this.wait(60000);
+    await this.wait(55000);
     console.log('join after delay');
     validateMediaDevicesExistence();
     validateRTCPeerConnection();
