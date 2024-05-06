eb85e9a0bd6da745915b003edfc36225e42f3a90
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2024-05-06 19:10:50 +0530

    fix: border

--- packages/roomkit-react/src/Prebuilt/components/RoleChangeModal.tsx
+++ packages/roomkit-react/src/Prebuilt/components/RoleChangeModal.tsx
@@ -166,7 +166,7 @@ export const RoleChangeModal = ({
   if (isMobile) {
     return (
       <Sheet.Root open={true} onOpenChange={onOpenChange} css={{ borderRadius: '$0 $0 0 0' }}>
-        <Sheet.Content css={{ p: '$10 $8', background: '$surface_default' }}>
+        <Sheet.Content css={{ p: '$10 $8', background: '$surface_dim', border: '1px solid $border_default' }}>
           <RoleChangeContent peer={peer} onOpenChange={onOpenChange} isMobile />
         </Sheet.Content>
       </Sheet.Root>
