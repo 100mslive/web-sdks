40f7c7f82e9d58dd0d08dc8c4bfd087509814ecd
Kaustubh Kumar <kaustubhkumar05@gmail.com>
2024-05-17 13:36:51 +0530

    feat: custom ice servers in join config

--- packages/hms-video-store/src/sdk/index.ts
+++ packages/hms-video-store/src/sdk/index.ts
@@ -424,6 +424,7 @@ export class HMSSdk implements HMSInterface {
           this.localPeer!.peerId,
           { name: config.userName, metaData: config.metaData || '' },
           config.autoVideoSubscribe,
+          config.iceServers,
         )
         .then((initConfig: InitConfig | void) => {
           initSuccessful = true;
