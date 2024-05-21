377e5b41bccd8ca874738f106455e1acbb7c5918
Amar Bathwal <110378139+amar-1995@users.noreply.github.com>
2024-05-21 15:50:13 +0530

    feat: empty screen for viewers

--- packages/roomkit-react/src/Prebuilt/components/AppData/AppData.tsx
+++ packages/roomkit-react/src/Prebuilt/components/AppData/AppData.tsx
@@ -21,7 +21,6 @@ import { useSetAppDataByKey } from './useUISettings';
 import {
   APP_DATA,
   CHAT_SELECTOR,
-  DEFAULT_WAITING_VIEWER_ROLE,
   POLL_STATE,
   SIDE_PANE_OPTIONS,
   UI_MODE_GRID,
@@ -56,7 +55,6 @@ const initialAppData = {
   [APP_DATA.hlsStarted]: false,
   [APP_DATA.rtmpStarted]: false,
   [APP_DATA.recordingStarted]: false,
-  [APP_DATA.waitingViewerRole]: DEFAULT_WAITING_VIEWER_ROLE,
   [APP_DATA.dropdownList]: [],
   [APP_DATA.authToken]: '',
   [APP_DATA.minimiseInset]: false,
