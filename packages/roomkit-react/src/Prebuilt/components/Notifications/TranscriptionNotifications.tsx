a679023460df44936f8322256d12eb91c87a768d
raviteja83 <ravi.theja@100ms.live>
2024-05-29 11:53:44 +0530

    refactor: transcription notifications

---
+++ packages/roomkit-react/src/Prebuilt/components/Notifications/TranscriptionNotifications.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect } from 'react';
+import { match } from 'ts-pattern';
+import { HMSNotificationTypes, HMSTranscriptionState, useHMSNotifications } from '@100mslive/react-sdk';
+import { AlertTriangleIcon, ClosedCaptionIcon, OpenCaptionIcon } from '@100mslive/react-icons';
+// @ts-ignore: No implicit Any
+import { ToastManager } from '../Toast/ToastManager';
+// @ts-ignore: No implicit Any
+import { useSetAppDataByKey } from '../AppData/useUISettings';
+import { CAPTION_TOAST } from '../../common/constants';
+
+export const TranscriptionNotifications = () => {
+  const notification = useHMSNotifications(HMSNotificationTypes.TRANSCRIPTION_STATE_UPDATED);
+  const [toastId, setToastId] = useSetAppDataByKey(CAPTION_TOAST.captionToast);
+
+  useEffect(() => {
+    if (!notification?.data) {
+      return;
+    }
+
+    console.debug(`[${notification.type}]`, notification);
+    const transcriptionStates = notification.data;
+    if (transcriptionStates.length > 0) {
+      let id = '';
+      match({ state: transcriptionStates[0].state, error: transcriptionStates[0].error })
+        .when(
+          ({ error }) => !!error,
+          () => {
+            ToastManager.removeToast(toastId);
+            id = ToastManager.addToast({
+              title: `Failed to enable Closed Caption`,
+              variant: 'error',
+              icon: <AlertTriangleIcon style={{ marginRight: '0.5rem' }} />,
+            });
+          },
+        )
+        .with({ state: HMSTranscriptionState.STARTED }, () => {
+          ToastManager.removeToast(toastId);
+          id = ToastManager.addToast({
+            title: `Closed Captioning enabled for everyone`,
+            variant: 'standard',
+            duration: 2000,
+            icon: <OpenCaptionIcon style={{ marginRight: '0.5rem' }} />,
+          });
+        })
+        .with({ state: HMSTranscriptionState.STOPPED }, () => {
+          ToastManager.removeToast(toastId);
+          id = ToastManager.addToast({
+            title: `Closed Captioning disabled for everyone`,
+            variant: 'standard',
+            duration: 2000,
+            icon: <ClosedCaptionIcon style={{ marginRight: '0.5rem' }} />,
+          });
+        })
+        .otherwise(() => null);
+      setToastId(id);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [notification, setToastId]);
+
+  return null;
+};
