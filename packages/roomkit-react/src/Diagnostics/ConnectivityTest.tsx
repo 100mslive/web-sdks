8e77bb7e5d75c86da9cc3b8eeed1a1041561e8b9
eswarclynn <esuclintblazer@gmail.com>
2024-06-12 23:57:33 +0530

    fix: connectivity test report

--- packages/roomkit-react/src/Diagnostics/ConnectivityTest.tsx
+++ packages/roomkit-react/src/Diagnostics/ConnectivityTest.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { ConnectivityCheckResult, ConnectivityState } from '@100mslive/react-sdk';
-import { CrossCircleIcon } from '@100mslive/react-icons';
-import { Box } from '../Layout';
+import { CheckCircleIcon, CrossCircleIcon, LinkIcon } from '@100mslive/react-icons';
+import { Box, Flex } from '../Layout';
 import { Loading } from '../Loading';
 import { Text } from '../Text';
 import { hmsDiagnostics } from './hms';
@@ -16,11 +16,107 @@ const ConnectivityStateMessage = {
   [ConnectivityState.COMPLETED]: 'Completed',
 };
 
-export const ConnectivityTestStepResult = ({ title }: { title: string }) => {
+export const ConnectivityTestStepResult = ({
+  title,
+  success,
+  children,
+}: {
+  title: string;
+  success?: boolean;
+  children: React.ReactNode;
+}) => {
   return (
     <Box css={{ my: '$10', p: '$10', r: '$1', bg: '$surface_bright' }}>
-      <Text css={{ c: '$on_primary_medium' }}>{title}</Text>
+      <Text css={{ c: '$on_primary_medium', mb: '$6' }}>{title}</Text>
+      {success ? (
+        <Flex>
+          <Text css={{ c: '$alert_success' }}>
+            <CheckCircleIcon width="1.5rem" height="1.5rem" />
+          </Text>
+          <Text variant="lg" css={{ ml: '$4' }}>
+            Connected
+          </Text>
+        </Flex>
+      ) : (
+        <Flex>
+          <Text css={{ c: '$alert_error_bright' }}>
+            <CrossCircleIcon width="1.5rem" height="1.5rem" />
+          </Text>
+          <Text variant="lg" css={{ ml: '$4' }}>
+            Failed
+          </Text>
+        </Flex>
+      )}
+      <Box>{children}</Box>
     </Box>
+  );
+};
+
+const DetailedInfo = ({
+  title,
+  value,
+  Icon,
+}: {
+  title: string;
+  value: string;
+  Icon?: (props: React.SVGProps<SVGSVGElement>) => React.JSX.Element;
+}) => {
+  return (
+    <Box css={{ w: '50%', mt: '$6' }}>
+      <Text variant="caption" css={{ fontWeight: '$semiBold', c: '$on_primary_medium' }}>
+        {title}
+      </Text>
+      <Flex css={{ mt: '$xs', alignItems: 'flex-start' }}>
+        {Icon && (
+          <Text>
+            <Icon width="1rem" height="1rem" />
+          </Text>
+        )}
+        <Text variant="caption" css={{ ml: '$4' }}>
+          {value}
+        </Text>
+      </Flex>
+    </Box>
+  );
+};
+
+const MediaServerResult = ({ result }: { result?: ConnectivityCheckResult['mediaServerReport'] }) => {
+  return (
+    <ConnectivityTestStepResult
+      title="Media server connection test"
+      success={result?.isPublishICEConnected && result.isSubscribeICEConnected}
+    >
+      <Flex>
+        <DetailedInfo
+          title="Media Captured"
+          value={result?.stats?.audio.bytesSent ? 'Yes' : 'No'}
+          Icon={result?.stats?.audio.bytesSent ? CheckCircleIcon : CrossCircleIcon}
+        />
+        <DetailedInfo
+          title="Media Published"
+          value={result?.stats?.audio.bitrateSent ? 'Yes' : 'No'}
+          Icon={result?.stats?.audio.bytesSent ? CheckCircleIcon : CrossCircleIcon}
+        />
+        {result?.connectionQualityScore ? (
+          <DetailedInfo title="Connection Quality Score (CQS)" value={`${result.connectionQualityScore} (out of 5)`} />
+        ) : null}
+      </Flex>
+    </ConnectivityTestStepResult>
+  );
+};
+
+const SignallingResult = ({ result }: { result?: ConnectivityCheckResult['signallingReport'] }) => {
+  return (
+    <ConnectivityTestStepResult title="Signalling server connection test" success={result?.isConnected}>
+      <Flex>
+        <DetailedInfo
+          title="Signalling Gateway"
+          value={result?.isConnected ? 'Reachable' : 'Unreachable'}
+          Icon={result?.isConnected ? CheckCircleIcon : CrossCircleIcon}
+        />
+        <DetailedInfo title="Websocket URL" value={result?.websocketUrl || 'N/A'} Icon={LinkIcon} />
+      </Flex>
+    </ConnectivityTestStepResult>
   );
 };
 
@@ -59,23 +155,28 @@ export const ConnectivityTest = () => {
   }
 
   if (result) {
-    console.log(result.errors);
+    console.log(result);
     return (
       <Box css={{ w: '100%' }}>
         <Text css={{ c: '$on_primary_medium' }}>Connectivity test has been completed.</Text>
-        <ConnectivityTestStepResult title="Signalling server connection test" />
-        <ConnectivityTestStepResult title="Media server connection test" />
+        <SignallingResult result={result?.signallingReport} />
+        <MediaServerResult result={result?.mediaServerReport} />
       </Box>
     );
   }
 
   return (
     <Box css={{ w: '100%', textAlign: 'center' }}>
-      <Text css={{ c: '$primary_bright', mb: '$4' }}>
-        <Loading />
+      <Text css={{ c: '$primary_bright' }}>
+        <Loading size="3.5rem" color="currentColor" />
       </Text>
-      <Text variant="h6">Checking your connection...</Text>
-      <Text variant="body2">{`${ConnectivityStateMessage[progress]}...`}</Text>
+      <Text variant="h6" css={{ mt: '$8' }}>
+        Checking your connection...
+      </Text>
+      <Text
+        variant="body2"
+        css={{ c: '$on_primary_medium', mt: '$4' }}
+      >{`${ConnectivityStateMessage[progress]}...`}</Text>
     </Box>
   );
 };
