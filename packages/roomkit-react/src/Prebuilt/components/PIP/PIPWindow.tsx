3a04f557e2e25e82ac7dbba524bb232312e234d1
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2024-07-02 13:16:02 +0530

    fix: embed is not reactive

--- packages/roomkit-react/src/Prebuilt/components/PIP/PIPWindow.tsx
+++ packages/roomkit-react/src/Prebuilt/components/PIP/PIPWindow.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
+// import React from 'react';
+import { createPortal } from 'react-dom';
+
 type PIPWindowProps = {
   pipWindow: Window;
   children: React.ReactNode;
 };
 
 export const PIPWindow = ({ pipWindow, children }: PIPWindowProps) => {
-  const container = pipWindow.document.createElement('div');
-  pipWindow.document.body.appendChild(container);
-  return <div ref={node => node && container.appendChild(node)}>{children}</div>;
+  return createPortal(children, pipWindow.document.body);
 };
+
+// export const PIPWindow = ({ pipWindow, children }: PIPWindowProps) => {
+//   const container = pipWindow.document.createElement('div');
+//   pipWindow.document.body.appendChild(container);
+//   return <div ref={node => node && container.appendChild(node)}>{children}</div>;
+// };
