d172ccfb28a077a5b1add902ae04059a002833d9
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2024-07-03 15:02:06 +0530

    fix: dynamic require of react

--- packages/roomkit-react/src/Prebuilt/components/PIP/PIPWindow.tsx
+++ packages/roomkit-react/src/Prebuilt/components/PIP/PIPWindow.tsx
@@ -1,4 +1,4 @@
-// import React from 'react';
+import React from 'react';
 import { createPortal } from 'react-dom';
 
 type PIPWindowProps = {
@@ -9,9 +9,3 @@ type PIPWindowProps = {
 export const PIPWindow = ({ pipWindow, children }: PIPWindowProps) => {
   return createPortal(children, pipWindow.document.body);
 };
-
-// export const PIPWindow = ({ pipWindow, children }: PIPWindowProps) => {
-//   const container = pipWindow.document.createElement('div');
-//   pipWindow.document.body.appendChild(container);
-//   return <div ref={node => node && container.appendChild(node)}>{children}</div>;
-// };
