ea9d18998ae6fe9f184e9f9dad0e39fb5bdfad29
Gulzar <gulzarhome54@gmail.com>
2024-07-08 10:50:20 +0400

    Update HMSNotificationMethod.ts

--- packages/hms-video-store/src/notification-manager/HMSNotificationMethod.ts
+++ packages/hms-video-store/src/notification-manager/HMSNotificationMethod.ts
@@ -36,5 +36,5 @@ export enum HMSNotificationMethod {
   ROOM_INFO = 'room-info',
   SESSION_INFO = 'session-info',
   WHITEBOARD_UPDATE = 'on-whiteboard-update',
-  ON_PEER_SFU_MIGRATE = "on-peer-sfu-migrate",
+  ON_PEER_SFU_MIGRATE = 'on-peer-sfu-migrate',
 }
