a953646d7f912c282e6ab7ff15ff514e2552673f
Kaustubh Kumar <kaustubhkumar05@gmail.com>
2024-07-19 15:27:27 +0530

    fix: skip notification request for beam

--- packages/react-sdk/src/hooks/useAwayNotifications.ts
+++ packages/react-sdk/src/hooks/useAwayNotifications.ts
@@ -1,17 +1,17 @@
 // write a hook to use the MDN notifications API to show a notification when the user is away from the page
 import { useCallback } from 'react';
-import { selectLocalPeerRole } from '@100mslive/hms-video-store';
+import { selectLocalPeerRoleName } from '@100mslive/hms-video-store';
 import { useHMSStore } from '../primitives/HmsRoomProvider';
 
 // Do not prompt if preview is not available. Skips for beam
 export const useAwayNotifications = () => {
-  const localPeerRole = useHMSStore(selectLocalPeerRole)?.name;
+  const localPeerRoleName = useHMSStore(selectLocalPeerRoleName);
   const requestPermission = useCallback(async () => {
-    if (!Notification || Notification?.permission === 'granted' || localPeerRole === '__internal_recorder') {
+    if (!Notification || Notification?.permission === 'granted' || localPeerRoleName === '__internal_recorder') {
       return;
     }
     await Notification.requestPermission();
-  }, []);
+  }, [localPeerRoleName]);
 
   const showNotification = useCallback((title: string, options?: NotificationOptions) => {
     if (
