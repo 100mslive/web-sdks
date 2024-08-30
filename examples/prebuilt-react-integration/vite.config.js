fad96a0dd96d59de887eee1fdf76424cc70879e7
raviteja83 <ravi.theja@100ms.live>
2024-08-30 15:26:25 +0530

    fix: build issue

--- examples/prebuilt-react-integration/vite.config.js
+++ examples/prebuilt-react-integration/vite.config.js
@@ -1,9 +1,26 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { basename } from 'path';
+import fs from 'fs';
+
+function mediapipe_workaround() {
+  return {
+    name: 'mediapipe_workaround',
+    load(id) {
+      if (basename(id) === 'selfie_segmentation.js') {
+        let code = fs.readFileSync(id, 'utf-8');
+        code += 'exports.SelfieSegmentation = SelfieSegmentation;';
+        return { code };
+      } else {
+        return null;
+      }
+    },
+  };
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), mediapipe_workaround()],
   define: {
     'process.env': {},
   },
