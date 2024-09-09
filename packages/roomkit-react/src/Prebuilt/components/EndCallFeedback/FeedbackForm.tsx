0fe4017f43d6921ab0c89d5bc8f766e93a2b5c28
Amar Bathwal <110378139+amar-1995@users.noreply.github.com>
2024-09-09 11:23:36 +0530

    fix: horizontal scroll issue mweb for call end feedback

--- packages/roomkit-react/src/Prebuilt/components/EndCallFeedback/FeedbackForm.tsx
+++ packages/roomkit-react/src/Prebuilt/components/EndCallFeedback/FeedbackForm.tsx
@@ -39,7 +39,7 @@ export const FeedbackModal = ({
     return (
       <Sheet.Root open={index !== FEEBACK_INDEX.INIT} onOpenChange={onOpenChange}>
         <Sheet.Content
-          css={{ bg: '$surface_dim', p: '$10' }}
+          css={{ bg: '$surface_dim', p: '$10', overflowY: 'auto' }}
           onPointerDownOutside={avoidDefaultDomBehavior}
           onInteractOutside={avoidDefaultDomBehavior}
         >
