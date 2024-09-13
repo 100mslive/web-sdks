3af74400c19818fb60404e519e1d9d597e2c13af
Amar Bathwal <110378139+amar-1995@users.noreply.github.com>
2024-09-13 16:58:01 +0530

    fix: make hls player take more space when in fullscreen

--- packages/roomkit-react/src/Prebuilt/layouts/HLSView.jsx
+++ packages/roomkit-react/src/Prebuilt/layouts/HLSView.jsx
@@ -485,11 +485,6 @@ const HLSView = () => {
       css={{
         flex: isLandscape ? '2 1 0' : '1 1 0',
         transition: 'all 0.3s ease-in-out',
-        '&:fullscreen': {
-          '& video': {
-            height: 'unset !important',
-          },
-        },
       }}
     >
       {hlsViewRef.current && (isMobile || isLandscape) && (
@@ -541,6 +536,7 @@ const HLSView = () => {
             onMouseMove={onHoverHandler}
             onMouseLeave={onHoverHandler}
             onClick={onClickHandler}
+            isFullScreen={isFullScreen}
             onDoubleClick={e => {
               onDoubleClickHandler(e);
             }}
@@ -728,6 +724,7 @@ const HLSView = () => {
                           selection={currentSelectedQuality}
                           onQualityChange={handleQuality}
                           isAuto={isUserSelectedAuto}
+                          containerRef={hlsViewRef.current}
                         />
                       ) : null}
                       {isFullScreenSupported ? (
