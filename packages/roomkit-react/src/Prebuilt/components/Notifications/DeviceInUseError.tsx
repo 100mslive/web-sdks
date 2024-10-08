17d3b0bd5d3c05cfb7f61bd2a726c1b262163bea
eswarclynn <esuclintblazer@gmail.com>
2024-10-08 13:37:04 +0530

    fix: device type

--- packages/roomkit-react/src/Prebuilt/components/Notifications/DeviceInUseError.tsx
+++ packages/roomkit-react/src/Prebuilt/components/Notifications/DeviceInUseError.tsx
@@ -16,17 +16,35 @@ const Instruction = ({ number, description }: { number: string; description: str
 export function DeviceInUseError() {
   const notification = useHMSNotifications(HMSNotificationTypes.ERROR);
   const [showDeviceInUseModal, setShowDeviceInUseModal] = useState(false);
+  const [deviceType, setDeviceType] = useState('');
 
   useEffect(() => {
-    if (notification?.data?.code === 3003) {
-      ToastManager.addToast({
-        title: `Error: ${notification.data?.message} - ${notification.data?.description}`,
-        action: (
-          <Button outlined variant="standard" css={{ w: 'max-content' }} onClick={() => setShowDeviceInUseModal(true)}>
-            Help
-          </Button>
-        ),
-      });
+    const error = notification?.data;
+    if (!error || error.code !== 3003) {
+      return;
+    }
+
+    const errorMessage = error?.message;
+    ToastManager.addToast({
+      title: `Error: ${errorMessage} - ${error?.description}`,
+      action: (
+        <Button outlined variant="standard" css={{ w: 'max-content' }} onClick={() => setShowDeviceInUseModal(true)}>
+          Help
+        </Button>
+      ),
+    });
+
+    const hasAudio = errorMessage.includes('audio');
+    const hasVideo = errorMessage.includes('video');
+    const hasScreen = errorMessage.includes('screen');
+    if (hasAudio && hasVideo) {
+      setDeviceType('camera and microphone');
+    } else if (hasAudio) {
+      setDeviceType('microphone');
+    } else if (hasVideo) {
+      setDeviceType('camera');
+    } else if (hasScreen) {
+      setDeviceType('screen');
     }
   }, [notification]);
 
@@ -40,17 +58,17 @@ export function DeviceInUseError() {
       <DialogContent title="Device Access Error">
         <DialogRow>
           <Text variant="body2">
-            We weren't able to access your camera since it's either in use by another application or is not configured
-            properly. Please follow the following instructions to resolve this issue.
+            We weren't able to access your {deviceType} since it's either in use by another application or is not
+            configured properly. Please follow the following instructions to resolve this issue.
           </Text>
         </DialogRow>
         <Instruction
           number="1"
-          description="Please check if the camera is in use by another browser or application and close it."
+          description={`Please check if the ${deviceType} device(s) are in use by another browser or application and close it.`}
         />
         <Instruction
           number="2"
-          description="Go to Browser Settings > Privacy and security > Site settings > Camera and check if your preferred device is selected as default."
+          description={`Go to Browser Settings > Privacy and security > Site settings > ${deviceType} and check if your preferred device is selected as default.`}
         />
         <Instruction number="3" description="Try restarting the browser." />
         <Instruction
