04c1ab66899607f15306b48b6b00b8bc9254aaea
KaustubhKumar05 <kaustubhkumar05@gmail.com>
2024-10-30 11:34:54 +0530

    fix: use gap var

--- packages/roomkit-react/src/Prebuilt/components/hooks/useTileLayout.tsx
+++ packages/roomkit-react/src/Prebuilt/components/hooks/useTileLayout.tsx
@@ -118,8 +118,8 @@ export const useTileLayout = ({
         // Beam layout breaks at 480p resolution because the gap of $4 between tiles is not accounted for
         // There could be more such cases, this is a generic fix
         for (let i = 0; i < row.length; i++) {
-          row[i].width = tileWidth - (8 / maxCols) * (maxCols - 1);
-          row[i].height = tileHeight - (8 / maxRows) * (maxRows - 1);
+          row[i].width = tileWidth - (gap / maxCols) * (maxCols - 1);
+          row[i].height = tileHeight - (gap / maxRows) * (maxRows - 1);
         }
       }
     }
