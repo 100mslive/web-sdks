8e5a73468ed4f51cd718bb22096d2522efa637c2
Kaustubh Kumar <kaustubhkumar05@gmail.com>
2024-11-15 11:36:46 +0530

    fix: animate resize to prevent flickers (#3303)

--- packages/roomkit-react/src/Prebuilt/components/VideoLayouts/Grid.tsx
+++ packages/roomkit-react/src/Prebuilt/components/VideoLayouts/Grid.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AnimatePresence } from 'framer-motion';
 import { TrackWithPeerAndDimensions } from '@100mslive/react-sdk';
 import { Box } from '../../../Layout';
 // @ts-ignore: No implicit Any
@@ -9,35 +10,37 @@ export const Grid = React.forwardRef<HTMLDivElement, { tiles: TrackWithPeerAndDi
   ({ tiles, edgeToEdge }, ref) => {
     const videoTileProps = useVideoTileContext();
     return (
-      <Box
-        ref={ref}
-        css={{
-          flex: '1 1 0',
-          gap: '$4',
-          display: 'flex',
-          placeContent: 'center',
-          alignItems: 'center',
-          justifyContent: 'center',
-          flexFlow: 'row wrap',
-          minHeight: 0,
-          '@lg': { gap: edgeToEdge ? 0 : '$4' },
-        }}
-      >
-        {tiles?.map(tile => {
-          return (
-            <VideoTile
-              key={tile.track?.id || tile.peer?.id}
-              width={tile.width}
-              height={tile.height}
-              peerId={tile.peer?.id}
-              trackId={tile.track?.id}
-              rootCSS={{ padding: 0 }}
-              objectFit="contain"
-              {...videoTileProps}
-            />
-          );
-        })}
-      </Box>
+      <AnimatePresence>
+        <Box
+          ref={ref}
+          css={{
+            flex: '1 1 0',
+            gap: '$4',
+            display: 'flex',
+            placeContent: 'center',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexFlow: 'row wrap',
+            minHeight: 0,
+            '@lg': { gap: edgeToEdge ? 0 : '$4' },
+          }}
+        >
+          {tiles?.map(tile => {
+            return (
+              <VideoTile
+                key={tile.track?.id || tile.peer?.id}
+                width={tile.width}
+                height={tile.height}
+                peerId={tile.peer?.id}
+                trackId={tile.track?.id}
+                rootCSS={{ padding: 0 }}
+                objectFit="contain"
+                {...videoTileProps}
+              />
+            );
+          })}
+        </Box>
+      </AnimatePresence>
     );
   },
 );
