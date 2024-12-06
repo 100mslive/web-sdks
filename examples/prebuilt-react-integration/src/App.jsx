5b4e5edc32f8fbb2e2ba04d841a8908bd026255a
raviteja83 <ravi.theja@100ms.live>
2024-12-06 19:59:30 +0530

    fix: add zipy for testing

--- examples/prebuilt-react-integration/src/App.jsx
+++ examples/prebuilt-react-integration/src/App.jsx
@@ -1,9 +1,15 @@
 import { HMSPrebuilt, Diagnostics } from '@100mslive/roomkit-react';
 import { getRoomCodeFromUrl } from './utils';
+import { init } from 'zipyai';
+import { useEffect } from 'react';
 
 export default function App() {
   const roomCode = getRoomCodeFromUrl();
   const isDiagnostics = location.pathname.startsWith('/diagnostics');
+
+  useEffect(() => {
+    init('d3c5934b')
+  }, []);
 
   if (isDiagnostics) {
     return <Diagnostics />;
