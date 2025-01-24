6408d976b8139e48d78b3a7c87ecfe7868689411
Alden Quimby <aldenquimby@gmail.com>
2025-01-24 12:50:20 -0500

    feat(prebuilt): allow custom footer in participant side pane

---
+++ packages/roomkit-react/src/Prebuilt/types.d.ts
@@ -0,0 +1,7 @@
+import type { ReactNode } from 'react';
+
+declare module '@100mslive/types-prebuilt/elements/participant_list' {
+  export interface ParticipantList {
+    footer?: ReactNode;
+  }
+}
