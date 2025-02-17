79fb7b2d3dc40794c552eba5abed7c9e1a02a375
raviteja83 <ravi.theja@100ms.live>
2025-02-17 14:53:19 +0530

    fix: add more logs

--- packages/hms-video-store/src/device-manager/DeviceManager.ts
+++ packages/hms-video-store/src/device-manager/DeviceManager.ts
@@ -484,6 +484,7 @@ export class DeviceManager implements HMSDeviceManager {
   // eslint-disable-next-line complexity
   public autoSelectAudioOutput = async () => {
     // do this only after join so the earpiece would be selected at the right time
+    console.log('joinedAR', this.store.getLocalPeer()?.joinedAt);
     if ('ondevicechange' in navigator.mediaDevices || !this.store.getLocalPeer()?.joinedAt) {
       return;
     }
