c9e630f7f067b04501ce2e8bdfc8b1bf0dfeca2c
raviteja83 <ravi.theja@100ms.live>
2025-03-11 14:52:19 +0530

    fix: remove room code

--- examples/next-js-sample/src/app/page.js
+++ examples/next-js-sample/src/app/page.js
@@ -4,5 +4,5 @@ const HMSPrebuilt = dynamic(() => import('@100mslive/roomkit-react').then(module
   ssr: false,
 });
 export default function Home() {
-  return <HMSPrebuilt roomCode="thirsty-malachite-quail" />;
+  return <HMSPrebuilt roomCode="<roomcode>" />;
 }
