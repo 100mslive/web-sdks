f6c7b961ee7e609c9bb63c0a591907e975f1c75d
raviteja83 <ravi.theja@100ms.live>
2025-03-13 18:10:39 +0530

    fix: reduce delay to 500

--- packages/hms-video-store/src/device-manager/DeviceManager.ts
+++ packages/hms-video-store/src/device-manager/DeviceManager.ts
@@ -506,7 +506,7 @@ export class DeviceManager implements HMSDeviceManager {
         }
 
         await localAudioTrack.setSettings({ deviceId: earpiece?.deviceId }, true);
-        await sleep(1000);
+        await sleep(500);
         this.earpieceSelected = true;
       }
       await localAudioTrack.setSettings(
