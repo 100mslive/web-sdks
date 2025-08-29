6b925d39a77ba44a643fef003d7e1ab08eccaa8c
raviteja83 <ravi.theja@100ms.live>
2025-08-29 21:58:58 +0530

    fix: remove HMSPrebuilt component and replace with iframe for app integration

--- examples/prebuilt-react-integration/src/App.jsx
+++ examples/prebuilt-react-integration/src/App.jsx
@@ -1,13 +1,18 @@
-import { HMSPrebuilt, Diagnostics } from '@100mslive/roomkit-react';
-import { getRoomCodeFromUrl } from './utils';
+import { Diagnostics } from '@100mslive/roomkit-react';
 
 export default function App() {
-  const roomCode = getRoomCodeFromUrl();
   const isDiagnostics = location.pathname.startsWith('/diagnostics');
 
   if (isDiagnostics) {
     return <Diagnostics />;
   }
 
-  return <HMSPrebuilt roomCode={roomCode} />;
+  return (
+    <iframe
+      title="100ms-app"
+      allow="camera *;microphone *;display-capture *"
+      src="https://ravitheja.app.100ms.live/preview/thirsty-malachite-quail"
+      style={{ height: '100vh', width: '640px', border: 0 }}
+    ></iframe>
+  );
 }
