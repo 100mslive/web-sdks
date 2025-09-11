b902d24d5a38c5d99ef4ca4b5f51bdd2d5a0fab5
raviteja83 <ravi.theja@100ms.live>
2025-09-11 10:36:20 +0530

    feat: update example to use prebuilt ref

--- examples/prebuilt-react-integration/src/App.jsx
+++ examples/prebuilt-react-integration/src/App.jsx
@@ -1,13 +1,24 @@
 import { HMSPrebuilt, Diagnostics } from '@100mslive/roomkit-react';
+import { useEffect, useRef } from 'react';
 import { getRoomCodeFromUrl } from './utils';
 
 export default function App() {
   const roomCode = getRoomCodeFromUrl();
   const isDiagnostics = location.pathname.startsWith('/diagnostics');
+  const prebuiltRef = useRef();
+
+  useEffect(() => {
+    if (prebuiltRef.current) {
+      // you can access the prebuilt instance methods here
+      console.log('HMSPrebuilt instance:', prebuiltRef.current);
+      const { hmsActions } = prebuiltRef.current;
+      hmsActions.enableBeamSpeakerLabelsLogging();
+    }
+  }, []);
 
   if (isDiagnostics) {
     return <Diagnostics />;
   }
 
-  return <HMSPrebuilt roomCode={roomCode} />;
+  return <HMSPrebuilt roomCode={roomCode} ref={prebuiltRef} />;
 }
